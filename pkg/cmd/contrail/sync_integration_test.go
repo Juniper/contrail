edafea5b4cda0701272cf434c8a88e7cb2ab2d78
Daniel Furman <daniel.furman@codilime.com>
2018-02-07 14:17:09 +0100

    [WiP] Postgres Sync dump integration tests
    Test that already existing virtual network resource is synchronized
    by Sync service from Postgres to etcd.
    Needed services are started using pkg/testutil/integration.
    Remaining testing utility code might be moved to pkg/testutil in future Change.
    
    Still to do:
    - fill more properties of tested virtual networks
    - change Sync not to throw error on Dump context cancellation
    - configure etcd and postgres on CI
    
    Partial-Bug: #1772623
    Closes-Bug: #1765357
    Change-Id: I00a7a107b72667d4b126193671993485a7880bef

---
+++ pkg/cmd/contrail/sync_integration_test.go
@@ -0,0 +1,170 @@
+package contrail
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/testutil/integration"
+	"github.com/coreos/etcd/clientv3"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	etcdJSONPrefix             = "json"
+	virtualNetworkKey          = "virtual_network"
+	virtualNetworkSingularPath = "/virtual-network"
+	virtualNetworkPluralPath   = "/virtual-networks"
+	etcdWatchTimeout           = 10 * time.Second
+)
+
+func TestSyncSynchronizesPostgresDataToEtcdUsingJSONStorage(t *testing.T) {
+	t.Skip("Skipping because of not sufficient CI test env") // TODO(Daniel): resolve
+
+	s := integration.NewRunningAPIServer(t, "../../..")
+	defer s.Close(t)
+	hc := integration.NewHTTPAPIClient(t, s.URL())
+	ec := integration.NewEtcdClient(t)
+	defer ec.Close(t)
+
+	testID := generateTestID(t)
+	vnRedUUID := testID + "-red-uuid"
+	vnGreenUUID := testID + "-green-uuid"
+	vnBlueUUID := testID + "-blue-uuid"
+	uuids := []string{vnRedUUID, vnGreenUUID, vnBlueUUID}
+
+	checkNoVirtualNetworksInAPIServer(t, hc, uuids)
+	checkNoVirtualNetworksInEtcd(t, ec, uuids)
+
+	vnRedWatch, redCtx, redCancel := watchVirtualNetworkInEtcd(ec, vnRedUUID)
+	defer redCancel()
+	vnGreenWatch, greenCtx, greenCancel := watchVirtualNetworkInEtcd(ec, vnGreenUUID)
+	defer greenCancel()
+	vnBlueWatch, blueCtx, blueCancel := watchVirtualNetworkInEtcd(ec, vnBlueUUID)
+	defer blueCancel()
+
+	createThreeVirtualNetworks(t, hc, uuids)
+
+	vnRed := readVirtualNetwork(t, hc, vnRedUUID)
+	vnGreen := readVirtualNetwork(t, hc, vnGreenUUID)
+	vnBlue := readVirtualNetwork(t, hc, vnBlueUUID)
+
+	closeSync := integration.RunSync(t)
+	defer closeSync(t)
+
+	wrRed := retrieveEtcdCreateEvent(redCtx, t, vnRedWatch)
+	wrGreen := retrieveEtcdCreateEvent(greenCtx, t, vnGreenWatch)
+	wrBlue := retrieveEtcdCreateEvent(blueCtx, t, vnBlueWatch)
+
+	checkSyncedVirtualNetwork(t, wrRed, vnRed)
+	checkSyncedVirtualNetwork(t, wrGreen, vnGreen)
+	checkSyncedVirtualNetwork(t, wrBlue, vnBlue)
+
+	hc.DeleteResource(t, path.Join(virtualNetworkSingularPath, vnRedUUID))
+	hc.DeleteResource(t, path.Join(virtualNetworkSingularPath, vnGreenUUID))
+	hc.DeleteResource(t, path.Join(virtualNetworkSingularPath, vnBlueUUID))
+
+	waitForSyncToFinishDump() // TODO(Daniel): change Sync not to throw error on Dump context cancellation
+}
+
+func generateTestID(t *testing.T) string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%v-%v", t.Name(), rand.Uint64())
+}
+
+func checkNoVirtualNetworksInAPIServer(t *testing.T, hc *integration.HTTPAPIClient, uuids []string) {
+	for _, uuid := range uuids {
+		hc.CheckResourceDoesNotExist(t, path.Join(virtualNetworkSingularPath, uuid))
+	}
+}
+
+func checkNoVirtualNetworksInEtcd(t *testing.T, ec *integration.EtcdClient, uuids []string) {
+	for _, uuid := range uuids {
+		ec.CheckKeyDoesNotExist(t, virtualNetworkEtcdKey(uuid))
+	}
+}
+
+func watchVirtualNetworkInEtcd(ec *integration.EtcdClient, uuid string) (clientv3.WatchChan, context.Context,
+	context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), etcdWatchTimeout)
+	w := ec.Watch(ctx, virtualNetworkEtcdKey(uuid))
+	return w, ctx, cancel
+}
+
+func virtualNetworkEtcdKey(uuid string) string {
+	return path.Join(etcdJSONPrefix, virtualNetworkKey, uuid)
+}
+
+func createThreeVirtualNetworks(t *testing.T, hc *integration.HTTPAPIClient, uuids []string) {
+	hc.CreateResource(
+		t,
+		virtualNetworkPluralPath,
+		&models.CreateVirtualNetworkRequest{VirtualNetwork: &models.VirtualNetwork{
+			UUID:                uuids[0],
+			FQName:              []string{"default", "admin", uuids[0] + "-fq-name"},
+			Perms2:              &models.PermType2{Owner: "admin"},
+			MacLearningEnabled:  true,
+			PortSecurityEnabled: true,
+			FabricSnat:          true,
+		}},
+	)
+
+	hc.CreateResource(
+		t,
+		virtualNetworkPluralPath,
+		&models.CreateVirtualNetworkRequest{VirtualNetwork: &models.VirtualNetwork{
+			UUID:           uuids[1],
+			FQName:         []string{"default", "admin", uuids[1] + "-fq-name"},
+			Perms2:         &models.PermType2{Owner: "admin"},
+			FabricSnat:     true,
+			PBBEtreeEnable: true,
+			IsShared:       true,
+		}},
+	)
+
+	hc.CreateResource(
+		t,
+		virtualNetworkPluralPath,
+		&models.CreateVirtualNetworkRequest{VirtualNetwork: &models.VirtualNetwork{
+			UUID:   uuids[2],
+			FQName: []string{"default", "admin", uuids[2] + "-fq-name"},
+			Perms2: &models.PermType2{Owner: "admin"},
+		}},
+	)
+}
+
+func readVirtualNetwork(t *testing.T, hc *integration.HTTPAPIClient, uuid string) *models.VirtualNetwork {
+	var responseData models.GetVirtualNetworkResponse
+	hc.GetResource(t, virtualNetworkSingularPath+"/"+uuid, &responseData)
+	return responseData.VirtualNetwork
+}
+
+func retrieveEtcdCreateEvent(ctx context.Context, t *testing.T, watch clientv3.WatchChan) *clientv3.WatchResponse {
+	wr := <-watch
+	assert.NoError(t, wr.Err(), "watching virtual network failed")
+	assert.NotEqual(t, ctx.Err(), context.DeadlineExceeded, "watching virtual network timed out")
+	assert.Equal(t, 1, len(wr.Events))
+	assert.True(t, wr.Events[0].IsCreate())
+	return &wr
+}
+
+func checkSyncedVirtualNetwork(t *testing.T, wr *clientv3.WatchResponse, expectedVN *models.VirtualNetwork) {
+	syncedVN := decodeVirtualNetworkJSON(t, wr.Events[0].Kv.Value)
+	assert.Equal(t, expectedVN, syncedVN, "synced VN does not match created VN")
+}
+
+func decodeVirtualNetworkJSON(t *testing.T, vnBytes []byte) *models.VirtualNetwork {
+	var vn models.VirtualNetwork
+	assert.NoError(t, json.Unmarshal(vnBytes, &vn))
+	return &vn
+}
+
+func waitForSyncToFinishDump() {
+	time.Sleep(100 * time.Millisecond)
+}
---
+++ pkg/testutil/integration/api_server.go
@@ -0,0 +1,151 @@
+package integration
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http/httptest"
+	"path"
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/apisrv"
+	"github.com/Juniper/contrail/pkg/apisrv/keystone"
+	"github.com/Juniper/contrail/pkg/db"
+	pkglog "github.com/Juniper/contrail/pkg/log"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	dbUser     = "root"
+	dbPassword = "contrail123"
+	dbName     = "contrail_test"
+
+	authEndpointSuffix = "/keystone/v3"
+	defaultDomainID    = "default"
+	defaultDomainName  = "DefaultDomain"
+	adminProjectID     = "admin"
+	adminProjectName   = "AdminProject"
+	adminRoleID        = "admin"
+	adminRoleName      = "AdminRole"
+	adminUserID        = "admin"
+	adminUserName      = "adminUser"
+	adminUserPassword  = "adminPassword"
+)
+
+// APIServer is embedded API Server for testing purposes.
+type APIServer struct {
+	apiServer  *apisrv.Server
+	testServer *httptest.Server
+	log        *logrus.Entry
+}
+
+// NewRunningAPIServer creates new running test API Server.
+// Call Close() method to release its resources.
+func NewRunningAPIServer(t *testing.T, repoRootPath string) *APIServer {
+	setViperConfig(map[string]interface{}{
+		"database.type":               db.DriverPostgreSQL,
+		"database.host":               "localhost",
+		"database.user":               dbUser,
+		"database.name":               dbName,
+		"database.password":           dbPassword,
+		"database.dialect":            db.DriverPostgreSQL,
+		"database.max_open_conn":      100,
+		"database.connection_retries": 10,
+		"database.retry_period":       3,
+		"database.debug":              true,
+		"keystone.local":              true,
+		"keystone.assignment.type":    "static",
+		"keystone.assignment.data":    keystoneAssignment(),
+		"keystone.store.type":         "memory",
+		"keystone.store.expire":       3600,
+		"keystone.insecure":           true,
+		"server.read_timeout":         10,
+		"server.write_timeout":        5,
+		"server.log_api":              true,
+		"static_files.public":         path.Join(repoRootPath, "public"),
+		"tls.enabled":                 false,
+		"log_level":                   "debug",
+	})
+	configureDebugLogging(t)
+
+	log := pkglog.NewLogger("api-server")
+	log.WithField("config", fmt.Sprintf("%+v", viper.AllSettings())).Debug("Creating API Server")
+	s, err := apisrv.NewServer()
+	require.NoError(t, err, "creating API Server failed")
+
+	ts := httptest.NewServer(s.Echo)
+
+	viper.Set("keystone.authurl", ts.URL+authEndpointSuffix)
+	err = s.Init()
+	require.NoError(t, err, "initialization of test API Server failed")
+
+	return &APIServer{
+		apiServer:  s,
+		testServer: ts,
+		log:        log,
+	}
+}
+
+func keystoneAssignment() *keystone.StaticAssignment {
+	a := keystone.StaticAssignment{
+		Domains: map[string]*keystone.Domain{
+			defaultDomainID: {
+				ID:   defaultDomainID,
+				Name: defaultDomainName,
+			},
+		},
+		Projects: make(map[string]*keystone.Project),
+		Users:    make(map[string]*keystone.User),
+	}
+	a.Projects[adminProjectID] = &keystone.Project{
+		Domain: a.Domains[defaultDomainID],
+		ID:     adminProjectID,
+		Name:   adminProjectName,
+	}
+	a.Users[adminUserID] = &keystone.User{
+		Domain:   a.Domains[defaultDomainID],
+		ID:       adminUserID,
+		Name:     adminUserName,
+		Password: adminUserPassword,
+		Roles: []*keystone.Role{
+			{
+				ID:      adminRoleID,
+				Name:    adminRoleName,
+				Project: a.Projects[adminProjectID],
+			},
+		},
+	}
+	return &a
+}
+
+func setViperConfig(config map[string]interface{}) {
+	for k, v := range config {
+		viper.Set(k, v)
+	}
+}
+
+func configureDebugLogging(t *testing.T) {
+	err := pkglog.Configure("debug")
+	assert.NoError(t, err, "configuring logging failed")
+}
+
+// URL returns server base URL.
+func (s *APIServer) URL() string {
+	return s.testServer.URL
+}
+
+// Database returns database handle.
+func (s *APIServer) Database() *sql.DB {
+	return s.apiServer.DB
+}
+
+// Close closes server.
+func (s *APIServer) Close(t *testing.T) {
+	s.log.Debug("Closing test API server")
+	err := s.apiServer.Close()
+	assert.NoError(t, err, "closing API Server failed")
+
+	s.testServer.Close()
+}
