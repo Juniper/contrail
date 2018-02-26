cc3a6d11a5fe0afd8daa89ab598fb03ff09d3040
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-02-26 09:41:21 +0000

    Implement cluster command
    Partial-Bug: 1751875
    
    Change-Id: I46d8c8ad4314de40d2bcba599fe7d0a365851c9c

---
+++ pkg/cluster/constants.go
@@ -0,0 +1,32 @@
+package cluster
+
+const (
+	defaultResource               = "contrail-cluster"
+	defaultResourcePath           = "/" + defaultResource
+	defaultNodeRes                = "node"
+	defaultNodeResPath            = "/" + defaultNodeRes
+	defaultConfigNodeRes          = "contrail-config-node"
+	defaultConfigNodeResPath      = "/" + defaultConfigNodeRes
+	defaultConfigDBNodeRes        = "contrail-config-database-node"
+	defaultConfigDBNodeResPath    = "/" + defaultConfigDBNodeRes
+	defaultControlNodeRes         = "contrail-control-node"
+	defaultControlNodeResPath     = "/" + defaultControlNodeRes
+	defaultWebuiNodeRes           = "contrail-webui-node"
+	defaultWebuiNodeResPath       = "/" + defaultWebuiNodeRes
+	defaultAnalyticsNodeRes       = "contrail-analytics-node"
+	defaultAnalyticsNodeResPath   = "/" + defaultAnalyticsNodeRes
+	defaultAnalyticsDBNodeRes     = "contrail-analytics-database-node"
+	defaultAnalyticsDBNodeResPath = "/" + defaultAnalyticsDBNodeRes
+	defaultVrouterNodeRes         = "contrail-vrouter-node"
+	defaultVrouterNodeResPath     = "/" + defaultVrouterNodeRes
+	defaultWorkRoot               = "/var/tmp/contrail_cluster"
+	defaultTemplateRoot           = "./pkg/cluster/configs"
+	defaultInstanceTemplate       = "instances.tmpl"
+	defaultInstanceFile           = "instances.yml"
+	defaultProvisioner            = "ansible"
+	defaultAnsibleRepo            = "contrail-ansible-deployer"
+	defaultAnsibleRepoURL         = "https://github.com/Juniper/" + defaultAnsibleRepo + ".git"
+	defaultClusterProvPlay        = "playbooks/install_contrail.yml"
+	defaultInstanceProvPlay       = "playbooks/provision_instances.yml"
+	defaultInstanceConfPlay       = "playbooks/configure_instances.yml"
+)
---
+++ pkg/cluster/manage.go
@@ -0,0 +1,47 @@
+package cluster
+
+import (
+	"errors"
+
+	pkglog "github.com/Juniper/contrail/pkg/log"
+	"github.com/sirupsen/logrus"
+)
+
+// manager inerface to manage clusters
+type manager interface {
+	manage() error
+}
+
+type oneShotManager struct {
+	cluster *Cluster
+	log     *logrus.Entry
+}
+
+// Manage a cluster once
+func (o *oneShotManager) manage() error {
+	provisioner, err := newProvisioner(o.cluster)
+	if err != nil {
+		return err
+	}
+	err = provisioner.provision()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func newOneShotManager(cluster *Cluster) (*oneShotManager, error) {
+	return &oneShotManager{
+		cluster: cluster,
+		log:     pkglog.NewLogger("oneshot-manager"),
+	}, nil
+}
+
+func newManager(cluster *Cluster) (manager, error) {
+	switch cluster.managerType {
+	case "oneshot":
+		return newOneShotManager(cluster)
+	}
+	//TODO(ijohnson) Support daemon manager with etcd
+	return nil, errors.New("unsupported manager type")
+}
