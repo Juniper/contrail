1b823b0a93a2e96a36fe87eaafafa7dfbb88a023
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-03-20 01:34:49 +0000

    Support multiple keystone authentication
    Support dynamic proxy
    
    Partial-Bug: 1758266
    Change-Id: I947925de1bce46f5d8bf9ab224028469c88e1cb8

---
+++ pkg/apisrv/keystone/endpoint.go
@@ -0,0 +1,124 @@
+package keystone
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/serviceif"
+)
+
+type keystoneEndpoint struct {
+	privateURL string
+	publicURL  string
+}
+
+type clusterEndpoint struct {
+	dbService     serviceif.Service
+	endpointStore *common.EndpointStore
+	clusterID     string
+	token         string
+	ctx           context.Context
+}
+
+func (ce *clusterEndpoint) readEndpoint() (*string, *string, error) {
+	ctx := common.NoAuth(context.Background())
+	values := []string{"keystone"}
+	filters := []*models.Filter{&models.Filter{
+		Key:    "name",
+		Values: values,
+	}}
+	var spec models.ListSpec
+	if ce.clusterID == "" {
+		spec = models.ListSpec{
+			Limit:   100,
+			Filters: filters,
+		}
+	} else {
+		parents := []string{ce.clusterID}
+		fields := []string{"public_url", "private_url"}
+		spec = models.ListSpec{
+			Limit:       100,
+			Filters:     filters,
+			ParentUUIDs: parents,
+			Fields:      fields,
+		}
+	}
+	request := &models.ListEndpointRequest{Spec: &spec}
+	response, err := ce.dbService.ListEndpoint(ctx, request)
+	if err != nil {
+		return nil, nil, err
+	}
+	endpoints := response.Endpoints
+	if len(endpoints) == 1 {
+		ce.clusterID = endpoints[0].ParentUUID
+		return &endpoints[0].PublicURL, &endpoints[0].PrivateURL, nil
+	} else if len(endpoints) > 1 {
+		return nil, nil,
+			fmt.Errorf("Multimatch of keystone endpoint for cluster: %s",
+				ce.clusterID)
+	}
+	return nil, nil, nil
+}
+
+func (ce *clusterEndpoint) getEndpoint() (*keystoneEndpoint, error) {
+	e, ok := ce.endpointStore.Data.Load(ce.clusterID)
+	if !ok {
+		return nil, fmt.Errorf("keystone endpoint for cluster: %s not found", ce.clusterID)
+	}
+	ep, ok := e.(*keystoneEndpoint)
+	return ep, nil
+}
+
+func (ce *clusterEndpoint) syncEndpoint() (*keystoneEndpoint, error) {
+	// Read from db
+	public, private, err := ce.readEndpoint()
+	if err != nil {
+		return nil, err
+	}
+	// clusterID should be updated during sync
+	// check if a cluster is created
+	if ce.clusterID == "" {
+		// No cluster found
+		return nil, common.ErrorNotFound
+	}
+	ep := &keystoneEndpoint{}
+	if public != nil {
+		ep.publicURL = *public
+	}
+	if private != nil {
+		ep.privateURL = *public
+	}
+	// sync the endpoint to store
+	ce.endpointStore.Data.Store(ce.clusterID, ep)
+	return ep, nil
+}
+
+func (ce *clusterEndpoint) authenticate() (context.Context, error) {
+	var ok error
+	var newCtx context.Context
+	ep, err := ce.getEndpoint()
+	if err != nil {
+		// endpoint not in memory, sync and authenticate
+		ep, err = ce.syncEndpoint()
+		if err != nil {
+			return nil, err
+		}
+		auth := newAuth(ep.privateURL, true) // TODO:(ijohnson) add insecure in schema
+		newCtx, ok = authenticate(ce.ctx, auth, ce.token)
+	} else {
+		// endpoint found in memory, try authenticate
+		auth := newAuth(ep.privateURL, true) // TODO:(ijohnson) add insecure in schema
+		newCtx, err = authenticate(ce.ctx, auth, ce.token)
+		if err != nil {
+			// sync endpoint and re try authenticate
+			ep, err = ce.syncEndpoint()
+			if err != nil {
+				return nil, err
+			}
+			newCtx, ok = authenticate(ce.ctx, auth, ce.token)
+		}
+	}
+	return newCtx, ok
+}
---
+++ pkg/apisrv/proxy_test.go
@@ -0,0 +1,211 @@
+package apisrv
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/flosch/pongo2"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	privatePortList  = "private_port_list"
+	publicPortList   = "public_port_list"
+	privateAuthToken = "private_auth_token"
+	publicAuthToken  = "public_auth_token"
+)
+
+type mockPortsResponse struct {
+	Name string `json:"name"`
+}
+
+type mockAuthTokenResponse struct {
+	Token string `json:"token"`
+}
+
+func mockServer(routes map[string]interface{}) *httptest.Server {
+	// Echo instance
+	e := echo.New()
+
+	// Routes
+	for route, handler := range routes {
+		e.GET(route, handler.(echo.HandlerFunc))
+	}
+	mockServer := httptest.NewServer(e)
+	return mockServer
+}
+
+func runEndpointTest(t *testing.T, clusterName string,
+	extraTasks bool) (string, *TestScenario, *httptest.Server, *httptest.Server) {
+	routes := map[string]interface{}{
+		"/ports": echo.HandlerFunc(func(c echo.Context) error {
+			return c.JSON(http.StatusOK,
+				&mockPortsResponse{Name: clusterName + privatePortList})
+		}),
+	}
+	neutronPrivate := mockServer(routes)
+	routes = map[string]interface{}{
+		"/ports": echo.HandlerFunc(func(c echo.Context) error {
+			return c.JSON(http.StatusOK,
+				&mockPortsResponse{Name: clusterName + publicPortList})
+		}),
+	}
+	neutronPublic := mockServer(routes)
+
+	context := pongo2.Context{
+		"extra_tasks":         extraTasks,
+		"cluster_name":        clusterName,
+		"endpoint_name":       "neutorn",
+		"neutron_private_url": neutronPrivate.URL,
+		"neutron_public_url":  neutronPublic.URL,
+	}
+
+	testFile := GetTestFromTemplate(t, "./test_data/test_endpoint.tmpl", context)
+
+	var testScenario TestScenario
+	err := LoadTestScenario(&testScenario, testFile)
+	assert.NoError(t, err, "failed to load test data")
+	RunTestScenario(t, &testScenario)
+
+	return testFile, &testScenario, neutronPublic, neutronPrivate
+}
+
+func verifyProxy(t *testing.T, testScenario *TestScenario, url string,
+	clusterName string, key string, expected string) bool {
+	for _, client := range testScenario.Clients {
+		var response map[string]interface{}
+		_, err := client.Read(url, &response)
+		assert.NoError(t, err, "failed to proxy %s", url)
+		ok := common.AssertEqual(t,
+			map[string]interface{}{key: clusterName + expected},
+			response,
+			fmt.Sprintf("Unexpected Response: %s", response))
+		if !ok {
+			return ok
+		}
+	}
+	return true
+}
+
+func TestProxyEndpoint(t *testing.T) {
+	// Create a cluster and its neutron endpoint
+	clusterAName := "clusterA"
+	testFile, testScenario, neutronPublic, neutronPrivate := runEndpointTest(
+		t, clusterAName, true)
+	defer os.Remove(testFile) // remove tempfile after test
+	defer neutronPrivate.Close()
+	defer neutronPublic.Close()
+
+	// Wait a sec for the dynamic proxy to be created/updated
+	time.Sleep(time.Second)
+
+	// verify proxies
+	ok := verifyProxy(t, testScenario,
+		"/neutron/ports", clusterAName, "name", publicPortList)
+	if !ok {
+		return
+	}
+	ok = verifyProxy(t, testScenario,
+		"/neutron/private/ports", clusterAName, "name", privatePortList)
+	if !ok {
+		return
+	}
+
+	// create one more cluster/neutron endpoint for new cluster
+	clusterBName := "clusterB"
+	testFile, testScenario, neutronPublic, neutronPrivate = runEndpointTest(
+		t, clusterBName, false)
+	defer os.Remove(testFile) // remove tempfile after test
+	defer neutronPrivate.Close()
+	defer neutronPublic.Close()
+	// Wait a sec for the dynamic proxy to be created/updated
+	time.Sleep(time.Second)
+
+	// verify new proxies
+	ok = verifyProxy(t, testScenario,
+		"/"+clusterBName+"_uuid/neutron/ports",
+		clusterBName, "name", publicPortList)
+	if !ok {
+		return
+	}
+	ok = verifyProxy(t, testScenario,
+		"/"+clusterBName+"_uuid/neutron/private/ports",
+		clusterBName, "name", privatePortList)
+	if !ok {
+		return
+	}
+
+	// verify existing proxies, make sure the proxy prefix is updated with cluster id
+	ok = verifyProxy(t, testScenario,
+		"/"+clusterAName+"_uuid/neutron/ports",
+		clusterAName, "name", publicPortList)
+	if !ok {
+		return
+	}
+	ok = verifyProxy(t, testScenario,
+		"/"+clusterAName+"_uuid/neutron/private/ports",
+		clusterAName, "name", privatePortList)
+	if !ok {
+		return
+	}
+}
+
+func TestKeystoneEndpoint(t *testing.T) {
+	clusterName := "clusterA"
+	routes := map[string]interface{}{
+		"/v3/auth/tokens": echo.HandlerFunc(func(c echo.Context) error {
+			return c.JSON(http.StatusOK,
+				&mockAuthTokenResponse{Token: clusterName + privateAuthToken})
+		}),
+	}
+	ksPrivate := mockServer(routes)
+	defer ksPrivate.Close()
+
+	routes = map[string]interface{}{
+		"/v3/auth/tokens": echo.HandlerFunc(func(c echo.Context) error {
+			return c.JSON(http.StatusOK,
+				&mockAuthTokenResponse{Token: clusterName + publicAuthToken})
+		}),
+	}
+	ksPublic := mockServer(routes)
+	defer ksPublic.Close()
+
+	context := pongo2.Context{
+		"extra_tasks":   true,
+		"cluster_name":  clusterName,
+		"endpoint_name": "keystone",
+		"private_url":   ksPrivate.URL,
+		"public_url":    ksPublic.URL,
+	}
+
+	testFile := GetTestFromTemplate(t, "./test_data/test_endpoint.tmpl", context)
+	defer os.Remove(testFile) // remove tempfile after test
+
+	var testScenario TestScenario
+	err := LoadTestScenario(&testScenario, testFile)
+	assert.NoError(t, err, "failed to load test data")
+	RunTestScenario(t, &testScenario)
+
+	// Wait a sec for the dynamic proxy to be created/updated
+	time.Sleep(time.Second)
+
+	// verify proxies
+	ok := verifyProxy(t, &testScenario,
+		"/keystone/v3/auth/tokens",
+		clusterName, "token", publicAuthToken)
+	if !ok {
+		return
+	}
+	ok = verifyProxy(t, &testScenario,
+		"/keystone/private/v3/auth/tokens",
+		clusterName, "token", privateAuthToken)
+	if !ok {
+		return
+	}
+}
