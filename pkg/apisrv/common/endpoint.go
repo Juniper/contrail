e4a525774f908f9ca9595c5f4ec822648b2a6df5
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-03-20 01:34:49 +0000

    Support multiple keystone authentication
    Support dynamic proxy
    
    Partial-Bug: 1758266
    Change-Id: I947925de1bce46f5d8bf9ab224028469c88e1cb8

---
+++ pkg/apisrv/common/endpoint.go
@@ -0,0 +1,103 @@
+package common
+
+import (
+	"sync"
+
+	"github.com/Juniper/contrail/pkg/models"
+)
+
+const (
+	// Public scope of the endpoint url
+	Public = "public"
+	// Private scope of the endpoint url
+	Private = "private"
+)
+
+// TargetStore is used to store service specific endpoint targets in-memory
+type TargetStore struct {
+	Data       *sync.Map
+	nextTarget string
+}
+
+// EndpointStore is used to store cluster specific endpoints in-memory
+type EndpointStore struct {
+	Data *sync.Map
+}
+
+//MakeTargetStore is used to make an in-memory endpoint target store.
+func MakeTargetStore() *TargetStore {
+	return &TargetStore{
+		Data:       new(sync.Map),
+		nextTarget: "",
+	}
+}
+
+//MakeEndpointStore is used to make an in-memory endpoint store.
+func MakeEndpointStore() *EndpointStore {
+	return &EndpointStore{
+		Data: new(sync.Map),
+	}
+}
+
+//Read endpoint target from memory
+func (t *TargetStore) Read(id string) *models.Endpoint {
+	ep, ok := t.Data.Load(id)
+	if !ok {
+		return nil
+	}
+	endpoint, ok := ep.(*models.Endpoint)
+	return endpoint
+}
+
+//Write endpoint target in-memory
+func (t *TargetStore) Write(id string, endpoint *models.Endpoint) {
+	t.Data.Store(id, endpoint)
+}
+
+//Next endpoint target from memory is read(roundrobin)
+func (t *TargetStore) Next(scope string) (endpointURL string) {
+	endpointURL = ""
+	t.Data.Range(func(id, endpoint interface{}) bool {
+		ids := id.(string)
+		if t.nextTarget == "" {
+			t.nextTarget = ids
+		}
+		if endpointURL != "" {
+			t.nextTarget = ids
+			// exit Range iteration as next target is identified
+			return false
+		}
+		switch scope {
+		case Public:
+			if ids == t.nextTarget {
+				endpointURL = endpoint.(*models.Endpoint).PublicURL
+				// let Range iterate till nextServer is identified
+				return true
+			}
+		case Private:
+			if ids == t.nextTarget {
+				endpointURL = endpoint.(*models.Endpoint).PrivateURL
+				// let Range iterate till nextServer is identified
+				return true
+			}
+		}
+		return true
+	})
+	return endpointURL
+}
+
+//Read endpoint targets store from memory
+func (e *EndpointStore) Read(endpointKey string) *TargetStore {
+	p, ok := e.Data.Load(endpointKey)
+	if !ok {
+		return nil
+	}
+	endpointStore, ok := p.(*TargetStore)
+	return endpointStore
+
+}
+
+//Write endpoint targets store in-memory
+func (e *EndpointStore) Write(endpointKey string, endpointStore *TargetStore) {
+	e.Data.Store(endpointKey, endpointStore)
+}
