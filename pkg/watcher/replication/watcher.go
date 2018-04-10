67cd4ba7a5ec3753c0da1d7210793a0a7cd537da
Michał Błotniak <michal.blotniak@codilime.com>
2018-04-10 12:03:22 +0200

    Add subscription object
    Change-Id: I88128f94d0625b437dc6dc0a60dad827e48d7422
    Partial-Bug: #1758826t

--- pkg/watcher/replication/watcher.go
+++ pkg/watcher/replication/watcher.go
@@ -2,6 +2,9 @@ package replication
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"time"
 
 	pkglog "github.com/Juniper/contrail/pkg/log"
 	"github.com/jackc/pgx"
@@ -14,87 +17,168 @@ type abstractCanal interface {
 	Close()
 }
 
-type noopCanal struct{}
-
-func (c *noopCanal) Run() error { return nil }
-
-func (c *noopCanal) Close() {}
-
-// BinlogWatcher uses canal to read MySQL binlog events.
-type BinlogWatcher struct {
+// MySQLWatcher uses canal to read MySQL binlog events.
+type MySQLWatcher struct {
 	canal abstractCanal
 	log   *logrus.Entry
 }
 
-// NewBinlogWatcher creates new BinlogWatcher listening on provided canal.
-func NewBinlogWatcher(c abstractCanal) *BinlogWatcher {
-	if c == nil {
-		c = &noopCanal{}
-	}
-
-	return &BinlogWatcher{
+// NewMySQLWatcher creates new BinlogWatcher listening on provided canal.
+func NewMySQLWatcher(c abstractCanal) *MySQLWatcher {
+	return &MySQLWatcher{
 		canal: c,
-		log:   pkglog.NewLogger("binlog-watcher"),
+		log:   pkglog.NewLogger("mysql-watcher"),
 	}
 }
 
 // Watch starts listening on a canal.
-func (w *BinlogWatcher) Watch(context.Context) error {
+func (w *MySQLWatcher) Watch(context.Context) error {
 	w.log.Debug("Watching events on MySQL binlog")
 	return w.canal.Run()
 }
 
 // Close closes canal.
-func (w *BinlogWatcher) Close() {
+func (w *MySQLWatcher) Close() {
 	w.log.Debug("Stopping watching events on MySQL binlog")
 	w.canal.Close()
 }
 
-// MessageHandler handles pgoutput logical replication messages.
-type MessageHandler interface {
-	Handle(pgoutput.Message) error
+// PostgresSubscriptionConfig stores configuration for logical replication connection used for Subsctiption object.
+type PostgresSubscriptionConfig struct {
+	pgx.ConnConfig
+
+	Name          string // TODO(Michal): change to slotName
+	Publication   string
+	StatusTimeout time.Duration
 }
 
-type subscriptionStarter interface {
-	Start(context.Context, *pgx.ReplicationConn, pgoutput.Handler) error
+type postgresWatcherConnection interface {
+	io.Closer
+	GetReplicationSlot(name string) (lastLSN uint64, snapshotName string, err error)
+	RenewPublication(name string) error
+	StartReplication(slot, publication string, startLSN uint64) error
+	WaitForReplicationMessage(ctx context.Context) (*pgx.ReplicationMessage, error)
+	SendStatus(lastLSN uint64) error
 }
 
-// SubscriptionWatcher uses subscription to read PostgreSQL logical replciation messages.
-type SubscriptionWatcher struct {
-	conn    *pgx.ReplicationConn // TODO(Michal): Find a way to change this into interface
-	starter subscriptionStarter
+// PostgresWatcher allows subscribing to Postgresql logical replication messages.
+type PostgresWatcher struct {
+	PostgresSubscriptionConfig
 
-	handler MessageHandler
+	lastLSN uint64 // lastLSN holds max WAL LSN value processed by subscription.
+
 	cancel  context.CancelFunc
-
-	log *logrus.Entry
+	conn    postgresWatcherConnection
+	handler pgoutput.Handler
+	log     *logrus.Entry
 }
 
-// NewSubscriptionWatcher returns new SubscriptionWatcher.
-func NewSubscriptionWatcher(
-	replicationConn *pgx.ReplicationConn,
-	starter subscriptionStarter,
-	handler MessageHandler,
-) *SubscriptionWatcher {
-	return &SubscriptionWatcher{
-		conn:    replicationConn,
-		starter: starter,
-		handler: handler,
-		log:     pkglog.NewLogger("subscription-watcher"),
+// NewPostgresWatcher creates new watcher and initialises its connections.
+func NewPostgresWatcher(
+	config PostgresSubscriptionConfig,
+	handler pgoutput.Handler,
+) (*PostgresWatcher, error) {
+	conn, err := newPostgresReplicationConnection(config.ConnConfig)
+	if err != nil {
+		return nil, err
 	}
+
+	return &PostgresWatcher{
+		PostgresSubscriptionConfig: config,
+		conn:    conn,
+		handler: handler,
+		log:     pkglog.NewLogger("postgres-watcher"),
+	}, nil
 }
 
 // Watch starts subscription and store context cancel function.
-func (w *SubscriptionWatcher) Watch(ctx context.Context) error {
+func (w *PostgresWatcher) Watch(ctx context.Context) error {
 	w.log.Debug("Watching events on PostgreSQL replication slot")
 	ctx, cancel := context.WithCancel(ctx)
 	w.cancel = cancel
 
-	return w.starter.Start(ctx, w.conn, w.handler.Handle)
+	var snapshotName string
+	slotLSN, snapshotName, err := w.conn.GetReplicationSlot(w.Name)
+	if err != nil {
+		return fmt.Errorf("error getting replication slot: %v", err)
+	}
+	w.log.Debug("consistentPoint: ", pgx.FormatLSN(slotLSN))
+	w.log.Debug("snapshotName: ", snapshotName)
+
+	w.lastLSN = slotLSN // TODO(Michal): get lastLSN from etcd
+
+	err = w.conn.RenewPublication(w.Publication)
+	if err != nil {
+		return fmt.Errorf("failed to create publication: %s", err)
+	}
+	w.log.Debug("Created publication: ", w.Publication)
+
+	_ = snapshotName // TODO(Michal): Use snapshotName to load initial database state
+
+	err = w.conn.StartReplication(w.Name, w.Publication, 0)
+	if err != nil {
+		return fmt.Errorf("failed to start replication: %s", err)
+	}
+
+	return w.loop(ctx)
 }
 
-// Close stops subscription by calling cancel function.
-func (w *SubscriptionWatcher) Close() {
+func (w *PostgresWatcher) loop(ctx context.Context) error {
+	tick := time.NewTicker(w.StatusTimeout).C
+	for {
+		select {
+		case <-ctx.Done():
+			return w.conn.Close()
+		case <-tick:
+			w.log.Debug("Sending standby status with position: ", pgx.FormatLSN(w.lastLSN))
+			if err := w.conn.SendStatus(w.lastLSN); err != nil {
+				return err
+			}
+		default:
+			wctx, cancel := context.WithTimeout(ctx, w.StatusTimeout)
+			message, err := w.conn.WaitForReplicationMessage(wctx)
+			cancel()
+			if err == context.DeadlineExceeded {
+				continue
+			}
+			if err != nil {
+				return fmt.Errorf("replication failed: %s", err)
+			}
+			if err = w.handleMessage(message); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (w *PostgresWatcher) handleMessage(msg *pgx.ReplicationMessage) error {
+	if msg.WalMessage != nil {
+		if msg.WalMessage.WalStart > w.lastLSN {
+			w.lastLSN = msg.WalMessage.WalStart
+		}
+		logmsg, err := pgoutput.Parse(msg.WalMessage.WalData)
+		if err != nil {
+			return fmt.Errorf("invalid pgoutput message: %s", err)
+		}
+		if err := w.handler(logmsg); err != nil {
+			return fmt.Errorf("error handling waldata: %s", err)
+		}
+	}
+
+	if msg.ServerHeartbeat != nil {
+		if msg.ServerHeartbeat.ReplyRequested == 1 {
+			w.log.Info("Server requested reply, sending standby status with position: ", pgx.FormatLSN)
+			if err := w.conn.SendStatus(w.lastLSN); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Close stops subscription by calling cancel function of context passed in Watch.
+func (w *PostgresWatcher) Close() {
 	w.log.Debug("Stopping watching events on PostgreSQL replication slot")
 	w.cancel()
 }
--- pkg/watcher/replication/watcher_test.go
+++ pkg/watcher/replication/watcher_test.go
@@ -2,37 +2,159 @@ package replication
 
 import (
 	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"testing"
+	"time"
 
+	pkglog "github.com/Juniper/contrail/pkg/log"
 	"github.com/jackc/pgx"
 	"github.com/kyleconroy/pgoutput"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestSubscriptionWatcherWatch(t *testing.T) {
-	started := false
-	s := mockStarter(func(ctx context.Context, c *pgx.ReplicationConn, h pgoutput.Handler) error {
-		started = true
-		return nil
-	})
-
-	w := NewSubscriptionWatcher(nil, s, NewPgoutputEventHandler(nil))
-
-	err := w.Watch(context.Background())
-
-	assert.NoError(t, err)
-
-	assert.True(t, started)
-	assert.NotNil(t, w.cancel)
-	w.cancel()
+type oner interface {
+	On(string, ...interface{}) *mock.Call
 }
 
-func TestSubscriptionWatcherClose(t *testing.T) {
+func TestPostgresWatcherWatch(t *testing.T) {
+	slot, publication, snapshot, lsn := "test-sub", "test-pub", "snapshot-id", uint64(2778)
+
+	tests := []struct {
+		name            string
+		initMock        func(oner)
+		expectedMessage pgoutput.Message
+	}{
+		{
+			name: "GetReplicationSlot fails",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(uint64(0), "", assert.AnError).Once()
+			},
+		},
+		{
+			name: "RenewPublication fails",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name: "StartReplication fails",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(nil).Once()
+				o.On("StartReplication", slot, publication, uint64(0)).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name: "StartReplication fails",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(nil).Once()
+				o.On("StartReplication", slot, publication, uint64(0)).Return(assert.AnError).Once()
+			},
+		},
+		{
+			name: "WaitForReplicationMessage returns unknown error",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(nil).Once()
+				o.On("StartReplication", slot, publication, uint64(0)).Return(nil).Once()
+				o.On("WaitForReplicationMessage", mock.Anything).Return((*pgx.ReplicationMessage)(nil), assert.AnError).Once()
+			},
+		},
+		{
+			name: "WaitForReplicationMessage returns context deadline and then unknown error",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(nil).Once()
+				o.On("StartReplication", slot, publication, uint64(0)).Return(nil).Once()
+				o.On("WaitForReplicationMessage", mock.Anything).Return((*pgx.ReplicationMessage)(nil), context.DeadlineExceeded).Twice()
+				o.On("WaitForReplicationMessage", mock.Anything).Return((*pgx.ReplicationMessage)(nil), assert.AnError).Once()
+			},
+		},
+		{
+			name: "receive message, pass it to handler and then fail",
+			initMock: func(o oner) {
+				o.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+				o.On("RenewPublication", publication).Return(nil).Once()
+				o.On("StartReplication", slot, publication, uint64(0)).Return(nil).Once()
+				o.On("WaitForReplicationMessage", mock.Anything).Return(
+					&pgx.ReplicationMessage{
+						WalMessage: &pgx.WalMessage{
+							WalData: getBeginData(pgoutput.Begin{}),
+						},
+					},
+					nil,
+				).Twice()
+				o.On("WaitForReplicationMessage", mock.Anything).Return((*pgx.ReplicationMessage)(nil), assert.AnError).Once()
+			},
+			expectedMessage: pgoutput.Begin{
+				Timestamp: time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var receivedMsg pgoutput.Message
+			h := func(m pgoutput.Message) error {
+				fmt.Println(m)
+				receivedMsg = m
+				return nil
+			}
+			m := &mockPostgresWatcherConnection{}
+			if tt.initMock != nil {
+				tt.initMock(m)
+			}
+			w := givenPostgresWatcher(slot, publication, m, h)
+
+			err := w.Watch(context.Background())
+
+			assert.Error(t, err)
+			assert.NotNil(t, w.cancel)
+			w.cancel()
+
+			assert.Equal(t, tt.expectedMessage, receivedMsg)
+			m.AssertExpectations(t)
+		})
+	}
+
+}
+
+func TestPostgresWatcherContextCancellation(t *testing.T) {
+	// given
+	slot, publication, snapshot, lsn := "test-sub", "test-pub", "snapshot-id", uint64(2778)
+
+	closeErr := errors.New("some closing error")
+
+	m := &mockPostgresWatcherConnection{}
+	m.On("GetReplicationSlot", mock.Anything, mock.Anything, mock.Anything).Return(lsn, snapshot, nil).Once()
+	m.On("RenewPublication", publication).Return(nil).Once()
+	m.On("StartReplication", slot, publication, uint64(0)).Return(nil).Once()
+	m.On("Close").Return(closeErr).Once()
+
+	w := givenPostgresWatcher(slot, publication, m, nil)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// when
+	cancel()
+	err := w.Watch(ctx)
+
+	// then
+	assert.Equal(t, closeErr, err)
+	m.AssertExpectations(t)
+}
+
+func TestPostgresWatcherClose(t *testing.T) {
 	canceled := false
 	cancel := func() {
 		canceled = true
 	}
-	w := NewSubscriptionWatcher(nil, nil, nil)
+	w := givenPostgresWatcher("", "", &mockPostgresWatcherConnection{}, nil)
 	w.cancel = cancel
 
 	w.Close()
@@ -40,30 +162,75 @@ func TestSubscriptionWatcherClose(t *testing.T) {
 	assert.True(t, canceled)
 }
 
-type mockStarter func(context.Context, *pgx.ReplicationConn, pgoutput.Handler) error
-
-func (s mockStarter) Start(ctx context.Context, c *pgx.ReplicationConn, h pgoutput.Handler) error {
-	return s(ctx, c, h)
+func getBeginData(m pgoutput.Begin) []byte {
+	b := make([]byte, 21)
+	b[0] = 'B'
+	binary.BigEndian.PutUint64(b[1:], m.LSN)
+	binary.BigEndian.PutUint64(b[9:], 0)
+	binary.BigEndian.PutUint32(b[17:], uint32(m.XID))
+	return b
 }
 
-func TestBinlogWatcherIsNoopByDefault(t *testing.T) {
-	w := givenBinlogWatcher(nil)
+func givenPostgresWatcher(
+	slot, publication string,
+	conn postgresWatcherConnection,
+	handler pgoutput.Handler,
+) *PostgresWatcher {
+	return &PostgresWatcher{
+		PostgresSubscriptionConfig: PostgresSubscriptionConfig{
+			StatusTimeout: time.Second,
+			Name:          slot,
+			Publication:   publication,
+		},
+		conn:    conn,
+		handler: handler,
+		log:     pkglog.NewLogger("postgres-watcher"),
+	}
+}
 
-	err := w.Watch(context.Background())
-	assert.NoError(t, err)
+type mockPostgresWatcherConnection struct {
+	mock.Mock
+}
 
-	w.Close()
+func (m *mockPostgresWatcherConnection) Close() error {
+	args := m.MethodCalled("Close")
+	return args.Error(0)
+}
+
+func (m *mockPostgresWatcherConnection) GetReplicationSlot(name string) (lastLSN uint64, snapshotName string, err error) {
+	args := m.MethodCalled("GetReplicationSlot", name)
+	return args.Get(0).(uint64), args.String(1), args.Error(2)
+}
+
+func (m *mockPostgresWatcherConnection) RenewPublication(name string) error {
+	args := m.MethodCalled("RenewPublication", name)
+	return args.Error(0)
+}
+
+func (m *mockPostgresWatcherConnection) StartReplication(slot string, publication string, startLSN uint64) error {
+	args := m.MethodCalled("StartReplication", slot, publication, startLSN)
+	return args.Error(0)
+}
+
+func (m *mockPostgresWatcherConnection) WaitForReplicationMessage(ctx context.Context) (*pgx.ReplicationMessage, error) {
+	args := m.MethodCalled("WaitForReplicationMessage", ctx)
+	return args.Get(0).(*pgx.ReplicationMessage), args.Error(1)
+}
+
+func (m *mockPostgresWatcherConnection) SendStatus(lastLSN uint64) error {
+	args := m.MethodCalled("SendStatus", lastLSN)
+	return args.Error(0)
 }
 
 func TestWatchFailsWhenCanalStartFails(t *testing.T) {
-	w := givenBinlogWatcher(&failingCanalStub{})
+	w := NewMySQLWatcher(&failingCanalStub{})
 	err := w.Watch(context.Background())
 	assert.Error(t, err)
 }
 
 func TestWatchStartsCanal(t *testing.T) {
 	s := &succeedingCanalSpy{}
-	w := givenBinlogWatcher(s)
+	w := NewMySQLWatcher(s)
 
 	err := w.Watch(context.Background())
 
@@ -74,16 +241,12 @@ func TestWatchStartsCanal(t *testing.T) {
 
 func TestStopClosesCanal(t *testing.T) {
 	s := &succeedingCanalSpy{}
-	w := givenBinlogWatcher(s)
+	w := NewMySQLWatcher(s)
 
 	w.Close()
 
 	assert.Equal(t, 0, s.startCounter)
 	assert.Equal(t, 1, s.closeCounter)
-}
-
-func givenBinlogWatcher(c abstractCanal) *BinlogWatcher {
-	return NewBinlogWatcher(c)
 }
 
 type failingCanalStub struct{}
