ad171acdec0903e4f093041133089b7b5fccb680
Michał Błotniak <michal.blotniak@codilime.com>
2018-04-10 12:03:22 +0200

    Add subscription object
    Change-Id: I88128f94d0625b437dc6dc0a60dad827e48d7422
    Partial-Bug: #1758826t

--- pkg/watcher/replication/watcher_test.go
+++ pkg/watcher/replication/watcher_test.go
@@ -4,19 +4,17 @@ import (
 	"context"
 	"testing"
 
-	"github.com/jackc/pgx"
-	"github.com/kyleconroy/pgoutput"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSubscriptionWatcherWatch(t *testing.T) {
 	started := false
-	s := mockStarter(func(ctx context.Context, c *pgx.ReplicationConn, h pgoutput.Handler) error {
+	s := mockStarter(func(ctx context.Context) error {
 		started = true
 		return nil
 	})
 
-	w := NewSubscriptionWatcher(nil, s, NewPgoutputEventHandler(nil))
+	w := NewSubscriptionWatcher(s)
 
 	err := w.Watch(context.Background())
 
@@ -32,7 +30,7 @@ func TestSubscriptionWatcherClose(t *testing.T) {
 	cancel := func() {
 		canceled = true
 	}
-	w := NewSubscriptionWatcher(nil, nil, nil)
+	w := NewSubscriptionWatcher(nil)
 	w.cancel = cancel
 
 	w.Close()
@@ -40,10 +38,10 @@ func TestSubscriptionWatcherClose(t *testing.T) {
 	assert.True(t, canceled)
 }
 
-type mockStarter func(context.Context, *pgx.ReplicationConn, pgoutput.Handler) error
+type mockStarter func(context.Context) error
 
-func (s mockStarter) Start(ctx context.Context, c *pgx.ReplicationConn, h pgoutput.Handler) error {
-	return s(ctx, c, h)
+func (s mockStarter) Start(ctx context.Context) error {
+	return s(ctx)
 }
 
 func TestBinlogWatcherIsNoopByDefault(t *testing.T) {
