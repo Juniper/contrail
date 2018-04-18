d4430da1a1df8b070af47e0842b191e6be832eb0
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-04-18 21:09:44 +0000

    Support token get with user-token in local
    keystone.
    - support project list in local keystone
    - support using cluster keystone after
      single cluster create.
    - add/delete endpoints during cluster create/update/delete
    
    Closes-Bug: 1758266
    
    Change-Id: Icc957498b0551854e40fb625e8a089d5912f3e7a

---
+++ pkg/apisrv/keystone/client.go
@@ -0,0 +1,57 @@
+package keystone
+
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+
+	"github.com/databus23/keystone"
+)
+
+const (
+	keystoneVersion = "v3"
+)
+
+// KeystoneClient represents a client.
+// nolint
+type KeystoneClient struct {
+	AuthURL      string `yaml:"authurl"`
+	LocalAuthURL string `yaml:"local_authurl"`
+	httpClient   *http.Client
+	InSecure     bool `yaml:"insecure"`
+}
+
+// NewKeystoneClient makes keystone client.
+func NewKeystoneClient(authURL string, insecure bool) *KeystoneClient {
+	c := &KeystoneClient{
+		AuthURL:      authURL,
+		LocalAuthURL: authURL,
+		InSecure:     insecure,
+	}
+	c.Init()
+	return c
+}
+
+// Init is used to initialize a keystone client.
+func (k *KeystoneClient) Init() {
+	tr := &http.Transport{
+		Dial:            (&net.Dialer{}).Dial,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: k.InSecure},
+	}
+	client := &http.Client{
+		Transport: tr,
+	}
+	k.httpClient = client
+}
+
+// SetAuthURL uses specified auth url in the keystone auth.
+func (k *KeystoneClient) SetAuthURL(authURL string) {
+	k.AuthURL = authURL + "/" + keystoneVersion
+}
+
+// NewAuth creates new keystone auth
+func (k *KeystoneClient) NewAuth() *keystone.Auth {
+	auth := keystone.New(k.AuthURL)
+	auth.Client = k.httpClient
+	return auth
+}
