27f029ffea0906941544bb9661891b1c119ce4ef
Michał Błotniak <michal.blotniak@codilime.com>
2018-04-20 10:33:14 +0200

    Mount sourcedir in DB containers
    Passing huge init SQL files via stdin to docker exec caused random
    deadlocks. To avoid this files are mounted and read by DB clients inside
    containers.
    
    Note that containers in dev environments should be rebuilt
    with `make testenv`.
    
    Change-Id: Ia7688c7048da88cb016c28682754145dc37c547b
    Partial-Bug: #1765506

--- tools/reset_db_mysql.sh
+++ tools/reset_db_mysql.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-TOP=$(dirname "$0")
+TOP=/go/src/github.com/Juniper/contrail/tools
 
-echo "Resetting MySQL..."
-docker exec contrail_mysql mysql -uroot -pcontrail123 -e "drop database if exists contrail_test;" && echo "Drop db ok"
-docker exec contrail_mysql mysql -uroot -pcontrail123 -e "create database contrail_test;" && echo "Create db ok"
-docker exec --interactive contrail_mysql mysql -uroot -pcontrail123 contrail_test < $TOP/init_mysql.sql && echo "Init db ok"
+echo "Mounts:"
+docker inspect -f '{{ range $i, $m := .Mounts }}{{ $m.Source }}:{{ $m.Destination }}{{"\n"}}{{end}}' contrail_mysql
+
+docker exec contrail_mysql mysql -uroot -pcontrail123 -e "drop database if exists contrail_test;"
+docker exec contrail_mysql mysql -uroot -pcontrail123 -e "create database contrail_test;"
+docker exec --interactive contrail_mysql sh -c "mysql -uroot -pcontrail123 contrail_test < $TOP/init_mysql.sql"
