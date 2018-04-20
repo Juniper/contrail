b00a43730b9908bcc6df75d24786b52942d1b379
Michał Błotniak <michal.blotniak@codilime.com>
2018-04-20 10:33:14 +0200

    Mount sourcedir in DB containers
    Passing huge init SQL files via stdin to docker exec caused random
    deadlocks. To avoid this files are mounted and read by DB clients inside
    containers.
    
    Note that containers in dev environments should be rebuilt
    with `make testenv`.
    
    Change-Id: Ia7688c7048da88cb016c28682754145dc37c547b
    Partial-Bug: #1765506

--- tools/reset_db_psql.sh
+++ tools/reset_db_psql.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-TOP=$(dirname "$0")
+TOP=/go/github.com/Juniper/contrail/tools
+
+echo "Mounts:"
+docker inspect -f '{{ range $i, $m := .Mounts }}{{ $m.Source }}:{{ $m.Destination }}{{"\n"}}{{end}}' contrail_postgres
 
 docker exec contrail_postgres psql -U postgres -c "drop database contrail_test"
 docker exec contrail_postgres psql -U postgres -c "create database contrail_test"
-docker exec --interactive contrail_postgres psql -U postgres contrail_test < $TOP/init_psql.sql
+docker exec --interactive contrail_postgres psql -U postgres contrail_test -f $TOP/init_psql.sql
