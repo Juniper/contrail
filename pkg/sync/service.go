b6e230c2392f1de3e23dada5392d50809f4e1922
Nachi Ueno <nati.ueno@gmail.com>
2018-04-26 14:07:40 -0700

    Consolidate configuraion files
    In this commit,  we refactor configuraion parameters to more simple
    configuraion management. Also we integrate multiple processes,  and
    make turn on/off by configuraion file.
    
    Closes-Bug: #1763538
    Change-Id: Ic0d5ae35fa15e1048b1cb93ef9125b9b6699dd8e

--- pkg/sync/service.go
+++ pkg/sync/service.go
@@ -164,10 +164,10 @@ func createMySQLWatcher(log *logrus.Entry, sink replication.RowSink) (watchClose
 
 func canalConfig() *mysqlcanal.Config {
 	c := mysqlcanal.NewDefaultConfig()
-	c.Addr = viper.GetString("host")
-	c.User = viper.GetString("user")
-	c.Password = viper.GetString("password")
-	c.Dump.Databases = []string{viper.GetString("name")}
+	c.Addr = viper.GetString("database.host")
+	c.User = viper.GetString("database.user")
+	c.Password = viper.GetString("database.password")
+	c.Dump.Databases = []string{viper.GetString("database.name")}
 	c.Dump.DiscardErr = false
 	c.ServerID = randomServerID()
 	return c
