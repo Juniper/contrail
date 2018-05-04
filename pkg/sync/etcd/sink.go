9f34f10c18c18c40fe9c295fc8534e6979a878f3
Michał Błotniak <michal.blotniak@codilime.com>
2018-05-04 10:52:47 +0200

    Rename package, handle partial updates, add Codec
    Etcd package was renamed to Sink to avoid coupling with db
    implementation.
    Partial updates are handled with mergo since it does the job well
    in not overwriting ref lists with nulls.
    Add sink.Codec interface allowing to implement pluggable data serializers.
    
    Change-Id: I0bccd2a76e75b36caa7e0ea7dd73278fc1e50c7f
    Partial-Bug: #1758826

--- pkg/sync/etcd/sink.go
+++
@@ -1,8 +0,0 @@
-package etcd
-
-// Sink represents service that handler transfers data to.
-type Sink interface {
-	Create(resourceName string, pk string, obj interface{}) error // TODO change to proto Message
-	Update(resourceName string, pk string, obj interface{}) error // TODO proto message
-	Delete(resourceName string, pk string) error
-}
