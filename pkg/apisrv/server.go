4591b5b6b6fcac7efd8d8ef9ecd4d8de330cd4b1
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-05-11 21:44:12 +0000

    Reading static path root from  the config file
    Change-Id: I4f3c5fc190e68ecfb96ec7359d6fad2933023efa
    Closes-Bug: 1770688

--- pkg/apisrv/server.go
+++ pkg/apisrv/server.go
@@ -141,14 +141,20 @@ func (s *Server) Init() error {
 	if keystoneAuthURL != "" {
 		keystoneClient = keystone.NewKeystoneClient(keystoneAuthURL,
 			viper.GetBool("keystone.insecure"))
-		e.Use(keystone.AuthMiddleware(keystoneClient,
-			[]string{
-				"/keystone/v3/auth/tokens",
-				"/proxy/keystone/v3/auth/tokens",
-				"/keystone/v3/auth/projects",
-				"/v3/auth/tokens",
-				"/public"},
-			endpointStore))
+		skipPaths := []string{
+			"/keystone/v3/auth/tokens",
+			"/proxy/keystone/v3/auth/tokens",
+			"/keystone/v3/auth/projects",
+			"/v3/auth/tokens",
+		}
+		for root, _ := range viper.GetStringMap("static_files") {
+			if !strings.HasPrefix(root, "/") {
+				root = "/" + root
+			}
+			skipPaths = append(skipPaths, root)
+		}
+		e.Use(keystone.AuthMiddleware(
+			keystoneClient, skipPaths, endpointStore))
 	} else if viper.GetBool("no_auth") {
 		e.Use(noAuthMiddleware())
 	}
