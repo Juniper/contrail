3c07a6102d90e939abef653911ff37ea96f9781a
Jan Darowski <jan.darowski@codilime.com>
2018-05-24 15:27:47 +0200

    Add basic GlobalSystemConfig validation - Asn validation - User defined statistics validation - BGP ports range validation
    Partial-bug: #1773170
    Change-Id: I7eebbef83ba515cdcd284164ef93e0bc6231855a

---
+++ pkg/types/global_system_config.go
@@ -0,0 +1,143 @@
+package types
+
+import (
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/db"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+)
+
+// CreateGlobalSystemConfig by design should never be called.
+// GlobalSystemConfig can only be created by DBInit, never by user request
+func (sv *ContrailTypeLogicService) CreateGlobalSystemConfig(
+	ctx context.Context,
+	request *models.CreateGlobalSystemConfigRequest) (resp *models.CreateGlobalSystemConfigResponse, err error) {
+	return nil, common.ErrorBadRequest("Trying to call create GlobalSystemConfig outside of DBInit")
+}
+
+// UpdateGlobalSystemConfig performs type specific checks for update.
+func (sv *ContrailTypeLogicService) UpdateGlobalSystemConfig(
+	ctx context.Context,
+	request *models.UpdateGlobalSystemConfigRequest) (*models.UpdateGlobalSystemConfigResponse, error) {
+	var resp *models.UpdateGlobalSystemConfigResponse
+
+	err := db.DoInTransaction(
+		ctx,
+		sv.DB.DB(),
+		func(ctx context.Context) (err error) {
+			// TODO(Jan Darowski) JBE-431 - Add proper uuid update when using draft object (enable_security_policy_draft).
+			oldObjResp, err := sv.DB.GetGlobalSystemConfig(ctx, &models.GetGlobalSystemConfigRequest{ID: request.GetGlobalSystemConfig().GetUUID()})
+			updateObj := request.GlobalSystemConfig
+
+			if err != nil {
+				return err
+			}
+
+			if oldObjResp == nil {
+				return errors.Errorf("No GlobalSystemConfig found to update")
+			}
+			oldObj := oldObjResp.GlobalSystemConfig
+
+			err = sv.checkUdc(updateObj)
+			if err != nil {
+				return err
+			}
+
+			err = sv.checkAsn(ctx, updateObj)
+			if err != nil {
+				return err
+			}
+
+			err = sv.checkBgpaasPorts(ctx, updateObj, oldObj)
+			if err != nil {
+				return err
+			}
+
+			resp, err = sv.Next().UpdateGlobalSystemConfig(ctx, request)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "GlobalSystemConfig update validation failed")
+	}
+	return resp, nil
+}
+
+func (sv *ContrailTypeLogicService) checkUdc(obj *models.GlobalSystemConfig) (err error) {
+	if obj.UserDefinedLogStatistics == nil {
+		return nil
+	}
+	return obj.UserDefinedLogStatistics.ValidateRegexps()
+}
+
+func (sv *ContrailTypeLogicService) checkAsn(ctx context.Context, updateObj *models.GlobalSystemConfig) (err error) {
+
+	globalAsn := updateObj.AutonomousSystem
+	if globalAsn == 0 {
+		return nil
+	}
+
+	vnList, err := sv.DB.ListVirtualNetwork(ctx, &models.ListVirtualNetworkRequest{Spec: &models.ListSpec{Fields: []string{"route_target_list"}}})
+	if err != nil {
+		return err
+	}
+
+	var multiError common.MultiError
+	for _, vn := range vnList.VirtualNetworks {
+		rtList := vn.RouteTargetList
+		for _, rt := range rtList.RouteTarget {
+			userDefined, err := models.IsStringRouteTargetUserDefined(rt, globalAsn)
+			if err != nil {
+				return err
+			}
+			if !userDefined {
+				multiError = append(multiError, errors.Errorf("\t- %s (%s) have route target %s configured\n", vn.FQName, vn.UUID, rt))
+			}
+		}
+	}
+
+	if multiError != nil {
+		return errors.Wrapf(multiError, "Virtual networks are configured with a route target with this ASN %d and route target value in the same range as used by automatically allocated route targets:\n", globalAsn)
+	}
+	return nil
+}
+
+func (sv *ContrailTypeLogicService) checkBgpaasPorts(ctx context.Context, updateObj *models.GlobalSystemConfig, oldObj *models.GlobalSystemConfig) (err error) {
+	if updateObj.BgpaasParameters == nil {
+		return nil
+	}
+
+	err = updateObj.BgpaasParameters.ValidatePortRange()
+	if err != nil {
+		return err
+	}
+
+	newPortsRange := updateObj.GetBgpaasParameters()
+	oldPortsRange := models.GetDefaultBGPaaServiceParameters()
+
+	if oldObj.BgpaasParameters != nil {
+		oldPortsRange = oldObj.BgpaasParameters
+	}
+
+	bgpaasList, err := sv.DB.ListBGPAsAService(ctx, &models.ListBGPAsAServiceRequest{Spec: &models.ListSpec{Count: true}})
+
+	if err != nil {
+		return err
+	}
+
+	if len(bgpaasList.BGPAsAServices) > 0 {
+		if !newPortsRange.EnclosesRange(oldPortsRange) {
+			return errors.Errorf("BGP Port range cannot be shrunk. Old: (%d %d) New: (%d %d)",
+				oldPortsRange.PortStart,
+				oldPortsRange.PortEnd,
+				newPortsRange.PortStart,
+				newPortsRange.PortEnd)
+		}
+	}
+	return nil
+}
