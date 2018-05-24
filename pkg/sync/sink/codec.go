d6f137b356b2b1d2dc4a9dc01e77b73ffec53aa3
Michał Błotniak <michal.blotniak@codilime.com>
2018-05-24 18:29:52 +0200

    [WIP] Redesign sync to handle ref tables
    Previous type agnostic approach wasn't flexible enough to handle ref
    tables. That operation required access to schema level Ref fields and
    had to mutate 2 different schema objects (refs and backrefs), which
    is possible only in generated code.
    
    In this design we generate method for every table which allows code that
    is per database table type (object, refs, shares).
    
    Change-Id: I853df07e851a5f87bc052848e17d61ca55fafb99
    Partial-Bug: #1772623

--- pkg/sync/sink/codec.go
+++ pkg/sync/sink/codec.go
@@ -2,7 +2,6 @@ package sink
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"path"
 
@@ -14,46 +13,57 @@ import (
 // Codec can encode objects and update encoded data with new data object.
 type Codec interface {
 	Encode(obj db.Object) ([]byte, error)
-	Update(data []byte, obj db.Object) ([]byte, error)
+	Decode(data []byte, dst db.Object) error
 	Key() string
 }
 
+// ResourceKey builds resource key string for given parameters that is passed to sink.
+func ResourceKey(c Codec, resourceName, pk interface{}) string {
+	return path.Join(c.Key(), fmt.Sprint(resourceName), fmt.Sprint(pk))
+}
+
+// UpdateResourceData uses codec to decode oldData, merges it with updateObjec,
+// decodes result with codec and returns serialized form.
+func UpdateResourceData(c Codec, oldData []byte, updateObject db.Object) ([]byte, error) {
+	if updateObject == nil {
+		return oldData, nil
+	}
+
+	// Get fresh instance of updateObject's type by cloning and clearing the clone.
+	dst := proto.Clone(updateObject)
+	dst.Reset()
+
+	// Decode oldData into the clone.
+	if err := c.Decode(oldData, dst); err != nil {
+		return nil, err
+	}
+	// Merge new object with old one.
+	if err := mergo.Merge(dst, updateObject, mergo.WithOverride); err != nil {
+		return nil, err
+	}
+	// Return serialized form.
+	return c.Encode(dst)
+}
+
 // JSONCodec is Codec which uses JSON format for storing data.
-type JSONCodec struct{}
+var JSONCodec Codec = jsonCodec{}
+
+type jsonCodec struct{}
 
 // Encode serializes data to JSON format.
-func (j *JSONCodec) Encode(obj db.Object) ([]byte, error) {
+func (j jsonCodec) Encode(obj db.Object) ([]byte, error) {
 	return json.Marshal(obj)
 }
 
-// Update updates JSON-encoded data with obj field values and returns serialized output.
-func (j *JSONCodec) Update(data []byte, obj db.Object) ([]byte, error) {
-	if obj == nil {
-		return nil, errors.New("got nil db.Object")
-	}
-	// Get new instance of the same type as obj.
-	dst := proto.Clone(obj)
-	dst.Reset()
-
-	// Unmarshal old data into fresh instance.
-	err := json.Unmarshal(data, dst)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling previous object data: %v", err)
-	}
-
-	// Merge new obj to old.
-	if err := mergo.Merge(dst, obj, mergo.WithOverride); err != nil {
-		return nil, fmt.Errorf("error merging structs: %v", err)
-	}
-
-	return j.Encode(dst)
+// Decode deserializes data to map[string]interface{}.
+func (j jsonCodec) Decode(data []byte, dst db.Object) error {
+	return json.Unmarshal(data, dst)
 }
 
 // Key returns codec identifier.
-func (j *JSONCodec) Key() string {
+func (j jsonCodec) Key() string {
 	return "json"
 }
 
-func resourceKey(c Codec, resourceName, pk string) string {
-	return path.Join(c.Key(), resourceName, pk)
-}
+// ProtoCodec is Codec which uses protobuf format for storing data.
+var ProtoCodec Codec // TODO(Michal): implement
