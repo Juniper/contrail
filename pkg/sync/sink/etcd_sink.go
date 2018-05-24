314f1580f6e20cd96fe2d039d7cddc78b7ff462f
Michał Błotniak <michal.blotniak@codilime.com>
2018-05-24 18:29:52 +0200

    WIP: Resolve ref tables
    Change-Id: I853df07e851a5f87bc052848e17d61ca55fafb99

--- pkg/sync/sink/etcd_sink.go
+++ pkg/sync/sink/etcd_sink.go
@@ -82,14 +82,51 @@ func (s *ETCDSink) Update(resourceName string, pk string, object db.Object) erro
 
 // Delete removes from etcd "<resourceName>/json/<resourcePrimaryKey>" key.
 func (s *ETCDSink) Delete(resourceName string, pk string) error {
-	s.log.WithFields(logrus.Fields{"key": resourceKey(s.codec, resourceName, pk)}).Debugf(
+	key := resourceKey(s.codec, resourceName, pk)
+	s.log.WithFields(logrus.Fields{"key": key}).Debugf(
 		"Deleting %s-encoded resource from etcd", s.codec.Key())
 	ctx, cancel := context.WithTimeout(context.Background(), kvClientRequestTimeout)
 	defer cancel()
 
-	_, err := s.kvClient.Delete(ctx, resourceKey(s.codec, resourceName, pk))
+	_, err := s.kvClient.Delete(ctx, key)
 	if err != nil {
 		return fmt.Errorf("delete JSON-encoded resource in etcd: %s", err)
 	}
+	return nil
+}
+
+func (s *ETCDSink) CreateRef(from string, to string, pkFrom interface{}, pkTo interface{}) error {
+	fromKey := resourceKey(s.codec, from, pkFrom)
+	toKey := resourceKey(s.codec, to, pkTo)
+
+	s.log.WithFields(logrus.Fields{"from": fromKey, "to": toKey}).Debugf(
+		"Creating refs in %s-encoded objects", s.codec.Key())
+	ctx, cancel := context.WithTimeout(context.Background(), kvClientRequestTimeout)
+	defer cancel()
+
+	err := s.inTransaction(ctx, func(stm conc.STM) error {
+		oldValue := stm.Get(fromKey)
+		dict, err := s.codec.Decode([]byte(oldValue))
+		if err != nil {
+			return err
+		}
+		refKey := fmt.Sprintf("%s_refs", to)
+		if slice, ok := dict[refKey].([]interface{}); ok {
+			dict[refKey] = append(slice, map[string]interface{}{"uuid": pkTo})
+		} // TODO handle error
+
+		p, err := s.codec.Encode(dict)
+		if err != nil {
+			return fmt.Errorf("encode object to %s: %s", s.codec.Key(), err)
+		}
+
+		stm.Put(key, string(p))
+		return nil
+	})
+
+	return nil
+}
+
+func (s *ETCDSink) DeleteRef(from string, to string, pkFrom interface{}, pkTo interface{}) error {
 	return nil
 }
--- pkg/sync/sink/sink.go
+++ pkg/sync/sink/sink.go
@@ -7,4 +7,7 @@ type Sink interface {
 	Create(resourceName string, pk string, obj db.Object) error
 	Update(resourceName string, pk string, obj db.Object) error
 	Delete(resourceName string, pk string) error
+
+	CreateRef(from, to string, pkFrom, pkTo interface{}) error
+	DeleteRef(from, to string, pkFrom, pkTo interface{}) error
 }
