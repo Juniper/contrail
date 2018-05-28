3bcf616329fffecd6f1da63e961d87352aa0209e
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-05-28 18:51:43 +0200

    Support quota per project - architecture
    This contains just core architecture and draft implementation for
    VirtualNetworks and SecurityGroups
    
    Further commits are needed to correct errors and finish implementation
    
    Change-Id: I6ad22b82f5a6fd9f6d8578d734466c0f78fdcfb1
    Partial-Bug: #1773375

--- pkg/schema/template.go
+++ pkg/schema/template.go
@@ -138,6 +138,18 @@ func LoadTemplates(path string) ([]*TemplateConfig, error) {
 
 //ApplyTemplates applies templates and generate codes.
 func ApplyTemplates(api *API, templateBase string, config []*TemplateConfig) error {
+
+	// Make custom filters available for everyone
+
+	/* When called like this: {{ dict_value|dict_get_JSONSchema_by_string_key:key_var }}
+	then: dict_value is here as `in' variable and key_var is here as `param'
+	This is needed to obtain value from map with a key in variable (not as a hardcoded string)
+	*/
+	pongo2.RegisterFilter("dict_get_JSONSchema_by_string_key", func(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+		m := in.Interface().(map[string]*JSONSchema)
+		return pongo2.AsValue(m[param.String()]), nil
+	})
+
 	for _, templateConfig := range config {
 		err := templateConfig.apply(templateBase, api)
 		if err != nil {
