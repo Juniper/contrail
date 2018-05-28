30424f2ddf52c73da8d99d4a7a07d2170dc6b43c
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-05-28 10:49:01 +0200

    [WIP] Support floating ip specific logic
    - check if parent type is instance-ip
    - get floating ip subnets
    - ip allocation and deallocation
    - some basic tests for CreateFloatingIP method
    - unit tests
    - apisrv basic main tests
    
    Depends-On: If8f3e223dcbe532d37aa8570d0297a7a6b4f0064
    Depends-On: I2e9e695665d0492d66c262b22c7c424c349b2cf3
    Change-Id: I6cbabcaaab2c9f2a86ed915c2ed99608b7f51eeb
    Partial-bug: #1773748

---
+++ pkg/types/floating_ip.go
@@ -0,0 +1,238 @@
+package types
+
+import (
+	"strings"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/db"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/types/ipam"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+// CreateFloatingIP does pre check for floating ip.
+func (sv *ContrailTypeLogicService) CreateFloatingIP(
+	ctx context.Context,
+	request *models.CreateFloatingIPRequest) (*models.CreateFloatingIPResponse, error) {
+
+	var response *models.CreateFloatingIPResponse
+	floatingIP := request.GetFloatingIP()
+	err := db.DoInTransaction(
+		ctx,
+		sv.DB.DB(),
+		func(ctx context.Context) error {
+			var err error
+			if sv.checkIfParentTypeIsInstanceIP(floatingIP) {
+				response, err = sv.Next().CreateFloatingIP(ctx, request)
+				return err
+			}
+
+			virtualNetwork, err := sv.getVirtualNetwork(ctx, floatingIP)
+			if err != nil {
+				return err
+			}
+
+			ipAddress := floatingIP.GetFloatingIPAddress()
+			if len(ipAddress) > 0 {
+				var isAllocated bool
+				isAllocated, err = sv.checkIfRequestedIPAddressIsFree(ctx, virtualNetwork, ipAddress)
+				if err != nil {
+					return err
+				}
+				if isAllocated {
+					return grpc.Errorf(codes.AlreadyExists, "Ip address already in use")
+				}
+			}
+
+			floatingIPAddress, err := sv.tryToAllocateIPAddress(ctx, virtualNetwork, floatingIP)
+			if err != nil {
+				return err
+			}
+
+			floatingIP.FloatingIPAddress = floatingIPAddress
+			response, err = sv.Next().CreateFloatingIP(ctx, request)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	return response, err
+}
+
+// DeleteFloatingIP does post actions for delete floating ip.
+func (sv *ContrailTypeLogicService) DeleteFloatingIP(
+	ctx context.Context,
+	request *models.DeleteFloatingIPRequest) (*models.DeleteFloatingIPResponse, error) {
+
+	var response *models.DeleteFloatingIPResponse
+
+	id := request.GetID()
+	err := db.DoInTransaction(
+		ctx,
+		sv.DB.DB(),
+		func(ctx context.Context) error {
+			floatingIP, err := sv.getFloatingIP(ctx, id)
+			if err != nil {
+				return err
+			}
+
+			response, err = sv.Next().DeleteFloatingIP(ctx, request)
+			if err != nil {
+				return err
+			}
+
+			if sv.checkIfParentTypeIsInstanceIP(floatingIP) {
+				return nil
+			}
+
+			virtualNetwork, err := sv.getVirtualNetwork(ctx, floatingIP)
+			if err != nil {
+				return err
+			}
+
+			return sv.deallocateIPAddress(ctx, virtualNetwork, floatingIP)
+		})
+
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) deallocateIPAddress(ctx context.Context, virtualNetwork *models.VirtualNetwork,
+	floatingIP *models.FloatingIP) error {
+
+	deallocateIPParams := &ipam.DeallocateIPRequest{
+		VirtualNetwork: virtualNetwork,
+		IPAddress:      floatingIP.GetFloatingIPAddress(),
+	}
+
+	return sv.AddressManager.DeallocateIP(ctx, deallocateIPParams)
+}
+
+func (sv *ContrailTypeLogicService) checkIfParentTypeIsInstanceIP(floatingIP *models.FloatingIP) bool {
+	return floatingIP.IsParentTypeInstanceIp()
+}
+
+func (sv *ContrailTypeLogicService) checkIfRequestedIPAddressIsFree(ctx context.Context,
+	virtualNetwork *models.VirtualNetwork, ipAddress string) (bool, error) {
+
+	isIPAllocatedRequest := &ipam.IsIPAllocatedRequest{
+		VirtualNetwork: virtualNetwork,
+		IPAddress:      ipAddress,
+	}
+
+	isAllocated, err := sv.AddressManager.IsIPAllocated(ctx, isIPAllocatedRequest)
+	if err != nil {
+		return false, err
+	}
+	return isAllocated, err
+}
+
+func (sv *ContrailTypeLogicService) tryToAllocateIPAddress(ctx context.Context,
+	virtualNetwork *models.VirtualNetwork, floatingIP *models.FloatingIP) (string, error) {
+
+	floatingIPPoolSubnets, err := sv.getFloatingIPPoolSubnets(ctx, floatingIP)
+	if err != nil {
+		return "", common.ErrorBadRequest("floating-ip-pool lookup failed with error:" + err.Error())
+	}
+
+	var floatingIPAddress string
+	if floatingIPPoolSubnets == nil || len(floatingIPPoolSubnets.GetSubnetUUID()) == 0 {
+		// Subnet specification was not found on the floating-ip-pool.
+		// Proceed to allocated floating-ip from any of the subnets
+		// on the virtual-network.
+		floatingIPAddress, _, err = sv.AddressManager.AllocateIP(
+			ctx, &ipam.AllocateIPRequest{
+				VirtualNetwork: virtualNetwork,
+				IPAddress:      floatingIP.GetFloatingIPAddress(),
+			})
+		if err != nil {
+			return "", err
+		}
+		return floatingIPAddress, nil
+	}
+	var subnetsTried []string
+	for _, floatingIPPoolSubnetUUID := range floatingIPPoolSubnets.GetSubnetUUID() {
+		// Record the subnets that we try to allocate from.
+		subnetsTried = append(subnetsTried, floatingIPPoolSubnetUUID)
+
+		allocateIPParams := &ipam.AllocateIPRequest{
+			VirtualNetwork: virtualNetwork,
+			IPAddress:      floatingIP.GetFloatingIPAddress(),
+			SubnetUUID:     floatingIPPoolSubnetUUID,
+		}
+
+		floatingIPAddress, _, err = sv.AddressManager.AllocateIP(ctx, allocateIPParams)
+		if _, ok := err.(ipam.ErrSubnetExhausted); ok {
+			// This subnet is exhausted. Try next subnet.
+			continue
+		}
+
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if floatingIPAddress == "" {
+		// Floating-ip could not be allocated from any of the
+		// configured subnets. Return ResourceExhausted
+		return "", grpc.Errorf(codes.ResourceExhausted, "subnets tried: %s", strings.Join(subnetsTried, ", "))
+	}
+	return floatingIPAddress, nil
+}
+
+// Get virtual network associated with floatingIP and floatingIPPool
+func (sv *ContrailTypeLogicService) getVirtualNetwork(
+	ctx context.Context, floatingIP *models.FloatingIP) (*models.VirtualNetwork, error) {
+
+	floatingIPPoolResponse, err := sv.DB.GetFloatingIPPool(ctx,
+		&models.GetFloatingIPPoolRequest{
+			ID: floatingIP.GetParentUUID(),
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	virtualNetworkResponse, err := sv.DB.GetVirtualNetwork(ctx,
+		&models.GetVirtualNetworkRequest{
+			ID: floatingIPPoolResponse.GetFloatingIPPool().GetParentUUID(),
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	return virtualNetworkResponse.GetVirtualNetwork(), nil
+}
+
+func (sv *ContrailTypeLogicService) getFloatingIP(
+	ctx context.Context, id string) (*models.FloatingIP, error) {
+
+	floatingIPResponse, err := sv.DB.GetFloatingIP(ctx,
+		&models.GetFloatingIPRequest{
+			ID: id,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return floatingIPResponse.GetFloatingIP(), nil
+}
+
+// Get all subnets configured on the floating-ip-pool.
+// It is acceptable that list of subnets may be absent or empty
+func (sv *ContrailTypeLogicService) getFloatingIPPoolSubnets(
+	ctx context.Context,
+	floatingIP *models.FloatingIP) (*models.FloatingIpPoolSubnetType, error) {
+
+	floatingIPPoolResponse, err := sv.DB.GetFloatingIPPool(ctx,
+		&models.GetFloatingIPPoolRequest{
+			ID: floatingIP.GetParentUUID(),
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	floatingIPPool := floatingIPPoolResponse.GetFloatingIPPool()
+	return floatingIPPool.GetFloatingIPPoolSubnets(), nil
+}
