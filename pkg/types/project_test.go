ec4a7e40473134a3d44ba4bdb3e4db2c9467495a
Tomasz Młynarczyk <t.mlynarczyk5@gmail.com>
2018-05-30 19:12:15 +0200

    check vxlan config in project
    Base commit for project type validation with vxlan validation.
    
    Change-Id: I9a8708861feb3e0edd21bb6cbdb2f22a05892b90
    Partial-Bug: #1776439

---
+++ pkg/types/project_test.go
@@ -0,0 +1,89 @@
+package types
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestVxlanConfigField(t *testing.T) {
+	_, ok := reflect.TypeOf(models.Project{}).FieldByName(vxlanConfigField)
+	assert.True(t, ok, "models.Project should have vxlanConfigFiled property")
+}
+
+func TestCheckVxlanConfig(t *testing.T) {
+	tests := []struct {
+		name           string
+		updateRequest  *services.UpdateProjectRequest
+		currentProject *models.Project
+		errorCode      codes.Code
+	}{
+		{
+			name: "No vxlan change requested",
+			updateRequest: &services.UpdateProjectRequest{
+				FieldMask: types.FieldMask{Paths: []string{"Foo", "Bar"}},
+				Project:   &models.Project{VxlanRouting: false},
+			},
+			currentProject: &models.Project{
+				VxlanRouting:   true,
+				LogicalRouters: []*models.LogicalRouter{{}},
+			},
+		},
+		{
+			name: "Vxlan will not change",
+			updateRequest: &services.UpdateProjectRequest{
+				FieldMask: types.FieldMask{Paths: []string{vxlanConfigField}},
+				Project:   &models.Project{VxlanRouting: true},
+			},
+			currentProject: &models.Project{
+				VxlanRouting:   true,
+				LogicalRouters: []*models.LogicalRouter{{}},
+			},
+		},
+		{
+			name: "No vxlan routers attached",
+			updateRequest: &services.UpdateProjectRequest{
+				FieldMask: types.FieldMask{Paths: []string{vxlanConfigField}},
+				Project:   &models.Project{VxlanRouting: false},
+			},
+			currentProject: &models.Project{
+				VxlanRouting:   true,
+				LogicalRouters: []*models.LogicalRouter{},
+			},
+		},
+		{
+			name: "Vxlan routers already attached",
+			updateRequest: &services.UpdateProjectRequest{
+				FieldMask: types.FieldMask{Paths: []string{vxlanConfigField}},
+				Project:   &models.Project{VxlanRouting: false},
+			},
+			currentProject: &models.Project{
+				VxlanRouting:   true,
+				LogicalRouters: []*models.LogicalRouter{{}},
+			},
+			errorCode: codes.InvalidArgument,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var sv ContrailTypeLogicService
+			err := sv.checkVxlanConfig(tt.currentProject, tt.updateRequest)
+			if tt.errorCode != codes.OK {
+				assert.Error(t, err)
+				status, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, tt.errorCode, status.Code())
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
