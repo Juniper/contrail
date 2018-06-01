2ebf681780385063524f8f0dee7d0fd716b9f5e1
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-06-01 11:21:25 +0200

    [WIP] Support IPAM specific logic - check existance of ipamSubnets - check if subnet is flat - check overlap in subnets - check ipam subnets configuration TODO: - create subnet using allocator - implement update method
    Partial-bug: #1774600
    
    Change-Id: Ic8cacf1701eac56fa59a16595aeb8a366bfa0698

---
+++ pkg/types/network_ipam.go
@@ -0,0 +1,169 @@
+package types
+
+import (
+	"net"
+
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/types/ipam"
+)
+
+func isSubnetOverlap(subnet1, subnet2 *net.IPNet) bool {
+	return subnet1.Contains(subnet2.IP) || subnet2.Contains(subnet1.IP)
+}
+
+func appendSubnetIfNoOverlap(subnets []*net.IPNet, subnet *net.IPNet) ([]*net.IPNet, error) {
+	for _, existingSubnet := range subnets {
+		if isSubnetOverlap(subnet, existingSubnet) {
+			return nil, errors.Errorf(
+				"overlapping addresses: %s, %s", subnet.String(), existingSubnet.String())
+		}
+	}
+	return append(subnets, subnet), nil
+}
+
+func checkSubnetsOverlap(ipamSubnets []*models.IpamSubnetType) error {
+	subnets := []*net.IPNet{}
+	for _, subnet := range ipamSubnets {
+		n, err := subnet.Subnet.Net()
+		if err != nil {
+			return err
+		}
+		subnets, err = appendSubnetIfNoOverlap(subnets, n)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CreateNetworkIpam do pre check for network ipam
+func (sv *ContrailTypeLogicService) CreateNetworkIpam(
+	ctx context.Context,
+	request *services.CreateNetworkIpamRequest) (response *services.CreateNetworkIpamResponse, err error) {
+
+	networkIpam := request.GetNetworkIpam()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			ipamSubnets := networkIpam.GetIpamSubnets()
+			if ipamSubnets == nil {
+				response, err = sv.Next().CreateNetworkIpam(ctx, request)
+				return err
+			}
+
+			if !networkIpam.IsFlatSubnet() {
+				return common.ErrorBadRequest("Ipam subnets are allowed only with flat-subnet")
+			}
+
+			err = checkSubnetsOverlap(ipamSubnets.GetSubnets())
+			if err != nil {
+				return common.ErrorBadRequest(err.Error())
+			}
+
+			for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+				err = ipamSubnet.CheckIfSubnetParamsAreValid()
+				if err != nil {
+					return err
+				}
+			}
+
+			ipamUUID := networkIpam.GetUUID()
+			for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+				subnetUUID, err := sv.createIpamSubnet(ctx, ipamSubnet, ipamUUID)
+				if err != nil {
+					return err
+				}
+				ipamSubnet.SubnetUUID = subnetUUID
+			}
+
+			response, err = sv.Next().CreateNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// DeleteNetworkIpam do pre check for network ipam deletion
+func (sv *ContrailTypeLogicService) DeleteNetworkIpam(
+	ctx context.Context,
+	request *services.DeleteNetworkIpamRequest) (response *services.DeleteNetworkIpamResponse, err error) {
+
+	id := request.GetID()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			var networkIpam *models.NetworkIpam
+			networkIpam, err = sv.getNetworkIpam(ctx, id)
+			if err != nil {
+				return err
+			}
+
+			ipamSubnets := networkIpam.GetIpamSubnets()
+			if networkIpam != nil && networkIpam.IsFlatSubnet() && ipamSubnets != nil {
+				for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+					err := sv.deleteIpamSubnet(ctx, ipamSubnet.GetSubnetUUID())
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+			response, err = sv.Next().DeleteNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// UpdateNetworkIpam do pre check for network ipam update
+func (sv *ContrailTypeLogicService) UpdateNetworkIpam(
+	ctx context.Context,
+	request *services.UpdateNetworkIpamRequest) (response *services.UpdateNetworkIpamResponse, err error) {
+
+	//networkIpam := request.GetNetworkIpam()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			//TODO ipam_mgmt check
+			//TODO check new subnet method
+			//TODO check flat-subnet
+			//TODO rest of update checks
+			response, err = sv.Next().UpdateNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) getNetworkIpam(
+	ctx context.Context,
+	id string) (*models.NetworkIpam, error) {
+
+	networkIpamRes, err := sv.DataService.GetNetworkIpam(ctx, &services.GetNetworkIpamRequest{ID: id})
+	if err != nil {
+		return nil, err
+	}
+	return networkIpamRes.GetNetworkIpam(), err
+}
+
+func (sv *ContrailTypeLogicService) createIpamSubnet(
+	ctx context.Context,
+	ipamSubnet *models.IpamSubnetType,
+	ipamUUID string) (subnetUUID string, err error) {
+	createIpamSubnetParams := &ipam.CreateIpamSubnetRequest{
+		IpamSubnet:      ipamSubnet,
+		NetworkIpamUUID: ipamUUID,
+	}
+	return sv.AddressManager.CreateIpamSubnet(ctx, createIpamSubnetParams)
+}
+
+func (sv *ContrailTypeLogicService) deleteIpamSubnet(
+	ctx context.Context,
+	subnetUUID string) error {
+	deleteIpamSubnetParams := &ipam.DeleteIpamSubnetRequest{
+		SubnetUUID: subnetUUID,
+	}
+	return sv.AddressManager.DeleteIpamSubnet(ctx, deleteIpamSubnetParams)
+}
