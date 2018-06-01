ce42d52babfb24b31a4aee4f869f14fc22ae8a51
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-06-01 11:21:25 +0200

    [WIP] Support IPAM specific logic - check existing of ipamSubnets - check if subnet is flat - check overlap in subnets - check ipam subnets configuration TODO: - create subnet using allocator - implement update method
    Partial-bug: #1774600
    
    Change-Id: Ic8cacf1701eac56fa59a16595aeb8a366bfa0698

---
+++ pkg/types/network_ipam.go
@@ -0,0 +1,170 @@
+package types
+
+import (
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/types/ipam"
+	"golang.org/x/net/context"
+	"net"
+)
+
+func isSubnetOverlap(subnet1, subnet2 *net.IPNet) bool {
+	return subnet1.Contains(subnet2.IP) || subnet2.Contains(subnet1.IP)
+}
+
+func appendSubnetIfNoOverlap(subnets []*net.IPNet, subnet *net.IPNet) ([]*net.IPNet, error) {
+	for _, existingSubnet := range subnets {
+		if isSubnetOverlap(subnet, existingSubnet) {
+			return nil, common.ErrorBadRequest(
+				"Overlapping addresses: " + subnet.String() + "," + existingSubnet.String())
+		}
+	}
+	return append(subnets, subnet), nil
+}
+
+func mergeSubnetIfNoOverlap(subnets []*net.IPNet, ipamSubnets []*models.IpamSubnetType) ([]*net.IPNet, error) {
+	for _, subnet := range ipamSubnets {
+		n, err := subnet.Subnet.Net()
+		if err != nil {
+			return nil, err
+		}
+		subnets, err = appendSubnetIfNoOverlap(subnets, n)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return subnets, nil
+}
+
+// CreateNetworkIpam do pre check for network ipam
+func (service *ContrailTypeLogicService) CreateNetworkIpam(
+	ctx context.Context,
+	request *services.CreateNetworkIpamRequest) (response *services.CreateNetworkIpamResponse, err error) {
+
+	networkIpam := request.GetNetworkIpam()
+	err = service.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			ipamSubnets := networkIpam.GetIpamSubnets()
+			if ipamSubnets == nil {
+				response, err = service.Next().CreateNetworkIpam(ctx, request)
+				return err
+			}
+
+			if !networkIpam.IsFlatSubnet() {
+				return common.ErrorBadRequest("Ipam subnets are allowed only with flat-subnet")
+			}
+
+			subnets := []*net.IPNet{}
+			subnets, err = mergeSubnetIfNoOverlap(subnets, ipamSubnets.GetSubnets())
+			if err != nil {
+				return err
+			}
+
+			for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+				err = ipamSubnet.CheckIfSubnetParamsAreValid()
+				if err != nil {
+					return err
+				}
+			}
+
+			ipamUUID := networkIpam.GetUUID()
+
+			for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+				subnetUUID, err := service.createIpamSubnet(ctx, ipamSubnet, ipamUUID)
+				if err != nil {
+					return err
+				}
+				ipamSubnet.SubnetUUID = subnetUUID
+			}
+
+			response, err = service.Next().CreateNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// DeleteNetworkIpam do pre check for network ipam deletion
+func (service *ContrailTypeLogicService) DeleteNetworkIpam(
+	ctx context.Context,
+	request *services.DeleteNetworkIpamRequest) (response *services.DeleteNetworkIpamResponse, err error) {
+
+	id := request.GetID()
+	err = service.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			var networkIpam *models.NetworkIpam
+			networkIpam, err = service.getNetworkIpam(ctx, id)
+			if err != nil {
+				return err
+			}
+
+			ipamSubnets := networkIpam.GetIpamSubnets()
+			if networkIpam == nil || !networkIpam.IsFlatSubnet() || ipamSubnets == nil {
+				response, err = service.Next().DeleteNetworkIpam(ctx, request)
+				return err
+			}
+
+			for _, ipamSubnet := range ipamSubnets.GetSubnets() {
+				err := service.deleteIpamSubnet(ctx, ipamSubnet.GetSubnetUUID())
+				if err != nil {
+					return err
+				}
+			}
+
+			response, err = service.Next().DeleteNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// UpdateNetworkIpam do pre check for network ipam update
+func (service *ContrailTypeLogicService) UpdateNetworkIpam(
+	ctx context.Context,
+	request *services.UpdateNetworkIpamRequest) (response *services.UpdateNetworkIpamResponse, err error) {
+
+	//networkIpam := request.GetNetworkIpam()
+	err = service.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			//TODO ipam_mgmt check
+			//TODO check new subnet method
+			//TODO check flat-subnet
+			//TODO rest of update checks
+			response, err = service.Next().UpdateNetworkIpam(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+func (service *ContrailTypeLogicService) getNetworkIpam(
+	ctx context.Context,
+	id string) (*models.NetworkIpam, error) {
+
+	networkIpamRes, err := service.DataService.GetNetworkIpam(ctx, &services.GetNetworkIpamRequest{ID: id})
+	if err != nil {
+		return nil, err
+	}
+	return networkIpamRes.GetNetworkIpam(), err
+}
+
+func (service *ContrailTypeLogicService) createIpamSubnet(
+	ctx context.Context,
+	ipamSubnet *models.IpamSubnetType,
+	ipamUUID string) (string, error) {
+	createIpamSubnetParams := &ipam.CreateIpamSubnetRequest{
+		IpamSubnet:      ipamSubnet,
+		NetworkIpamUUID: ipamUUID,
+	}
+	return service.AddressManager.CreateIpamSubnet(ctx, createIpamSubnetParams)
+}
+
+func (service *ContrailTypeLogicService) deleteIpamSubnet(
+	ctx context.Context,
+	subnetUUID string) error {
+	deleteIpamSubnetParams := &ipam.DeleteIpamSubnetRequest{
+		SubnetUUID: subnetUUID,
+	}
+	return service.AddressManager.DeleteIpamSubnet(ctx, deleteIpamSubnetParams)
+}
---
+++ pkg/types/network_ipam_test.go
@@ -0,0 +1,184 @@
+package types
+
+import (
+	"testing"
+
+	"database/sql"
+	"github.com/Juniper/contrail/pkg/db"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/mock"
+	"github.com/Juniper/contrail/pkg/types/ipam"
+	"github.com/Juniper/contrail/pkg/types/ipam/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+)
+
+type testNetIpamParams struct {
+	uuid             string
+	ipamSubnetMethod string
+	ipamSubnets      *models.IpamSubnets
+}
+
+func createTestNetworkIpam(testParams *testNetIpamParams) *models.NetworkIpam {
+	networkIpam := models.MakeNetworkIpam()
+	networkIpam.UUID = testParams.uuid
+	networkIpam.IpamSubnetMethod = testParams.ipamSubnetMethod
+	networkIpam.IpamSubnets = testParams.ipamSubnets
+	return networkIpam
+}
+
+func networkIpamNextServMocks(service *ContrailTypeLogicService) {
+	ipamMock := service.AddressManager.(*ipammock.MockAddressManager)
+	ipamMock.EXPECT().CreateIpamSubnet(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(ctx context.Context, request *ipam.CreateIpamSubnetRequest) (subnetUUID string, err error) {
+			return "uuuu-uuuu-iiii-dddd", nil
+		}).AnyTimes()
+	ipamMock.EXPECT().DeleteIpamSubnet(gomock.Any(), gomock.Any()).Return(nil).AnyTimes()
+
+}
+
+func networkIpamIPAMMocks(service *ContrailTypeLogicService) {
+	nextServiceMock := service.Next().(*servicesmock.MockService)
+	nextServiceMock.EXPECT().CreateNetworkIpam(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(ctx context.Context, request *services.CreateNetworkIpamRequest) (response *services.CreateNetworkIpamResponse, err error) {
+			return &services.CreateNetworkIpamResponse{NetworkIpam: request.NetworkIpam}, nil
+		}).AnyTimes()
+	nextServiceMock.EXPECT().DeleteNetworkIpam(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(ctx context.Context, request *services.DeleteNetworkIpamRequest) (response *services.DeleteNetworkIpamResponse, err error) {
+			return &services.DeleteNetworkIpamResponse{ID: request.ID}, nil
+		}).AnyTimes()
+
+}
+
+func TestCreateNetworkIpam(t *testing.T) {
+	tests := []struct {
+		name              string
+		testNetIpamParams *testNetIpamParams
+		fails             bool
+		changedUUID       bool
+	}{
+		{
+			name: "Create succeed because of empty ipam_subnets list",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "notFlat",
+			},
+			fails:       false,
+			changedUUID: false,
+		},
+		{
+			name: "Create succeeds because of empty flat subnet",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "flat-subnet",
+				ipamSubnets:      &models.IpamSubnets{},
+			},
+			fails:       false,
+			changedUUID: false,
+		},
+		{
+			name: "Create fails because of non-empty ipam_subnets list and not flat subnet",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "notFlat",
+				ipamSubnets:      &models.IpamSubnets{},
+			},
+			fails:       true,
+			changedUUID: false,
+		},
+		{
+			name: "Create network ipam with specific ipam_subnets",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "flat-subnet",
+				ipamSubnets:      &models.IpamSubnets{Subnets: []*models.IpamSubnetType{{Subnet: &models.SubnetType{IPPrefix: "10.0.0.0", IPPrefixLen: 24}}}},
+			},
+			fails:       false,
+			changedUUID: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			service := makeMockedContrailTypeLogicService(t, mockCtrl)
+			networkIpamIPAMMocks(service)
+			networkIpamNextServMocks(service)
+
+			emptyTx := sql.Tx{}
+			ctx := context.WithValue(nil, db.Transaction, &emptyTx)
+
+			networkIpam := createTestNetworkIpam(tt.testNetIpamParams)
+			createNetworkIpamRequest := &services.CreateNetworkIpamRequest{NetworkIpam: networkIpam}
+			createNetworkIpamResponse, err := service.CreateNetworkIpam(ctx, createNetworkIpamRequest)
+
+			if tt.fails {
+				assert.Error(t, err)
+				assert.Nil(t, createNetworkIpamResponse)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.NotNil(t, createNetworkIpamResponse)
+
+			if tt.changedUUID {
+				receivedUUID := createNetworkIpamResponse.NetworkIpam.IpamSubnets.Subnets[0].SubnetUUID
+				assert.Equal(t, "uuuu-uuuu-iiii-dddd", receivedUUID)
+			}
+			mockCtrl.Finish()
+		})
+	}
+
+}
+
+func TestDeleteNetworkIpam(t *testing.T) {
+	deleteIpamDBMock := func(service *ContrailTypeLogicService, getNetworkIpamResponse *services.GetNetworkIpamResponse) {
+		dataService := service.DataService.(*servicesmock.MockService)
+		dataService.EXPECT().GetNetworkIpam(gomock.Any(), gomock.Any()).Return(getNetworkIpamResponse, nil)
+	}
+
+	tests := []struct {
+		name              string
+		testNetIpamParams *testNetIpamParams
+	}{
+		{
+			name: "Delete succeeds because of lack of ipam subnets",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "notFlat",
+			},
+		},
+		{
+			name: "Delete succeeds for non-empty ipam network",
+			testNetIpamParams: &testNetIpamParams{
+				uuid:             "uuid",
+				ipamSubnetMethod: "flat-subnet",
+				ipamSubnets:      &models.IpamSubnets{Subnets: []*models.IpamSubnetType{{Subnet: &models.SubnetType{IPPrefix: "10.0.0.0", IPPrefixLen: 24}}}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			service := makeMockedContrailTypeLogicService(t, mockCtrl)
+			networkIpamIPAMMocks(service)
+			networkIpamNextServMocks(service)
+
+			emptyTx := sql.Tx{}
+			ctx := context.WithValue(nil, db.Transaction, &emptyTx)
+
+			networkIpam := createTestNetworkIpam(tt.testNetIpamParams)
+			getNetworkIpamResponse := &services.GetNetworkIpamResponse{NetworkIpam: networkIpam}
+			deleteIpamDBMock(service, getNetworkIpamResponse)
+			deleteNetworkIpamRequest := &services.DeleteNetworkIpamRequest{ID: getNetworkIpamResponse.GetNetworkIpam().GetUUID()}
+			deleteNetworkIpamResponse, err := service.DeleteNetworkIpam(ctx, deleteNetworkIpamRequest)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, deleteNetworkIpamResponse)
+			mockCtrl.Finish()
+		})
+	}
+}
