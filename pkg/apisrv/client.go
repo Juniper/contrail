8a514c8c0685bcc4dfa49b17f3c63fbd0c567fd2
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-06-05 23:22:35 +0000

    Make client retry once after refreshing token
    during 401's(token might have expired)
    
    Change-Id: I15ab941f7a1a90b7c56ac35940debfc5eee5ec88
    Closes-Bug: 1774264

--- pkg/apisrv/client.go
+++ pkg/apisrv/client.go
@@ -191,9 +191,22 @@ func (c *Client) Do(method, path string, data interface{}, output interface{}, e
 			"data":   data,
 		}).Debug("Executing API Server request")
 	}
-	resp, err := c.httpClient.Do(request)
-	if err != nil {
-		return nil, err
+	retryCount := 1
+	doRequest := true
+	var resp *http.Response
+	for doRequest {
+		resp, err = c.httpClient.Do(request)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode == 401 && retryCount != 0 {
+			// token might be expired, refresh token and retry
+			// once if unauthenticated
+			c.Login()
+			retryCount -= 1
+		} else {
+			doRequest = false
+		}
 	}
 	defer resp.Body.Close() // nolint: errcheck
 	err = checkStatusCode(expected, resp.StatusCode)
