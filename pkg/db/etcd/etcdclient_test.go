ed8838a2b835cf12a2c338c4c582be674fcd1f6b
Michał Błotniak <michal.blotniak@codilime.com>
2018-06-05 18:06:10 +0200

    Refactor db/etcd and compilation
    Simplified pkg/db/etcd/Client and changed it's interface to be
    compatible with sync service.
    
    Refactored Intent Compiler to work with new etcd client.
    
    Note that pkg/db/etcd/etcdclient.go file should be renamed to
    etcd/client.go, but the name was left unchanged for simplier code
    review.
    
    Change-Id: I59dc16a5ed40c13e252e27a18b26761de92bda9e
    Partial-Bug: #1772623

---
+++ pkg/db/etcd/etcdclient_test.go
@@ -0,0 +1,61 @@
+package etcd
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_InTransaction(t *testing.T) {
+	tests := []struct {
+		name    string
+		ctx     context.Context
+		do      func(context.Context) error
+		wantErr bool
+	}{
+		{
+			name:    "transaction already in context function returns no error",
+			ctx:     WithTxn(context.Background(), &stmTxn{}),
+			do:      func(context.Context) error { return nil },
+			wantErr: false,
+		},
+		{
+			name:    "transaction already in context function returns error",
+			ctx:     WithTxn(context.Background(), &stmTxn{}),
+			do:      func(context.Context) error { return assert.AnError },
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				log: log.NewLogger("testclient-log"),
+			}
+			err := c.InTransaction(tt.ctx, tt.do)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestClient_InTransactionTxnPassing(t *testing.T) {
+	c := &Client{
+		log: log.NewLogger("testclient-log"),
+	}
+	txn := &stmTxn{}
+	ctx := WithTxn(context.Background(), txn)
+
+	err := c.InTransaction(ctx, func(ctx context.Context) error {
+		if GetTxn(ctx) == nil {
+			return assert.AnError
+		}
+		return nil
+	})
+
+	assert.NoError(t, err)
+}
