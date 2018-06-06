ddcd6001cf598fc3516c184261c3ddff84c4d9f0
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-06-06 17:04:01 +0200

    Added tests
    Change-Id: I6069c11789a404ef9bede5c275a233116ea96bea

--- pkg/types/service.go
+++ pkg/types/service.go
@@ -1,8 +1,11 @@
 package types
 
 import (
-	"github.com/Juniper/contrail/pkg/db"
+	"database/sql"
+
 	"github.com/Juniper/contrail/pkg/serviceif"
+	"golang.org/x/net/context"
+	"github.com/Juniper/contrail/pkg/models"
 )
 
 const (
@@ -10,8 +13,17 @@ const (
 	VirtualNetworkIDPoolKey = "virtual_network_id"
 )
 
+//DBServiceInterface makes mocking DBService possible in type logic tests
+type DBServiceInterface interface {
+	serviceif.Service
+	DB() *sql.DB
+	AllocateInt(context.Context, string) (int64, error)
+	DeallocateInt(context.Context, string, int64) error
+	GetLinkedBGPVPNviaRouter(ctx context.Context, virtualNetwork *models.VirtualNetwork) ([]string, error)
+}
+
 //ContrailTypeLogicService is a service for implementing type specific logic
 type ContrailTypeLogicService struct {
 	serviceif.BaseService
-	DB *db.Service
+	DB             DBServiceInterface
 }
