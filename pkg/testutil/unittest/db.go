0ce4f9be87c3a95ef7538b935c3f0ce8558f6e46
Prasanna Mucharikar <mprasanna@juniper.net>
2018-06-05 22:20:05 -0700

    Initial commit for virtual network UT. testutils contain common infra for UT.
    Change-Id: I1800074c8beeb80fa04364bca8cf036b05d6256c
    Partial-bug: #1775332

---
+++ pkg/testutil/unittest/db.go
@@ -0,0 +1,94 @@
+package unittest
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Juniper/contrail/pkg/db"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/ExpansiveWorlds/instrumentedsql"
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/go-sql-driver/mysql"
+	"github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+const (
+	dbDSNFormatMySQL      = "%s:%s@tcp(%s:3306)/%s"
+	dbDSNFormatPostgreSQL = "sslmode=disable user=%s password=%s host=%s dbname=%s"
+)
+
+var testDB *sql.DB
+
+//TestDbService used outside of testutils package to access DB
+var TestDbService *db.Service
+
+func makeConnection(dbType, databaseConnection string) (*sql.DB, error) {
+	if viper.GetBool("database.debug") {
+		logger := instrumentedsql.LoggerFunc(db.LogQuery)
+		switch dbType {
+		case db.MYSQL:
+			dbType = "instrumented-" + dbType
+			sql.Register(dbType, instrumentedsql.WrapDriver(&mysql.MySQLDriver{}, instrumentedsql.WithLogger(logger)))
+		case db.POSTGRES:
+			dbType = "instrumented-" + dbType
+			sql.Register(dbType, instrumentedsql.WrapDriver(&pq.Driver{}, instrumentedsql.WithLogger(logger)))
+		}
+	}
+
+	return sql.Open(dbType, databaseConnection)
+}
+
+//CreateTestDbService Create TestDB and DBService for Unit tests
+// nolint
+func CreateTestDbService(m *testing.M) {
+	viper.SetConfigName("contrail")
+	viper.AddConfigPath("../apisrv")
+	viper.ReadInConfig() // nolint: errcheck
+	viper.SetEnvPrefix("contrail")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	common.SetLogLevel()
+	var err error
+	dbConfig := viper.GetStringMap("test_database")
+
+	var code int
+	for _, iConfig := range dbConfig {
+		config := common.InterfaceToInterfaceMap(iConfig)
+		driver := config["type"].(string)
+
+		var dbDSNFormat string
+		switch driver {
+		case db.DriverPostgreSQL:
+			dbDSNFormat = dbDSNFormatPostgreSQL
+		case db.DriverMySQL:
+			dbDSNFormat = dbDSNFormatMySQL
+		}
+		dsn := fmt.Sprintf(
+			dbDSNFormat,
+			config["user"].(string),
+			config["password"].(string),
+			config["host"].(string),
+			config["name"].(string),
+		)
+
+		testDB, err = makeConnection(driver, dsn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer testDB.Close() // nolint: errcheck
+		TestDbService = db.NewService(testDB, config["dialect"].(string))
+
+		log.Info("Running test for " + driver)
+		code = m.Run()
+		log.Info("finished")
+		//if code != 0 {
+		//	os.Exit(code)
+		//}
+	}
+	os.Exit(code)
+}
