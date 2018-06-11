7fbb9969079c7b4bc4011bb9a9623a6758f13f4b
Michał Błotniak <michal.blotniak@codilime.com>
2018-06-11 17:34:18 +0200

    Intent compiler concurrency testing with STM
    Distributed locks were replaced by STM transactions. New behavior is
    tested by test that triggers data race condition between two intent
    compilers working in parallel.
    
    Change-Id: I7471bfbf163eafe6fd3d34fd059b1f8e2022d2b0
    Partial-Bug: #1776647

---
+++ pkg/testutil/integration/etcd_locker.go
@@ -0,0 +1,34 @@
+package integration
+
+import (
+	"testing"
+
+	etcdlock "github.com/DavidCai1993/etcd-lock"
+	pkglog "github.com/Juniper/contrail/pkg/log"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+// EtcdLocker is etcd locker extending etcdlock.Locker with test functionality and using etcd gRPC protocol.
+type EtcdLocker struct {
+	*etcdlock.Locker
+	log *logrus.Entry
+}
+
+// NewEtcdLocker is a constructor of etcd client.
+func NewEtcdLocker(t *testing.T) *EtcdLocker {
+	l := pkglog.NewLogger("etcd-locker")
+	l.WithFields(logrus.Fields{"endpoint": EtcdEndpoint}).Debug("Connecting")
+
+	locker, err := etcdlock.NewLocker(etcdlock.LockerOptions{
+		Address:     EtcdEndpoint,
+		DialOptions: []grpc.DialOption{grpc.WithInsecure()},
+	})
+	require.NoError(t, err, "connecting etcdlock failed")
+
+	return &EtcdLocker{
+		Locker: locker,
+		log:    l,
+	}
+}
