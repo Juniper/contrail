a9b0b9075d5de4fb46cc4b90d3fd4d74bdd9522c
Nachi Ueno <nati.ueno@gmail.com>
2018-06-14 16:46:53 -0700

    Cache database and websocket API
    Partial-bug: #1777733
    Change-Id: I381a6b82d3501222cfffb7972a82da2690ef3444

---
+++ pkg/apisrv/watch.go
@@ -0,0 +1,42 @@
+package apisrv
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/labstack/echo"
+	"golang.org/x/net/websocket"
+)
+
+const (
+	watchTimeout = 10 * time.Second
+)
+
+func (s *Server) watchHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	auth := common.GetAuthCTX(ctx)
+	if !auth.IsAdmin() {
+		return common.ErrorPermissionDenied
+	}
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		watcher := s.Cache.AddWatcher(ctx, 0)
+		for {
+			select {
+			case e := <-watcher.Chan():
+				update, err := json.Marshal(e)
+				if err != nil {
+					c.Logger().Error(err)
+				}
+				err = websocket.Message.Send(ws, string(update))
+				if err != nil {
+					c.Logger().Error(err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
