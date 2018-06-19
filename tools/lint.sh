00d3b44f6d20a3c067a125408109e51a35c96d67
Daniel Furman <daniel.furman@codilime.com>
2018-06-19 16:28:32 +0200

    [WiP] Enable necessary linters
    Change-Id: I34c16d351a6bd592eb36a7a166fdbafe6e2f6177

--- tools/lint.sh
+++ tools/lint.sh
@@ -11,14 +11,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Several tools for majority of the code are disabled.
-# TODO(daniel): run the same set of tools for all Go files
-# TODO(tomasz): enable dupl, lll, unparam
-gometalinter \
-    --config .gometalinter.json \
-    --disable gocyclo \
-    ./...
-
-gometalinter \
-    --config .gometalinter.json \
-	./cmd/... ./pkg/cmd/... ./pkg/agent/... ./pkg/log/... ./pkg/sync/... ./pkg/testutil/...
+gometalinter --config .gometalinter.json ./...
