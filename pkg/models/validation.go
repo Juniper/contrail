36d5b8cab95f05a65fafaabcf5ccd4ac96e37021
Józef Wołoch <jozef.woloch@codilime.com>
2018-06-20 18:38:43 +0200

    WIP: Create Resource Validation
    Done:
    - parent_type checks
    - checking fields with presence "true"
    
    In progress:
    - tests
    
    TODO:
    - format validation
    
    Change-Id: I2b320986816fc2a3d010a65fcf37d6db1fa206de
    Partial-Bug: #1773339

--- pkg/models/validation.go
+++ pkg/models/validation.go
@@ -1,6 +1,15 @@
 package models
 
-import fmt "fmt"
+import (
+	fmt "fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"net"
+
+	errors "github.com/pkg/errors"
+)
 
 //NewTypeValidatorWithFormat creates new TypeValidator with format validators
 func NewTypeValidatorWithFormat() (*TypeValidator, error) {
@@ -11,25 +20,49 @@ func NewTypeValidatorWithFormat() (*TypeValidator, error) {
 
 	// Register all format validators
 
-	tv.SchemaValidator.addFormatValidator("hostname", func(hostname string) error {
+	tv.SchemaValidator.addFormatValidator("hostname", func(value string) error {
 		// Validate hostname
 		// TODO
 		return nil
 	})
 
-	tv.SchemaValidator.addFormatValidator("date-time", func(hostname string) error {
+	tv.SchemaValidator.addFormatValidator("date-time", func(value string) error {
 		// Validate date-time
 		// TODO
 		return nil
 	})
 
-	tv.SchemaValidator.addFormatValidator("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", func(hostname string) error {
+	tv.SchemaValidator.addFormatValidator("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", func(value string) error {
 		// Validate ^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$
 		// TODO
 		return nil
 	})
 
-	// TODO check all required format validators registered.
+	tv.SchemaValidator.addFormatValidator("service_interface_type_format", func(value string) error {
+		// Validate service_interface_type_format
+		restrictions := map[string]struct{}{
+
+			"management": {},
+			"left":       {},
+			"right":      {},
+		}
+
+		_, present := restrictions[value]
+
+		if present {
+			return nil
+		}
+
+		regexStr := "other[0-9]*"
+
+		r, _ := regexp.Compile(regexStr)
+
+		if !r.MatchString(value) {
+			return errors.Errorf("ServiceInterfaceType value (%s) must be either one of "+
+				"[%s] or match \"%s\"", value, strings.Join(mapKeys(restrictions), ", "), regexStr)
+		}
+		return nil
+	})
 
 	return tv, nil
 }
@@ -42,6 +75,75 @@ type SchemaValidator struct {
 //TypeValidator embedding SchemaValidator validator. It enables defining custom validation for each type
 type TypeValidator struct {
 	SchemaValidator
+}
+
+func (tv *TypeValidator) ValidateAllowedAddressPair(obj *AllowedAddressPair) error {
+	err := tv.SchemaValidator.ValidateAllowedAddressPair(obj)
+
+	if err != nil {
+		return err
+	}
+
+	if obj.AddressMode != "active-standby" {
+		return nil
+	}
+
+	ip := net.ParseIP(obj.IP.IPPrefix)
+
+	if ip.To4() != nil {
+		if obj.IP.IPPrefixLen < 120 {
+			return errors.Errorf("IPv6 Prefix length lesser than 120 is not acceptable")
+		}
+	} else {
+		if obj.IP.IPPrefixLen < 24 {
+			return errors.Errorf("IPv4 Prefix length lesser than 24 is not acceptable")
+		}
+	}
+	return nil
+}
+
+func (tv *TypeValidator) ValidateCommunityAttributes(obj *CommunityAttributes) error {
+
+	restrictions := map[string]struct{}{
+
+		"no-export":           {},
+		"accept-own":          {},
+		"no-advertise":        {},
+		"no-export-subconfed": {},
+		"no-reoriginate":      {},
+	}
+
+	regexStr := "[0-9]+:[0-9]+"
+
+	r, _ := regexp.Compile(regexStr)
+
+	for _, value := range obj.CommunityAttribute {
+
+		_, present := restrictions[value]
+
+		if present {
+			continue
+		}
+
+		if !r.MatchString(value) {
+			return errors.Errorf("CommunityAttribute value (%s) must be either one of "+
+				"[%s] or match \"%s\"", value, strings.Join(mapKeys(restrictions), ", "), regexStr)
+		}
+		asn := strings.Split(value, ":")
+
+		i, err := strconv.Atoi(asn[0])
+
+		if err != nil {
+			return errors.Wrapf(err, "Error while parsing CommunityAttribute.")
+		}
+
+		if i > 65535 {
+			return errors.Errorf("Out of range ASN value %s. ASN values cannot exceed 65535.", i)
+
+		}
+	}
+
+	return nil
 }
 
 func (sv *SchemaValidator) addFormatValidator(format string, validator func(string) error) {
