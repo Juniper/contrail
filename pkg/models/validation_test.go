949cb184674309b93ddf7820b16ff89a1e818edb
Józef Wołoch <jozef.woloch@codilime.com>
2018-06-20 18:38:43 +0200

    WIP: Create Resource Validation
    Done:
    - parent_type checks
    - checking fields with presence "true"
    
    In progress:
    - tests
    
    TODO:
    - format validation
    
    Change-Id: I2b320986816fc2a3d010a65fcf37d6db1fa206de
    Partial-Bug: #1773339

---
+++ pkg/models/validation_test.go
@@ -0,0 +1,168 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSchemaValidationJobTemplate(t *testing.T) {
+	tests := []struct {
+		name        string
+		jobTemplate *JobTemplate
+		fails       bool
+	}{
+		{
+			name:        "Empty struct - validation fails",
+			jobTemplate: &JobTemplate{},
+			fails:       true,
+		},
+		{
+			name: "Validation pass",
+			jobTemplate: &JobTemplate{
+				UUID:                      "beef",
+				ParentUUID:                "beef-beef",
+				ParentType:                "global-system-config",
+				JobTemplatePlaybooks:      &PlaybookInfoListType{},
+				JobTemplateMultiDeviceJob: true,
+				FQName: []string{"a", "b"},
+			},
+			fails: false,
+		},
+		{
+			name: "Missing required string property",
+			jobTemplate: &JobTemplate{
+				UUID:                      "",
+				ParentUUID:                "beef-beef",
+				ParentType:                "global-system-config",
+				JobTemplatePlaybooks:      &PlaybookInfoListType{},
+				JobTemplateMultiDeviceJob: true,
+				FQName: []string{"a", "b"},
+			},
+			fails: true,
+		},
+		// {
+		// 	name: "Missing required integer property",
+		// 	jobTemplate: &JobTemplate{
+		// 		UUID:                      "beef",
+		// 		ParentUUID:                "beef-beef",
+		// 		ParentType:                "global-system-config",
+		// 		JobTemplatePlaybooks:      &PlaybookInfoListType{},
+		// 		JobTemplateMultiDeviceJob: true,
+		// 		FQName: []string{"a", "b"},
+		// 	},
+		// 	fails: true,
+		// },
+		// {
+		// 	name: "Missing required boolean property",
+		// 	jobTemplate: &JobTemplate{
+		// 		UUID:                      "beef",
+		// 		ParentUUID:                "beef-beef",
+		// 		ParentType:                "global-system-config",
+		// 		JobTemplatePlaybooks:      &PlaybookInfoListType{},
+		// 		JobTemplateMultiDeviceJob: false,
+		// 		FQName: []string{"a", "b"},
+		// 	},
+		// 	fails: true,
+		// },
+		{
+			name: "Missing required object property",
+			jobTemplate: &JobTemplate{
+				UUID:                      "beef",
+				ParentUUID:                "beef-beef",
+				ParentType:                "global-system-config",
+				JobTemplatePlaybooks:      nil,
+				JobTemplateMultiDeviceJob: true,
+				FQName: []string{"a", "b"},
+			},
+			fails: true,
+		},
+		{
+			name: "Bad parent type",
+			jobTemplate: &JobTemplate{
+				UUID:                      "beef",
+				ParentUUID:                "beef-beef",
+				ParentType:                "hogehoge",
+				JobTemplatePlaybooks:      &PlaybookInfoListType{},
+				JobTemplateMultiDeviceJob: true,
+				FQName: []string{"a", "b"},
+			},
+			fails: true,
+		},
+	}
+
+	tv, err := NewTypeValidatorWithFormat()
+
+	assert.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tv.ValidateJobTemplate(tt.jobTemplate)
+			if tt.fails {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestSchemaValidationBgpFamilyAttributes(t *testing.T) {
+	tests := []struct {
+		name                string
+		bgpFamilyAttributes *BgpFamilyAttributes
+		fails               bool
+	}{
+		{
+			name:                "Empty struct - validation fails",
+			bgpFamilyAttributes: &BgpFamilyAttributes{},
+			fails:               true,
+		},
+		{
+			name: "Validation pass",
+			bgpFamilyAttributes: &BgpFamilyAttributes{
+				AddressFamily: "inet",
+				LoopCount:     0,
+			},
+			fails: false,
+		},
+		{
+			name: "Bad string value",
+			bgpFamilyAttributes: &BgpFamilyAttributes{
+				AddressFamily: "hogehoge",
+			},
+			fails: true,
+		},
+		{
+			name: "Number value too small",
+			bgpFamilyAttributes: &BgpFamilyAttributes{
+				AddressFamily: "inet",
+				LoopCount:     -1,
+			},
+			fails: true,
+		},
+		{
+			name: "Number value too big",
+			bgpFamilyAttributes: &BgpFamilyAttributes{
+				AddressFamily: "inet",
+				LoopCount:     17,
+			},
+			fails: true,
+		},
+	}
+
+	tv, err := NewTypeValidatorWithFormat()
+
+	assert.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tv.ValidateBgpFamilyAttributes(tt.bgpFamilyAttributes)
+			if tt.fails {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
