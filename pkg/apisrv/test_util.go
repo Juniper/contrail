28a64fafee0c2b741330447f0fc300b3918ee341
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-06-26 15:11:01 +0200

    Virtual Network - check provider network property
    Support for Virtual Network resource specific logic:
     - check create/update provider network property.
    
    Improved error reporting in apisrv tests by adding message to error
    description.
    
    Partial-bug: #1770744
    Change-Id: I430937f21dde7b1fc3a1811ff27d69eee303f654

--- pkg/apisrv/test_util.go
+++ pkg/apisrv/test_util.go
@@ -241,7 +241,7 @@ func runTestScenario(t *testing.T, testScenario *TestScenario, clients clientsLi
 		assert.NoError(t, err, fmt.Sprintf("In test scenario '%v' task '%v' failed", testScenario.Name, task))
 
 		task.Expect = common.YAMLtoJSONCompat(task.Expect)
-		ok := common.AssertEqual(t, task.Expect, task.Request.Output, fmt.Sprintf("In test scenaio '%v' task' %v' failed", testScenario.Name, task))
+		ok := common.AssertEqual(t, task.Expect, task.Request.Output, fmt.Sprintf("In test scenario '%v' task' %v' failed", testScenario.Name, task))
 		if !ok {
 			log.Errorf("Assertion error was: %+v", err)
 			break
