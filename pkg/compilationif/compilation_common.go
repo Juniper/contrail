b1b42abc2c252d661a6051bd8a8d1a9c9b215f26
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-07-09 16:04:45 +0200

    [WIP] compilationif: Use DependencyProcessor
    compilation.HandleResource: Add a test with a NetworkPolicy reference.
    (The NetworkPolicy itself is not created in the test.)
    
    Remove compilation.HandleResource.
    
    Reorder imports.
    
    Closes-Bug: #1781195
    Change-Id: Ib29e675955e6a8dbc68950fc5728e80bbe11d5c4

--- pkg/compilationif/compilation_common.go
+++ pkg/compilationif/compilation_common.go
@@ -2,12 +2,14 @@ package compilationif
 
 import (
 	"context"
-	"strings"
+	"sync"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/Juniper/contrail/pkg/compilation/plugins/contrail/dependencies"
+	"github.com/Juniper/contrail/pkg/db/etcd"
 	"github.com/Juniper/contrail/pkg/services"
-	"github.com/pkg/errors"
 )
 
 // NewCompilationService makes a compilation service.
@@ -27,28 +29,67 @@ type CompilationService struct {
 // HandleEtcdMessages
 func (service *CompilationService) HandleEtcdMessages(ctx context.Context, oper int32, key, value string) {
 	log.Printf("HandleEtcdMessages: Got Msg: Oper-%d, Key-%s, Value-%s", oper, key, value)
-	subkeys := strings.Split(key, "/")
-	if len(subkeys) != 4 {
-		log.Errorf("HandleEtcdMessages: Malformed Key: %s", key)
-		return
-	}
-
-	resourceType := subkeys[2]
-	uuid := subkeys[3]
-
-	err := service.HandleResource(ctx, oper, resourceType, uuid, value)
+	err := service.handleEtcdMessages(ctx, oper, key, value)
 	if err != nil {
-		log.WithFields(log.Fields{
-			"err":      err,
-			"resource": resourceType,
-		}).Debug("HandleEtcdMessages: Error Handling Etcd Message")
+		// TODO Add args to the message
+		log.WithError(err).Error("Failed to handle etcd message")
 	}
 }
 
-func (service *CompilationService) HandleResource(ctx context.Context, oper int32, resourceType, uuid, value string) error {
-	handle, ok := functions[resourceType]
-	if !ok {
-		return errors.Errorf("Unknown resource type: %s", resourceType)
+func (service *CompilationService) handleEtcdMessages(ctx context.Context, oper int32, key, value string) error {
+	processor := &services.ServiceEventProcessor{Service: service}
+	// TODO Extract a separate service that handles dependencies
+	// TODO Make eventProducer not connect to ETCD
+	eventProducer, err := etcd.NewEventProducer(processor)
+	if err != nil {
+		return errors.Wrap(err, "failed to create an ETCD event producer")
 	}
-	return handle(service, ctx, oper, uuid, value)
+
+	event, err := eventProducer.ParseEvent(oper, key, []byte(value))
+	if err != nil {
+		return errors.Wrap(err, "failed to parse ETCD event")
+	}
+
+	allEvents := service.resolveDependencies(event)
+
+	for _, event := range allEvents {
+		_, err := processor.Process(ctx, event)
+		if err != nil {
+			// TODO Add event and causer event to the error
+			return errors.Wrap(err, "failed to process dependent event")
+		}
+	}
+
+	return nil
+}
+
+func (service *CompilationService) resolveDependencies(event *services.Event) []*services.Event {
+	dependencyProcessor := dependencies.NewDependencyProcessor(ObjsCache)
+	resource := event.GetResource()
+	dependencyProcessor.Evaluate(resource, resource.Kind(), "Self")
+	allResources := dependencyProcessor.GetResources()
+
+	result := make([]*services.Event, 0)
+	allResources.Range(func(rawKind, rawResourceMap interface{}) bool {
+		kind, _ := rawKind.(string)
+		resourceMap, _ := rawResourceMap.(*sync.Map)
+
+		resourceMap.Range(func(rawUUID, rawResource interface{}) bool {
+			uuid, _ := rawUUID.(string)
+
+			result = append(result, makeEmptyUpdateEvent(kind, uuid, rawResource))
+			return true
+		})
+		return true
+	})
+	return result
+}
+
+func makeEmptyUpdateEvent(goType string, uuid string, resource interface{}) *services.Event {
+	return services.NewEvent(&services.EventOption{
+		Kind: goType,
+		UUID: uuid,
+		Operation: services.OperationUpdate,
+		Data: map[string]interface{}{},
+	})
 }
