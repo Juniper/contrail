8bcfaec5d99a4a34bd9a07f0d27e08d989438497
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-07-09 10:24:24 +0200

    Adding floating-ip-pool validation
    Adding floating-ip-pool validation. If the subnet is specified -
    verifying if the subnet exists in the virtual machine, otherwise there
    is nothing to validate.
    
    Change-Id: Icd4080e4fc5df2631da7b39560fdaa68f58a2e54
    Closes-bug: 1780744

---
+++ pkg/types/floating_ip_pool.go
@@ -0,0 +1,57 @@
+package types
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateFloatingIPPool validates if subnets in the floating-ip-pool object exist in
+//the virtual-network. If subnet info is not specified, there is nothing to validate
+func (sv *ContrailTypeLogicService) CreateFloatingIPPool(
+	ctx context.Context,
+	request *services.CreateFloatingIPPoolRequest) (*services.CreateFloatingIPPoolResponse, error) {
+
+	var response *services.CreateFloatingIPPoolResponse
+	floatingIPPool := request.GetFloatingIPPool()
+	err := sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			var err error
+			var virtualNetwork *models.VirtualNetwork
+
+			if floatingIPPool.IsParentTypeVirtualNetwork() && floatingIPPool.HasSubnets() {
+				virtualNetwork, err = sv.getVirtualNetworkFromFloatingIPPool(ctx, floatingIPPool)
+				if err != nil {
+					return err
+				}
+
+				err = floatingIPPool.CheckAreSubnetsInVirtualNetworkSubnets(virtualNetwork)
+				if err != nil {
+					return err
+				}
+			}
+
+			response, err = sv.BaseService.CreateFloatingIPPool(ctx, request)
+			return err
+		})
+
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) getVirtualNetworkFromFloatingIPPool(
+	ctx context.Context, floatingIPPool *models.FloatingIPPool) (*models.VirtualNetwork, error) {
+
+	virtualNetworkResponse, err := sv.DataService.GetVirtualNetwork(ctx,
+		&services.GetVirtualNetworkRequest{
+			ID: floatingIPPool.GetParentUUID(),
+		})
+	if err != nil {
+		return nil, common.ErrorBadRequestf("Missing virtual-network with uuid %s: : %v",
+			floatingIPPool.GetParentUUID(), err)
+	}
+
+	return virtualNetworkResponse.GetVirtualNetwork(), nil
+}
