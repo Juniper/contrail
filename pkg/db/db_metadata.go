72e7a4912e92157fc7f1d4c6b3d47e98320d177f
Michał Błotniak <michal.blotniak@codilime.com>
2018-07-10 11:23:08 +0200

    Implement fq_name and parent_uuid rules
    Name, DisplayName, ParentType, FQName and ParentUUID fields are
    filled if only some of fields are provided
    
    Empty fields are set according to rules:
    - Name gets last element of FQName or 'default-<typename>' if FQName is empty
    - DisplayName is set to value of Name
    - ParentType gets value from schema if object can have only one parent type
    - FQName extends parent FQName retrieved by ParentUUID or gets default pointing
      to 'default-<parenttype>'
    - ParentUUID is retrieved from metadata using FQName without last element
    
    Change-Id: Ifecc4fb5d58e05447a6b6bfe147f0a1566add0d7
    Partial-Bug: #1780450

--- pkg/db/db_metadata.go
+++ pkg/db/db_metadata.go
@@ -2,56 +2,79 @@ package db
 
 import (
 	"bytes"
+	"context"
 	"database/sql"
 	"fmt"
 
 	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
 	"github.com/pkg/errors"
 )
 
-//MetaData represents resource meta data.
-type MetaData struct {
-	UUID   string
-	FQName []string
-	Type   string
+// MetaData represents resource meta data.
+//
+// TODO(Michal): services.ContrailService relying on this struct caused circular
+// 	dependency. Move MetaData definition to db package after extracting metadata
+// 	dependent code from service package.
+type MetaData = services.MetaData
+
+// CreateMetaData creates fqname, uuid pair with type.
+func (db *Service) CreateMetaData(ctx context.Context, metaData *MetaData) error {
+	return db.DoInTransaction(ctx, func(ctx context.Context) error {
+		tx := GetTransaction(ctx)
+		_, err := tx.Exec(
+			"insert into metadata (uuid,type,fq_name) values ("+
+				db.Dialect.values("uuid", "type", "fq_name")+");",
+			metaData.UUID, metaData.Type, models.FQNameToString(metaData.FQName))
+		err = handleError(err)
+		return errors.Wrap(err, "failed to create metadata")
+	})
 }
 
-//CreateMetaData creates fqname, uuid pair with type.
-func (db *Service) CreateMetaData(tx *sql.Tx, metaData *MetaData) error {
-	_, err := tx.Exec(
-		"insert into metadata (uuid,type,fq_name) values ("+
-			db.Dialect.values("uuid", "type", "fq_name")+");",
-		metaData.UUID, metaData.Type, models.FQNameToString(metaData.FQName))
-	err = handleError(err)
-	return errors.Wrap(err, "failed to create metadata")
-}
+// GetMetaData gets metadata from database.
+func (db *Service) GetMetaData(ctx context.Context, uuid string, fqName []string) (*MetaData, error) {
+	var uuidString, typeString, fqNameString string
 
-//GetMetaData gets metadata from database.
-func (db *Service) GetMetaData(tx *sql.Tx, uuid string, fqName []string) (*MetaData, error) {
-	var query bytes.Buffer
-	query.WriteString("select uuid,type,fq_name from metadata where ")
-	var row *sql.Row
+	if err := db.DoInTransaction(ctx, func(ctx context.Context) error {
+		tx := GetTransaction(ctx)
+		var query bytes.Buffer
+		query.WriteString("select uuid,type, fq_name from metadata where ")
+		var row *sql.Row
+		var where string
+		if uuid != "" {
+			where = "uuid = " + db.Dialect.placeholder(1)
+			query.WriteString(where)
+			row = tx.QueryRow(query.String(), uuid)
+		} else if fqName != nil {
+			where = "fq_name = " + db.Dialect.placeholder(1)
+			query.WriteString(where)
+			row = tx.QueryRow(query.String(), models.FQNameToString(fqName))
+		} else {
+			return fmt.Errorf("uuid and fqName unspecified ")
+		}
+		err := row.Scan(&uuidString, &typeString, &fqNameString)
+		if err != nil {
+			return errors.Wrapf(handleError(err), "failed to get metadata (%v")
+		}
 
-	if uuid != "" {
-		query.WriteString("uuid = " + db.Dialect.placeholder(1))
-		row = tx.QueryRow(query.String(), uuid)
-	} else if fqName != nil {
-		query.WriteString("fq_name = " + db.Dialect.placeholder(1))
-		row = tx.QueryRow(query.String(), models.FQNameToString(fqName))
-	} else {
-		return nil, fmt.Errorf("uuid and fqName unspecified ")
+		return nil
+	}); err != nil {
+		return nil, err
 	}
-	metaData := &MetaData{}
-	var fqNameString string
-	err := row.Scan(&metaData.UUID, &metaData.Type, &fqNameString)
-	err = handleError(err)
-	metaData.FQName = models.ParseFQName(fqNameString)
-	return metaData, errors.Wrap(err, "failed to get metadata")
+
+	return &MetaData{
+		UUID:   uuidString,
+		FQName: models.ParseFQName(fqNameString),
+		Type:   typeString,
+	}, nil
 }
 
-//DeleteMetaData deltes metadata by uuid.
-func (db *Service) DeleteMetaData(tx *sql.Tx, uuid string) error {
-	_, err := tx.Exec("delete from metadata where uuid = "+db.Dialect.placeholder(1), uuid)
-	err = handleError(err)
-	return errors.Wrap(err, "failed to delete metadata")
+// DeleteMetaData deletes metadata by uuid.
+func (db *Service) DeleteMetaData(ctx context.Context, uuid string) error {
+	return db.DoInTransaction(ctx, func(ctx context.Context) error {
+		tx := GetTransaction(ctx)
+		_, err := tx.Exec("delete from metadata where uuid = "+db.Dialect.placeholder(1), uuid)
+		err = handleError(err)
+		return errors.Wrap(err, "failed to delete metadata")
+	})
 }
