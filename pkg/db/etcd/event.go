c234b8c62668caf4482918f79538a380c03260d7
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-07-10 17:29:55 +0200

    etcd: Extract (*EventProducer).ParseEvent
    Partial-Bug: #1781195
    Change-Id: I003122bc80895f039ba47cd54d15e58a37eb6e51

--- pkg/db/etcd/event.go
+++ pkg/db/etcd/event.go
@@ -6,11 +6,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Juniper/contrail/pkg/services"
 	"github.com/coreos/etcd/mvcc/mvccpb"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+
+	"github.com/Juniper/contrail/pkg/services"
 )
 
 //EventProducer watches etcd and call event processor.
@@ -36,46 +37,65 @@ func NewEventProducer(processor services.EventProcessor) (p *EventProducer, err 
 	return p, nil
 }
 
-func parseKey(key string) (string, string) {
-	subkeys := strings.Split(key, "/")
-	return subkeys[2], subkeys[3]
-}
-
 // HandleMessage handles message received from etcd pubsub.
 func (p *EventProducer) HandleMessage(
 	ctx context.Context, index int64, oper int32, key string, newValue []byte,
 ) {
 	log.Debug("Index: %d, oper: %d, Got Message %s: %s",
 		index, oper, key, newValue)
-	var data map[string]interface{}
+	event, err := p.ParseEvent(oper, key, newValue)
+	if err != nil {
+		log.WithError(err).Error("Failed to parse event")
+	}
+	p.Processor.Process(ctx, event) // nolint: errcheck
+}
+
+// ParseEvent returns an Event corresponding to a change in ETCD.
+func (p *EventProducer) ParseEvent(oper int32, key string, newValue []byte) (*services.Event, error) {
 
 	//TODO(nati) use sync.Codec
 
-	var operation string
 	kind, uuid := parseKey(key)
 
-	switch oper {
-	case int32(mvccpb.PUT):
-		operation = services.OperationCreate
+	operation, err := parseOperation(oper)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse ETCD operation")
+	}
+
+	var data map[string]interface{}
+	if operation == services.OperationCreate {
 		err := json.Unmarshal(newValue, &data)
 		if err != nil {
-			log.Warn("decode error for %s", string(newValue))
-			return
+			return nil, errors.Wrapf(err, "failed to decode %s", string(newValue))
 		}
-	case int32(mvccpb.DELETE):
-		operation = services.OperationDelete
 	}
-	e := services.NewEvent(&services.EventOption{
+
+	event := services.NewEvent(&services.EventOption{
 		UUID:      uuid,
 		Kind:      kind,
 		Operation: operation,
 		Data:      data,
 	})
-	if e == nil {
-		log.Warn("invalid event %v", data)
-		return
+	if event == nil {
+		return nil, errors.Errorf("invalid event %v", data)
 	}
-	p.Processor.Process(ctx, e) // nolint: errcheck
+	return event, nil
+}
+
+func parseKey(key string) (string, string) {
+	subkeys := strings.Split(key, "/")
+	return subkeys[2], subkeys[3]
+}
+
+func parseOperation(etcdOperation int32) (string, error) {
+	switch etcdOperation {
+	case int32(mvccpb.PUT):
+		return services.OperationCreate, nil
+	case int32(mvccpb.DELETE):
+		return services.OperationDelete, nil
+	default:
+		return "", errors.Errorf("unsupported ETCD operation: %v", etcdOperation)
+	}
 }
 
 //Start watch etcd.
