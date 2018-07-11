6d9f9ae3cd99aa92e4742fbbb878a71d91478299
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-07-11 16:55:21 +0200

    [WIP] DependencyProcessor: Use a sync.Map for cache
    Partial-Bug: #1781195
    Change-Id: Ia9019e9178e5627bdf903a94433a3dfeba3097a7

--- pkg/compilation/plugins/contrail/dependencies/dependencies_test.go
+++ pkg/compilation/plugins/contrail/dependencies/dependencies_test.go
@@ -4,51 +4,51 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/Juniper/contrail/pkg/models"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
 )
 
 // TestDependencies - test
 func TestDependencies(t *testing.T) {
-	ObjsCache := make(map[string]map[string]interface{})
+	cache := &sync.Map{}
 
-	ObjsCache["virtual_network"] = make(map[string]interface{})
-	Vn1 := models.VirtualNetwork{}
+	Vn1 := &models.VirtualNetwork{}
 	Vn1.UUID = "Virtual-Network-1"
-	ObjsCache["virtual_network"][Vn1.UUID] = &Vn1
+	putIntoCache(t, cache, "VirtualNetwork", Vn1)
 
-	Vn2 := models.VirtualNetwork{}
+	Vn2 := &models.VirtualNetwork{}
 	Vn2.UUID = "Virtual-Network-2"
-	ObjsCache["virtual_network"][Vn2.UUID] = &Vn2
+	putIntoCache(t, cache, "VirtualNetwork", Vn2)
 
-	ObjsCache["NetworkPolicy"] = make(map[string]interface{})
-	Np1 := models.NetworkPolicy{}
+	Np1 := &models.NetworkPolicy{}
 	Np1.UUID = "Network-policy-1"
-	ObjsCache["NetworkPolicy"][Np1.UUID] = &Np1
+	putIntoCache(t, cache, "NetworkPolicy", Np1)
 
-	Np2 := models.NetworkPolicy{}
+	Np2 := &models.NetworkPolicy{}
 	Np2.UUID = "Network-policy-2"
-	ObjsCache["NetworkPolicy"][Np2.UUID] = &Np2
+	putIntoCache(t, cache, "NetworkPolicy", Np2)
 
-	Np1Ref := models.VirtualNetworkNetworkPolicyRef{}
+	Np1Ref := &models.VirtualNetworkNetworkPolicyRef{}
 	Np1Ref.UUID = Np1.UUID
 	Np1Ref.To = []string{"default-domain", "default-project", "Network-Policy-1"}
-	Vn1.NetworkPolicyRefs = append(Vn1.NetworkPolicyRefs, &Np1Ref)
+	Vn1.NetworkPolicyRefs = append(Vn1.NetworkPolicyRefs, Np1Ref)
 
-	Np2Ref := models.VirtualNetworkNetworkPolicyRef{}
+	Np2Ref := &models.VirtualNetworkNetworkPolicyRef{}
 	Np2Ref.UUID = Np2.UUID
 	Np2Ref.To = []string{"default-domain", "default-project", "Network-Policy-2"}
-	Vn1.NetworkPolicyRefs = append(Vn1.NetworkPolicyRefs, &Np2Ref)
+	Vn1.NetworkPolicyRefs = append(Vn1.NetworkPolicyRefs, Np2Ref)
 
-	Vn2.NetworkPolicyRefs = append(Vn2.NetworkPolicyRefs, &Np2Ref)
+	Vn2.NetworkPolicyRefs = append(Vn2.NetworkPolicyRefs, Np2Ref)
 
-	Np1.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, &Vn1)
-	Np2.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, &Vn1)
+	Np1.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, Vn1)
+	Np2.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, Vn1)
 
-	Np2.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, &Vn2)
+	Np2.VirtualNetworkBackRefs = append(Np1.VirtualNetworkBackRefs, Vn2)
 
-	d := NewDependencyProcessor(ObjsCache)
+	d := NewDependencyProcessor(cache)
 	d.Evaluate(Vn1, "VirtualNetwork", "Self")
 	resources := d.GetResources()
 
@@ -59,6 +59,23 @@ func TestDependencies(t *testing.T) {
 	policies := mustLoad(t, resources, "NetworkPolicy")
 	mustLoad(t, policies, Np1.UUID)
 	mustLoad(t, policies, Np2.UUID)
+}
+
+func putIntoCache(t *testing.T, cache *sync.Map, kind string, resource services.Resource) {
+	resourceMap := ensureSubmap(t, cache, kind)
+	resourceMap.Store(resource.GetUUID(), resource)
+}
+
+func ensureSubmap(t *testing.T, cache *sync.Map, key string) (submap *sync.Map) {
+	rawSubmap, ok := cache.Load(key)
+	if !ok {
+		submap = &sync.Map{}
+		cache.Store(key, submap)
+		return
+	}
+	submap, ok = rawSubmap.(*sync.Map)
+	require.True(t, ok)
+	return
 }
 
 func mustLoad(t *testing.T, rawSyncMap interface{}, key string) interface{} {
