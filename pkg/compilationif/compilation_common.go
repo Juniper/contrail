43f85a599401ae9bd0458c49eb6409b4d174970b
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-07-12 13:42:54 +0200

    [WIP] compilationif: Use DependencyProcessor
    Closes-Bug: #1781195
    Change-Id: Id4aabec81d38c8cb64037f6d208f269dec8e59a1

--- pkg/compilationif/compilation_common.go
+++ pkg/compilationif/compilation_common.go
@@ -2,10 +2,12 @@ package compilationif
 
 import (
 	"context"
+	"sync"
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/Juniper/contrail/pkg/compilation/plugins/contrail/dependencies"
 	"github.com/Juniper/contrail/pkg/db/etcd"
 	"github.com/Juniper/contrail/pkg/services"
 )
@@ -42,6 +44,46 @@ func (service *CompilationService) handleEtcdMessages(ctx context.Context, oper 
 		return errors.Wrap(err, "failed to parse ETCD event")
 	}
 
-	_, err = processor.Process(ctx, event)
-	return err
+	allEvents := service.resolveDependencies(event)
+
+	for _, event := range allEvents {
+		_, err := processor.Process(ctx, event)
+		if err != nil {
+			// TODO Add event and causer event to the error
+			return errors.Wrap(err, "failed to process dependent event")
+		}
+	}
+
+	return nil
+}
+
+func (service *CompilationService) resolveDependencies(event *services.Event) []*services.Event {
+	dependencyProcessor := dependencies.NewDependencyProcessor(ObjsCache)
+	resource := event.GetResource()
+	dependencyProcessor.Evaluate(resource, resource.Kind(), "Self")
+	allResources := dependencyProcessor.GetResources()
+
+	result := make([]*services.Event, 0)
+	allResources.Range(func(rawKind, rawResourceMap interface{}) bool {
+		kind, _ := rawKind.(string)
+		resourceMap, _ := rawResourceMap.(*sync.Map)
+
+		resourceMap.Range(func(rawUUID, rawResource interface{}) bool {
+			uuid, _ := rawUUID.(string)
+
+			result = append(result, makeEmptyUpdateEvent(kind, uuid, rawResource))
+			return true
+		})
+		return true
+	})
+	return result
+}
+
+func makeEmptyUpdateEvent(goType string, uuid string, resource interface{}) *services.Event {
+	return services.NewEvent(&services.EventOption{
+		Kind:      goType,
+		UUID:      uuid,
+		Operation: services.OperationUpdate,
+		Data:      map[string]interface{}{},
+	})
 }
