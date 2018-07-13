4c2a224c048bc2843c6da5fd263c94b7db86f62f
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-07-13 10:29:19 +0200

    compilation/logic: Create ACLs for SecurityGroup
    So that the unit test made from the demo scenario passes.
    
    Closes-Bug: 1781566
    Change-Id: Ife399d6a9bbb43a2c5d2827b23b84cde14010128

---
+++ pkg/compilation/logic/security_group.go
@@ -0,0 +1,32 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateSecurityGroup creates default AccessControlList's for the already created SecurityGroup.
+func (s *Service) CreateSecurityGroup(
+	ctx context.Context, request *services.CreateSecurityGroupRequest,
+) (*services.CreateSecurityGroupResponse, error) {
+	ingressACL, egressACL := defaultSecurityGroupACLs(request.SecurityGroup)
+
+	_, err := s.api.CreateAccessControlList(ctx, &services.CreateAccessControlListRequest{
+		AccessControlList: ingressACL,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create ingress access control list")
+	}
+
+	_, err = s.api.CreateAccessControlList(ctx, &services.CreateAccessControlListRequest{
+		AccessControlList: egressACL,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create egress access control list")
+	}
+
+	return &services.CreateSecurityGroupResponse{SecurityGroup: request.SecurityGroup}, nil
+}
