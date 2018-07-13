3ca41346935166f78d25d78130039bce672bb76d
Michał Błotniak <michal.blotniak@codilime.com>
2018-07-13 17:29:59 +0200

    Project - create default aps
    Change-Id: Icd9b84b50e8ae8b74d35ecb0eb9fa0960e9845ab
    Partial-bug: #1776234

--- pkg/types/project.go
+++ pkg/types/project.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Juniper/contrail/pkg/common"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
+	"github.com/pkg/errors"
 )
 
 // CreateProject do checks for create project.
@@ -16,11 +17,12 @@ func (sv *ContrailTypeLogicService) CreateProject(
 	err = sv.InTransactionDoer.DoInTransaction(
 		ctx,
 		func(ctx context.Context) error {
-
 			response, err = sv.Next().CreateProject(ctx, request)
-			//TODO: ensure default application policy set
+			if err != nil {
+				return err
+			}
 
-			return err
+			return sv.ensureDefaultApplicationPolicySet(ctx, request.Project)
 		})
 
 	return response, err
@@ -55,11 +57,22 @@ func (sv *ContrailTypeLogicService) UpdateProject(
 // DeleteProject do checks for delete project.
 func (sv *ContrailTypeLogicService) DeleteProject(
 	ctx context.Context, request *services.DeleteProjectRequest,
-) (response *services.DeleteProjectResponse, err error) {
-	err = sv.InTransactionDoer.DoInTransaction(
+) (*services.DeleteProjectResponse, error) {
+	var response *services.DeleteProjectResponse
+
+	err := sv.InTransactionDoer.DoInTransaction(
 		ctx,
 		func(ctx context.Context) error {
 			//TODO: pre dbe delete
+			project, err := sv.getProject(ctx, request.GetID())
+			if err != nil {
+				return err
+			}
+
+			if err = sv.deleteDefaultApplicationPolicySet(ctx, project); err != nil {
+				return err
+			}
+
 			response, err = sv.Next().DeleteProject(ctx, request)
 			return err
 		})
@@ -96,5 +109,51 @@ func (sv *ContrailTypeLogicService) checkVxlanConfig(
 			" cannot be done when Logical Routers are configured")
 	}
 
+	return nil
+}
+
+func (sv *ContrailTypeLogicService) ensureDefaultApplicationPolicySet(
+	ctx context.Context, project *models.Project,
+) error {
+	apsName := models.DefaultNameForKind(models.KindApplicationPolicySet)
+
+	aps := models.MakeApplicationPolicySet()
+	aps.FQName = models.ChildFQName(project.FQName, apsName)
+	aps.ParentType = models.KindProject
+	aps.ParentUUID = project.UUID
+	aps.Name = apsName
+	aps.DisplayName = apsName
+	aps.AllApplications = true
+
+	response, err := sv.APIService.CreateApplicationPolicySet(ctx, &services.CreateApplicationPolicySetRequest{
+		ApplicationPolicySet: aps,
+	})
+	if common.IsConflict(err) {
+		return nil // object already exists - do nothing
+	} else if err != nil {
+		return errors.Wrap(err, "unexpected error creating default application policy set for project")
+	}
+	// new object - create ref
+
+	project.ApplicationPolicySetRefs = append(
+		project.ApplicationPolicySetRefs,
+		&models.ProjectApplicationPolicySetRef{UUID: response.GetApplicationPolicySet().GetUUID()},
+	)
+	return nil
+}
+
+func (sv *ContrailTypeLogicService) deleteDefaultApplicationPolicySet(
+	ctx context.Context, project *models.Project,
+) error {
+	defaultAPSName := models.DefaultNameForKind(models.KindApplicationPolicySet)
+
+	for _, aps := range project.GetApplicationPolicySets() {
+		if aps.GetName() == defaultAPSName {
+			_, err := sv.APIService.DeleteApplicationPolicySet(ctx, &services.DeleteApplicationPolicySetRequest{ID: aps.UUID})
+			if err != nil {
+				return errors.Wrap(err, "unexpected error deleting child application policy set")
+			}
+		}
+	}
 	return nil
 }
--- pkg/types/project_test.go
+++ pkg/types/project_test.go
@@ -1,15 +1,19 @@
 package types
 
 import (
+	"context"
 	"testing"
 
-	"github.com/Juniper/contrail/pkg/models"
-	"github.com/Juniper/contrail/pkg/services"
-
 	"github.com/gogo/protobuf/types"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/mock"
 )
 
 func TestCheckVxlanConfig(t *testing.T) {
@@ -77,6 +81,74 @@ func TestCheckVxlanConfig(t *testing.T) {
 				assert.Equal(t, tt.errorCode, status.Code())
 			} else {
 				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestEnsureDefaultApplicationPolicySet(t *testing.T) {
+	tests := []struct {
+		name      string
+		project   models.Project
+		initMocks func(*ContrailTypeLogicService)
+		fails     bool
+	}{
+		{
+			name: "create returns internal",
+			initMocks: func(s *ContrailTypeLogicService) {
+				m := s.APIService.(*servicesmock.MockService)
+
+				m.EXPECT().CreateApplicationPolicySet(
+					gomock.Not(gomock.Nil()),
+					gomock.Not(gomock.Nil()),
+				).Return(nil, common.ErrorInternal).Times(1)
+			},
+			fails: true,
+		},
+		{
+			name: "create returns conflict",
+			initMocks: func(s *ContrailTypeLogicService) {
+				m := s.APIService.(*servicesmock.MockService)
+
+				m.EXPECT().CreateApplicationPolicySet(
+					gomock.Not(gomock.Nil()),
+					gomock.Not(gomock.Nil()),
+				).Return(nil, common.ErrorConflict).Times(1)
+			},
+		},
+		{
+			name: "create returns ApplicationPolicySet object",
+			initMocks: func(s *ContrailTypeLogicService) {
+				m := s.APIService.(*servicesmock.MockService)
+
+				m.EXPECT().CreateApplicationPolicySet(
+					gomock.Not(gomock.Nil()),
+					gomock.Not(gomock.Nil()),
+				).Return(
+					&services.CreateApplicationPolicySetResponse{
+						ApplicationPolicySet: models.MakeApplicationPolicySet(),
+					}, nil,
+				).Times(1)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			service := makeMockedContrailTypeLogicService(mockCtrl)
+			if tt.initMocks != nil {
+				tt.initMocks(service)
+			}
+
+			err := service.ensureDefaultApplicationPolicySet(context.Background(), &tt.project)
+			if tt.fails {
+				t.Log(err)
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
 			}
 		})
 	}
