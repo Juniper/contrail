e0f29528a12046672aa05426fcb1d146ada7127c
Daniel Furman <daniel.furman@codilime.com>
2018-07-16 17:30:35 +0200

    Install protoc-gen-gogo from vendor
    Recently updated gogo/protobuf library broken code generation.
    Install protoc-gen-gogo from vendor to fix it.
    
    Change-Id: I3cc7fde2d1dc3773c5c03468e1a41c0e1b705b25
    Partial-Bug: #1772623

--- tools/deps.sh
+++ tools/deps.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 go get -u github.com/golang/dep/cmd/dep
-go get github.com/alecthomas/gometalinter
-go get github.com/mitchellh/gox
-go get github.com/mattn/goveralls
-gometalinter --install
-go get github.com/gogo/protobuf/protoc-gen-gogo
+go get -u github.com/alecthomas/gometalinter
+go get -u github.com/mitchellh/gox
+go get -u github.com/mattn/goveralls
 go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
-go get github.com/go-openapi/spec
+go get -u github.com/go-openapi/spec
+go install ./vendor/github.com/gogo/protobuf/protoc-gen-gogo
 go install ./vendor/github.com/golang/mock/mockgen
+gometalinter --install
 
 if [ "$(uname)" == 'Darwin' ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
-    unzip protoc-3.5.1-osx-x86_64.zip "bin/protoc"
+    unzip -o protoc-3.5.1-osx-x86_64.zip "bin/protoc"
     rm protoc-3.5.1-osx-x86_64.zip
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
-    unzip protoc-3.5.1-linux-x86_64.zip "bin/protoc"
+    unzip -o protoc-3.5.1-linux-x86_64.zip "bin/protoc"
     rm protoc-3.5.1-linux-x86_64.zip
 else
 	echo "Your platform ($(uname -a)) is not supported."
