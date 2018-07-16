88c76c68e02af3b9fb0029a6871ed2f54d5c0e68
Daniel Furman <daniel.furman@codilime.com>
2018-07-16 12:09:53 +0200

    [WiP] Disable gotype linter
    Gotype started to repeatably fail on CI.
    The root cause of those failures is unknown.
    
    Gotype performs type-checks of source code that are
    already performed during compilation, so it can be disabled.
    
    Change-Id: I3cc7fde2d1dc3773c5c03468e1a41c0e1b705b25
    Partial-Bug: #1772623

--- tools/deps.sh
+++ tools/deps.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 go get -u github.com/golang/dep/cmd/dep
-go get github.com/alecthomas/gometalinter
-go get github.com/mitchellh/gox
-go get github.com/mattn/goveralls
+go get -u github.com/alecthomas/gometalinter
+go get -u github.com/mitchellh/gox
+go get -u github.com/mattn/goveralls
 gometalinter --install
-go get github.com/gogo/protobuf/protoc-gen-gogo
+go get -u github.com/gogo/protobuf/protoc-gen-gogo
 go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
-go get github.com/go-openapi/spec
+go get -u github.com/go-openapi/spec
 go install ./vendor/github.com/golang/mock/mockgen
 
 if [ "$(uname)" == 'Darwin' ]; then
