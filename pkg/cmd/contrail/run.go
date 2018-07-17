317630dbe32db1a57aab83650d94a1d4bf62139f
Nachi <nati.ueno@gmail.com>
2018-07-17 13:21:41 -0700

    EventProducer for binlog
    Add option for directly watch binlog for cache.
    
    Closes-bug: #1782621
    Change-Id: I92cd777835863f854c57885b75a796503fd330ba

--- pkg/cmd/contrail/run.go
+++ pkg/cmd/contrail/run.go
@@ -18,6 +18,8 @@ import (
 	syncp "github.com/Juniper/contrail/pkg/sync"
 )
 
+var cacheDB *cache.DB
+
 func init() {
 	Contrail.AddCommand(processCmd)
 }
@@ -33,104 +35,145 @@ var processCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		wg := &sync.WaitGroup{}
-		var cacheDB *cache.DB
-		ctx := context.Background()
-		if viper.GetBool("cache.enabled") {
-			log.Debug("cache service enabled")
-			cacheDB = cache.New(uint64(viper.GetInt64("cache.max_history")))
-			if viper.GetBool("cache.cassandra.enabled") {
-				log.Debug("cassandra watcher enabled for cache")
-				processor := cassandra.NewEventProducer(
-					cacheDB,
-					getQueueName(),
-					viper.GetString("cache.cassandra.host"),
-					viper.GetInt("cache.cassandra.port"),
-					viper.GetDuration("cache.cassandra.timeout"),
-					viper.GetString("cache.cassandra.amqp"),
-				)
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					err := processor.Start(ctx)
-					if err != nil {
-						log.Warn(err)
-					}
-				}()
-			}
-			if viper.GetBool("cache.etcd.enabled") {
-				log.Debug("etcd watcher enabled for cache")
-				processor, err := etcd.NewEventProducer(cacheDB)
-				if err != nil {
-					log.Fatal(err)
-				}
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					err := processor.Start(ctx)
-					if err != nil {
-						log.Warn(err)
-					}
-				}()
-			}
-		}
-
-		if viper.GetBool("server.enabled") {
-			server, err := apisrv.NewServer()
-			if err != nil {
-				log.Fatal(err)
-			}
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				server.Cache = cacheDB
-				if err = server.Init(); err != nil {
-					log.Fatal(err)
-				}
-
-				if err = server.Run(); err != nil {
-					log.Fatal(err)
-				}
-			}()
-		}
-		if viper.GetBool("agent.enabled") {
-			wg.Add(1)
-			go func() {
-				startAgent()
-				wg.Done()
-			}()
-		}
-		if viper.GetBool("sync.enabled") {
-			wg.Add(1)
-			go func() {
-				startSync()
-				wg.Done()
-			}()
-		}
-		if viper.GetBool("compilation.enabled") {
-			wg.Add(1)
-			go func() {
-				startCompilationService()
-				wg.Done()
-			}()
-		}
+		StartProcesses(wg)
 		wg.Wait()
 	},
 }
 
-func startSync() {
+//StartProcesses starts processes based on config.
+func StartProcesses(wg *sync.WaitGroup) {
+	startCacheService(wg)
+	startServer(wg)
+	startAgent(wg)
+	startSync(wg)
+	startCompilationService(wg)
+}
+
+func startCacheService(wg *sync.WaitGroup) {
+	if !viper.GetBool("cache.enabled") {
+		return
+	}
+	log.Debug("cache service enabled")
+	cacheDB = cache.New(uint64(viper.GetInt64("cache.max_history")))
+	startCassandraWatcher(wg)
+	startEtcdWatcher(wg)
+	startRDBMSWatcher(wg)
+}
+
+func startCassandraWatcher(wg *sync.WaitGroup) {
+	ctx := context.Background()
+	if !viper.GetBool("cache.cassandra.enabled") {
+		return
+	}
+	log.Debug("cassandra watcher enabled for cache")
+	processor := cassandra.NewEventProducer(
+		cacheDB,
+		getQueueName(),
+		viper.GetString("cache.cassandra.host"),
+		viper.GetInt("cache.cassandra.port"),
+		viper.GetDuration("cache.cassandra.timeout"),
+		viper.GetString("cache.cassandra.amqp"),
+	)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := processor.Start(ctx)
+		if err != nil {
+			log.Warn(err)
+		}
+	}()
+}
+
+func startEtcdWatcher(wg *sync.WaitGroup) {
+	ctx := context.Background()
+	if !viper.GetBool("cache.etcd.enabled") {
+		return
+	}
+	log.Debug("etcd watcher enabled for cache")
+	processor, err := etcd.NewEventProducer(cacheDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := processor.Start(ctx)
+		if err != nil {
+			log.Warn(err)
+		}
+	}()
+}
+
+func startRDBMSWatcher(wg *sync.WaitGroup) {
+	ctx := context.Background()
+	if !viper.GetBool("cache.rdbms.enabled") {
+		return
+	}
+	log.Debug("rdbms watcher enabled for cache")
+	processor, err := syncp.NewEventProducer(cacheDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wg.Add(1)
+	go func() {
+		defer processor.Close()
+		defer wg.Done()
+		err := processor.Start(ctx)
+		if err != nil {
+			log.Warn(err)
+		}
+	}()
+}
+
+func startServer(wg *sync.WaitGroup) {
+	if !viper.GetBool("server.enabled") {
+		return
+	}
+	server, err := apisrv.NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.Cache = cacheDB
+	if err = server.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		if err = server.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+
+func startSync(wg *sync.WaitGroup) {
+	if !viper.GetBool("sync.enabled") {
+		return
+	}
+	wg.Add(1)
+
 	s, err := syncp.NewService()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer s.Close()
 
-	err = s.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		defer s.Close()
+		defer wg.Done()
+		err = s.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
 
-func startCompilationService() {
+func startCompilationService(wg *sync.WaitGroup) {
+	if !viper.GetBool("compilation.enabled") {
+		return
+	}
+
 	server, err := compilation.NewIntentCompilationService()
 	if err != nil {
 		log.Fatal(err)
@@ -138,19 +181,30 @@ func startCompilationService() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if err = server.Run(ctx); err != nil {
-		log.Fatal(err)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err = server.Run(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
 
-func startAgent() {
+func startAgent(wg *sync.WaitGroup) {
+	if !viper.GetBool("agent.enabled") {
+		return
+	}
+	wg.Add(1)
 	a, err := agent.NewAgentByConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		if err := a.Watch(); err != nil {
-			log.Error(err)
+	go func() {
+		defer wg.Done()
+		for {
+			if err := a.Watch(); err != nil {
+				log.Fatal(err)
+			}
 		}
-	}
+	}()
 }
