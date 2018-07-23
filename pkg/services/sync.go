6613f75544ab0ddab3337d2916e994bb931cb808
Daniel Furman <daniel.furman@codilime.com>
2018-07-23 16:04:30 +0200

    Extract Convert code and document EventList.Sort()
    - extract Convert from cmd/contrailutil to pkg/convert package
    - document that EventList.Sort() ignores parent-child relationship
      and write test for that
    - improve DB error messages
    
    Change-Id: If15bb044f26bc8a0124f1c7f857c7633bc4972d6
    Partial-Bug: #1772623

--- pkg/services/sync.go
+++ pkg/services/sync.go
@@ -2,13 +2,12 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"net/http"
-
-	"github.com/labstack/echo"
-	log "github.com/sirupsen/logrus"
 
 	"github.com/Juniper/contrail/pkg/common"
 	"github.com/Juniper/contrail/pkg/models"
+	"github.com/labstack/echo"
 )
 
 // MetaData represents resource meta data.
@@ -22,7 +21,7 @@ type metadataGetter interface {
 	GetMetaData(ctx context.Context, uuid string, fqName []string) (*MetaData, error)
 }
 
-// nolint
+// ContrailService handles API requests.
 type ContrailService struct {
 	BaseService
 
@@ -30,17 +29,18 @@ type ContrailService struct {
 	TypeValidator  *models.TypeValidator
 }
 
-//RESTSync handles a bulk create request.
-func (service *ContrailService) RESTSync(c echo.Context) error {
+// restSync handles Sync request.
+func (service *ContrailService) restSync(c echo.Context) error {
 	events := &EventList{}
 	if err := c.Bind(events); err != nil {
-		log.WithFields(log.Fields{
-			"err": err,
-		}).Debug("bind failed on sync")
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid JSON format")
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid JSON format: %v", err))
 	}
-	ctx := c.Request().Context()
-	responses, err := events.Process(ctx, service)
+
+	if err := events.Sort(); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Sorting events failed: %v", err))
+	}
+
+	responses, err := events.Process(c.Request().Context(), service)
 	if err != nil {
 		return common.ToHTTPError(err)
 	}
