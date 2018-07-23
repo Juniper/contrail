3042b236b4cfa5a692b7b8bc46d223bb385f1716
Daniel Furman <daniel.furman@codilime.com>
2018-07-23 16:04:30 +0200

    Extract Convert code and document EventList.Sort()
    - extract Convert from cmd/contrailutil to pkg/convert package
    - document that EventList.Sort() ignores parent-child relationship
      and write test for that
    - improve DB error messages
    
    Change-Id: If15bb044f26bc8a0124f1c7f857c7633bc4972d6
    Partial-Bug: #1772623

--- pkg/cmd/contrailutil/convert.go
+++ pkg/cmd/contrailutil/convert.go
@@ -1,28 +1,11 @@
 package contrailutil
 
 import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/Juniper/contrail/pkg/db"
-	"github.com/Juniper/contrail/pkg/db/cassandra"
-	etcdclient "github.com/Juniper/contrail/pkg/db/etcd"
-
-	"github.com/Juniper/contrail/pkg/common"
-	"github.com/Juniper/contrail/pkg/services"
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-)
 
-const (
-	yamlType          = "yaml"
-	cassandraType     = "cassandra"
-	cassandraDumpType = "cassandra_dump"
-	rdbmsType         = "rdbms"
-	etcdType          = "etcd"
+	"github.com/Juniper/contrail/pkg/convert"
 )
 
 func init() {
@@ -41,90 +24,20 @@ var inType, inFile string
 var outType, outFile string
 var cassandraPort, cassandraTimeout int
 
-func readYAML() (*services.EventList, error) {
-	var events services.EventList
-	err := common.LoadFile(inFile, &events)
-	return &events, err
-}
-
-func writeYAML(events *services.EventList) error {
-	return common.SaveFile(outFile, events)
-}
-
-func readRDBMS() (*services.EventList, error) {
-	dbService, err := db.NewServiceFromConfig()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to connect DB")
-	}
-	ctx := context.Background()
-	return services.Dump(ctx, dbService)
-}
-
-func writeRDBMS(events *services.EventList) error {
-	dbService, err := db.NewServiceFromConfig()
-	if err != nil {
-		return errors.Wrap(err, "failed to connect DB")
-	}
-	ctx := context.Background()
-	err = dbService.DoInTransaction(ctx,
-		func(ctx context.Context) error {
-			_, err = events.Process(ctx, dbService)
-			return err
-		})
-	return err
-}
-
-func writeEtcd(events *services.EventList) error {
-	etcdNotifierPath := viper.GetString("etcd.path")
-	etcdNotifierService, err := etcdclient.NewNotifierService(etcdNotifierPath)
-	if err != nil {
-		return errors.Wrap(err, "failed to connect etcd")
-	}
-	etcdNotifierService.SetNext(&services.BaseService{})
-	ctx := context.Background()
-	_, err = events.Process(ctx, etcdNotifierService)
-	return err
-}
-
 var convertCmd = &cobra.Command{
 	Use:   "convert",
 	Short: "convert data format",
 	Long:  `This command converts data formats from one to another`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var events *services.EventList
-		var err error
-		switch inType {
-		case cassandraType:
-			events, err = cassandra.DumpCassandra(
-				inFile, cassandraPort, time.Duration(cassandraTimeout)*time.Second)
-		case cassandraDumpType:
-			events, err = cassandra.ReadCassandraDump(inFile)
-		case yamlType:
-			events, err = readYAML()
-		case rdbmsType:
-			events, err = readRDBMS()
-		default:
-			err = fmt.Errorf("Unsupported input type %s", inType)
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = events.Sort()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		switch outType {
-		case rdbmsType:
-			err = writeRDBMS(events)
-		case yamlType:
-			err = writeYAML(events)
-		case etcdType:
-			err = writeEtcd(events)
-		default:
-			err = fmt.Errorf("Unsupported input type %s", inType)
-			log.Fatal("Unsupported output type")
-		}
+		err := convert.Convert(&convert.Config{
+			InType:           inType,
+			InFile:           inFile,
+			OutType:          outType,
+			OutFile:          outFile,
+			CassandraPort:    cassandraPort,
+			CassandraTimeout: cassandraTimeout,
+			EtcdNotifierPath: viper.GetString("etcd.path"),
+		})
 		if err != nil {
 			log.Fatal(err)
 		}
--- pkg/services/sync.go
+++ pkg/services/sync.go
@@ -2,10 +2,10 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
-	log "github.com/sirupsen/logrus"
 
 	"github.com/Juniper/contrail/pkg/common"
 	"github.com/Juniper/contrail/pkg/models"
@@ -22,7 +22,7 @@ type metadataGetter interface {
 	GetMetaData(ctx context.Context, uuid string, fqName []string) (*MetaData, error)
 }
 
-// nolint
+// ContrailService handles API requests.
 type ContrailService struct {
 	BaseService
 
@@ -30,17 +30,16 @@ type ContrailService struct {
 	TypeValidator  *models.TypeValidator
 }
 
-//RESTSync handles a bulk create request.
+// RESTSync handles Sync API request.
 func (service *ContrailService) RESTSync(c echo.Context) error {
 	events := &EventList{}
 	if err := c.Bind(events); err != nil {
-		log.WithFields(log.Fields{
-			"err": err,
-		}).Debug("bind failed on sync")
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid JSON format")
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("invalid JSON format: %v", err))
 	}
-	ctx := c.Request().Context()
-	responses, err := events.Process(ctx, service)
+
+	// TODO: Call events.Sort()
+
+	responses, err := events.Process(c.Request().Context(), service)
 	if err != nil {
 		return common.ToHTTPError(err)
 	}
