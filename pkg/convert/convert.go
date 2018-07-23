c170fe921315a0398e754089f05c26e3eb58fe67
Daniel Furman <daniel.furman@codilime.com>
2018-07-23 16:04:30 +0200

    Extract Convert code and document EventList.Sort()
    - extract Convert from cmd/contrailutil to pkg/convert package
    - document that EventList.Sort() ignores parent-child relationship
      and write test for that
    - improve DB error messages
    
    Change-Id: If15bb044f26bc8a0124f1c7f857c7633bc4972d6
    Partial-Bug: #1772623

---
+++ pkg/convert/convert.go
@@ -0,0 +1,138 @@
+package convert
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/db"
+	"github.com/Juniper/contrail/pkg/db/cassandra"
+	"github.com/Juniper/contrail/pkg/db/etcd"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// Data source and destination types.
+const (
+	YAMLType          = "yaml"
+	CassandraType     = "cassandra"
+	CassandraDumpType = "cassandra_dump"
+	RDBMSType         = "rdbms"
+	EtcdType          = "etcd"
+)
+
+// Config for Convert command.
+type Config struct {
+	InType           string
+	InFile           string
+	OutType          string
+	OutFile          string
+	CassandraPort    int
+	CassandraTimeout int
+	EtcdNotifierPath string
+}
+
+// Convert converts data from one format to another.
+func Convert(c *Config) error {
+	events, err := readData(c.InType, c.InFile, c.CassandraPort, c.CassandraTimeout)
+	if err != nil {
+		return errors.Wrapf(err, "reading events from %v failed", c.InType)
+	}
+
+	err = events.Sort()
+	if err != nil {
+		return errors.Wrap(err, "sorting events failed")
+	}
+
+	err = writeData(events, c.OutType, c.OutFile, c.EtcdNotifierPath)
+	if err != nil {
+		return errors.Wrapf(err, "writing events to %v failed", c.OutType)
+	}
+	return nil
+}
+
+func readData(inType, inFile string, cassandraPort, cassandraTimeout int) (*services.EventList, error) {
+	var events *services.EventList
+	var err error
+	switch inType {
+	case CassandraType:
+		events, err = cassandra.DumpCassandra(
+			inFile,
+			cassandraPort,
+			time.Duration(cassandraTimeout)*time.Second,
+		)
+	case CassandraDumpType:
+		events, err = cassandra.ReadCassandraDump(inFile)
+	case YAMLType:
+		events, err = readYAML(inFile)
+	case RDBMSType:
+		events, err = readRDBMS()
+	default:
+		return nil, errors.Errorf("unsupported input type %v", inType)
+	}
+
+	return events, err
+}
+
+func writeData(events *services.EventList, outType, outFile, etcdNotifierPath string) error {
+	var err error
+	switch outType {
+	case RDBMSType:
+		err = writeRDBMS(events)
+	case YAMLType:
+		err = writeYAML(events, outFile)
+	case EtcdType:
+		err = writeEtcd(events, etcdNotifierPath)
+	default:
+		return errors.Errorf("unsupported output type %v", outType)
+	}
+
+	return err
+}
+
+func readYAML(inFile string) (*services.EventList, error) {
+	var events services.EventList
+	err := common.LoadFile(inFile, &events)
+	return &events, err
+}
+
+func writeYAML(events *services.EventList, outFile string) error {
+	return common.SaveFile(outFile, events)
+}
+
+func readRDBMS() (*services.EventList, error) {
+	dbService, err := db.NewServiceFromConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect DB")
+	}
+
+	return services.Dump(context.Background(), dbService)
+}
+
+func writeRDBMS(events *services.EventList) error {
+	dbService, err := db.NewServiceFromConfig()
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to DB")
+	}
+
+	return dbService.DoInTransaction(
+		context.Background(),
+		func(ctx context.Context) error {
+			_, err = events.Process(ctx, dbService)
+			return err
+		},
+	)
+}
+
+func writeEtcd(events *services.EventList, etcdNotifierPath string) error {
+	etcdNotifierService, err := etcd.NewNotifierService(etcdNotifierPath)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to etcd")
+	}
+
+	etcdNotifierService.SetNext(&services.BaseService{})
+
+	_, err = events.Process(context.Background(), etcdNotifierService)
+	return errors.Wrap(err, "processing events on etcdNotifierService failed")
+}
