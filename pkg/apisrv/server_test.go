b94a5a5188fd71727cc365f4dcc015d7eaf96160
Michał Błotniak <michal.blotniak@codilime.com>
2018-07-24 17:10:57 +0200

    Implement ref-update endpoint
    Change-Id: Ifc7921be4c3e997068493f371e720e182da656e8
    Partial-bug: #1782364

--- pkg/apisrv/server_test.go
+++ pkg/apisrv/server_test.go
@@ -75,6 +75,11 @@ func TestInstanceIP(t *testing.T) {
 	RunTest(t, "./test_data/test_instance_ip.yml")
 }
 
+func TestRefUpdate(t *testing.T) {
+	AddKeystoneProjectAndUser(APIServer, t.Name())
+	RunTest(t, "./test_data/test_ref_update.yml")
+}
+
 func TestGRPC(t *testing.T) {
 	AddKeystoneProjectAndUser(APIServer, "TestGRPC")
 	restClient := client.NewHTTP(
