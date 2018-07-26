3f7bf845ac812d84c886a23ecb96333136469065
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-07-26 14:13:07 +0200

    Handle requests containing id-perms with null uuid
    When request has id-perms defined but they don't contain
    uuid, unique identifier is randomly chosen.
    
    Change-Id: Id41e592aacc3d62c4b9cc22e92976ed114a7fad0
    Closes-bug: 1783553

---
+++ pkg/models/id_perms.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// InitIDPerms initializes
+func InitIDPerms(idPerms *IdPermsType, uuid string) {
+
+	if idPerms == nil {
+		idPerms = &IdPermsType{
+			Enable: true,
+		}
+	}
+
+	if idPerms.UUID == nil {
+		uuid := strings.Replace(uuid, "-", "", 4)
+		uuidHigh, err1 := strconv.ParseInt(uuid[:len(uuid)/2], 16, 64)
+		uuidLow, err2 := strconv.ParseInt(uuid[len(uuid)/2:], 16, 64)
+		if len(uuid) != 32 || err1 != nil || err2 != nil {
+			random := rand.New(rand.NewSource(time.Now().UnixNano()))
+			uuidHigh = random.Int63()
+			uuidLow = random.Int63()
+		}
+
+		idPerms.UUID = &UuidType{
+			UUIDMslong: uuidHigh,
+			UUIDLslong: uuidLow,
+		}
+	}
+}
