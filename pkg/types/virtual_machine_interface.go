3d181d7a028c00d0d338618bcf99f3d7c6da443a
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-07-30 13:10:24 +0200

    Adding basic virtual-machine-interface validation
    -check if vmi has virtual-network-refs
    -check if virtual-network-ref correspond to existing virtual-network from db
    
    Closes-bug: 1784360
    
    Change-Id: I0eee79111e79cf08b62bdc2ce3ce63e7d521a33e

---
+++ pkg/types/virtual_machine_interface.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateVirtualMachineInterface validates if there is at least one virtual-network
+//reference and allocates MAC-address
+func (sv *ContrailTypeLogicService) CreateVirtualMachineInterface(
+	ctx context.Context,
+	request *services.CreateVirtualMachineInterfaceRequest) (*services.CreateVirtualMachineInterfaceResponse, error) {
+
+	var response *services.CreateVirtualMachineInterfaceResponse
+	virtualMachineInterface := request.GetVirtualMachineInterface()
+	err := sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+
+			_, err := sv.getVirtualNetworkFromVirtualMachineInterface(ctx, virtualMachineInterface)
+			if err != nil {
+				return err
+			}
+
+			//TODO further validation
+			//mac-address allocation
+
+			response, err = sv.BaseService.CreateVirtualMachineInterface(ctx, request)
+			return err
+		})
+
+	return response, err
+}
+
+//nolint TODO remove (unused return value)
+func (sv *ContrailTypeLogicService) getVirtualNetworkFromVirtualMachineInterface(
+	ctx context.Context, virtualMachineInterface *models.VirtualMachineInterface) (*models.VirtualNetwork, error) {
+
+	if len(virtualMachineInterface.GetVirtualNetworkRefs()) == 0 {
+		return nil, common.ErrorBadRequest("virtual_network_refs are not defined")
+	}
+
+	uuid := virtualMachineInterface.GetVirtualNetworkRefs()[0].GetUUID()
+	virtualNetworkResponse, err := sv.ReadService.GetVirtualNetwork(ctx,
+		&services.GetVirtualNetworkRequest{
+			ID: uuid,
+		})
+	if err != nil {
+		return nil, common.ErrorBadRequestf("Missing virtual-network with uuid %s: %v", uuid, err)
+	}
+
+	return virtualNetworkResponse.GetVirtualNetwork(), nil
+}
