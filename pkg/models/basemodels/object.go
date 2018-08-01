5ef2dd28a289cc0595813eb376afacc3594d4828
Michał Błotniak <michal.blotniak@codilime.com>
2018-08-01 09:51:37 +0200

    Implement prop-collection-update
    Implemented /prop-collection-update endpoint - that required methods for
    dynamic object retrieval and processing. For that purpose model.Object
    interface was creates, implemented services.GetObject method and added
    new methods in models for manipulating Map and List Properties.
    
    Change-Id: I48c701d81792ea45d11cf79dc27f7914bf7f338d
    Closes-Bug: #1785278

---
+++ pkg/models/basemodels/object.go
@@ -0,0 +1,14 @@
+package basemodels
+
+import (
+	"github.com/gogo/protobuf/proto"
+)
+
+// Object is generic model instance.
+type Object interface {
+	proto.Message
+	ToMap() map[string]interface{}
+	Kind() string
+	Depends() []string
+	ApplyPropCollectionUpdate(*PropCollectionUpdate) (updated map[string]interface{}, err error)
+}
