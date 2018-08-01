ac433c646329af08daa1e1887d85bdbb64f966a5
Praneet Bachheti <praneetb@juniper.net>
2018-08-01 09:15:50 -0700

    Contrail plugin framework uses dependency tracker to evaluate resources
    Change-Id: I30f2b96363d6df48534f9f2262809fee1df67ff4
    Partial-Bug: 1753615

--- pkg/compilation/plugins/contrail/dependencies/dependencies_processor.go
+++ pkg/compilation/plugins/contrail/dependencies/dependencies_processor.go
@@ -24,7 +24,7 @@ import (
 )
 
 // NewDependencyProcessor - creates new instance
-func NewDependencyProcessor(objCache map[string]map[string]interface{}) *DependencyProcessor {
+func NewDependencyProcessor(objCache *sync.Map) *DependencyProcessor {
 	d := &DependencyProcessor{cache: objCache}
 	d.Init()
 	return d
@@ -33,7 +33,7 @@ func NewDependencyProcessor(objCache map[string]map[string]interface{}) *Depende
 // DependencyProcessor stores resources dependency
 type DependencyProcessor struct {
 	resources *sync.Map
-	cache     map[string]map[string]interface{}
+	cache     *sync.Map
 }
 
 // Init - initializes the dependency processor
@@ -59,17 +59,7 @@ func (d *DependencyProcessor) GetResources() *sync.Map {
 
 // GetResourcesPretty - Get Resources from the dependency processor list
 func (d *DependencyProcessor) GetResourcesPretty() string {
-	tmpMap := make(map[string]map[string]interface{})
-	d.resources.Range(func(k, v interface{}) bool {
-		key := k.(string)
-		value := v.(*sync.Map)
-		tmpMap[key] = make(map[string]interface{})
-		value.Range(func(k1, v1 interface{}) bool {
-			tmpMap[key][k1.(string)] = v1
-			return true
-		})
-		return true
-	})
+	tmpMap := d.GetResources()
 
 	b, err := json.MarshalIndent(tmpMap, "", "  ")
 	if err != nil {
@@ -99,8 +89,19 @@ func (d *DependencyProcessor) canAdd(key string, obj interface{}) bool {
 	return true
 }
 
+// getCachedObject gets the cached object
+func (d *DependencyProcessor) getCachedObject(objTypeStr, uuid string) interface{} {
+	if objMap, ok := d.cache.Load(objTypeStr); ok {
+		if obj, present := objMap.(*sync.Map).Load(uuid); present {
+			return obj
+		}
+		return nil
+	}
+	return nil
+}
+
 // Evaluate - Evaluates object dependency based on the ReactionMap
-func (d *DependencyProcessor) Evaluate(obj interface{}, objTypeStr, fromTypeStr string) {
+func (d *DependencyProcessor) Evaluate(obj interface{}, objTypeStr, fromTypeStr string) { // nolint: gocyclo
 	if _, ok := ReactionMap[objTypeStr]; !ok {
 		return
 	}
@@ -116,14 +117,17 @@ func (d *DependencyProcessor) Evaluate(obj interface{}, objTypeStr, fromTypeStr 
 		for _, fieldName := range fieldsToExtract {
 			refObjTypeValues, err := reflections.GetField(obj, fieldName)
 			if err != nil {
-				// Refs dont exit, ignore
+				// Refs dont exist, ignore
 				continue
 			}
 			objValues := reflect.ValueOf(refObjTypeValues)
 			for i := 0; i < objValues.Len(); i++ {
 				interfaceObj := objValues.Index(i).Elem().Interface()
 				uuid, _ := reflections.GetField(interfaceObj, "UUID")
-				refObj := d.cache[refObjTypeStr][uuid.(string)]
+				refObj := d.getCachedObject(refObjTypeStr, uuid.(string))
+				if refObj == nil {
+					continue
+				}
 				log.Infof("Evaluating: Object: %s %s(%s) From: %s", fieldName, refObjTypeStr, uuid.(string), objTypeStr)
 				d.Evaluate(refObj, refObjTypeStr, objTypeStr)
 			}
@@ -132,7 +136,7 @@ func (d *DependencyProcessor) Evaluate(obj interface{}, objTypeStr, fromTypeStr 
 		for _, fieldName := range fieldsToExtract {
 			refObjTypeValues, err := reflections.GetField(obj, fieldName)
 			if err != nil {
-				// BackRefs dont exit, ignore
+				// BackRefs dont exist, ignore
 				continue
 			}
 			objValues := reflect.ValueOf(refObjTypeValues)
