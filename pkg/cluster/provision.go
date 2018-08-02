4bba8ed0dcf0a1029cfcdb9f17a7febcdc0f5389
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-08-02 06:28:46 +0000

    Support provisioning contrail datapath
    encryption.
    
    Change-Id: Ia180b2129aaa69f8df1511c5bf06679cad576ef2
    Closes-Bug: #1785002

--- pkg/cluster/provision.go
+++ pkg/cluster/provision.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 
 	"github.com/flosch/pongo2"
 	"github.com/sirupsen/logrus"
@@ -52,7 +53,10 @@ func (p *provisionCommon) applyTemplate(templateSrc string, context map[string]i
 	if err != nil {
 		return nil, err
 	}
-	return output, nil
+	// strip empty lines in output content
+	regex, _ := regexp.Compile("\n[ \r\n\t]*\n")
+	outputString := regex.ReplaceAllString(string(output), "\n")
+	return []byte(outputString), nil
 }
 
 func (p *provisionCommon) writeToFile(path string, content []byte) error {
