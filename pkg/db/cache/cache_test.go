cedb530faa4b6d94854299842fd1bb68caa31fbe
Józef Wołoch <jozef.woloch@codilime.com>
2018-08-06 12:22:11 +0200

    ReadService implementation for CacheDB
    Change-Id: Idcbf8a0cafa5dc496846e6787647944b14e2a7ae
    Closes-bug: 1786216

--- pkg/db/cache/cache_test.go
+++ pkg/db/cache/cache_test.go
@@ -104,159 +104,288 @@ func TestCache(t *testing.T) {
 	// _, ok := cache.idMap["vn0"]
 	// assert.Equal(t, false, ok, "compaction failed")
 }
+
+type dependencyTestAssertion func(
+	t *testing.T,
+	event *services.Event,
+	result *services.Event,
+	cache *DB,
+)
+
+type dependecyTestScenario struct {
+	name      string
+	event     *services.Event
+	assertion dependencyTestAssertion
+}
+
 func TestDependencyResolution(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
-	cache := NewDB(6)
+	cache := NewDB(2)
 
-	vnTestUUID := "vn_blue"
-	vn := makeTestVirtualNetwork(&baseResourceParams{uuid: vnTestUUID})
-
-	event1, err := cache.processTestEvent(&services.Event{
-		Version: 0,
-		Request: &services.Event_CreateVirtualNetworkRequest{
-			CreateVirtualNetworkRequest: &services.CreateVirtualNetworkRequest{
-				VirtualNetwork: vn,
+	vnBlue := &models.VirtualNetwork{
+		UUID: "vn_blue",
+	}
+	vnWithParent := &models.VirtualNetwork{
+		UUID:       vnBlue.UUID,
+		Name:       "blue",
+		ParentUUID: "domain",
+	}
+	ri1 := &models.RoutingInstance{
+		UUID:       "ri_uuid1",
+		ParentUUID: vnBlue.GetUUID(),
+	}
+	ri2 := &models.RoutingInstance{
+		UUID:       "ri_uuid2",
+		ParentUUID: vnBlue.GetUUID(),
+		RoutingInstanceRefs: []*models.RoutingInstanceRoutingInstanceRef{
+			{
+				UUID: ri1.UUID,
 			},
 		},
-	})
-	assert.NoError(t, err)
+	}
 
-	assert.Equal(t, vnTestUUID, vn.UUID)
-	e := cache.Get(vnTestUUID)
-	assert.Equal(t, event1, e)
-	assert.Equal(t, "", e.GetResource().GetParentUUID())
-
-	vn.ParentUUID = "domain"
-	event2, err := cache.processTestEvent(&services.Event{
-		Version: 1,
-		Request: &services.Event_UpdateVirtualNetworkRequest{
-			UpdateVirtualNetworkRequest: &services.UpdateVirtualNetworkRequest{
-				VirtualNetwork: vn,
+	tests := []dependecyTestScenario{
+		{
+			name: "Create vn_blue",
+			event: &services.Event{
+				Version: 0,
+				Request: &services.Event_CreateVirtualNetworkRequest{
+					CreateVirtualNetworkRequest: &services.CreateVirtualNetworkRequest{
+						VirtualNetwork: vnBlue,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				assert.Equal(t, result, e)
+				assert.Equal(t, "", e.GetResource().GetParentUUID())
 			},
 		},
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, e.GetResource().GetParentUUID(), "domain")
-
-	e = cache.Get(vnTestUUID)
-	assert.Equal(t, event2, e)
-	assert.NotEqual(t, event1, event2)
-
-	riUUID1 := "ri_uuid1"
-	ri := makeTestRoutingInstance(&riParams{
-		baseResourceParams: baseResourceParams{
-			uuid: riUUID1, parentUUID: vn.GetUUID(),
-		},
-	})
-
-	_, err = cache.processTestEvent(&services.Event{
-		Version: 2,
-		Request: &services.Event_CreateRoutingInstanceRequest{
-			CreateRoutingInstanceRequest: &services.CreateRoutingInstanceRequest{
-				RoutingInstance: ri,
+		{
+			name: "Update vn_blue with parent uuid",
+			event: &services.Event{
+				Version: 1,
+				Request: &services.Event_UpdateVirtualNetworkRequest{
+					UpdateVirtualNetworkRequest: &services.UpdateVirtualNetworkRequest{
+						VirtualNetwork: vnWithParent,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				assert.Equal(t, result, e)
+				assert.Equal(t, services.OperationUpdate, e.Operation())
+				assert.Equal(t, e.GetResource().GetParentUUID(), "domain")
 			},
 		},
-	})
-	assert.NoError(t, err)
-
-	e = cache.Get(vnTestUUID)
-
-	vn = e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
-	assert.Len(t, vn.RoutingInstances, 1)
-	assert.Equal(t, riUUID1, vn.RoutingInstances[0].UUID)
-
-	riUUID2 := "ri_uuid2"
-	ri = makeTestRoutingInstance(&riParams{
-		baseResourceParams: baseResourceParams{
-			uuid: riUUID2, parentUUID: vn.GetUUID()}, riRefs: []*models.RoutingInstanceRoutingInstanceRef{
-			{UUID: riUUID1},
-		},
-	})
-
-	_, err = cache.processTestEvent(&services.Event{
-		Version: 3,
-		Request: &services.Event_CreateRoutingInstanceRequest{
-			CreateRoutingInstanceRequest: &services.CreateRoutingInstanceRequest{
-				RoutingInstance: ri,
+		{
+			name: "Create routing instance 1",
+			event: &services.Event{
+				Version: 2,
+				Request: &services.Event_CreateRoutingInstanceRequest{
+					CreateRoutingInstanceRequest: &services.CreateRoutingInstanceRequest{
+						RoutingInstance: ri1,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				v := e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
+				assert.Len(t, v.RoutingInstances, 1)
+				assert.Equal(t, ri1.UUID, v.RoutingInstances[0].UUID)
 			},
 		},
-	})
-	assert.NoError(t, err)
-	e = cache.Get(vnTestUUID)
+		{
+			name: "Create routing instance 2",
+			event: &services.Event{
+				Version: 3,
+				Request: &services.Event_CreateRoutingInstanceRequest{
+					CreateRoutingInstanceRequest: &services.CreateRoutingInstanceRequest{
+						RoutingInstance: ri2,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				v := e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
+				assert.Len(t, v.RoutingInstances, 2)
+				assert.Equal(t, ri2.UUID, v.RoutingInstances[1].UUID)
 
-	vn = e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
-	assert.Len(t, vn.RoutingInstances, 2)
-	assert.Equal(t, riUUID2, vn.RoutingInstances[1].UUID)
-
-	e = cache.Get(riUUID1)
-	ri = e.GetCreateRoutingInstanceRequest().GetRoutingInstance()
-	assert.Len(t, ri.RoutingInstanceBackRefs, 1)
-	assert.Equal(t, riUUID2, ri.RoutingInstanceBackRefs[0].UUID)
-
-	event4, err := cache.processTestEvent(&services.Event{
-		Version: 4,
-		Request: &services.Event_DeleteRoutingInstanceRequest{
-			DeleteRoutingInstanceRequest: &services.DeleteRoutingInstanceRequest{
-				ID: riUUID2,
+				e = cache.Get(ri1.UUID)
+				ri := e.GetCreateRoutingInstanceRequest().GetRoutingInstance()
+				assert.Len(t, ri.RoutingInstanceBackRefs, 1)
+				assert.Equal(t, ri2.UUID, ri.RoutingInstanceBackRefs[0].UUID)
 			},
 		},
-	})
-	assert.NoError(t, err)
+		{
+			name: "Delete routing instance 2",
+			event: &services.Event{
+				Version: 4,
+				Request: &services.Event_DeleteRoutingInstanceRequest{
+					DeleteRoutingInstanceRequest: &services.DeleteRoutingInstanceRequest{
+						ID: ri2.UUID,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				v := e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
+				assert.Len(t, v.RoutingInstances, 1)
+				assert.Equal(t, ri1.UUID, v.RoutingInstances[0].UUID)
 
-	e = cache.Get(riUUID2)
-	assert.Equal(t, event4, e)
-
-	e = cache.Get(vnTestUUID)
-	vn = e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
-	assert.Len(t, vn.RoutingInstances, 1)
-	assert.Equal(t, riUUID1, vn.RoutingInstances[0].UUID)
-
-	e = cache.Get(riUUID1)
-	ri = e.GetCreateRoutingInstanceRequest().GetRoutingInstance()
-	assert.Len(t, ri.RoutingInstanceBackRefs, 0)
-
-	event5, err := cache.processTestEvent(&services.Event{
-		Version: 5,
-		Request: &services.Event_DeleteVirtualNetworkRequest{
-			DeleteVirtualNetworkRequest: &services.DeleteVirtualNetworkRequest{
-				ID: vnTestUUID,
+				e = cache.Get(ri1.UUID)
+				ri := e.GetCreateRoutingInstanceRequest().GetRoutingInstance()
+				assert.Len(t, ri.RoutingInstanceBackRefs, 0)
 			},
 		},
-	})
-	assert.NoError(t, err)
+		{
+			name: "Delete routing instance 1",
+			event: &services.Event{
+				Version: 5,
+				Request: &services.Event_DeleteRoutingInstanceRequest{
+					DeleteRoutingInstanceRequest: &services.DeleteRoutingInstanceRequest{
+						ID: ri1.UUID,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				vnBlue = e.GetUpdateVirtualNetworkRequest().GetVirtualNetwork()
+				assert.Len(t, vnBlue.RoutingInstances, 0)
+			},
+		},
+		{
+			name: "Delete virtual network",
+			event: &services.Event{
+				Version: 6,
+				Request: &services.Event_DeleteVirtualNetworkRequest{
+					DeleteVirtualNetworkRequest: &services.DeleteVirtualNetworkRequest{
+						ID: vnBlue.UUID,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				r := e.GetResource()
+				assert.Equal(t, result, e)
+				assert.Equal(t, services.OperationDelete, e.Operation())
+				assert.NotEqual(t, vnBlue.ParentUUID, r.GetParentUUID())
 
-	e = cache.Get(vnTestUUID)
-	r := e.GetResource()
-	assert.Equal(t, event5, e)
-	assert.Equal(t, services.OperationDelete, e.Operation())
-	assert.NotEqual(t, vn.ParentUUID, r.GetParentUUID())
+				e = cache.Get(ri2.UUID)
+				assert.Nil(t, e)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e, err := cache.Process(context.Background(), tt.event)
+			assert.NoError(t, err)
+			assert.Equal(t, e, tt.event)
+			tt.assertion(t, tt.event, e, cache)
+		})
+	}
 }
 
-type baseResourceParams struct {
-	uuid       string
-	parentUUID string
-}
+func TestReadInterfaceAndMaxHistory(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	cache := NewDB(1)
 
-func makeTestVirtualNetwork(vnParams *baseResourceParams) *models.VirtualNetwork {
-	vn := models.MakeVirtualNetwork()
-	vn.UUID = vnParams.uuid
-	vn.ParentUUID = vnParams.parentUUID
-	return vn
-}
+	vnBlue := &models.VirtualNetwork{
+		UUID: "vn_blue",
+	}
 
-type riParams struct {
-	baseResourceParams
-	riRefs []*models.RoutingInstanceRoutingInstanceRef
-}
+	tests := []dependecyTestScenario{
+		{
+			name: "Create vn_blue",
+			event: &services.Event{
+				Version: 0,
+				Request: &services.Event_CreateVirtualNetworkRequest{
+					CreateVirtualNetworkRequest: &services.CreateVirtualNetworkRequest{
+						VirtualNetwork: vnBlue,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				listResponse, err := cache.ListVirtualNetwork(context.Background(), &services.ListVirtualNetworkRequest{})
+				assert.NoError(t, err)
+				assert.Equal(t, listResponse.VirtualNetworkCount, int64(1))
+				assert.Equal(t, listResponse.VirtualNetworks[0].UUID, vnBlue.UUID)
 
-func makeTestRoutingInstance(rip *riParams) *models.RoutingInstance {
-	ri := models.MakeRoutingInstance()
-	ri.UUID = rip.uuid
-	ri.ParentUUID = rip.parentUUID
-	ri.RoutingInstanceRefs = rip.riRefs
-	return ri
-}
+				getResponse, err := cache.GetVirtualNetwork(context.Background(), &services.GetVirtualNetworkRequest{
+					ID: vnBlue.UUID,
+				})
+				assert.NoError(t, err)
+				assert.Equal(t, getResponse.VirtualNetwork.UUID, vnBlue.UUID)
+			},
+		},
+		{
+			name: "Delete virtual network",
+			event: &services.Event{
+				Version: 1,
+				Request: &services.Event_DeleteVirtualNetworkRequest{
+					DeleteVirtualNetworkRequest: &services.DeleteVirtualNetworkRequest{
+						ID: vnBlue.UUID,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				assert.Equal(t, result, e)
+				assert.Equal(t, services.OperationDelete, e.Operation())
 
-func (cache *DB) processTestEvent(event *services.Event) (*services.Event, error) {
-	return cache.Process(context.Background(), event)
+				r, err := cache.ListVirtualNetwork(context.Background(), &services.ListVirtualNetworkRequest{})
+				assert.NoError(t, err)
+				assert.Equal(t, r.VirtualNetworkCount, int64(0))
+
+				_, err = cache.AddWatcher(context.Background(), 1)
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "Create vn_blue",
+			event: &services.Event{
+				Version: 2,
+				Request: &services.Event_CreateVirtualNetworkRequest{
+					CreateVirtualNetworkRequest: &services.CreateVirtualNetworkRequest{
+						VirtualNetwork: vnBlue,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				e := cache.Get(vnBlue.UUID)
+				assert.Equal(t, result, e)
+				assert.Equal(t, services.OperationCreate, e.Operation())
+
+				r, err := cache.ListVirtualNetwork(context.Background(), &services.ListVirtualNetworkRequest{})
+				assert.NoError(t, err)
+				assert.Equal(t, r.VirtualNetworkCount, int64(1))
+				assert.Equal(t, r.VirtualNetworks[0].UUID, vnBlue.UUID)
+			},
+		},
+		{
+			name: "Delete virtual network",
+			event: &services.Event{
+				Version: 3,
+				Request: &services.Event_DeleteVirtualNetworkRequest{
+					DeleteVirtualNetworkRequest: &services.DeleteVirtualNetworkRequest{
+						ID: vnBlue.UUID,
+					},
+				},
+			},
+			assertion: func(t *testing.T, event *services.Event, result *services.Event, cache *DB) {
+				_, err := cache.AddWatcher(context.Background(), 1)
+				assert.Error(t, err)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e, err := cache.Process(context.Background(), tt.event)
+			assert.NoError(t, err)
+			assert.Equal(t, e, tt.event)
+			tt.assertion(t, tt.event, e, cache)
+		})
+	}
 }
