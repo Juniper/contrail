3d471004b41c8e9d68f2f10b48ac83ad56e82f66
Kamil Braun <kamil.braun@codilime.com>
2018-08-10 15:55:56 +0200

    Allocate security group ID
    Closes-Bug: #1786493
    Change-Id: I002c1f355c144863213d4bdfffb543c8452df2ad

---
+++ pkg/types/security_group.go
@@ -0,0 +1,132 @@
+package types
+
+import (
+	"context"
+
+	protobuf "github.com/gogo/protobuf/types"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+const (
+	// SecurityGroupIDMinAlloc is the minimal integer used for security group IDs that are automatically allocated.
+	SecurityGroupIDMinAlloc = 8000000
+)
+
+// CreateSecurityGroup performs type specific validation and setup for creating security groups.
+func (sv *ContrailTypeLogicService) CreateSecurityGroup(
+	ctx context.Context, request *services.CreateSecurityGroupRequest) (
+	response *services.CreateSecurityGroupResponse, err error) {
+
+	sg := request.SecurityGroup
+
+	if err = sv.disallowManualSecurityGroupID(nil, sg, nil); err != nil {
+		return nil, err
+	}
+
+	err = sv.InTransactionDoer.DoInTransaction(ctx, func(ctx context.Context) error {
+		// TODO: handle configured security group ID
+		if sg.SecurityGroupID, err = sv.allocateSecurityGroupID(ctx); err != nil {
+			return err
+		}
+
+		if response, err = sv.BaseService.CreateSecurityGroup(ctx, request); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return response, err
+}
+
+// UpdateSecurityGroup performs type specific validation and setup for updating security groups.
+func (sv *ContrailTypeLogicService) UpdateSecurityGroup(
+	ctx context.Context, request *services.UpdateSecurityGroupRequest) (
+	response *services.UpdateSecurityGroupResponse, err error) {
+
+	sg := request.SecurityGroup
+
+	err = sv.InTransactionDoer.DoInTransaction(ctx, func(ctx context.Context) error {
+		var getResponse *services.GetSecurityGroupResponse
+		if getResponse, err = sv.ReadService.GetSecurityGroup(ctx, &services.GetSecurityGroupRequest{
+			ID: sg.UUID,
+		}); err != nil {
+			return err
+		}
+		current := getResponse.GetSecurityGroup()
+
+		if err = sv.disallowManualSecurityGroupID(current, sg, &request.FieldMask); err != nil {
+			return err
+		}
+
+		response, err = sv.BaseService.UpdateSecurityGroup(ctx, request)
+		return err
+	})
+
+	return response, err
+}
+
+// DeleteSecurityGroup performs type specific validation and teardown for deleting security groups.
+func (sv *ContrailTypeLogicService) DeleteSecurityGroup(
+	ctx context.Context, request *services.DeleteSecurityGroupRequest) (
+	response *services.DeleteSecurityGroupResponse, err error) {
+
+	uuid := request.ID
+
+	err = sv.InTransactionDoer.DoInTransaction(ctx, func(ctx context.Context) error {
+		var getResponse *services.GetSecurityGroupResponse
+		if getResponse, err = sv.ReadService.GetSecurityGroup(ctx, &services.GetSecurityGroupRequest{
+			ID: uuid,
+		}); err != nil {
+			return err
+		}
+
+		sg := getResponse.GetSecurityGroup()
+
+		// TODO: handle configured security group ID
+		if err = sv.deallocateSecurityGroupID(ctx, sg.SecurityGroupID); err != nil {
+			return err
+		}
+
+		response, err = sv.BaseService.DeleteSecurityGroup(ctx, request)
+		return err
+	})
+
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) allocateSecurityGroupID(ctx context.Context) (int64, error) {
+	id, err := sv.IntPoolAllocator.AllocateInt(ctx, SecurityGroupIDPoolKey)
+	if err != nil {
+		return 0, err
+	}
+	return id + SecurityGroupIDMinAlloc, nil
+}
+
+func (sv *ContrailTypeLogicService) deallocateSecurityGroupID(ctx context.Context, id int64) error {
+	return sv.IntPoolAllocator.DeallocateInt(ctx, SecurityGroupIDPoolKey, id-SecurityGroupIDMinAlloc)
+}
+
+func (sv *ContrailTypeLogicService) disallowManualSecurityGroupID(
+	current *models.SecurityGroup, requested *models.SecurityGroup, fieldMask *protobuf.FieldMask) error {
+	if current == nil {
+		if requested.SecurityGroupID == 0 {
+			return nil
+		}
+
+		return common.ErrorForbidden("cannot set the security group ID, it's allocated by the server")
+	}
+
+	if !common.ContainsString(fieldMask.GetPaths(), models.SecurityGroupFieldSecurityGroupID) {
+		return nil
+	}
+
+	if current.SecurityGroupID == requested.SecurityGroupID {
+		return nil
+	}
+
+	return common.ErrorForbidden("cannot update the security group ID, it's allocated by the server")
+}
---
+++ pkg/types/security_group_test.go
@@ -0,0 +1,111 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/mock"
+	"github.com/Juniper/contrail/pkg/types/mock"
+)
+
+func TestSecurityGroupLogicValidation(t *testing.T) {
+
+	sg := models.MakeSecurityGroup()
+	sg.UUID = "sg_uuid"
+
+	sg_with_id := models.MakeSecurityGroup()
+	sg_with_id.UUID = sg.UUID
+	sg_with_id.SecurityGroupID = 8000001
+
+	sg_with_id2 := models.MakeSecurityGroup()
+	sg_with_id2.UUID = sg.UUID
+	sg_with_id2.SecurityGroupID = 8000002
+
+	runTest(t, "create security group",
+		func(t *testing.T, sv *ContrailTypeLogicService) {
+			sv.IntPoolAllocator.(*typesmock.MockIntPoolAllocator).
+				EXPECT().AllocateInt(gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil())).
+				Return(int64(1), nil).Times(1)
+
+			sv.Next().(*servicesmock.MockService).
+				EXPECT().CreateSecurityGroup(gomock.Not(gomock.Nil()), &services.CreateSecurityGroupRequest{
+				SecurityGroup: sg_with_id,
+			}).Return(&services.CreateSecurityGroupResponse{SecurityGroup: sg_with_id}, nil).Times(1)
+
+			ctx := context.Background()
+			res, err := sv.CreateSecurityGroup(ctx, &services.CreateSecurityGroupRequest{
+				SecurityGroup: sg,
+			})
+
+			if assert.NoError(t, err) {
+				assert.Equal(t, sg_with_id, res.SecurityGroup)
+			}
+		})
+
+	runTest(t, "fail to create security group with explicit ID",
+		func(t *testing.T, sv *ContrailTypeLogicService) {
+			ctx := context.Background()
+			_, err := sv.CreateSecurityGroup(ctx, &services.CreateSecurityGroupRequest{
+				SecurityGroup: sg_with_id,
+			})
+
+			assert.Error(t, err)
+		})
+
+	runTest(t, "update security group with explicit ID equal to the old ID",
+		func(t *testing.T, sv *ContrailTypeLogicService) {
+			sv.ReadService.(*servicesmock.MockReadService).
+				EXPECT().GetSecurityGroup(gomock.Not(gomock.Nil()), &services.GetSecurityGroupRequest{
+				ID: sg_with_id.UUID,
+			}).Return(&services.GetSecurityGroupResponse{SecurityGroup: sg_with_id}, nil).Times(1)
+
+			sv.Next().(*servicesmock.MockService).
+				EXPECT().UpdateSecurityGroup(gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil())).
+				DoAndReturn(func(_ context.Context, request *services.UpdateSecurityGroupRequest) (
+					*services.UpdateSecurityGroupResponse, error) {
+					return &services.UpdateSecurityGroupResponse{
+						SecurityGroup: request.SecurityGroup,
+					}, nil
+				}).Times(1)
+
+			ctx := context.Background()
+			res, err := sv.UpdateSecurityGroup(ctx, &services.UpdateSecurityGroupRequest{
+				SecurityGroup: sg_with_id,
+				FieldMask: types.FieldMask{
+					Paths: []string{
+						models.SecurityGroupFieldSecurityGroupID,
+					},
+				},
+			})
+
+			if assert.NoError(t, err) {
+				assert.Equal(t, sg_with_id, res.SecurityGroup)
+			}
+		})
+
+	runTest(t, "fail to update security group with explicit ID different than the old ID",
+		func(t *testing.T, sv *ContrailTypeLogicService) {
+			sv.ReadService.(*servicesmock.MockReadService).
+				EXPECT().GetSecurityGroup(gomock.Not(gomock.Nil()), &services.GetSecurityGroupRequest{
+				ID: sg_with_id.UUID,
+			}).Return(&services.GetSecurityGroupResponse{SecurityGroup: sg_with_id}, nil).Times(1)
+
+			ctx := context.Background()
+			_, err := sv.UpdateSecurityGroup(ctx, &services.UpdateSecurityGroupRequest{
+				SecurityGroup: sg_with_id2,
+				FieldMask: types.FieldMask{
+					Paths: []string{
+						models.SecurityGroupFieldSecurityGroupID,
+					},
+				},
+			})
+
+			assert.Error(t, err)
+		})
+}
