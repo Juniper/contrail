369bdb81d37563c9e5f8d6afabb07d8bbb445ca9
Michał Błotniak <michal.blotniak@codilime.com>
2018-08-23 17:08:21 -0700

    Create models.Codec and add some update utilities
    Added helpers usefull in update cases:
    - ApplyMap method that allows updating model objects using map
    - GetFromMapByPath that gets values from nested maps
    - ApplyFieldMask that filters map according to fieldmask
    
    Change-Id: I9072a3b9d798a8052ecb65e88259fb9504a01b5d
    Partial-Bug: 1784379

---
+++ pkg/models/basemodels/field_mask.go
@@ -0,0 +1,90 @@
+package basemodels
+
+import (
+	"strings"
+
+	"github.com/gogo/protobuf/types"
+)
+
+//MapToFieldMask returns updated fields masks.
+func MapToFieldMask(data map[string]interface{}) types.FieldMask {
+	var mask types.FieldMask
+	mask.Paths = paths(data, "")
+	return mask
+}
+
+type toMapper interface {
+	ToMap() map[string]interface{}
+}
+
+func paths(data map[string]interface{}, prefix string) []string {
+	var result []string
+	for key, value := range data {
+		switch v := value.(type) {
+		case map[string]interface{}:
+			if prefix != "" {
+				result = append(result, paths(v, prefix+key+".")...)
+			} else {
+				result = append(result, paths(v, key+".")...)
+			}
+		case toMapper:
+			m := v.ToMap()
+			if prefix != "" {
+				result = append(result, paths(m, prefix+key+".")...)
+			} else {
+				result = append(result, paths(m, key+".")...)
+			}
+		default:
+			result = append(result, prefix+key)
+		}
+	}
+	return result
+}
+
+// GetFromMapByPath gets map value by provided as slice of strings.
+func GetFromMapByPath(data map[string]interface{}, path []string) (value interface{}, ok bool) {
+	if len(path) == 0 {
+		return nil, false
+	}
+	for _, field := range path[:len(path)-1] {
+		next, _ := data[field].(map[string]interface{})
+		if next == nil {
+			return nil, false
+		}
+		data = next
+	}
+	value, ok = data[path[len(path)-1]]
+	return value, ok
+}
+
+// ApplyFieldMask creates new map that contain only fields specified in fieldmask.
+func ApplyFieldMask(m map[string]interface{}, fm types.FieldMask) map[string]interface{} {
+	if m == nil {
+		return nil
+	}
+	result := map[string]interface{}{}
+	for _, path := range fm.Paths {
+		fields := strings.Split(path, ".")
+		value, ok := GetFromMapByPath(m, fields)
+		if len(fields) == 0 || !ok {
+			continue
+		}
+		cur := result
+		for _, field := range fields[:len(fields)-1] {
+			nested := nestMap(cur, field)
+			cur = nested
+		}
+		cur[fields[len(fields)-1]] = value
+	}
+	return result
+}
+
+func nestMap(m map[string]interface{}, key string) map[string]interface{} {
+	nested, ok := m[key].(map[string]interface{})
+	if ok {
+		return nested
+	}
+	nested = map[string]interface{}{}
+	m[key] = nested
+	return nested
+}
