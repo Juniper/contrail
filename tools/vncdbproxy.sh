5b78d5213a488dfab920e2d01a0bf53ff9eadc83
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-08-24 11:16:11 +0200

    Add workaround for missing etcd support in control
    Add vncdbproxy service which will act as a cassandra and rabbit proxy.
    It puts contrail resources in correct form in cassandra and rabbit.
    
    Change-Id: Ib4516ba278d9b2d006c9d58af45908ebbd2a9cf4
    Partial-Bug:#1788809

---
+++ tools/vncdbproxy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Usage: vncdbproxy.sh <CONFIGDBSERVER_IP>"
+    exit 1
+fi
+
+docker rm -f vncdbproxy
+
+docker build ../docker/vnc_db_proxy/ -t vncdbproxy
+
+docker run \
+    --name vncdbproxy \
+    -p 8091:8082 \
+    -d \
+    -e CONFIG_API_PORT=8082 \
+    -e CONFIG_API_INTROSPECT_PORT=8084 \
+    -e LOG_LEVEL=SYS_NOTICE \
+    -e log_local=true \
+    -e AUTH_MODE=none \
+    -e AAA_MODE=cloud-admin \
+    -e ZOOKEEPER_SERVERS=$1:2181 \
+    -e CONFIGDB_SERVERS=$1:9161 \
+    -e RABBITMQ_SERVERS=$1:5673 \
+    vncdbproxy
