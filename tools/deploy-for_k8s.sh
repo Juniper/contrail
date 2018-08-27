28f01e0b52aaf5cb6538be0a26aab9b451c9d0a5
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-08-27 18:04:50 +0200

    [WIP] vnc_db_proxy workaround for automation k8s scripts
    Change-Id: I907892e0f4ad784bad4c5172113a3e26e67a532e
    Closes-Bug: 1789175
    Depends-On: Ib4516ba278d9b2d006c9d58af45908ebbd2a9cf4

--- tools/deploy-for_k8s.sh
+++ tools/deploy-for_k8s.sh
@@ -42,6 +42,7 @@ install_golang()
 	cd /tmp
 	curl -o go.tar.gz https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 	sudo tar --overwrite -C /usr -xzf go.tar.gz
+	sudo yum install -y wget unzip
 	export PATH="$PATH:/usr/go/bin"
 	hash -r
 	go env
@@ -64,9 +65,10 @@ make install
 "$ThisDir/testenv.sh" -n host postgres
 
 KubemanagerDir='/etc/contrail/kubemanager'
-#Stop kubemanager
+#Stop kubemanager and original config-node
 cd "$KubemanagerDir"
 docker-compose down
+docker-compose -f /etc/contrail/config/docker-compose.yaml down
 cd "$RootDir"
 
 Dumpfile="$HOME/dump-$$.yaml"
@@ -76,7 +78,7 @@ contrailutil convert -i 127.0.0.1 -p 9041 --intype cassandra --outtype yaml -o "
 # Build and run contrail-go2 docker
 build_docker
 AtomizerDocker='contrail-go-config-node'
-[ "$(docker ps -a -f "name=$AtomizerDocker" --format '{{.ID}}' | wc -l)" -ne 0 ] && docker rm "$AtomizerDocker"
+[ "$(docker ps -a -f "name=$AtomizerDocker" --format '{{.ID}}' | wc -l)" -ne 0 ] && docker rm -f "$AtomizerDocker"
 docker run -d --name "$AtomizerDocker" -p "$PORT:$PORT" --net host contrail-go-config
 GoConfigIP='127.0.0.1' # networking mode 'host'
 
@@ -85,6 +87,9 @@ GoConfigIP='127.0.0.1' # networking mode 'host'
 
 # Convert cassandra data to etcd and feed etcd
 contrailutil convert --intype yaml --in "$Dumpfile" --outtype rdbms -c docker/contrail_go/etc/contrail-atomizer.yml
+
+# Run vnc-db-proxy
+./tools/vncdbproxy.sh -n host
 
 # Modify k8s config (subst contrail-go-config IP address as config-node) and restart if needed
 ModifyKubeConfig=1
--- tools/vncdbproxy.sh
+++ tools/vncdbproxy.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-    echo "Usage: vncdbproxy.sh <CONFIGDBSERVER_IP>"
-    exit 1
-fi
-
 TOP=$(cd "$(dirname "$0")" && cd ../ && pwd)
 
-docker rm -f vncdbproxy
+Usage()
+{
+	echo "Usage: vncdbproxy.sh [-k] [-n <NetworkMode>] [CONFIGDBSERVER_IP]"
+}
 
+ServicesIpAddress='127.0.0.1'
+RemoveDockers=1
+Network='bridge' # This is default for `docker run` if no `--net` param is specified
+while :; do
+	case "$1" in
+		'-n') Network="$2"; shift 2;;
+		'-k') RemoveDockers=0; shift;;
+		'-h') Usage; exit 0;;
+		*) break;;
+	esac
+done
+
+if [ "$Network" = 'bridge' ]; then
+	[ "$1" = '' ] && { Usage; exit 1; }
+	ServicesIpAddress="$1"
+fi
+
+[ $RemoveDockers -eq 1 ] && docker rm -f vncdbproxy
 docker build "$TOP/docker/vnc_db_proxy/" -t vncdbproxy
 
 docker run \
     --name vncdbproxy \
+    --net "$Network" \
     -p 8091:8082 \
     -d \
     -e CONFIG_API_PORT=8082 \
@@ -21,7 +38,7 @@ docker run \
     -e log_local=true \
     -e AUTH_MODE=none \
     -e AAA_MODE=cloud-admin \
-    -e ZOOKEEPER_SERVERS="$1":2181 \
-    -e CONFIGDB_SERVERS="$1":9161 \
-    -e RABBITMQ_SERVERS="$1":5673 \
+    -e ZOOKEEPER_SERVERS="$ServicesIpAddress:2181" \
+    -e CONFIGDB_SERVERS="$ServicesIpAddress:9161" \
+    -e RABBITMQ_SERVERS="$ServicesIpAddress:5673" \
     vncdbproxy
