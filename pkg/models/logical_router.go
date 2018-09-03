a7d8e4f8c6fc7265b19813df474939d707f4c6d5
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-09-03 09:37:26 +0200

    [WIP]Adding post create/update logical router validation
    -creating/updating internal virtual network
    
    Change-Id: I63b2ba9fe2e789a93d04ce163ab5440f19757ef3
    Closes-bug: 1789891

--- pkg/models/logical_router.go
+++ pkg/models/logical_router.go
@@ -1,5 +1,6 @@
 package models
 
+<<<<<<< 8e8d69379981e06d9ed655e8e3edb3397cb7f007
 const (
 	noneString = "None"
 )
@@ -12,4 +13,37 @@ func (lr *LogicalRouter) GetVXLanIDInLogicaRouter() string {
 	}
 
 	return id
+=======
+import (
+	"strconv"
+	"strings"
+
+	"github.com/Juniper/contrail/pkg/common"
+)
+
+const (
+	noneString       = "None"
+	internalVNPrefix = "__contrail_lr_internal_vn_"
+)
+
+// GetVXLanIDInLogicaRouter returns vxlan network identifier property
+func (lr *LogicalRouter) GetVXLanIDInLogicaRouter() (string, error) {
+	id := lr.GetVxlanNetworkIdentifier()
+	if id == noneString || id == "" {
+		return "", nil
+	}
+
+	_, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return id, common.ErrorBadRequestf("vxlan network id must be a number(%s)", id)
+	}
+
+	return id, nil
+}
+
+// GetInternalVNName returns proper internal virtual network name
+func (lr *LogicalRouter) GetInternalVNName() string {
+	name := []string{internalVNPrefix, lr.GetUUID(), "__"}
+	return strings.Join(name, "")
+>>>>>>> [WIP]Adding post create/update logical router validation
 }
--- pkg/types/logical_router.go
+++ pkg/types/logical_router.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"context"
+	"strconv"
+	"strings"
 
 	"github.com/gogo/protobuf/types"
 
@@ -23,7 +25,7 @@ func (sv *ContrailTypeLogicService) CreateLogicalRouter(
 		func(ctx context.Context) error {
 			var err error
 
-			err = sv.checkForExternalGateway(ctx, logicalRouter, nil)
+			vxLanRouting, err := sv.checkForExternalGateway(ctx, logicalRouter, nil)
 			if err != nil {
 				return err
 			}
@@ -38,13 +40,31 @@ func (sv *ContrailTypeLogicService) CreateLogicalRouter(
 				return err
 			}
 
-			logicalRouter.VxlanNetworkIdentifier = logicalRouter.GetVXLanIDInLogicaRouter()
+			logicalRouter.VxlanNetworkIdentifier, err = logicalRouter.GetVXLanIDInLogicaRouter()
+			if err != nil {
+				return err
+			}
+
+			if vxLanRouting {
+				var internalVN *models.VirtualNetwork
+				internalVN, err = sv.createInternalVirtualNetwork(ctx, logicalRouter)
+				if err != nil {
+					return err
+				}
+
+				vnRef := &models.LogicalRouterVirtualNetworkRef{
+					UUID: internalVN.GetUUID(),
+					To:   internalVN.GetFQName(),
+					Attr: &models.LogicalRouterVirtualNetworkType{
+						LogicalRouterVirtualNetworkType: "InternalVirtualNetwork",
+					},
+				}
+				request.LogicalRouter.AddVirtualNetworkRef(vnRef)
+			}
 
 			//TODO check router supports vpn type
 			response, err = sv.BaseService.CreateLogicalRouter(ctx, request)
 			return err
-
-			//TODO post-create creating internal virtual network
 		})
 
 	return response, err
@@ -63,7 +83,7 @@ func (sv *ContrailTypeLogicService) UpdateLogicalRouter(
 		func(ctx context.Context) error {
 			var err error
 
-			err = sv.checkForExternalGateway(ctx, logicalRouter, &fieldMask)
+			vxLanRouting, err := sv.checkForExternalGateway(ctx, logicalRouter, &fieldMask)
 			if err != nil {
 				return err
 			}
@@ -83,13 +103,21 @@ func (sv *ContrailTypeLogicService) UpdateLogicalRouter(
 				return err
 			}
 
-			logicalRouter.VxlanNetworkIdentifier = logicalRouter.GetVXLanIDInLogicaRouter()
+			logicalRouter.VxlanNetworkIdentifier, err = logicalRouter.GetVXLanIDInLogicaRouter()
+			if err != nil {
+				return err
+			}
+
+			if vxLanRouting {
+				err = sv.updateInternalVirtualNetwork(ctx, logicalRouter, dbLogicalRouter, &fieldMask)
+				if err != nil {
+					return err
+				}
+			}
 
 			//TODO check BGP VPNs
 			response, err = sv.BaseService.UpdateLogicalRouter(ctx, request)
 			return err
-
-			//TODO post update changes
 		})
 
 	return response, err
@@ -112,27 +140,28 @@ func (sv *ContrailTypeLogicService) getLogicalRouter(
 func (sv *ContrailTypeLogicService) checkForExternalGateway(
 	ctx context.Context,
 	logicalRouter *models.LogicalRouter,
-	fm *types.FieldMask) error {
+	fm *types.FieldMask,
+) (bool, error) {
 
 	if fm != nil && !basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldParentUUID) &&
 		!basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldVirtualNetworkRefs) {
-		return nil
+		return false, nil
 	}
 
 	enabled, err := sv.isVxlanRoutingEnabled(ctx, logicalRouter)
 	if err != nil {
-		return err
+		return enabled, err
 	}
 
 	if enabled {
 		for _, vn := range logicalRouter.GetVirtualNetworkRefs() {
 			if vn.GetAttr().GetLogicalRouterVirtualNetworkType() != "InternalVirtualNetwork" {
-				return common.ErrorBadRequest("external gateway not supported with VxLAN")
+				return false, common.ErrorBadRequest("external gateway not supported with VxLAN")
 			}
 		}
 	}
 
-	return nil
+	return enabled, nil
 }
 
 func (sv *ContrailTypeLogicService) isVxlanRoutingEnabled(
@@ -255,4 +284,87 @@ func (sv *ContrailTypeLogicService) checkPortAvailability(
 	}
 
 	return nil
+}
+
+func (sv *ContrailTypeLogicService) createInternalVirtualNetwork(
+	ctx context.Context,
+	logicalRouter *models.LogicalRouter,
+) (*models.VirtualNetwork, error) {
+
+	internalVN := models.MakeVirtualNetwork()
+	internalVN.ParentUUID = logicalRouter.GetParentUUID()
+	internalVN.ParentType = models.KindProject
+	internalVN.Name = logicalRouter.GetInternalVNName()
+	internalVN.RouteTargetList = logicalRouter.GetConfiguredRouteTargetList()
+	internalVN.IDPerms = &models.IdPermsType{
+		Enable:      true,
+		UserVisible: false,
+	}
+
+	vxlanNetworkID, _ := strconv.ParseInt(logicalRouter.GetVxlanNetworkIdentifier(), 10, 64)
+	internalVN.VirtualNetworkProperties = &models.VirtualNetworkType{
+		VxlanNetworkIdentifier: vxlanNetworkID,
+		ForwardingMode:         models.L3Mode,
+	}
+
+	request := &services.CreateVirtualNetworkRequest{
+		VirtualNetwork: internalVN,
+	}
+
+	response, err := sv.BaseService.CreateVirtualNetwork(ctx, request)
+	return response.GetVirtualNetwork(), err
+}
+
+func (sv *ContrailTypeLogicService) updateInternalVirtualNetwork(
+	ctx context.Context,
+	logicalRouter *models.LogicalRouter,
+	dbLogicalRouter *models.LogicalRouter,
+	fm *types.FieldMask,
+) error {
+
+	if !basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldConfiguredRouteTargetList) &&
+		!basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldVxlanNetworkIdentifier) {
+		return nil
+	}
+
+	var uuid string
+	for _, vn := range dbLogicalRouter.GetVirtualNetworkRefs() {
+		if vn.GetAttr().GetLogicalRouterVirtualNetworkType() == "InternalVirtualNetwork" {
+			uuid = vn.GetUUID()
+			break
+		}
+	}
+
+	if uuid == "" {
+		return nil
+	}
+
+	updateVN := models.MakeVirtualNetwork()
+	updateVN.UUID = uuid
+	var updatePaths []string
+
+	if basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldConfiguredRouteTargetList) {
+		updateVN.RouteTargetList = logicalRouter.GetConfiguredRouteTargetList()
+		updatePaths = append(updatePaths, models.VirtualNetworkFieldRouteTargetList)
+	}
+
+	if basemodels.FieldMaskContains(*fm, models.LogicalRouterFieldVxlanNetworkIdentifier) {
+		vxlanNetworkID, _ := strconv.ParseInt(logicalRouter.GetVxlanNetworkIdentifier(), 10, 64)
+		updateVN.VirtualNetworkProperties = &models.VirtualNetworkType{
+			VxlanNetworkIdentifier: vxlanNetworkID,
+		}
+
+		path := []string{
+			models.VirtualNetworkFieldVirtualNetworkProperties,
+			models.VirtualNetworkTypeFieldVxlanNetworkIdentifier,
+		}
+		updatePaths = append(updatePaths, strings.Join(path, "."))
+	}
+
+	_, err := sv.WriteService.UpdateVirtualNetwork(ctx, &services.UpdateVirtualNetworkRequest{
+		VirtualNetwork: updateVN,
+		FieldMask:      types.FieldMask{Paths: updatePaths},
+	})
+
+	return err
 }
