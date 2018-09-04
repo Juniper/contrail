f55931a61dcf13da8ea0e8b32a7af24d0f422639
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-09-04 14:57:36 +0200

    Logical Router IC: Create default Route Target for Logical Router and ref to it
    Closes-Bug: 1790833
    Change-Id: I7538da6997aa6bd530c96676dcab08177a014cd1

---
+++ pkg/compilation/logic/common.go
@@ -0,0 +1,26 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+func createDefaultRouteTarget(ctx context.Context, evaluateContext *EvaluateContext,
+) (*models.RouteTarget, error) {
+	rtKey := fmt.Sprintf("target:%v:%v", defaultAutonomousSystem, generateRandomRouteTargetNumber())
+
+	rtResponse, err := evaluateContext.WriteService.CreateRouteTarget(
+		ctx,
+		&services.CreateRouteTargetRequest{
+			RouteTarget: &models.RouteTarget{
+				FQName:      []string{rtKey},
+				DisplayName: rtKey,
+			},
+		},
+	)
+
+	return rtResponse.GetRouteTarget(), err
+}
