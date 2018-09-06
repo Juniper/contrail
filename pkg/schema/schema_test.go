86387cfe14f8ac8b3d1739d55b9c5ab34fe9cf40
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-09-06 18:11:30 +0200

    Fix handling enum arrays for BgpRouter AddressFamilies
    Adding methods to override schema
    
    Change-Id: I33144ca2de3fd1cabdbf197ba51d70754befecbf
    Closes-Bug: 1790463

--- pkg/schema/schema_test.go
+++ pkg/schema/schema_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestSchema(t *testing.T) {
-	api, err := MakeAPI([]string{"test_data/schema"})
+	api, err := MakeAPI([]string{"test_data/schema"}, "")
 	assert.Nil(t, err, "API reading failed")
 	assert.Equal(t, 4, len(api.Types))
 	assert.Equal(t, 4, len(api.Schemas))
@@ -26,6 +26,32 @@ func TestSchema(t *testing.T) {
 		virtualNetwork.JSONSchema.PropertiesOrder)
 	assert.Equal(t, 3, len(virtualNetwork.Columns))
 	assert.Equal(t, 1004, virtualNetwork.References["network_ipam"].Index)
+}
+
+func TestSchemaEnums(t *testing.T) {
+	api, err := MakeAPI([]string{"test_data/schema_enums"}, "overrides")
+	assert.Nil(t, err, "API reading failed")
+	project := api.SchemaByID("project")
+	assert.NotNil(t, project, "Project can't be <nil>")
+	obj := api.SchemaByID("simple_object")
+	assert.NotNil(t, obj, "SimpleObject can't be <nil>")
+	// In addition 'uuid' and 'display_name' are added (+2)
+	assert.Equal(t, 3+2, len(obj.JSONSchema.Properties))
+
+	assert.NotNil(t, api.Types)
+	assert.Equal(t, 4, len(api.Types))
+	enumArr, ok := api.Types["ObjectThatReferencesEnumAsArray"]
+	assert.True(t, ok)
+	checkPropertyRepeated(t, enumArr)
+	enumArrOvrd, ok := api.Types["ObjectThatReferencesEnumAsArrayOverriden"]
+	assert.True(t, ok)
+	checkPropertyRepeated(t, enumArrOvrd)
+}
+
+func checkPropertyRepeated(t *testing.T, obj *JSONSchema) {
+	assert.NotNil(t, obj)
+	assert.Equal(t, 1, len(obj.Properties))
+	assert.NotNil(t, obj.Properties[obj.OrderedProperties[0].ID].Items)
 }
 
 func TestReferenceTableName(t *testing.T) {
