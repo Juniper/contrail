5352625f7bcdc0b167ae88ffd3dfd3288d185a1a
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-09-06 18:11:30 +0200

    [WIP] Fix handling enum arrays for BgpRouter AddressFamilies
    Adding methods to override schema
    
    Change-Id: I33144ca2de3fd1cabdbf197ba51d70754befecbf
    Closes-Bug: 1790463

--- pkg/schema/schema_test.go
+++ pkg/schema/schema_test.go
@@ -6,8 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func findDefinitionByID(id string, api *API) *Schema {
+	for _, def := range api.Definitions {
+		if def.ID == id {
+			return def
+		}
+	}
+	return nil
+}
+
 func TestSchema(t *testing.T) {
-	api, err := MakeAPI([]string{"test_data/schema"})
+	api, err := MakeAPI([]string{"test_data/schema"}, "")
 	assert.Nil(t, err, "API reading failed")
 	assert.Equal(t, 4, len(api.Types))
 	assert.Equal(t, 4, len(api.Schemas))
@@ -24,6 +33,22 @@ func TestSchema(t *testing.T) {
 		virtualNetwork.JSONSchema.PropertiesOrder)
 	assert.Equal(t, 3, len(virtualNetwork.Columns))
 	assert.Equal(t, 1004, virtualNetwork.References["network_ipam"].Index)
+}
+
+func TestSchemaEnums(t *testing.T) {
+	api, err := MakeAPI([]string{"test_data/schema_enums"}, "overrides")
+	assert.Nil(t, err, "API reading failed")
+	for k, v := range api.Types {
+		t.Logf("Type:    #####> %v => %#v", k, v)
+	}
+	for _, def := range api.Definitions {
+		t.Logf("Def ---------> %#v\n----------->%#v", def, def.JSONSchema)
+	}
+	enumAsRef := api.Types["EnumTestObject"]
+	assert.Nil(t, enumAsRef)
+	t.Logf("enumm def schema: %#v", enumAsRef)
+	assert.Nil(t, enumAsRef.Enum)
+	assert.Equal(t, 3, len(enumAsRef.Enum))
 }
 
 func TestReferenceTableName(t *testing.T) {
