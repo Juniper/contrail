e2c27f626f62e8a98c600a03b9737818de44027d
Michał Błotniak <michal.blotniak@codilime.com>
2018-09-07 17:55:54 +0200

    Allocate int in IC using remote int-pool
    Change-Id: I9a16042028d0bb4ed088691e15fbaec62d643a46
    Partial-Bug: 1783506

--- pkg/compilation/logic/route_target.go
+++ pkg/compilation/logic/route_target.go
@@ -2,16 +2,26 @@ package logic
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 )
 
+// TODO: get_autonomous_system method
+const (
+	defaultAutonomousSystem = 64512
+	routeTargetIntPoolID    = "route_target_number"
+)
+
 func createDefaultRouteTarget(
 	ctx context.Context, evaluateContext *EvaluateContext,
 ) (*models.RouteTarget, error) {
-	rtKey := fmt.Sprintf("target:%v:%v", defaultAutonomousSystem, generateRandomRouteTargetNumber())
+	target, err := evaluateContext.IntPoolAllocator.AllocateInt(ctx, routeTargetIntPoolID)
+	if err != nil {
+		return nil, err
+	}
+
+	rtKey := models.RouteTargetString(defaultAutonomousSystem, target)
 
 	rtResponse, err := evaluateContext.WriteService.CreateRouteTarget(
 		ctx,
