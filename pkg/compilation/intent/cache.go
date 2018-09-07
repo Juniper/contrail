9aad5c1eb0dcf7a0f84180fe389f51033b3a2bc6
Józef Wołoch <jozef.woloch@codilime.com>
2018-09-07 13:15:09 +0200

    Intent Compiler: Refactor cache interface
    Change-Id: Ic0473cac5f39bb2ce6e7047c662954cac5f5f0f2
    Closes-bug: 1791285

---
+++ pkg/compilation/intent/cache.go
@@ -0,0 +1,83 @@
+package intent
+
+import (
+	"sync"
+
+	"github.com/iancoleman/strcase"
+	log "github.com/sirupsen/logrus"
+)
+
+// Cache cache for intents
+type Cache struct {
+	intentMap *intentMap
+}
+
+// NewCache creates new cache for intents.
+func NewCache() *Cache {
+	return &Cache{
+		intentMap: newIntentMap(),
+	}
+}
+
+func newIntentMap() *intentMap {
+	return &intentMap{
+		internal: make(map[string]map[string]Intent),
+	}
+}
+
+type intentMap struct {
+	internal map[string]map[string]Intent
+	sync.RWMutex
+}
+
+func (m *intentMap) loadTypeMap(typeName string) map[string]Intent {
+	objMap, ok := m.internal[typeName]
+	if !ok {
+		objMap = make(map[string]Intent)
+		m.internal[typeName] = objMap
+	}
+	return objMap
+}
+
+func (m *intentMap) load(typeName, uuid string) (value Intent, ok bool) {
+	m.RLock()
+	defer m.RUnlock()
+	typeMap := m.loadTypeMap(typeName)
+	intent, ok := typeMap[uuid]
+	return intent, ok
+}
+
+func (m *intentMap) delete(typeName, uuid string) {
+	m.Lock()
+	defer m.Unlock()
+	typeMap := m.loadTypeMap(typeName)
+	delete(typeMap, uuid)
+}
+
+func (m *intentMap) store(typeName, uuid string, intent Intent) {
+	m.Lock()
+	defer m.Unlock()
+	typeMap := m.loadTypeMap(typeName)
+	typeMap[uuid] = intent
+}
+
+// Load loads intent from cache. It accepts as type both kebab-case and CamelCase
+func (c *Cache) Load(typeName, uuid string) (Intent, bool) {
+	typeName = strcase.ToCamel(typeName)
+	log.WithFields(log.Fields{"TypeName": typeName, "UUID": uuid}).Debug("Loading from cache")
+	return c.intentMap.load(typeName, uuid)
+}
+
+// Store puts intent into cache.
+func (c *Cache) Store(i Intent) {
+	typeName := strcase.ToCamel(i.Kind())
+	log.WithFields(log.Fields{"TypeName": typeName, "UUID": i.GetUUID()}).Debug("Storing in cache")
+	c.intentMap.store(typeName, i.GetUUID(), i)
+}
+
+// Delete deletes intent from cache. It accepts as type both snake-case and CamelCase
+func (c *Cache) Delete(typeName, uuid string) {
+	typeName = strcase.ToCamel(typeName)
+	log.WithFields(log.Fields{"TypeName": typeName, "UUID": uuid}).Debug("Deleting from cache")
+	c.intentMap.delete(typeName, uuid)
+}
--- pkg/compilation/logic/service.go
+++ pkg/compilation/logic/service.go
@@ -1,17 +1,51 @@
 package logic
 
-import "github.com/Juniper/contrail/pkg/services"
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/compilation/intent"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/pkg/errors"
+)
 
 // Service implementing Intent Compiler's type-specific logic.
 type Service struct {
 	services.BaseService
 	// WriteService is used to create/update/delete lower-level resources
 	WriteService services.WriteService
+	cache        *intent.Cache
 }
 
 // NewService creates a Service
-func NewService(apiClient services.WriteService) *Service {
+func NewService(apiClient services.WriteService, cache *intent.Cache) *Service {
 	return &Service{
 		WriteService: apiClient,
+		cache:        cache,
 	}
+}
+
+// TODO use GetObject from Intent interface instead of passing r
+func (s *Service) handleCreate(
+	ctx context.Context,
+	i intent.Intent,
+	intentLogic func(ctx context.Context, ec *intent.EvaluateContext) error,
+	r services.Resource,
+) error {
+	s.cache.Store(i)
+
+	ec := &intent.EvaluateContext{
+		WriteService: s.WriteService,
+	}
+
+	if intentLogic != nil {
+		err := intentLogic(ctx, ec)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := s.EvaluateDependencies(ctx, ec, r); err != nil {
+		return errors.Wrap(err, "failed to evaluate Logical Router dependencies")
+	}
+	return nil
 }
