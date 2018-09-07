4ca3d55f85056d57e14d4dc99e6f58a4704905d5
Józef Wołoch <jozef.woloch@codilime.com>
2018-09-07 13:15:09 +0200

    Intent Compiler: Refactor cache interface
    Change-Id: Ic0473cac5f39bb2ce6e7047c662954cac5f5f0f2
    Closes-bug: 1791285

--- pkg/compilation/logic/security_group_test.go
+++ pkg/compilation/logic/security_group_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/Juniper/contrail/pkg/compilationif"
+	"github.com/Juniper/contrail/pkg/compilation/intent"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 	"github.com/Juniper/contrail/pkg/services/mock"
@@ -202,12 +202,12 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 	defer mockCtrl.Finish()
 
 	mockAPIClient := servicesmock.NewMockWriteService(mockCtrl)
-	service := NewService(mockAPIClient)
+	cache := intent.NewCache()
+	service := NewService(mockAPIClient, cache)
 
 	expectCreateACL(mockAPIClient, expectedIngressACL)
 	expectCreateACL(mockAPIClient, expectedEgressACL)
 
-	compilationif.Init()
 	_, err := service.CreateSecurityGroup(context.Background(), &services.CreateSecurityGroupRequest{
 		SecurityGroup: securityGroup,
 	})
