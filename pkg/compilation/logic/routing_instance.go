8966ccd05c71d8d0bb604d7ece144a073d7ccace
Józef Wołoch <jozef.woloch@codilime.com>
2018-09-10 18:50:35 +0200

    Intent Compiler: Resolve dependencies in cache
    Change-Id: I2fdf01373fc8a182a0238c487f57f1dd486ac272
    Closes-bug: 1791759

--- pkg/compilation/logic/routing_instance.go
+++ pkg/compilation/logic/routing_instance.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Juniper/contrail/pkg/compilation/intent"
 	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
 	"github.com/Juniper/contrail/pkg/services"
 )
 
@@ -14,6 +15,21 @@ import (
 type RoutingInstanceIntent struct {
 	intent.BaseIntent
 	*models.RoutingInstance
+}
+
+// GetObject returns embedded resource object
+func (i *RoutingInstanceIntent) GetObject() basemodels.Object {
+	return i.RoutingInstance
+}
+
+// LoadRoutingInstanceIntent loads RoutingInstanceIntent from cache
+func LoadRoutingInstanceIntent(
+	c *intent.Cache,
+	uuid string,
+) *RoutingInstanceIntent {
+	i := c.Load(models.KindRoutingInstance, intent.ByUUID(uuid))
+	actual, _ := i.(*RoutingInstanceIntent)
+	return actual
 }
 
 // TODO: get_autonomous_system method and int pool allocator endpoint.
--- pkg/compilation/logic/virtual_network.go
+++ pkg/compilation/logic/virtual_network.go
@@ -3,10 +3,26 @@ package logic
 import (
 	"github.com/Juniper/contrail/pkg/compilation/intent"
 	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
 )
 
 // VirtualNetworkIntent intent
 type VirtualNetworkIntent struct {
 	intent.BaseIntent
 	*models.VirtualNetwork
+}
+
+// GetObject returns embedded resource object
+func (i *VirtualNetworkIntent) GetObject() basemodels.Object {
+	return i.VirtualNetwork
+}
+
+// LoadVirtualNetworkIntent loads VirtualNetworkIntent from cache
+func LoadVirtualNetworkIntent(
+	c *intent.Cache,
+	uuid string,
+) *VirtualNetworkIntent {
+	i := c.Load(models.KindVirtualNetwork, intent.ByUUID(uuid))
+	actual, _ := i.(*VirtualNetworkIntent)
+	return actual
 }
