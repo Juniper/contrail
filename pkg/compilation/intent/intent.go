58e4d3384ea086e39767ff79993060a1c8ddd558
Kamil Braun <kamil.braun@codilime.com>
2018-09-10 16:17:10 +0200

    Enable retrieving intents by FQName from IC cache
    Partial-Bug: #1791703
    Change-Id: I1a52dd59e3da7e6dc607d9a4ecc018fea89171dc

--- pkg/compilation/intent/intent.go
+++ pkg/compilation/intent/intent.go
@@ -9,12 +9,19 @@ import (
 // EvaluateContext contains context information for Resource to handle CRUD
 type EvaluateContext struct {
 	WriteService services.WriteService
+	IntentLoader Loader
 }
 
 // Intent contains Intent Compiler state for a resource.
 type Intent interface {
 	services.Resource
 	Evaluate(ctx context.Context, evaluateCtx *EvaluateContext) error
+}
+
+// Loader provides access to existing intents (e.g. using a cache)
+type Loader interface {
+	LoadByFQName(typeName string, fqName []string) (Intent, bool)
+	Load(typeName, uuid string) (Intent, bool)
 }
 
 // BaseIntent implements the default Evaluate interface
