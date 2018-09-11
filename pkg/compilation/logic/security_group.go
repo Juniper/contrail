9d998a12b092d174d3f841d0ae782cacac550a58
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2018-09-11 14:27:58 +0200

    compilation/logic: Implement DeleteSecurityGroup
    Closes-Bug: 1791948
    Change-Id: Ia4eaf68a3bc9d1178d08859ffb806777a3246965

--- pkg/compilation/logic/security_group.go
+++ pkg/compilation/logic/security_group.go
@@ -2,9 +2,11 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
 
+	"github.com/Juniper/contrail/pkg/common"
 	"github.com/Juniper/contrail/pkg/compilation/intent"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
@@ -35,6 +37,57 @@ func (s *Service) CreateSecurityGroup(
 	return s.BaseService.CreateSecurityGroup(ctx, request)
 }
 
+// DeleteSecurityGroup evaluates SecurityGroup dependencies.
+func (s *Service) DeleteSecurityGroup(
+	ctx context.Context,
+	request *services.DeleteSecurityGroupRequest,
+) (*services.DeleteSecurityGroupResponse, error) {
+
+	i, ok := loadSecurityGroupIntent(s.cache, request.ID)
+	if !ok {
+		return nil, errors.New("failed to process SecurityGroup deletion: SecurityGroupIntent not found in cache")
+	}
+
+	err := deleteDefaultACLs(ctx, s.WriteService, i)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to process SecurityGroup deletion")
+	}
+
+	s.cache.Delete(models.KindSecurityGroup, i.GetUUID())
+
+	return s.BaseService.DeleteSecurityGroup(ctx, request)
+}
+
+func deleteDefaultACLs(ctx context.Context, writeService services.WriteService, i *SecurityGroupIntent) error {
+	var multiError common.MultiError
+
+	err := deleteAndUnsetACL(ctx, writeService, &i.ingressACL, "ingress")
+	if err != nil {
+		multiError = append(multiError, err)
+	}
+
+	err = deleteAndUnsetACL(ctx, writeService, &i.egressACL, "egress")
+	if err != nil {
+		multiError = append(multiError, err)
+	}
+
+	if multiError != nil {
+		return multiError
+	}
+	return nil
+}
+
+func deleteAndUnsetACL(
+	ctx context.Context, writeService services.WriteService, acl **models.AccessControlList, aclType string,
+) error {
+	if *acl == nil {
+		return nil
+	}
+
+	err := deleteACL(ctx, writeService, (*acl).GetUUID())
+	return errors.Wrap(err, fmt.Sprintf("failed to delete %s access control list", aclType))
+}
+
 // Evaluate Creates default AccessControlList's for the already created SecurityGroup.
 func (i *SecurityGroupIntent) Evaluate(ctx context.Context, ec *intent.EvaluateContext) error {
 	ingressACL, egressACL := i.DefaultACLs()
@@ -54,6 +107,15 @@ func (i *SecurityGroupIntent) Evaluate(ctx context.Context, ec *intent.EvaluateC
 	return nil
 }
 
+func loadSecurityGroupIntent(cache *intent.Cache, uuid string) (intent *SecurityGroupIntent, ok bool) {
+	i, ok := cache.Load(models.KindSecurityGroup, uuid)
+	if !ok {
+		return nil, false
+	}
+	intent, ok = i.(*SecurityGroupIntent)
+	return intent, ok
+}
+
 func createACL(
 	ctx context.Context, writeService services.WriteService, acl *models.AccessControlList,
 ) (*models.AccessControlList, error) {
@@ -62,4 +124,12 @@ func createACL(
 			AccessControlList: acl,
 		})
 	return response.GetAccessControlList(), err
+}
+
+func deleteACL(ctx context.Context, writeService services.WriteService, uuid string) error {
+	_, err := writeService.DeleteAccessControlList(
+		ctx, &services.DeleteAccessControlListRequest{
+			ID: uuid,
+		})
+	return err
 }
