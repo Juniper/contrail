8c2c6b911894acd4100cfa4a06472e9ad98c5306
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-09-11 10:00:04 +0200

    Create BGP Router with simple types logic validation
    Closes-Bug: 1791903
    Change-Id: I58cbc9142816a54266ac9a15c257a93f870dae1d

---
+++ pkg/types/bgp_router.go
@@ -0,0 +1,71 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/common"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateBGPRouter creates BGP Router and checks if provided asn
+// is equal to asn in linked SubCluster.
+func (sv *ContrailTypeLogicService) CreateBGPRouter(
+	ctx context.Context, request *services.CreateBGPRouterRequest,
+) (*services.CreateBGPRouterResponse, error) {
+
+	var response *services.CreateBGPRouterResponse
+	bgpRouter := request.GetBGPRouter()
+	err := sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			var err error
+			var asn int64
+
+			if bgpRouterParams := bgpRouter.GetBGPRouterParameters(); bgpRouterParams != nil {
+				asn = bgpRouterParams.GetAutonomousSystem()
+			}
+
+			if err = sv.checkSubClusterASN(ctx, bgpRouter, asn); err != nil {
+				return err
+			}
+
+			response, err = sv.BaseService.CreateBGPRouter(ctx, request)
+			return err
+		})
+
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) getSubCluster(
+	ctx context.Context, id string,
+) (*models.SubCluster, error) {
+
+	subClusterResponse, err := sv.ReadService.GetSubCluster(
+		ctx,
+		&services.GetSubClusterRequest{
+			ID: id,
+		},
+	)
+
+	return subClusterResponse.GetSubCluster(), err
+}
+
+func (sv *ContrailTypeLogicService) checkSubClusterASN(
+	ctx context.Context, bgpRouter *models.BGPRouter, asn int64,
+) error {
+
+	subClusterRefs := bgpRouter.GetSubClusterRefs()
+	if len(subClusterRefs) != 0 && asn != 0 {
+		subCluster, err := sv.getSubCluster(ctx, subClusterRefs[0].GetUUID())
+		if err != nil {
+			return err
+		}
+
+		if asn != subCluster.GetSubClusterAsn() {
+			return common.ErrorBadRequestf("subcluster asn and bgp asn should be same")
+		}
+	}
+
+	return nil
+}
