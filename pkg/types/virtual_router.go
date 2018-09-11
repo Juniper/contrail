c965898f6532dcacd8b3557d2ee81049674fcc57
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-09-11 15:06:09 +0200

    [WIP] Virtual router api server type logic
    Adding virtual router api servier type specific logic
    
    Partial-Bug: #1792106
    Change-Id: If8c55947f0ad5394c4f88812eb43688954766beb

---
+++ pkg/types/virtual_router.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateVirtualRouter virtual-router create specific logic.
+func (sv *ContrailTypeLogicService) CreateVirtualRouter(
+	ctx context.Context, request *services.CreateVirtualRouterRequest
+) (*services.CreateVirtualRouterResponse, error) {
+	return sv.BaseService.CreateVirtualRouter(ctx, request)
+}
+
+// UpdateVirtualRouter virtual-router update specific logic.
+func (sv *ContrailTypeLogicService) UpdateVirtualRouter(
+	ctx context.Context, request *services.UpdateVirtualRouterRequest
+) (*services.UpdateVirtualRouterResponse, error) {
+	return sv.BaseService.UpdateVirtualRouter(ctx, request)
+}
+
+// DeleteVirtualRouter virtual-router delete specific logic.
+func (sv *ContrailTypeLogicService) DeleteVirtualRouter(
+	ctx context.Context, request *services.DeleteVirtualRouterRequest
+) (*services.DeleteVirtualRouterResponse, error) {
+	return sv.BaseService.DeleteVirtualRouter(ctx, request)
+}
