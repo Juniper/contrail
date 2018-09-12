a48e3decb11d19a85a72f142e4178bbf848980a8
Michał Błotniak <michal.blotniak@codilime.com>
2018-09-12 18:00:35 +0200

    Enable contrail_integration_test
    Enabled test pkg/cmd/contrail/contrail_integration_test.go and fixed
    bugs. Added new AssertEqual function - $uuid - that ensures that field
    value is in correct uuid format.
    Extracted integrationetcd package to avoid circular dependencies.
    
    Change-Id: I5bf0ea2094b93589e2016849d010bf7050ac6dc3
    Partial-Bug: #1792177

--- pkg/compilation/service_test.go
+++ pkg/compilation/service_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/Juniper/contrail/pkg/compilation"
 	"github.com/Juniper/contrail/pkg/compilation/watch"
 	"github.com/Juniper/contrail/pkg/db/etcd"
-	"github.com/Juniper/contrail/pkg/testutil/integration"
+	"github.com/Juniper/contrail/pkg/testutil/integration/etcd"
 )
 
 const (
@@ -24,7 +24,7 @@ const (
 )
 
 func setTestConfig() {
-	viper.Set("etcd.endpoints", integration.EtcdEndpoint)
+	viper.Set("etcd.endpoints", integrationetcd.Endpoint)
 	viper.Set("etcd.path", testEtcdPath)
 	viper.Set("etcd.grpc_insecure", true)
 	viper.Set("compilation.msg_index_string", testMessageIndexString)
@@ -34,7 +34,7 @@ func setTestConfig() {
 }
 
 func TestIntentCompilationServiceHandlesMessage(t *testing.T) {
-	etcdClient := integration.NewEtcdClient(t)
+	etcdClient := integrationetcd.NewEtcdClient(t)
 	_, err := etcdClient.Delete(context.Background(), testMessageIndexString)
 	assert.NoError(t, err)
 
@@ -73,7 +73,7 @@ func TestIntentCompilationServiceHandlesMessage(t *testing.T) {
 }
 
 func TestIntentCompilationServiceConcurrency(t *testing.T) {
-	etcdClient := integration.NewEtcdClient(t)
+	etcdClient := integrationetcd.NewEtcdClient(t)
 	_, err := etcdClient.Delete(context.Background(), testMessageIndexString)
 	assert.NoError(t, err)
 
