adb56a4fa673fba8340fa7d60f62e68a2828ec51
Ignacy Osetek <ignacy.osetek@codilime.com>
2018-09-17 12:58:50 +0200

    compilation/logic: Implement UpdateSecurityGroup
    Updates IngressACL and EgressACL entries
    
    Change-Id: Icd1414e4858daa0069ecf201eeb4b357103c0c14
    Closes-Bug: 1792405

--- pkg/compilation/logic/security_group_test.go
+++ pkg/compilation/logic/security_group_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/gogo/protobuf/types"
 	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
@@ -17,30 +18,6 @@ import (
 )
 
 func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
-	allPermissions := &models.PermType{
-		Owner:       "cloud-admin",
-		Group:       "cloud-admin-group",
-		OwnerAccess: 7,
-		OtherAccess: 7,
-		GroupAccess: 7,
-	}
-	ownerOnlyPerms2 := &models.PermType2{
-		Owner:        "950b2912-a742-47c8-acdb-ab361f173867",
-		OwnerAccess:  7,
-		GlobalAccess: 0,
-	}
-
-	localAddresses := &models.AddressType{
-		SecurityGroup: "local",
-	}
-	securityGroupAddresses := &models.AddressType{
-		SecurityGroup: "default-domain:project-blue:default",
-	}
-	securityGroupIDAddresses := &models.AddressType{
-		SecurityGroup: "8000002",
-	}
-	noAddresses := &models.AddressType{}
-
 	// https://review.opencontrail.org/#/c/44499/19/pkg/compilation/testdata/security_group.yml
 	// with default values stripped.
 	securityGroup := &models.SecurityGroup{
@@ -54,22 +31,22 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 			Enable:      true,
 			Description: "Default security group",
 			UserVisible: true,
-			Permissions: allPermissions,
+			Permissions: allPermissions(),
 			UUID: &models.UuidType{
 				UUIDMslong: 4466401091260269000,
 				// Should really be -9223372036854776000, but that overflows int64
 				UUIDLslong: 0,
 			},
 		},
-		Perms2: ownerOnlyPerms2,
+		Perms2: ownerOnlyPerms2(),
 		SecurityGroupEntries: &models.PolicyEntriesType{PolicyRule: []*models.PolicyRuleType{
 			{
 				Direction:    ">",
 				Protocol:     "any",
 				RuleUUID:     "bdf042c0-d2c2-4241-ba15-1c702c896e03",
 				Ethertype:    "IPv4",
-				SRCAddresses: []*models.AddressType{securityGroupAddresses},
-				DSTAddresses: []*models.AddressType{localAddresses},
+				SRCAddresses: []*models.AddressType{securityGroupAddresses()},
+				DSTAddresses: []*models.AddressType{localAddresses()},
 				SRCPorts:     []*models.PortType{models.AllPorts()},
 				DSTPorts:     []*models.PortType{models.AllPorts()},
 				ActionList: &models.ActionListType{
@@ -81,8 +58,8 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 				Protocol:     "any",
 				RuleUUID:     "1f77914a-0863-4f0d-888a-aee6a1988f6a",
 				Ethertype:    "IPv6",
-				SRCAddresses: []*models.AddressType{securityGroupAddresses},
-				DSTAddresses: []*models.AddressType{localAddresses},
+				SRCAddresses: []*models.AddressType{securityGroupAddresses()},
+				DSTAddresses: []*models.AddressType{localAddresses()},
 				SRCPorts:     []*models.PortType{models.AllPorts()},
 				DSTPorts:     []*models.PortType{models.AllPorts()},
 				ActionList: &models.ActionListType{
@@ -94,7 +71,7 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 				Protocol:     "any",
 				RuleUUID:     "b7c07625-e03e-43b9-a9fc-d11a6c863cc6",
 				Ethertype:    "IPv4",
-				SRCAddresses: []*models.AddressType{localAddresses},
+				SRCAddresses: []*models.AddressType{localAddresses()},
 				DSTAddresses: []*models.AddressType{models.AllIPv4Addresses()},
 				SRCPorts:     []*models.PortType{models.AllPorts()},
 				DSTPorts:     []*models.PortType{models.AllPorts()},
@@ -107,7 +84,7 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 				Protocol:     "any",
 				RuleUUID:     "6a5f3026-02bc-4ba1-abde-39abafd21f47",
 				Ethertype:    "IPv6",
-				SRCAddresses: []*models.AddressType{localAddresses},
+				SRCAddresses: []*models.AddressType{localAddresses()},
 				DSTAddresses: []*models.AddressType{models.AllIPv6Addresses()},
 				SRCPorts:     []*models.PortType{models.AllPorts()},
 				DSTPorts:     []*models.PortType{models.AllPorts()},
@@ -134,8 +111,8 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 						DSTPort:    models.AllPorts(),
 						Protocol:   "any",
 						Ethertype:  "IPv4",
-						SRCAddress: securityGroupIDAddresses,
-						DSTAddress: noAddresses,
+						SRCAddress: securityGroupIDAddresses("8000002"),
+						DSTAddress: noAddresses(),
 					},
 					ActionList: &models.ActionListType{
 						SimpleAction: "pass",
@@ -148,8 +125,8 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 						DSTPort:    models.AllPorts(),
 						Protocol:   "any",
 						Ethertype:  "IPv6",
-						SRCAddress: securityGroupIDAddresses,
-						DSTAddress: noAddresses,
+						SRCAddress: securityGroupIDAddresses("8000002"),
+						DSTAddress: noAddresses(),
 					},
 					ActionList: &models.ActionListType{
 						SimpleAction: "pass",
@@ -175,7 +152,7 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 						DSTPort:    models.AllPorts(),
 						Protocol:   "any",
 						Ethertype:  "IPv4",
-						SRCAddress: noAddresses,
+						SRCAddress: noAddresses(),
 						DSTAddress: models.AllIPv4Addresses(),
 					},
 					ActionList: &models.ActionListType{
@@ -189,7 +166,7 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 						DSTPort:    models.AllPorts(),
 						Protocol:   "any",
 						Ethertype:  "IPv6",
-						SRCAddress: noAddresses,
+						SRCAddress: noAddresses(),
 						DSTAddress: models.AllIPv6Addresses(),
 					},
 					ActionList: &models.ActionListType{
@@ -224,6 +201,175 @@ func TestCreateSecurityGroupCreatesACLs(t *testing.T) {
 			egressACL:     expectedEgressACL,
 		},
 		loadSecurityGroupIntent(cache, intent.ByUUID(securityGroup.GetUUID())))
+}
+
+func TestSecurityGroupUpdate(t *testing.T) {
+	ingressACLAfterCreate := &models.AccessControlList{
+		Name:       "ingress-access-control-list",
+		ParentType: "security-group",
+		ParentUUID: "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		AccessControlListEntries: &models.AclEntriesType{
+			Dynamic: false,
+			ACLRule: []*models.AclRuleType{
+				{
+					RuleUUID: "bdf042c0-d2c2-4241-ba15-1c702c896e03",
+					MatchCondition: &models.MatchConditionType{
+						SRCPort:    models.AllPorts(),
+						DSTPort:    models.AllPorts(),
+						Protocol:   "any",
+						Ethertype:  "IPv4",
+						SRCAddress: securityGroupIDAddresses("8000002"),
+						DSTAddress: noAddresses(),
+					},
+					ActionList: &models.ActionListType{
+						SimpleAction: "pass",
+					},
+				},
+			},
+		},
+	}
+
+	egressACLAfterCreate := &models.AccessControlList{
+		Name:       "egress-access-control-list",
+		ParentType: "security-group",
+		ParentUUID: "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		AccessControlListEntries: &models.AclEntriesType{
+			Dynamic: false,
+		},
+	}
+
+	ingressACLAfterUpdate := &models.AccessControlList{
+		Name:       "ingress-access-control-list",
+		ParentType: "security-group",
+		ParentUUID: "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		AccessControlListEntries: &models.AclEntriesType{
+			Dynamic: false,
+		},
+	}
+
+	egressACLAfterUpdate := &models.AccessControlList{
+		Name:       "egress-access-control-list",
+		ParentType: "security-group",
+		ParentUUID: "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		AccessControlListEntries: &models.AclEntriesType{
+			Dynamic: false,
+			ACLRule: []*models.AclRuleType{
+				{
+					RuleUUID: "b7c07625-e03e-43b9-a9fc-d11a6c863cc6",
+					MatchCondition: &models.MatchConditionType{
+						SRCPort:    models.AllPorts(),
+						DSTPort:    models.AllPorts(),
+						Protocol:   "any",
+						Ethertype:  "IPv4",
+						SRCAddress: noAddresses(),
+						DSTAddress: models.AllIPv4Addresses(),
+					},
+					ActionList: &models.ActionListType{
+						SimpleAction: "pass",
+					},
+				},
+			},
+		},
+	}
+
+	creSecurityGroup := &models.SecurityGroup{
+		UUID:            "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		ParentType:      "project",
+		ParentUUID:      "950b2912-a742-47c8-acdb-ab361f173867",
+		FQName:          []string{"default-domain", "project-blue", "default"},
+		DisplayName:     "default",
+		SecurityGroupID: 8000002,
+		IDPerms: &models.IdPermsType{
+			Enable:      true,
+			Description: "Default security group",
+			UserVisible: true,
+			Permissions: allPermissions(),
+			UUID: &models.UuidType{
+				UUIDMslong: 4466401091260269000,
+				// Should really be -9223372036854776000, but that overflows int64
+				UUIDLslong: 0,
+			},
+		},
+		Perms2: ownerOnlyPerms2(),
+		SecurityGroupEntries: &models.PolicyEntriesType{PolicyRule: []*models.PolicyRuleType{
+			{
+				Direction:    ">",
+				Protocol:     "any",
+				RuleUUID:     "bdf042c0-d2c2-4241-ba15-1c702c896e03",
+				Ethertype:    "IPv4",
+				SRCAddresses: []*models.AddressType{securityGroupAddresses()},
+				DSTAddresses: []*models.AddressType{localAddresses()},
+				SRCPorts:     []*models.PortType{models.AllPorts()},
+				DSTPorts:     []*models.PortType{models.AllPorts()},
+				ActionList: &models.ActionListType{
+					SimpleAction: "pass",
+				},
+			},
+		}},
+	}
+
+	updSecurityGroup := &models.SecurityGroup{
+		UUID: "3dfbd820-e4fc-414f-b1d9-d720ebe93cd8",
+		SecurityGroupEntries: &models.PolicyEntriesType{PolicyRule: []*models.PolicyRuleType{
+			{
+				Direction:    ">",
+				Protocol:     "any",
+				RuleUUID:     "b7c07625-e03e-43b9-a9fc-d11a6c863cc6",
+				Ethertype:    "IPv4",
+				SRCAddresses: []*models.AddressType{localAddresses()},
+				DSTAddresses: []*models.AddressType{models.AllIPv4Addresses()},
+				SRCPorts:     []*models.PortType{models.AllPorts()},
+				DSTPorts:     []*models.PortType{models.AllPorts()},
+				ActionList: &models.ActionListType{
+					SimpleAction: "pass",
+				},
+			},
+		}},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockAPIClient := servicesmock.NewMockWriteService(mockCtrl)
+	mockIntPoolAllocator := typesmock.NewMockIntPoolAllocator(mockCtrl)
+	cache := intent.NewCache()
+
+	service := NewService(mockAPIClient, mockIntPoolAllocator, cache)
+
+	expectCreateACL(mockAPIClient, ingressACLAfterCreate)
+	expectCreateACL(mockAPIClient, egressACLAfterCreate)
+	expectUpdateACL(mockAPIClient, ingressACLAfterUpdate)
+	expectUpdateACL(mockAPIClient, egressACLAfterUpdate)
+
+	_, err := service.CreateSecurityGroup(context.Background(), &services.CreateSecurityGroupRequest{
+		SecurityGroup: creSecurityGroup,
+	})
+
+	assert.Equal(t,
+		&SecurityGroupIntent{
+			SecurityGroup: creSecurityGroup,
+			ingressACL:    ingressACLAfterCreate,
+			egressACL:     egressACLAfterCreate,
+		},
+		loadSecurityGroupIntent(cache, intent.ByUUID(creSecurityGroup.GetUUID())),
+	)
+
+	assert.NoError(t, err)
+
+	_, err = service.UpdateSecurityGroup(context.Background(), &services.UpdateSecurityGroupRequest{
+		SecurityGroup: updSecurityGroup,
+	})
+
+	assert.Equal(t,
+		&SecurityGroupIntent{
+			SecurityGroup: updSecurityGroup,
+			ingressACL:    ingressACLAfterUpdate,
+			egressACL:     egressACLAfterUpdate,
+		},
+		loadSecurityGroupIntent(cache, intent.ByUUID(updSecurityGroup.GetUUID())),
+	)
+
+	assert.NoError(t, err)
 }
 
 func TestSecurityGroupDelete(t *testing.T) {
@@ -349,10 +495,42 @@ func TestLoadSecurityGroupIntent(t *testing.T) {
 	assert.Equal(t, expectedIntent, actualIntent)
 }
 
+func TestUpdateSecurityGroupIntent(t *testing.T) {
+	newIntent := &SecurityGroupIntent{
+		SecurityGroup: &models.SecurityGroup{UUID: "a", DisplayName: "before"},
+	}
+
+	cache := intent.NewCache()
+	cache.Store(newIntent)
+
+	loadedIntent := loadSecurityGroupIntent(cache, intent.ByUUID(newIntent.GetUUID()))
+	assert.NotNil(t, loadedIntent)
+	loadedIntent.DisplayName = "after"
+
+	assert.Equal(t, "after", newIntent.DisplayName)
+
+	expectedUpdatedIntent := &SecurityGroupIntent{
+		SecurityGroup: &models.SecurityGroup{UUID: "a", DisplayName: "after"},
+	}
+
+	actualIntent := loadSecurityGroupIntent(cache, intent.ByUUID(expectedUpdatedIntent.GetUUID()))
+	assert.NotNil(t, actualIntent)
+	assert.Equal(t, expectedUpdatedIntent, actualIntent)
+}
+
 func expectCreateACL(mockAPIClient *servicesmock.MockWriteService, expectedACL *models.AccessControlList) {
 	mockAPIClient.EXPECT().CreateAccessControlList(testutil.NotNil(), &services.CreateAccessControlListRequest{
 		AccessControlList: expectedACL,
 	}).Return(&services.CreateAccessControlListResponse{
+		AccessControlList: expectedACL,
+	}, nil).Times(1)
+}
+
+func expectUpdateACL(mockAPIClient *servicesmock.MockWriteService, expectedACL *models.AccessControlList) {
+	mockAPIClient.EXPECT().UpdateAccessControlList(testutil.NotNil(), &services.UpdateAccessControlListRequest{
+		AccessControlList: expectedACL,
+		FieldMask:         types.FieldMask{Paths: []string{models.AccessControlListFieldAccessControlListEntries}},
+	}).Return(&services.UpdateAccessControlListResponse{
 		AccessControlList: expectedACL,
 	}, nil).Times(1)
 }
@@ -369,4 +547,44 @@ func expectDeleteACLFailure(mockAPIClient *servicesmock.MockWriteService, expect
 	mockAPIClient.EXPECT().DeleteAccessControlList(testutil.NotNil(), &services.DeleteAccessControlListRequest{
 		ID: expectedUUID,
 	}).Return(nil, errors.New("failed to delete the ACL for some reason")).Times(1)
+}
+
+func allPermissions() *models.PermType {
+	return &models.PermType{
+		Owner:       "cloud-admin",
+		Group:       "cloud-admin-group",
+		OwnerAccess: 7,
+		OtherAccess: 7,
+		GroupAccess: 7,
+	}
+}
+
+func ownerOnlyPerms2() *models.PermType2 {
+	return &models.PermType2{
+		Owner:        "950b2912-a742-47c8-acdb-ab361f173867",
+		OwnerAccess:  7,
+		GlobalAccess: 0,
+	}
+}
+
+func localAddresses() *models.AddressType {
+	return &models.AddressType{
+		SecurityGroup: "local",
+	}
+}
+
+func securityGroupAddresses() *models.AddressType {
+	return &models.AddressType{
+		SecurityGroup: "default-domain:project-blue:default",
+	}
+}
+
+func securityGroupIDAddresses(id string) *models.AddressType {
+	return &models.AddressType{
+		SecurityGroup: id,
+	}
+}
+
+func noAddresses() *models.AddressType {
+	return &models.AddressType{}
 }
