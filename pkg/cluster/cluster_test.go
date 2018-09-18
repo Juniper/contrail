06655c80d2865688e6f8e0e53efbf18c02bc428e
zcui <zcui@juniper.net>
2018-09-18 10:11:01 -0700

    Support appformix deploy with contrail cli
    UT code
    
    Change-Id: I554629be21ff5d63656f272fcf2b30d0e2d94274
    Closes-bug: 1791775

--- pkg/cluster/cluster_test.go
+++ pkg/cluster/cluster_test.go
@@ -19,7 +19,9 @@ import (
 const (
 	allInOneClusterTemplatePath           = "./test_data/test_all_in_one_cluster.tmpl"
 	createPlaybooks                       = "./test_data/expected_ansible_create_playbook.yml"
+	createAppformixPlaybooks              = "./test_data/expected_ansible_create_appformix_playbook.yml"
 	updatePlaybooks                       = "./test_data/expected_ansible_update_playbook.yml"
+	updateAppformixPlaybooks              = "./test_data/expected_ansible_update_appformix_playbook.yml"
 	upgradePlaybooks                      = "./test_data/expected_ansible_upgrade_playbook.yml"
 	addComputePlaybooks                   = "./test_data/expected_ansible_add_compute_playbook.yml"
 	addCSNPlaybooks                       = "./test_data/expected_ansible_add_csn_playbook.yml"
@@ -29,6 +31,7 @@ const (
 	addComputeEncryptPlaybooks            = "./test_data/expected_ansible_add_compute_encrypt_playbook.yml"
 	allInOneKubernetesClusterTemplatePath = "./test_data/test_all_in_one_kubernetes_cluster.tmpl"
 	upgradePlaybooksKubernetes            = "./test_data/expected_ansible_upgrade_playbook_kubernetes.yml"
+	allInOneClusterWithAppformixTemplatePath = "./test_data/test_all_in_one_with_appformix.tmpl"
 	clusterID                             = "test_cluster_uuid"
 )
 
@@ -315,6 +318,125 @@ func runClusterTest(t *testing.T, expectedInstance, expectedInventory string,
 	assert.True(t, verifyClusterDeleted(), "Instance file is not deleted during cluster delete")
 }
 
+// nolint: gocyclo
+func runAppformixClusterTest(t *testing.T, expectedInstance, expectedInventory string,
+	context map[string]interface{}, expectedEndpoints map[string]string) {
+	// mock keystone to let access server after cluster create
+	keystoneAuthURL := viper.GetString("keystone.authurl")
+	ksPublic := apisrv.MockServerWithKeystone("127.0.0.1:35357", keystoneAuthURL)
+	defer ksPublic.Close()
+	ksPrivate := apisrv.MockServerWithKeystone("127.0.0.1:5000", keystoneAuthURL)
+	defer ksPrivate.Close()
+
+	// Create the cluster and related objects
+	var testScenario apisrv.TestScenario
+	err := apisrv.LoadTestScenario(&testScenario, allInOneClusterWithAppformixTemplatePath, context)
+	assert.NoError(t, err, "failed to load cluster test data")
+	cleanup := apisrv.RunDirtyTestScenario(t, &testScenario)
+	defer cleanup()
+	// create cluster config
+	config := &Config{
+		ID:           "alice",
+		Password:     "alice_password",
+		ProjectID:    "admin",
+		AuthURL:      apisrv.TestServer.URL + "/keystone/v3",
+		Endpoint:     apisrv.TestServer.URL,
+		InSecure:     true,
+		ClusterID:    clusterID,
+		Action:       "create",
+		LogLevel:     "debug",
+		TemplateRoot: "configs/",
+		Test:         true,
+	}
+	// create cluster
+	if _, err = os.Stat(executedPlaybooksPath()); err == nil {
+		// cleanup old executed playbook file
+		err = os.Remove(executedPlaybooksPath())
+		if err != nil {
+			assert.NoError(t, err, "failed to delete executed ansible playbooks yaml")
+		}
+	}
+	clusterManager, err := NewCluster(config)
+	assert.NoError(t, err, "failed to create cluster manager to create cluster")
+	err = clusterManager.Manage()
+	assert.NoError(t, err, "failed to manage(create) cluster")
+	assert.True(t, compareGeneratedInstances(t, expectedInstance),
+		"Instance file created during cluster create is not as expected")
+	if expectedInventory != "" {
+		assert.True(t, compareGeneratedInventory(t, expectedInventory),
+			"Inventory file created during cluster create is not as expected")
+		assert.True(t, verifyPlaybooks(t, createEncryptPlaybooks),
+			"Expected list of create playbooks are not executed")
+	} else {
+		assert.True(t, verifyPlaybooks(t, createAppformixPlaybooks),
+			"Expected list of create playbooks are not executed")
+	}
+	// Wait for the in-memory endpoint cache to get updated
+	apisrv.APIServer.ForceProxyUpdate()
+	// make sure all endpoints are created
+	err = verifyEndpoints(t, &testScenario, expectedEndpoints)
+	if err != nil {
+		assert.NoError(t, err, err.Error())
+	}
+
+	// update cluster
+	config.Action = "update"
+	// remove instances.yml to triger cluster update
+	err = os.Remove(generatedInstancesPath())
+	if err != nil {
+		assert.NoError(t, err, "failed to delete instances.yml")
+	}
+	if _, err = os.Stat(executedPlaybooksPath()); err == nil {
+		// cleanup executed playbook file
+		err = os.Remove(executedPlaybooksPath())
+		if err != nil {
+			assert.NoError(t, err, "failed to delete executed ansible playbooks yaml")
+		}
+	}
+	clusterManager, err = NewCluster(config)
+	assert.NoError(t, err, "failed to create cluster manager to update cluster")
+	err = clusterManager.Manage()
+	assert.NoError(t, err, "failed to manage(update) cluster")
+	assert.True(t, compareGeneratedInstances(t, expectedInstance),
+		"Instance file created during cluster update is not as expected")
+	if expectedInventory != "" {
+		assert.True(t, compareGeneratedInventory(t, expectedInventory),
+			"Inventory file created during cluster update is not as expected")
+		assert.True(t, verifyPlaybooks(t, updateEncryptPlaybooks),
+			"Expected list of update playbooks are not executed")
+	} else {
+		assert.True(t, verifyPlaybooks(t, updateAppformixPlaybooks),
+			"Expected list of update playbooks are not executed")
+	}
+	// Wait for the in-memory endpoint cache to get updated
+	apisrv.APIServer.ForceProxyUpdate()
+	// make sure all endpoints are recreated as part of update
+	err = verifyEndpoints(t, &testScenario, expectedEndpoints)
+	if err != nil {
+		assert.NoError(t, err, err.Error())
+	}
+
+	// IMPORT test (expected to create endpoints without triggering playbooks)
+	runClusterActionTest(t, testScenario, config,
+		"IMPORT", "", "", "", expectedEndpoints)
+
+	// delete cluster
+	config.Action = "delete"
+	if _, err = os.Stat(executedPlaybooksPath()); err == nil {
+		// cleanup executed playbook file
+		err = os.Remove(executedPlaybooksPath())
+		if err != nil {
+			assert.NoError(t, err, "failed to delete executed ansible playbooks yaml")
+		}
+	}
+	clusterManager, err = NewCluster(config)
+	assert.NoError(t, err, "failed to create cluster manager to delete cluster")
+	err = clusterManager.Manage()
+	assert.NoError(t, err, "failed to manage(delete) cluster")
+	// make sure cluster is removed
+	assert.True(t, verifyClusterDeleted(), "Instance file is not deleted during cluster delete")
+}
+
 func runAllInOneClusterTest(t *testing.T, computeType string) {
 	context := pongo2.Context{
 		"TYPE":            computeType,
@@ -343,6 +465,34 @@ func runAllInOneClusterTest(t *testing.T, computeType string) {
 	runClusterTest(t, expectedInstances, "", context, expectedEndpoints)
 }
 
+func runAllInOneWithAppformixTest(t *testing.T, computeType string) {
+	context := pongo2.Context{
+		"TYPE":            computeType,
+		"MGMT_INT_IP":     "127.0.0.1",
+		"CONTROL_NODES":   "",
+		"OPENSTACK_NODES": "",
+	}
+	expectedEndpoints := map[string]string{
+		"config":    "http://127.0.0.1:8082",
+		"nodejs":    "https://127.0.0.1:8143",
+		"telemetry": "http://127.0.0.1:8081",
+		"baremetal": "http://127.0.0.1:6385",
+		"swift":     "http://127.0.0.1:8080",
+		"glance":    "http://127.0.0.1:9292",
+		"compute":   "http://127.0.0.1:8774",
+		"keystone":  "http://127.0.0.1:5000",
+	}
+	expectedInstances := "./test_data/expected_all_in_one_with_appformix.yml"
+	switch computeType {
+	case "dpdk":
+		expectedInstances = "./test_data/expected_all_in_one_dpdk_instances.yml"
+	case "sriov":
+		expectedInstances = "./test_data/expected_all_in_one_sriov_instances.yml"
+	}
+
+	runAppformixClusterTest(t, expectedInstances, "", context, expectedEndpoints)
+}
+
 func TestAllInOneCluster(t *testing.T) {
 	runAllInOneClusterTest(t, "kernel")
 }
@@ -352,6 +502,9 @@ func TestAllInOneDpdkCluster(t *testing.T) {
 
 func TestAllInOneSriovCluster(t *testing.T) {
 	runAllInOneClusterTest(t, "sriov")
+}
+func TestAllInOneWitAppformix(t *testing.T) {
+	runAllInOneWithAppformixTest(t, "kernel")
 }
 
 func TestAllInOneClusterWithDatapathEncryption(t *testing.T) {
