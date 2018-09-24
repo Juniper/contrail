520ccad55050a6a73d30ac349ee7059745451840
Michał Błotniak <michal.blotniak@codilime.com>
2018-09-24 17:26:41 +0200

    Move apisrv tests to integration package
    Since all tests in apisrv deploy a running apisrv instance and yaml
    tests are capable of testing IC produced objects, the tests were
    moved to integration package.
    
    Change-Id: I1509ef7f1ca6adb3778413ba9a9a48809001ed6b
    Partial-Bug: #1792177

--- pkg/db/etcd/concurrency_test.go
+++ pkg/db/etcd/concurrency_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/Juniper/contrail/pkg/db/etcd"
-	"github.com/Juniper/contrail/pkg/testutil/integration/etcd"
+	"github.com/Juniper/contrail/pkg/integration/etcd"
 )
 
 const (
--- pkg/integration/main_test.go
+++ pkg/integration/main_test.go
@@ -1,4 +1,4 @@
-package apisrv
+package integration
 
 import (
 	"testing"
