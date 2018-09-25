b9ab97291682f2b81aea8e21973caea62e18b20b
Ignatious Johnson Christopher <ijohnson@juniper.net>
2018-09-25 23:55:18 +0000

    Support three auth types
    1. no-auth - api-server can be accessed without user/password
    2. basic-auth - * api-server is accessed with default scoped token.
                    *project scoped/domain scoped token not supported in
                     this mode.
                    *Even is client requests scoped token, local keystone
                     responds with unscoped token.
    3. keystone - Native keystone (scoped tokens required to access the
    api-server.)
    
    these types can be configured using auth_type variable in the config
    file
    
    Closes-Bug: 1790699
    
    Change-Id: If27b4ff8d83458ac72af72ba3e3ed6780ad6c32e

--- pkg/apisrv/keystone/assignment.go
+++ pkg/apisrv/keystone/assignment.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 
 	"github.com/Juniper/contrail/pkg/common"
+	kscommon "github.com/Juniper/contrail/pkg/keystone"
 )
 
 //Assignment is used to manage domain, project and user information.
 type Assignment interface {
-	FetchUser(id, password string) (*User, error)
-	ListProjects() []*Project
+	FetchUser(id, password string) (*kscommon.User, error)
+	ListProjects() []*kscommon.Project
 }
 
 //StaticAssignment is an implementation of Assignment based on static file.
 type StaticAssignment struct {
-	Domains  map[string]*Domain  `json:"domains"`
-	Projects map[string]*Project `json:"projects"`
-	Users    map[string]*User    `json:"users"`
+	Domains  map[string]*kscommon.Domain  `json:"domains"`
+	Projects map[string]*kscommon.Project `json:"projects"`
+	Users    map[string]*kscommon.User    `json:"users"`
 }
 
 //FetchUser is used to fetch a user by ID and Password.
-func (assignment *StaticAssignment) FetchUser(name, password string) (*User, error) {
+func (assignment *StaticAssignment) FetchUser(name, password string) (*kscommon.User, error) {
 	user, ok := assignment.Users[name]
 	if !ok {
 		return nil, fmt.Errorf("user %s not found", name)
@@ -32,8 +33,8 @@ func (assignment *StaticAssignment) FetchUser(name, password string) (*User, err
 }
 
 //ListProjects is used to list projects
-func (assignment *StaticAssignment) ListProjects() []*Project {
-	projects := []*Project{}
+func (assignment *StaticAssignment) ListProjects() []*kscommon.Project {
+	projects := []*kscommon.Project{}
 	for _, project := range assignment.Projects {
 		projects = append(projects, project)
 	}
