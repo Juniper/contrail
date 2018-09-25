f5a8d5dd8277f9e6629de563e5a68f36a57f3a2a
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-09-25 10:20:29 +0200

    Implement firewall rule type logic
    - added basic properties validation
    - setting protocol_id and tag_ids
    - added provisioning test cases
    
    Change-Id: I16f723efa30caa1a51c8f37e94348c13192aa31f
    Closes-bug: 1794253

---
+++ pkg/types/context.go
@@ -0,0 +1,27 @@
+package types
+
+import (
+	"context"
+)
+
+type key string
+
+const (
+	// InternalRequestKey used in context as additional propetry
+	InternalRequestKey key = "isInternal"
+)
+
+// MakeInternalRequestContext creates child context with additional information
+// that this context is for internal requests
+func MakeInternalRequestContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, InternalRequestKey, true)
+}
+
+// IsInternalRequest checks if context is for internal request
+func IsInternalRequest(ctx context.Context) bool {
+	if v := ctx.Value(InternalRequestKey); v != nil {
+		return v.(bool)
+	}
+
+	return false
+}
---
+++ pkg/types/draft_mode.go
@@ -0,0 +1,30 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/common"
+)
+
+type draftModeStateGetter interface {
+	GetDraftModeState() string
+}
+
+// DraftModeStateChecker checks if request contains draftModeState property
+type DraftModeStateChecker interface {
+	CheckDraftModeState(context.Context, draftModeStateGetter) error
+}
+
+func checkDraftModeState(ctx context.Context, dms draftModeStateGetter) error {
+	if IsInternalRequest(ctx) {
+		return nil
+	}
+
+	if dms.GetDraftModeState() != "" {
+		return common.ErrorBadRequest(
+			"security resource property 'draft_mode_state' is only readable",
+		)
+	}
+
+	return nil
+}
