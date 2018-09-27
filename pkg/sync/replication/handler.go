3028445b43945eceac525f40621929048c4175b7
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-09-27 15:02:39 +0200

    [WIP] Handling ref_ tables in Sync
    Change-Id: Ie0c3038c43dc48b3f9d02d3d0dfba956c4184556
    Closes-Bug: 1794740

--- pkg/sync/replication/handler.go
+++ pkg/sync/replication/handler.go
@@ -2,11 +2,11 @@ package replication
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	pkglog "github.com/Juniper/contrail/pkg/log"
 	"github.com/kyleconroy/pgoutput"
+	"github.com/pkg/errors"
 	"github.com/siddontang/go-mysql/canal"
 	"github.com/siddontang/go-mysql/mysql"
 	"github.com/siddontang/go-mysql/replication"
@@ -166,49 +166,44 @@ func (h *CanalEventHandler) OnRow(e *canal.RowsEvent) error {
 	}
 }
 
-// TODO(daniel): remove duplication
-// nolint: dupl
-func (h *CanalEventHandler) handleCreate(ctx context.Context, rows [][]interface{}, t *schema.Table) error {
+type handlerFunc func(context.Context, string, string, map[string]interface{}) error
+
+func (h *CanalEventHandler) handleOperation(
+	ctx context.Context,
+	rows [][]interface{},
+	t *schema.Table,
+	hndl handlerFunc,
+) error {
 	for _, row := range rows {
 		pk, err := getPrimaryKeyValue(row, t)
 		if err != nil {
-			return err
+			return errors.Wrapf(err, "table %v", t.Name)
 		}
 		kvs, err := getKeyValues(row, t.Columns)
 		if err != nil {
 			return fmt.Errorf("table %s error: %s", t, err)
 		}
-		if err := h.sink.Create(ctx, t.Name, pk, kvs); err != nil {
+		if err := hndl(ctx, t.Name, pk, kvs); err != nil {
 			return err
 		}
 	}
 	return nil
+
 }
 
-// TODO(daniel): remove duplication
-// nolint: dupl
+func (h *CanalEventHandler) handleCreate(ctx context.Context, rows [][]interface{}, t *schema.Table) error {
+	return errors.Wrap(h.handleOperation(ctx, rows, t, h.sink.Create), "operation CREATE")
+}
+
 func (h *CanalEventHandler) handleUpdate(ctx context.Context, rows [][]interface{}, t *schema.Table) error {
-	for _, row := range rows {
-		pk, err := getPrimaryKeyValue(row, t)
-		if err != nil {
-			return err
-		}
-		kvs, err := getKeyValues(row, t.Columns)
-		if err != nil {
-			return fmt.Errorf("table %s error: %s", t, err)
-		}
-		if err := h.sink.Update(ctx, t.Name, pk, kvs); err != nil {
-			return err
-		}
-	}
-	return nil
+	return errors.Wrap(h.handleOperation(ctx, rows, t, h.sink.Update), "operation UPDATE")
 }
 
 func (h *CanalEventHandler) handleDelete(ctx context.Context, rows [][]interface{}, t *schema.Table) error {
 	for _, row := range rows {
 		pk, err := getPrimaryKeyValue(row, t)
 		if err != nil {
-			return err
+			return errors.Wrapf(err, "delete from %v", t.Name)
 		}
 
 		if err := h.sink.Delete(ctx, t.Name, pk); err != nil {
@@ -218,31 +213,54 @@ func (h *CanalEventHandler) handleDelete(ctx context.Context, rows [][]interface
 	return nil
 }
 
+func ensureSinglePrimaryKey(keys []string) (string, error) {
+	if len(keys) == 0 {
+		return "", errors.New("no key values provided")
+	}
+	if len(keys) > 1 {
+		return "", errors.New("multi-column primary key is not supported")
+	}
+	return keys[0], nil
+}
+
 func getPrimaryKeyValue(row []interface{}, t *schema.Table) (string, error) {
-	v, err := canal.GetPKValues(t, row)
+	keys, err := getAllPrimaryKeys(row, t)
 	if err != nil {
 		return "", err
 	}
-	key, err := primaryKeyToString(v)
+	return ensureSinglePrimaryKey(keys)
+}
+
+func getAllPrimaryKeys(row []interface{}, t *schema.Table) ([]string, error) {
+	var keys []string
+	v, err := canal.GetPKValues(t, row)
 	if err != nil {
-		return "", fmt.Errorf("table %s: %v", t.Name, err)
+		return nil, err
 	}
-	return key, nil
+	keys, err = primaryKeyToStringSlice(v)
+	if err != nil {
+		return nil, errors.Wrapf(err, "table: %s error", t.Name)
+	}
+	return keys, nil
 }
 
 func primaryKeyToString(keyValues []interface{}) (string, error) {
-	if len(keyValues) == 0 {
-		return "", errors.New("no key values provided")
+	keys, err := primaryKeyToStringSlice(keyValues)
+	if err != nil {
+		return "", err
 	}
-	if len(keyValues) > 1 {
-		return "", errors.New("multi-column primary key is not supported")
-	}
-	pk := keyValues[0]
-	if pk == nil || pk == "" {
-		return "", errors.New("primary key value is nil or empty")
-	}
+	return ensureSinglePrimaryKey(keys)
+}
 
-	return fmt.Sprint(pk), nil
+func primaryKeyToStringSlice(keyValues []interface{}) ([]string, error) {
+	keys := []string{}
+	for i, pk := range keyValues {
+		if pk == nil || pk == "" {
+			return nil, fmt.Errorf("primary key value is nil or empty on key element at index %v", i)
+		}
+		keys = append(keys, fmt.Sprint(pk))
+	}
+	return keys, nil
 }
 
 // getKeyValues uses the fact that columns are in the same order as values in a row
