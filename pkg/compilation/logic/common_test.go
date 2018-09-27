45b9d0d4ed2d8fc4353636ea1a5fb0f920d16efe
Józef Wołoch <jozef.woloch@codilime.com>
2018-09-27 16:36:28 +0200

    Refactor dependencies processor
    Change-Id: I90b707753b5639873794d2228c28df7dab8c3878
    Closes-bug: 1794806

---
+++ pkg/compilation/logic/common_test.go
@@ -0,0 +1,15 @@
+package logic
+
+import (
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/compilation/dependencies"
+	"github.com/stretchr/testify/require"
+)
+
+func parseReactions(t *testing.T) dependencies.Reactions {
+	reactions, err := dependencies.ParseReactions("./dependencies.yml", "intent-compiler")
+	require.NoError(t, err)
+
+	return reactions
+}
