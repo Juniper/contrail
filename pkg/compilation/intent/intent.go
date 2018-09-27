9b80c7ada043f0648c7d2cd09dc7b45d7f9a55ca
Józef Wołoch <jozef.woloch@codilime.com>
2018-09-27 16:36:28 +0200

    (WiP) Refactor dependencies processor
    Change-Id: I90b707753b5639873794d2228c28df7dab8c3878
    Closes-bug: 1794806

--- pkg/compilation/intent/intent.go
+++ pkg/compilation/intent/intent.go
@@ -20,6 +20,7 @@ type Intent interface {
 	basemodels.Object
 	Evaluate(ctx context.Context, evaluateCtx *EvaluateContext) error
 	GetObject() basemodels.Object
+	GetDependencies() map[string]map[string]struct{}
 }
 
 // Loader provides access to existing intents (e.g. using a cache)
@@ -29,9 +30,47 @@ type Loader interface {
 
 // BaseIntent implements the default Evaluate interface
 type BaseIntent struct {
+	// Dependencies maps type of dependent intents to set of theirs uuids
+	Dependencies map[string]map[string]struct{}
+}
+
+func (b *BaseIntent) GetDependencies() map[string]map[string]struct{} {
+	return b.Dependencies
 }
 
 // Evaluate creates/updates/deletes lower-level resources when needed.
 func (b *BaseIntent) Evaluate(ctx context.Context, evaluateCtx *EvaluateContext) error {
 	return nil
+}
+
+func GetDependencies(
+	i Intent,
+	from string,
+	loader Loader,
+	reactionMap map[string]map[string]map[string]struct{},
+) []Intent {
+	intents := []Intent{}
+	intentReactionMap, ok := reactionMap[i.Kind()]
+	if !ok {
+		return intents
+	}
+	dependentTypes, ok := intentReactionMap[from]
+	if !ok {
+		return intents
+	}
+	intents = append(intents, i)
+	dependentIntents := i.GetDependencies()
+	for t, uuids := range dependentIntents {
+		_, ok := dependentTypes[t]
+		if ok {
+			for uuid := range uuids {
+				dependentIntent := loader.Load(t, ByUUID(uuid))
+				intents = append(intents, dependentIntent)
+				for _, k := range GetDependencies(dependentIntent, t, loader, reactionMap) {
+					intents = append(intents, k)
+				}
+			}
+		}
+	}
+	return intents
 }
