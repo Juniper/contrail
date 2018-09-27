73ee9a10a94045978813b200334a67108a3809c8
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-09-27 15:02:39 +0200

    [WIP] Handling ref_ tables in Sync
    Change-Id: Ie0c3038c43dc48b3f9d02d3d0dfba956c4184556
    Closes-Bug: 1794740

--- pkg/sync/sink/event_sink.go
+++ pkg/sync/sink/event_sink.go
@@ -2,9 +2,13 @@ package sink
 
 import (
 	"context"
+	"strings"
 
 	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/schema"
 	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/sync"
+	"github.com/pkg/errors"
 )
 
 // EventProcessorSink is a Sink that dispatches events to processor.
@@ -13,13 +17,30 @@ type EventProcessorSink struct {
 }
 
 // Create dispatches OperationCreate event to processor.
-func (e *EventProcessorSink) Create(ctx context.Context, resourceName string, pk string, obj basemodels.Object) error {
-	ev, err := services.NewEvent(&services.EventOption{
-		UUID:      pk,
-		Kind:      resourceName,
-		Data:      obj.ToMap(),
-		Operation: services.OperationCreate,
-	})
+func (e *EventProcessorSink) Create(ctx context.Context, resourceName string, pk []string, obj basemodels.Object) error {
+	var err error
+	var ev *services.Event
+	//func resolveReferenceTable(name string) (typeName, refType string) {
+	if strings.HasPrefix(resourceName, schema.RefPrefix) {
+		if len(pk) != 2 {
+			return errors.Errorf("expecting primary key with 2 items, got %d instead", len(pk))
+		}
+		typeName, typeRef := resolveReferenceTable(resourceName)
+		ev, err = services.NewEventFromRefUpdate(&services.RefUpdate{
+			Operation: services.RefOperationAdd,
+			Type:      typeName,
+			UUID:      pk[0],
+			RefType:   typeRef,
+			RefUUID:   pk[1],
+		})
+	} else {
+		ev, err = services.NewEvent(&services.EventOption{
+			UUID:      pk,
+			Kind:      resourceName,
+			Data:      obj.ToMap(),
+			Operation: services.OperationCreate,
+		})
+	}
 	if err != nil {
 		return err
 	}
@@ -27,7 +48,7 @@ func (e *EventProcessorSink) Create(ctx context.Context, resourceName string, pk
 }
 
 // Update dispatches OperationUpdate event to processor.
-func (e *EventProcessorSink) Update(ctx context.Context, resourceName string, pk string, obj basemodels.Object) error {
+func (e *EventProcessorSink) Update(ctx context.Context, resourceName string, pk []string, obj basemodels.Object) error {
 	ev, err := services.NewEvent(&services.EventOption{
 		UUID:      pk,
 		Kind:      resourceName,
@@ -41,12 +62,29 @@ func (e *EventProcessorSink) Update(ctx context.Context, resourceName string, pk
 }
 
 // Delete dispatches OperationDelete event to processor.
-func (e *EventProcessorSink) Delete(ctx context.Context, resourceName string, pk string) error {
-	ev, err := services.NewEvent(&services.EventOption{
-		UUID:      pk,
-		Kind:      resourceName,
-		Operation: services.OperationDelete,
-	})
+func (e *EventProcessorSink) Delete(ctx context.Context, resourceName string, pk []string) error {
+	var err error
+	var ev *services.Event
+	//func resolveReferenceTable(name string) (typeName, refType string) {
+	if strings.HasPrefix(resourceName, schema.RefPrefix) {
+		if len(pk) != 2 {
+			return errors.Errorf("expecting primary key with 2 items, got %d instead", len(pk))
+		}
+		typeName, typeRef := resolveReferenceTable(resourceName)
+		ev, err = services.NewEventFromRefUpdate(&services.RefUpdate{
+			Operation: services.RefOperationAdd,
+			Type:      typeName,
+			UUID:      pk[0],
+			RefType:   typeRef,
+			RefUUID:   pk[1],
+		})
+	} else {
+		ev, err = services.NewEvent(&services.EventOption{
+			UUID:      pk,
+			Kind:      resourceName,
+			Operation: services.OperationDelete,
+		})
+	}
 	if err != nil {
 		return err
 	}
