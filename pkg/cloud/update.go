067afdf24ff28619a2a85107f229da8516716d06
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-10-02 17:09:08 -0700

    Cloud pkg for multi-cloud support
    Introducing a new pkg known as "cloud", which will consists of methods to
    support multi-cloud functionality
    
    Change-Id: I2027bb0e5e516f1cd29a2d201c8d8175a02ddd06
    Partial-Bug: #1793603

---
+++ pkg/cloud/update.go
@@ -0,0 +1,110 @@
+package cloud
+
+import (
+	"context"
+	"fmt"
+
+	tf "github.com/hashicorp/terraform/terraform"
+
+	"github.com/Juniper/contrail/pkg/apisrv/client"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+func (c *Cloud) updateIPDetails(ctx context.Context, data *Data) error {
+	tfState, err := readStateFile(GetTFStateFile(c.config.CloudID))
+	if err != nil {
+		return err
+	}
+	for _, instance := range data.instances {
+		err := updateInstanceIP(ctx, instance, tfState)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func updateInstanceIP(ctx context.Context,
+	instance *instanceData, tfState *tf.State) error {
+
+	privateIP, err := getIPFromTFState(tfState,
+		fmt.Sprintf("private_ip.%s", instance.info.Name))
+	if err != nil {
+		return err
+	}
+
+	if gwRoleExists(instance) {
+		portObj, inErr := createPort("private", privateIP, instance.client)
+		if inErr != nil {
+			return inErr
+		}
+		inErr = addPortToNode(portObj, instance.info, instance.client)
+		if err != nil {
+			return inErr
+		}
+
+		publicIP, inErr := getIPFromTFState(tfState,
+			fmt.Sprintf("public_ip.%s", instance.info.Name))
+		if inErr != nil {
+			return inErr
+		}
+		inErr = addIPToNode(publicIP, instance.info, instance.client)
+		if err != nil {
+			return inErr
+		}
+	}
+
+	return addIPToNode(privateIP, instance.info, instance.client)
+
+}
+
+func gwRoleExists(instance *instanceData) bool {
+	for _, role := range instance.info.CloudInfo.Roles {
+		if role == "gateway" {
+			return true
+		}
+	}
+	return false
+}
+
+func createPort(portName string, ip string,
+	client *client.HTTP) (*models.Port, error) {
+
+	request := new(services.CreatePortRequest)
+	request.Port.Name = portName
+	request.Port.IPAddress = ip
+
+	response := new(services.CreatePortResponse)
+
+	_, err := client.Create("/ports", request, response)
+	if err != nil {
+		return nil, err
+	}
+	return response.GetPort(), nil
+}
+
+func addPortToNode(port *models.Port,
+	instance *models.Node, client *client.HTTP) error {
+
+	request := new(services.UpdateNodeRequest)
+	request.Node = instance
+	request.Node.Ports = append(request.Node.Ports, port)
+
+	_, err := client.Update("/node/"+request.Node.UUID,
+		request, new(services.UpdateNodeResponse))
+	return err
+
+}
+
+func addIPToNode(ip string,
+	instance *models.Node, client *client.HTTP) error {
+
+	request := new(services.UpdateNodeRequest)
+	request.Node = instance
+	request.Node.IPAddress = ip
+
+	_, err := client.Update("/node/"+request.Node.UUID,
+		request, new(services.UpdateNodeResponse))
+	return err
+}
