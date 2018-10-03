2975fbec19f70ba5d642d01a739865bb66f876cf
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-10-02 17:09:08 -0700

    Outline of cloud pkg for multi-cloud support
    Introducing a new pkg known as "cloud", which will consists of methods to
    support multi-cloud functionality
    
    Partial-Bug: #1793603
    
    Change-Id: I995c2ff74ea32e5ec6d7d73865ad8b01827105da

---
+++ pkg/cloud/update.go
@@ -0,0 +1,107 @@
+package cloud
+
+import (
+	"context"
+	"fmt"
+
+	tf "github.com/hashicorp/terraform/terraform"
+
+	"github.com/Juniper/contrail/pkg/apisrv/client"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+func (c *Cloud) updateIPDetails(ctx context.Context, data *Data) error {
+	tfState, err := readStateFile(GetTFStateFile(c.config.CloudID))
+	if err != nil {
+		return err
+	}
+	for _, instance := range data.instances {
+		err := updateInstanceIP(ctx, instance, tfState)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func updateInstanceIP(ctx context.Context, instance *instanceData, tfState *tf.State) error {
+
+	gwExists := gwRoleExists(instance)
+
+	privateIP, err := getIPFromTFState(tfState,
+		fmt.Sprintf("private_ip.%s", instance.info.Name))
+	if err != nil {
+		return err
+	}
+
+	if gwExists {
+		portObj, err := createPort(ctx, "private", privateIP, instance.client)
+		err = addPortToNode(ctx, portObj, instance.info, instance.client)
+		if err != nil {
+			return err
+		}
+		return nil
+
+		publicIP, err := getIPFromTFState(tfState,
+			fmt.Sprintf("public_ip.%s", instance.info.Name))
+		err = addIPToNode(ctx, publicIP, instance.info, instance.client)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = addIPToNode(ctx, privateIP, instance.info, instance.client)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func gwRoleExists(instance *instanceData) bool {
+	for _, role := range instance.info.CloudInfo.Roles {
+		if role == "gateway" {
+			return true
+		}
+	}
+	return false
+}
+
+func createPort(ctx context.Context, portName string, ip string, client *client.HTTP) (*models.Port, error) {
+
+	request := new(services.CreatePortRequest)
+	request.Port.Name = portName
+	request.Port.IPAddress = ip
+
+	portResp, err := client.CreatePort(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return portResp.GetPort(), nil
+}
+
+func addPortToNode(ctx context.Context, port *models.Port, instance *models.Node, client *client.HTTP) error {
+
+	request := new(services.UpdateNodeRequest)
+	request.Node = instance
+	request.Node.AddPort(port)
+
+	_, err := client.UpdateNode(ctx, request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func addIPToNode(ctx context.Context, ip string, instance *models.Node, client *client.HTTP) error {
+
+	request := new(services.UpdateNodeRequest)
+	request.Node = instance
+	request.Node.IPAddress = ip
+
+	_, err := client.UpdateNode(ctx, request)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pkg/cmd/contrail/cloud.go
@@ -0,0 +1,34 @@
+package contrail
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/Juniper/contrail/pkg/cloud"
+)
+
+func init() {
+	Contrail.AddCommand(cloudCmd)
+}
+
+var cloudCmd = &cobra.Command{
+	Use:   "cloud",
+	Short: "sub command cloud is used to manage public cloud infra",
+	Long: `Cloud is a sub command used to manage
+            public cloud infra. Currently
+            supported infra are Azure`,
+	Run: func(cmd *cobra.Command, args []string) {
+		manageCloud()
+	},
+}
+
+func manageCloud() {
+	manager, err := cloud.NewCloudManager(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := manager.Manage(); err != nil {
+		log.Fatal(err)
+	}
+}
