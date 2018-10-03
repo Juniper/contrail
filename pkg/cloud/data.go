baea0cedefd379cf924bebd02c66e962c035c312
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-10-02 17:09:08 -0700

    Outline of cloud pkg for multi-cloud support
    Introducing a new pkg known as "cloud", which will consists of methods to
    support multi-cloud functionality
    
    Partial-Bug: #1793603
    
    Change-Id: I995c2ff74ea32e5ec6d7d73865ad8b01827105da

---
+++ pkg/cloud/data.go
@@ -0,0 +1,600 @@
+package cloud
+
+import (
+	"github.com/Juniper/contrail/pkg/apisrv/client"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+//Data for cloud provider data
+type Data struct {
+	cloud               *Cloud
+	cloudInfo           *models.Cloud
+	projectName         string
+	credentials         []*models.Credential
+	cloudSubnets        []*models.CloudPrivateSubnet
+	cloudSecurityGroups []*models.CloudSecurityGroup
+	regions             []*regionData
+	accounts            []*accountData
+}
+
+type apiServer struct {
+	client *client.HTTP
+}
+
+type regionData struct {
+	info          *models.CloudRegion
+	virtualClouds []*virtualCloudData
+	apiServer
+}
+
+type virtualCloudData struct {
+	info      *models.VirtualCloud
+	sgs       []*sgData
+	instances []*instanceData
+	subnets   []*subnetData
+	apiServer
+}
+
+type instanceData struct {
+	info *models.Node
+	apiServer
+}
+
+type subnetData struct {
+	info *models.CloudPrivateSubnet
+	apiServer
+}
+
+type sgData struct {
+	info *models.CloudSecurityGroup
+	apiServer
+}
+
+type accountData struct {
+	info     *models.CloudAccount
+	projects []*projectData
+	apiServer
+}
+
+type projectData struct {
+	info  *models.CloudProject
+	users []*userData
+	apiServer
+}
+
+type userData struct {
+	info *models.CloudUser
+	apiServer
+}
+
+func (p *projectData) getProjectObject() (*models.CloudProject, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudProjectRequest)
+	request.ID = p.info.UUID
+
+	projResp, err := p.client.GetCloudProject(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return projResp.GetCloudProject(), nil
+
+}
+
+func (a *accountData) newProject(project *models.CloudProject) (*projectData, error) {
+	proj := &projectData{
+		info: project,
+		apiServer: apiServer{
+			client: a.client,
+		},
+	}
+
+	projObj, err := proj.getProjectObject()
+	if err != nil {
+		return nil, err
+	}
+
+	proj.info = projObj
+	return proj, nil
+}
+
+func (a *accountData) updateProjects() error {
+
+	for _, proj := range a.info.CloudProjects {
+		newProj, err := a.newProject(proj)
+		if err != nil {
+			return err
+		}
+
+		err = newProj.updateUsers()
+		if err != nil {
+			return err
+		}
+
+		a.projects = append(a.projects, newProj)
+
+	}
+	return nil
+}
+
+func (a *accountData) getAccountObject() (*models.CloudAccount, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudAccountRequest)
+	request.ID = a.info.UUID
+
+	accResp, err := a.client.GetCloudAccount(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return accResp.GetCloudAccount(), nil
+
+}
+
+func (d *Data) newAccount(account *models.CloudAccount) (*accountData, error) {
+
+	acc := &accountData{
+		info: account,
+		apiServer: apiServer{
+			client: d.cloud.APIServer,
+		},
+	}
+
+	accObj, err := acc.getAccountObject()
+	if err != nil {
+		return nil, err
+	}
+
+	acc.info = accObj
+	return acc, nil
+}
+
+func (d *Data) updateAccounts() error {
+	for _, account := range d.cloudInfo.CloudAccounts {
+		newAccount, err := d.newAccount(account)
+		if err != nil {
+			return err
+		}
+
+		err = newAccount.updateProjects()
+		if err != nil {
+			return err
+		}
+
+		d.accounts = append(d.accounts, newAccount)
+	}
+	return nil
+}
+
+func (s *subnetData) getPvtSubnetObject() (*models.CloudPrivateSubnet, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudPrivateSubnetRequest)
+	request.ID = s.info.UUID
+
+	subnetResp, err := s.client.GetCloudPrivateSubnet(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return subnetResp.GetCloudPrivateSubnet(), nil
+}
+
+func (v *virtualCloudData) newSubnet(subnet *models.CloudPrivateSubnet) (*subnetData, error) {
+
+	s := &subnetData{
+		info: subnet,
+		apiServer: apiServer{
+			client: v.client,
+		},
+	}
+
+	subnetObj, err := s.getPvtSubnetObject()
+	if err != nil {
+		return nil, err
+	}
+
+	s.info = subnetObj
+	return s, nil
+}
+
+func (v *virtualCloudData) updateSubnets() error {
+
+	for _, subnet := range v.info.CloudPrivateSubnets {
+		newSubnet, err := v.newSubnet(subnet)
+		if err != nil {
+			return err
+		}
+
+		if err != nil {
+			return err
+		}
+
+		v.subnets = append(v.subnets, newSubnet)
+	}
+	return nil
+}
+
+func (i *instanceData) getNodeObject() (*models.Node, error) {
+
+	ctx := returnContext()
+	request := new(services.GetNodeRequest)
+	request.ID = i.info.UUID
+
+	instResp, err := i.client.GetNode(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return instResp.GetNode(), nil
+}
+
+func (v *virtualCloudData) newInstance(instance *models.Node) (*instanceData, error) {
+
+	inst := &instanceData{
+		info: instance,
+		apiServer: apiServer{
+			client: v.client,
+		},
+	}
+
+	instObj, err := inst.getNodeObject()
+	if err != nil {
+		return nil, err
+	}
+
+	inst.info = instObj
+	return inst, nil
+}
+
+func (v *virtualCloudData) updateInstances() error {
+
+	for _, instance := range v.info.Nodes {
+		newI, err := v.newInstance(instance)
+		if err != nil {
+			return err
+		}
+
+		v.instances = append(v.instances, newI)
+	}
+	return nil
+}
+
+func (sg *sgData) getSGObject() (*models.CloudSecurityGroup, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudSecurityGroupRequest)
+	request.ID = sg.info.UUID
+
+	sgResp, err := sg.client.GetCloudSecurityGroup(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return sgResp.GetCloudSecurityGroup(), nil
+}
+
+func (v *virtualCloudData) newSG(mSG *models.CloudSecurityGroup) (*sgData, error) {
+
+	sg := &sgData{
+		info: mSG,
+		apiServer: apiServer{
+			client: v.client,
+		},
+	}
+
+	sgObj, err := sg.getSGObject()
+	if err != nil {
+		return nil, err
+	}
+
+	sg.info = sgObj
+	return sg, nil
+}
+
+func (v *virtualCloudData) updateSGs() error {
+
+	for _, sg := range v.info.CloudSecurityGroups {
+		newSG, err := v.newSG(sg)
+		if err != nil {
+			return err
+		}
+
+		if err != nil {
+			return err
+		}
+
+		v.sgs = append(v.sgs, newSG)
+	}
+	return nil
+}
+
+func (v *virtualCloudData) getVCloudObject() (*models.VirtualCloud, error) {
+
+	ctx := returnContext()
+	request := new(services.GetVirtualCloudRequest)
+	request.ID = v.info.UUID
+
+	vCloudResp, err := v.client.GetVirtualCloud(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return vCloudResp.GetVirtualCloud(), nil
+}
+
+func (r *regionData) newVCloud(vCloud *models.VirtualCloud) (*virtualCloudData, error) {
+
+	vc := &virtualCloudData{
+		info: vCloud,
+		apiServer: apiServer{
+			client: r.client,
+		},
+	}
+
+	vCloudObj, err := vc.getVCloudObject()
+	vc.info = vCloudObj
+
+	if err != nil {
+		return nil, err
+	}
+
+	return vc, nil
+}
+
+func (r *regionData) updateVClouds() error {
+
+	for _, vc := range r.info.VirtualClouds {
+		newVC, err := r.newVCloud(vc)
+		if err != nil {
+			return err
+		}
+
+		err = newVC.updateSGs()
+		if err != nil {
+			return err
+		}
+
+		err = newVC.updateInstances()
+		if err != nil {
+			return err
+		}
+
+		err = newVC.updateSubnets()
+		if err != nil {
+			return err
+		}
+
+		r.virtualClouds = append(r.virtualClouds, newVC)
+	}
+	return nil
+}
+
+func (r *regionData) getRegionObject() (*models.CloudRegion, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudRegionRequest)
+	request.ID = r.info.UUID
+
+	regResp, err := r.client.GetCloudRegion(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return regResp.GetCloudRegion(), nil
+
+}
+
+func (d *Data) newRegion(region *models.CloudRegion) (*regionData, error) {
+
+	reg := &regionData{
+		info: region,
+		apiServer: apiServer{
+			client: d.cloud.APIServer,
+		},
+	}
+
+	regObj, err := reg.getRegionObject()
+	reg.info = regObj
+
+	if err != nil {
+		return nil, err
+	}
+
+	return reg, nil
+}
+
+func (d *Data) updateRegions() error {
+
+	for _, region := range d.cloudInfo.CloudRegions {
+		newRegion, err := d.newRegion(region)
+		if err != nil {
+			return err
+		}
+
+		err = newRegion.updateVClouds()
+		if err != nil {
+			return err
+		}
+
+		d.regions = append(d.regions, newRegion)
+	}
+	return nil
+}
+
+func (u *userData) getUserObject() (*models.CloudUser, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudUserRequest)
+	request.ID = u.info.UUID
+
+	userResp, err := u.client.GetCloudUser(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return userResp.GetCloudUser(), nil
+
+}
+
+func (p *projectData) newUser(userRef *models.CloudProjectCloudUserRef) (*userData, error) {
+
+	u := &userData{
+		info: &models.CloudUser{
+			UUID: userRef.UUID,
+		},
+		apiServer: apiServer{
+			client: p.client,
+		},
+	}
+
+	userObj, err := u.getUserObject()
+	if err != nil {
+		return nil, err
+	}
+
+	u.info = userObj
+	return u, nil
+}
+
+func (p *projectData) updateUsers() error {
+
+	for _, u := range p.info.CloudUserRefs {
+		newUser, err := p.newUser(u)
+		if err != nil {
+			return err
+		}
+
+		p.users = append(p.users, newUser)
+	}
+	return nil
+}
+
+func (c *Cloud) listCloudSecurityGroup() ([]*models.CloudSecurityGroup, error) {
+
+	request := new(services.ListCloudSecurityGroupRequest)
+	ctx := returnContext()
+	response, err := c.APIServer.ListCloudSecurityGroup(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return response.CloudSecurityGroups, nil
+}
+
+func (c *Cloud) listCloudPrivatesubnet() ([]*models.CloudPrivateSubnet, error) {
+
+	request := new(services.ListCloudPrivateSubnetRequest)
+
+	ctx := returnContext()
+	response, err := c.APIServer.ListCloudPrivateSubnet(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return response.CloudPrivateSubnets, nil
+}
+
+func (c *Cloud) listCredential() ([]*models.Credential, error) {
+
+	request := new(services.ListCredentialRequest)
+	ctx := returnContext()
+
+	response, err := c.APIServer.ListCredential(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return response.Credentials, nil
+}
+
+func (c *Cloud) getCloudData() (*Data, error) {
+
+	cloudData, err := c.newCloudData()
+	if err != nil {
+		return nil, err
+	}
+
+	err = cloudData.update()
+	if err != nil {
+		return nil, err
+	}
+
+	return cloudData, nil
+
+}
+
+func (c *Cloud) getCloudObject() (*models.Cloud, error) {
+
+	ctx := returnContext()
+	request := new(services.GetCloudRequest)
+	request.ID = c.config.CloudID
+
+	cloudResp, err := c.APIServer.GetCloud(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return cloudResp.GetCloud(), nil
+
+}
+
+func (c *Cloud) newCloudData() (*Data, error) {
+
+	data := Data{}
+	data.cloud = c
+
+	cloudObject, err := c.getCloudObject()
+	if err != nil {
+		return nil, err
+	}
+
+	data.cloudInfo = cloudObject
+	return &data, nil
+
+}
+
+func (d *Data) update() error {
+
+	subnets, err := d.cloud.listCloudPrivatesubnet()
+	if err != nil {
+		return err
+	}
+	d.cloudSubnets = subnets
+
+	sgs, err := d.cloud.listCloudSecurityGroup()
+	if err != nil {
+		return err
+	}
+	d.cloudSecurityGroups = sgs
+
+	creds, err := d.cloud.listCredential()
+	if err != nil {
+		return err
+	}
+	d.credentials = creds
+
+	err = d.updateRegions()
+	if err != nil {
+		return err
+	}
+
+	err = d.updateAccounts()
+	if err != nil {
+		return err
+	}
+
+	// TODO(madhukar) - Needs to handle multiple projects
+	for _, account := range d.accounts {
+		for _, project := range account.projects {
+			if project.info.Name != "" {
+				d.projectName = project.info.Name
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (d *Data) isCloudCreated() bool {
+
+	status := d.cloudInfo.ProvisioningState
+	if d.cloud.config.Action == "create" && (status == "NOSTATE" || status == "") {
+		return false
+	}
+	d.cloud.log.Infof("Cloud %s already provisioned, STATE: %s", d.cloudInfo.UUID, status)
+	return true
+}
---
+++ pkg/cloud/terraform.go
@@ -0,0 +1,193 @@
+package cloud
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+
+	report "github.com/Juniper/contrail/pkg/cluster"
+	pkglog "github.com/Juniper/contrail/pkg/log"
+)
+
+const (
+	tfCommand   = "terraform"
+	tfStateFile = "terraform.tfstate"
+)
+
+type terraform struct {
+	cloud      *Cloud
+	log        *logrus.Entry
+	reporter   *report.Reporter
+	action     string
+	topoFile   string
+	secretFile string
+	mcDir      string
+	test       bool
+}
+
+func (c *Cloud) newTF(t *topology, s *secret) (*terraform, error) {
+
+	topoFile := t.getTopoFile()
+	secretFile := s.getSecretFile()
+	mcDir, err := getMultiCloudRepodir()
+	if err != nil {
+		return nil, err
+	}
+
+	logger := pkglog.NewFileLogger("reporter", c.config.LogFile)
+	pkglog.SetLogLevel(logger, c.config.LogLevel)
+
+	r := &report.Reporter{
+		API:      c.APIServer,
+		Resource: fmt.Sprintf("%s/%s", defaultCloudResourcePath, c.config.CloudID),
+		Log:      logger,
+	}
+
+	// create logger for secret
+	logger = pkglog.NewFileLogger("topology", c.config.LogFile)
+	pkglog.SetLogLevel(logger, c.config.LogLevel)
+
+	return &terraform{
+		cloud:      c,
+		log:        logger,
+		reporter:   r,
+		action:     c.config.Action,
+		topoFile:   topoFile,
+		secretFile: secretFile,
+		mcDir:      mcDir,
+		test:       c.config.Test,
+	}, nil
+}
+
+func (c *Cloud) manageTerraform(t *topology, s *secret, action string) error {
+
+	tf, err := c.newTF(t, s)
+	if err != nil {
+		return err
+	}
+
+	if action == deleteAction {
+		err = tf.destroy()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err = tf.createInputFile()
+	if err != nil {
+		return err
+	}
+
+	err = tf.initalize()
+	if err != nil {
+		return err
+	}
+
+	err = tf.apply()
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (tf *terraform) createInputFile() error {
+
+	cmd := getGenTopoFile(tf.mcDir)
+	args := strings.Split(fmt.Sprintf("-t %s -s %s",
+		tf.topoFile, tf.secretFile), " ")
+
+	tf.log.Infof("Generating input file (cloudType.tf.json)")
+
+	tf.log.Debugf("Command executed: %s %s", cmd,
+		strings.Join(args, " "))
+
+	workDir := tf.cloud.getWorkingDir()
+	if tf.test {
+		return testHelper(cmd, args, workDir)
+	}
+
+	err := execCmd(tf.reporter, cmd, args, workDir)
+	if err != nil {
+		return err
+	}
+	tf.log.Infof("Successfully generated infut file")
+	return nil
+}
+
+func (tf *terraform) initalize() error {
+
+	args := []string{"init"}
+
+	tf.log.Debugf("Command being executed : %s %s", tfCommand,
+		strings.Join(args, " "))
+
+	workDir := tf.cloud.getWorkingDir()
+	if tf.test {
+		return testHelper(tfCommand, args, workDir)
+	}
+
+	err := execCmd(tf.reporter, tfCommand, args, workDir)
+	if err != nil {
+		return err
+	}
+	tf.log.Infof("Terraform initialized successfully")
+	return nil
+}
+
+func (tf *terraform) apply() error {
+	args := []string{"apply", "-auto-approve"}
+
+	tf.log.Infof("Creating cloud infra using terraform")
+
+	tf.log.Debugf("Command being executed : %s %s", tfCommand,
+		strings.Join(args, " "))
+
+	workDir := tf.cloud.getWorkingDir()
+	if tf.test {
+		return testHelper(tfCommand, args, workDir)
+	}
+
+	err := execCmd(tf.reporter, tfCommand, args, workDir)
+	if err != nil {
+		return err
+	}
+	tf.log.Infof("Terraform created all resources successfully")
+	return nil
+}
+
+func (tf *terraform) destroy() error {
+
+	// teardown terraform
+
+	args := []string{"destroy", "-force"}
+
+	tf.log.Debugf("Command being executed : %s %s", tfCommand,
+		strings.Join(args, " "))
+
+	workDir := tf.cloud.getWorkingDir()
+	if tf.test {
+		return testHelper(tfCommand, args, workDir)
+	}
+
+	err := execCmd(tf.reporter, tfCommand, args, workDir)
+	if err != nil {
+		return err
+	}
+	tf.log.Infof("Terraform destroyed all resources successfully")
+
+	// Delete the tfStateFile created by terraform
+	args = []string{filepath.Join(workDir, tfStateFile)}
+	if !tf.test {
+		err := deleteFiles(tf.reporter, args)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
