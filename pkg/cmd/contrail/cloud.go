dc6371e51fef3ccad030cd41e9282e0626fd03d0
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-10-02 17:09:08 -0700

    Outline of cloud pkg for multi-cloud support
    Introducing a new pkg known as "cloud", which will consists of methods to
    support multi-cloud functionality
    
    Partial-Bug: #1793603
    
    Change-Id: I995c2ff74ea32e5ec6d7d73865ad8b01827105da

---
+++ pkg/cmd/contrail/cloud.go
@@ -0,0 +1,33 @@
+package contrail
+
+import (
+	"github.com/Juniper/contrail/pkg/cloud"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	Contrail.AddCommand(cloudCmd)
+}
+
+var cloudCmd = &cobra.Command{
+	Use:   "cloud",
+	Short: "sub command cloud is used to manage public cloud infra",
+	Long: `Cloud is a sub command used to manage
+            public cloud infra. Currently
+            supported infra are Azure`,
+	Run: func(cmd *cobra.Command, args []string) {
+		manageCloud()
+	},
+}
+
+func manageCloud() {
+	manager, err := cloud.NewCloudManager(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := manager.Manage(); err != nil {
+		log.Fatal(err)
+	}
+}
