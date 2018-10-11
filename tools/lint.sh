d0b2b507081420afba85c4eb6015899d4888b077
Daniel Furman <daniel.furman@codilime.com>
2018-10-11 10:39:55 +0200

    Fix "check" target and remove unused dependencies
    - use faster "dep check" command
    - remove unused dependencies
    - remove unnecessary files
    - other minor improvements
    
    Partial-bug: #1772623
    Change-Id: I71e012936f6e1108671389e7ea9bbba5786e181d

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -o nounset
-set -o pipefail
 
-[[ -z `go tool fix --diff ./pkg/` ]]
+[[ -z $(go tool fix --diff ./pkg/) ]]
 
-golangci-lint --config .golangci-lint.yml run ./... 2>&1
+golangci-lint --config .golangci.yml run ./... 2>&1
