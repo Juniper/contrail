71247ab9c7170a3e80fec6453ed1aa9750d27777
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-10-17 16:15:42 +0200

    Implement firewall policy resource validation
    - added create and update firewall policy resource validation
    
    Change-Id: I310bd691f04ef79479053ddd7550ea2d6bc30951
    Closes-bug: 1798388

---
+++ pkg/types/firewall_policy_test.go
@@ -0,0 +1,218 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/mock"
+)
+
+func TestCreateFirewallPolicy(t *testing.T) {
+	tests := []struct {
+		name               string
+		testFirewallPolicy models.FirewallPolicy
+		IsInternalRequest  bool
+		errorCode          codes.Code
+	}{
+		{
+			name: "Try to create with read-only draft-mode-state property",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:           "test-firewall-policy",
+				DraftModeState: "draft_mode_state",
+			},
+			errorCode: codes.InvalidArgument,
+		},
+		{
+			name: "Internal create request with draft-mode-state defined",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:           "test-firewall-policy",
+				DraftModeState: "draft_mode_state",
+			},
+			IsInternalRequest: true,
+		},
+		{
+			name: "Try to create when firewall-rule refs are scoped",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:   "test-firewall-policy",
+				FQName: []string{"default-policy-management", "test-firewall-policy"},
+				FirewallRuleRefs: []*models.FirewallPolicyFirewallRuleRef{
+					{
+						UUID: "firewall-rule-ref-uuid",
+						To:   []string{"firewall-rule-ref-uuid"},
+					},
+				},
+			},
+			errorCode: codes.InvalidArgument,
+		},
+		{
+			name: "Create firewall policy",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:   "test-firewall-policy",
+				FQName: []string{"default-domain", "default-project", "test-firewall-policy"},
+				FirewallRuleRefs: []*models.FirewallPolicyFirewallRuleRef{
+					{
+						UUID: "firewall-rule-ref-uuid",
+						To:   []string{"firewall-rule-ref-uuid"},
+					},
+				},
+			},
+			IsInternalRequest: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			service := makeMockedContrailTypeLogicService(mockCtrl)
+			setupNextServiceMocks(service)
+
+			createFirewallPolicyResponse, err := service.CreateFirewallPolicy(
+				getContext(tt.IsInternalRequest),
+				&services.CreateFirewallPolicyRequest{FirewallPolicy: &tt.testFirewallPolicy},
+			)
+			if tt.errorCode != codes.OK {
+				assert.Error(t, err)
+				status, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, tt.errorCode, status.Code())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(
+					t,
+					&services.CreateFirewallPolicyResponse{FirewallPolicy: &tt.testFirewallPolicy},
+					createFirewallPolicyResponse,
+				)
+			}
+		})
+	}
+}
+
+func TestUpdateFirewallPolicy(t *testing.T) {
+	tests := []struct {
+		name               string
+		testFirewallPolicy models.FirewallPolicy
+		dbFirewallPolicy   models.FirewallPolicy
+		IsInternalRequest  bool
+		errorCode          codes.Code
+	}{
+		{
+			name: "Try to update read-only draft-mode-state property",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:           "test-firewall-policy",
+				DraftModeState: "draft_mode_state",
+			},
+			errorCode: codes.InvalidArgument,
+		},
+		{
+			name: "Try to update firewall-rule refs",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID:   "test-firewall-policy",
+				FQName: []string{"default-policy-management", "test-firewall-policy"},
+				FirewallRuleRefs: []*models.FirewallPolicyFirewallRuleRef{
+					{
+						UUID: "firewall-rule-ref-uuid",
+						To:   []string{"default-domain", "default-project", "firewall-rule-ref-uuid"},
+					},
+				},
+			},
+			dbFirewallPolicy: models.FirewallPolicy{
+				FQName: []string{"default-policy-management", "test-firewall-policy"},
+			},
+			IsInternalRequest: true,
+			errorCode:         codes.InvalidArgument,
+		},
+		{
+			name: "Update firewall-rule refs",
+			testFirewallPolicy: models.FirewallPolicy{
+				UUID: "test-firewall-policy",
+				FirewallRuleRefs: []*models.FirewallPolicyFirewallRuleRef{
+					{
+						UUID: "firewall-rule-ref-uuid",
+						To:   []string{"firewall-rule-ref-uuid"},
+					},
+				},
+			},
+			dbFirewallPolicy: models.FirewallPolicy{
+				FQName: []string{"default-domain", "default-project", "test-firewall-policy"},
+			},
+			IsInternalRequest: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			service := makeMockedContrailTypeLogicService(mockCtrl)
+			setupNextServiceMocks(service)
+			setupReadServiceMock(service, &tt.dbFirewallPolicy)
+
+			updateFirewallPolicyResponse, err := service.UpdateFirewallPolicy(
+				getContext(tt.IsInternalRequest),
+				&services.UpdateFirewallPolicyRequest{FirewallPolicy: &tt.testFirewallPolicy},
+			)
+			if tt.errorCode != codes.OK {
+				assert.Error(t, err)
+				status, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, tt.errorCode, status.Code())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(
+					t,
+					&services.UpdateFirewallPolicyResponse{FirewallPolicy: &tt.testFirewallPolicy},
+					updateFirewallPolicyResponse,
+				)
+			}
+		})
+	}
+}
+
+func setupNextServiceMocks(s *ContrailTypeLogicService) {
+	nextService, _ := s.Next().(*servicesmock.MockService)
+	nextService.EXPECT().CreateFirewallPolicy(
+		gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil()),
+	).DoAndReturn(
+		func(_ context.Context, request *services.CreateFirewallPolicyRequest) (
+			response *services.CreateFirewallPolicyResponse, err error,
+		) {
+			return &services.CreateFirewallPolicyResponse{FirewallPolicy: request.FirewallPolicy}, nil
+		},
+	).MaxTimes(1)
+
+	nextService.EXPECT().UpdateFirewallPolicy(
+		gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil()),
+	).DoAndReturn(
+		func(_ context.Context, request *services.UpdateFirewallPolicyRequest) (
+			response *services.UpdateFirewallPolicyResponse, err error,
+		) {
+			return &services.UpdateFirewallPolicyResponse{FirewallPolicy: request.FirewallPolicy}, nil
+		},
+	).MaxTimes(1)
+}
+
+func setupReadServiceMock(s *ContrailTypeLogicService, databaseFP *models.FirewallPolicy) {
+	readService, _ := s.ReadService.(*servicesmock.MockReadService)
+	readService.EXPECT().GetFirewallPolicy(gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil())).Return(
+		&services.GetFirewallPolicyResponse{
+			FirewallPolicy: databaseFP,
+		},
+		nil,
+	).MaxTimes(1)
+}
+
+func getContext(isInternalRequest bool) context.Context {
+	if isInternalRequest {
+		return WithInternalRequest(context.Background())
+	}
+
+	return context.Background()
+}
