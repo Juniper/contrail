6d3d2c9164b4ad913c7bb254f8f814389cb06f71
Józef Wołoch <jozef.woloch@codilime.com>
2018-10-18 16:08:28 +0200

    Handle fieldmask during update in db
    Change-Id: I92c5a48eab3d26ab86af16866ddecafba199dfc6
    Closes-bug: 1798598

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -2,15 +2,73 @@ package db
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/Juniper/contrail/pkg/db/basedb"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 )
+
+func TestFieldMaskPaths(t *testing.T) {
+	tests := []struct {
+		name               string
+		structure          *basedb.Structure
+		path               string
+		expectedChildPaths []string
+		expectedLength     int
+	}{
+		{
+			name:               "top level simple property",
+			structure:          &FirewallRuleStructure,
+			path:               "uuid",
+			expectedChildPaths: nil,
+			expectedLength:     0,
+		},
+		{
+			name:               "nested simple property",
+			structure:          &FirewallRuleStructure,
+			path:               "match_tags.tag_list",
+			expectedChildPaths: nil,
+			expectedLength:     0,
+		},
+		{
+			name:      "top level complex property",
+			structure: &FirewallRuleStructure,
+			path:      "action_list",
+			expectedChildPaths: []string{
+				"action_list.gateway_name",
+				"action_list.mirror_to.analyzer_ip_address",
+			},
+			expectedLength: 20,
+		},
+		{
+			name:      "nested complex property",
+			structure: &FirewallRuleStructure,
+			path:      "service.src_ports",
+			expectedChildPaths: []string{
+				"service.src_ports.start_port",
+				"service.src_ports.end_port",
+			},
+			expectedLength: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			paths := tt.structure.GetChildPaths(tt.path)
+			for _, p := range tt.expectedChildPaths {
+				assert.Contains(t, paths, p)
+			}
+			logrus.Println(paths)
+			assert.Equal(t, tt.expectedLength, len(paths))
+		})
+	}
+}
 
 func TestDBScanRow(t *testing.T) {
 	tests := []struct {
