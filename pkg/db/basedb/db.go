dfb00a0d6a2c269237d46133e6e3e208fc47923b
Józef Wołoch <jozef.woloch@codilime.com>
2018-10-18 16:08:28 +0200

    Handle fieldmask during update in db
    Change-Id: I92c5a48eab3d26ab86af16866ddecafba199dfc6
    Closes-bug: 1798598

--- pkg/db/basedb/db.go
+++ pkg/db/basedb/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/ExpansiveWorlds/instrumentedsql"
@@ -258,4 +259,35 @@ func getDSNFormat(driver string) (string, error) {
 	default:
 		return "", errors.Errorf("undefined database driver: %v", driver)
 	}
+}
+
+type Structure map[string]interface{}
+
+func (s *Structure) getPaths(prefix string) []string {
+	var paths []string
+	for k, v := range *s {
+		p := prefix + "." + k
+		switch o := v.(type) {
+		case struct{}:
+			paths = append(paths, p)
+		case *Structure:
+			paths = append(paths, o.getPaths(p)...)
+		}
+	}
+	return paths
+}
+
+func (s *Structure) GetChildPaths(fieldMask string) (paths []string) {
+	innerStructure := s
+	for _, segment := range strings.Split(fieldMask, ".") {
+		switch o := (*innerStructure)[segment].(type) {
+		case struct{}:
+			return []string{fieldMask}
+		case *Structure:
+			innerStructure = o
+		default:
+			return nil
+		}
+	}
+	return innerStructure.getPaths(fieldMask)
 }
