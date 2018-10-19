caaeb0d3fac8f89b00d49b13bed3276469afcfde
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-10-19 12:56:49 +0200

    Propagate references from base schema to derived
    Closes-Bug: 1798784
    
    Conflicts:
    	pkg/compilation/logic/routing_instance_test.go
    	pkg/db/etcd/service_test.go
    	pkg/models/firewall_rule.go
    	pkg/models/validation_test.go
    	pkg/schema/schema.go
    	pkg/schema/schema_test.go
    	pkg/services/list_response_test.go
    	pkg/types/virtual_network_test.go
    
    Change-Id: I569c414f49d96978aa71e7c627317ff799b5c3f5

--- pkg/schema/schema_test.go
+++ pkg/schema/schema_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSchema(t *testing.T) {
@@ -24,6 +25,27 @@ func TestSchema(t *testing.T) {
 		virtualNetwork.JSONSchema.PropertiesOrder)
 	assert.Equal(t, 3, len(virtualNetwork.Columns))
 	assert.Equal(t, 1004, virtualNetwork.References["network_ipam"].Index)
+}
+
+func TestReferencesExtendBase(t *testing.T) {
+	api, err := MakeAPI([]string{"test_data/schema_extend"}, "")
+	require.Nil(t, err, "API reading failed")
+	assert.Equal(t, 5, len(api.Schemas))
+
+	base := api.SchemaByID("base")
+	require.NotNil(t, base, "Base object can't be <nil>")
+	assert.Equal(t, 1, len(base.ReferencesSlice))
+	assert.Equal(t, 0, len(base.References)) // References in base schemas are not processed
+
+	zeroRefObj := api.SchemaByID("derived_object")
+	require.NotNil(t, zeroRefObj, "derived_object schema shouldn't be <nil>")
+	assert.Equal(t, 1, len(zeroRefObj.ReferencesSlice))
+	assert.Equal(t, 1, len(zeroRefObj.References))
+
+	ownRefObj := api.SchemaByID("derived_own_refs_object")
+	require.NotNil(t, ownRefObj, "derived_own_refs_object schema shouldn't be <nil>")
+	assert.Equal(t, 2, len(ownRefObj.ReferencesSlice))
+	assert.Equal(t, 2, len(ownRefObj.References))
 }
 
 func TestReferenceTableName(t *testing.T) {
