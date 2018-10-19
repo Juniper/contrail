fee7b706e0467759081243f0812727fc6b9a428b
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-10-19 12:13:06 +0200

    Respect FieldMask in Sync process
    Change-Id: Ib2598dc44f29d0462fcee9e75e346d49e211340b
    Partial-Bug: 1794740

--- pkg/sync/sink/sink.go
+++ pkg/sync/sink/sink.go
@@ -3,13 +3,15 @@ package sink
 import (
 	"context"
 
+	"github.com/gogo/protobuf/types"
+
 	"github.com/Juniper/contrail/pkg/db/basedb"
 )
 
 // Sink represents service that handles resources object logic rather than db loic.
 type Sink interface {
 	Create(ctx context.Context, resourceName string, pk string, obj basedb.Object) error
-	Update(ctx context.Context, resourceName string, pk string, obj basedb.Object) error
+	Update(ctx context.Context, resourceName string, pk string, obj basedb.Object, fm types.FieldMask) error
 	Delete(ctx context.Context, resourceName string, pk string) error
 
 	CreateRef(ctx context.Context, resourceName string, pk []string, obj basedb.Object) error
