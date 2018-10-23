36dc04320c3e2096937b1b2b8750f86321f4b72e
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-10-23 16:27:44 +0200

    Use patched kube_manager with ETCD support
    Change-Id: I80cb2a31625ba0ea38e72e247902eb592f796e07
    Partial-Bug: #1792379

---
+++ tools/kube_manager_etcd/update-docker-compose.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import yaml
+
+docker_compose_path = "/etc/contrail/kubemanager/docker-compose.yaml"
+
+with open(docker_compose_path) as f:
+     docker_compose = yaml.load(f)
+
+docker_compose["services"]["kubemanager"]["image"] = "contrail-kubernetes-kube-manager:etcd"
+
+with open(docker_compose_path, "w") as f:
+    yaml.dump(docker_compose, f)
