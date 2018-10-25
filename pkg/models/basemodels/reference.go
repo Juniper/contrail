3a28fd5965be1a9821cba80c8cccf95128e3f463
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-10-25 13:26:13 +0200

    set-tag endpoint
    Implementation of set-tag endpoint.
    
    GRPC endpoint will be added in separate change, because this change is
    big enough.
    
    Co-authored-by: "Pawel Zadrozny <pawel.zny@gmail.com>"
    Co-authored-by: "Michał Błotniak <michal.blotniak@codilime.com>"
    Change-Id: I1f4be4065badfaf097333998f5dfc420a1a63117
    Partial-Bug: #1795683

--- pkg/models/basemodels/reference.go
+++ pkg/models/basemodels/reference.go
@@ -8,3 +8,93 @@ type Reference interface {
 	GetTo() []string
 	GetReferredKind() string
 }
+
+// References is wrapper type for reference slice.
+type References []Reference
+
+// Find returns first reference that fulfils the predicate.
+func (r References) Find(pred func(Reference) bool) Reference {
+	for _, ref := range r {
+		if pred(ref) {
+			return ref
+		}
+	}
+	return nil
+}
+
+// Filter removes all the values that doesn't match the predicate.
+func (r References) Filter(pred func(Reference) bool) References {
+	result := make(References, 0, len(r))
+	for _, ref := range r {
+		if pred(ref) {
+			result = append(result, ref)
+		}
+	}
+	return result
+}
+
+// Unique returns references without duplicates.
+func (r References) Unique() References {
+	set := map[string]struct{}{}
+
+	return r.Filter(func(ref Reference) bool {
+		uuid := ref.GetUUID()
+		if _, ok := set[uuid]; ok {
+			return false
+		}
+		set[uuid] = struct{}{}
+		return true
+	})
+}
+
+// ForEach performs f on each element of references slice.
+func ForEach(slice interface{}, f func(interface{})) bool {
+	switch l := slice.(type) {
+	case References:
+		for _, x := range l {
+			f(x)
+		}
+	case []Reference:
+		for _, x := range l {
+			f(x)
+		}
+	case []interface{}:
+		for _, x := range l {
+			f(x)
+		}
+	default:
+		return false
+	}
+	return true
+}
+
+// NewReference creates new generic reference that can be processed into proper reference type.
+func NewReference(uuid, kind string) Reference {
+	return &genericReference{uuid: uuid, kind: kind}
+}
+
+type genericReference struct {
+	uuid string
+	to   []string
+	kind string
+}
+
+func (g *genericReference) SetUUID(uuid string) {
+	g.uuid = uuid
+}
+
+func (g *genericReference) SetTo(to []string) {
+	g.to = to
+}
+
+func (g *genericReference) GetUUID() string {
+	return g.uuid
+}
+
+func (g *genericReference) GetTo() []string {
+	return g.to
+}
+
+func (g *genericReference) GetReferredKind() string {
+	return g.kind
+}
