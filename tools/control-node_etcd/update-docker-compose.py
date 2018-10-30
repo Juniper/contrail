d14990c5b70445f9c5ffb01176827f666e6ad69f
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-10-30 14:13:15 +0100

    [WIP] Don't use vnc proxy nor cassandra nor rabbit
    Use only psql and etcd for kube_manager and control-node
    
    Change-Id: I157514bc32552ef1a16aac0c4b86f406a7e0b8f1

---
+++ tools/control-node_etcd/update-docker-compose.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import yaml
+
+docker_compose_path = "/etc/contrail/control/docker-compose.yaml"
+
+with open(docker_compose_path) as f:
+    docker_compose = yaml.load(f)
+
+docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync"
+
+with open(docker_compose_path, "w") as f:
+    yaml.dump(docker_compose, f)
