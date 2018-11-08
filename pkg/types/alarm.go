77394828155fa4ce31e30740bc27c339c23d839e
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2018-11-08 10:16:53 +0100

    Adding alarm check logics
    Change-Id: Iaa935d707a3df74f69778750ecb1be29446f7032
    Closes-bug: 1803325

---
+++ pkg/types/alarm.go
@@ -0,0 +1,71 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/errutil"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+//CreateAlarm does pre check for alarm.
+func (sv *ContrailTypeLogicService) CreateAlarm(
+	ctx context.Context,
+	request *services.CreateAlarmRequest) (response *services.CreateAlarmResponse, err error) {
+
+	alarm := request.GetAlarm()
+
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+
+			// check if alarm rules are valid
+			err = alarm.Validate()
+			if err != nil {
+				return errutil.ErrorBadRequestf(err.Error())
+			}
+
+			response, err = sv.BaseService.CreateAlarm(ctx, request)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	return response, err
+}
+
+//UpdateAlarm does pre check for alarm.
+func (sv *ContrailTypeLogicService) UpdateAlarm(
+	ctx context.Context,
+	request *services.UpdateAlarmRequest) (response *services.UpdateAlarmResponse, err error) {
+
+	alarm := request.GetAlarm()
+	fm := request.GetFieldMask()
+
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+
+			// check if alarm rules was present in JSON
+			if basemodels.FieldMaskContains(&fm, models.AlarmFieldAlarmRules) {
+
+				// check if alarm rules are valid
+				err = alarm.Validate()
+				if err != nil {
+					return errutil.ErrorBadRequestf(err.Error())
+				}
+			}
+
+			if response, err = sv.BaseService.UpdateAlarm(ctx, request); err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+	return response, err
+}
