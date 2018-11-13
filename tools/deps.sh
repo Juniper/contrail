a6b87a9761c9c518cac087b42a05ae0d8a00c64c
Mark Liggett <mliggett@juniper.net>
2018-11-13 12:32:16 +0000

    Fix for - make deps on osx #92

--- tools/deps.sh
+++ tools/deps.sh
@@ -17,14 +17,16 @@ go install ./vendor/github.com/golang/mock/mockgen
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | \
 	bash -s -- -b $(go env GOPATH)/bin v1.10.2
 
+protoc_ver="3.5.1"
+
 if [ "$(uname)" == 'Darwin' ]; then
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
-    unzip -o protoc-3.5.1-osx-x86_64.zip "bin/protoc"
-    rm protoc-3.5.1-osx-x86_64.zip
+    wget https://github.com/google/protobuf/releases/download/v${protoc_ver}/protoc-${protoc_ver}-osx-x86_64.zip -O protoc-${protoc_ver}-osx-x86_64.zip
+    unzip -o protoc-${protoc_ver}-osx-x86_64.zip "bin/protoc"
+    rm protoc-${protoc_ver}-osx-x86_64.zip
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
-    unzip -o protoc-3.5.1-linux-x86_64.zip "bin/protoc"
-    rm protoc-3.5.1-linux-x86_64.zip
+    wget https://github.com/google/protobuf/releases/download/v${protoc_ver}/protoc-${protoc_ver}-linux-x86_64.zip -O protoc-${protoc_ver}-linux-x86_64.zip
+    unzip -o protoc-${protoc_ver}-linux-x86_64.zip "bin/protoc"
+    rm protoc-${protoc_ver}-linux-x86_64.zip
 else
 	echo "Your platform ($(uname -a)) is not supported."
     echo "Please manually install protoc"
