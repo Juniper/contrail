888ea5dcd70b7dd7fa8631c31a9dde8996bf9561
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2018-11-15 16:18:21 +0100

    [WIP] Run Atom with Patroni cluster
    Change-Id: Ifcca5c6b1f1aed7fd81fa6a13c07bb2fbf5bbca9
    Partial-bug: 1803566

--- tools/reset_db_mysql.sh
+++ tools/reset_db_mysql.sh
@@ -5,6 +5,9 @@ TOP=/go/src/github.com/Juniper/contrail/tools
 echo "Mounts:"
 docker inspect -f '{{ range $i, $m := .Mounts }}{{ $m.Source }}:{{ $m.Destination }}{{"\n"}}{{end}}' contrail_mysql
 
+docker exec contrail_mysql ls -al /go/src/github.com/Juniper/contrail
+docker exec contrail_mysql ls -al $TOP
+
 docker exec contrail_mysql mysql -uroot -pcontrail123 -e "drop database if exists contrail_test;"
 docker exec contrail_mysql mysql -uroot -pcontrail123 -e "create database contrail_test;"
 docker exec --interactive contrail_mysql sh -c "mysql -uroot -pcontrail123 contrail_test < $TOP/init_mysql.sql"
