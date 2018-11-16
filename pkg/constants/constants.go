8122846b60d6b3fa4c48ec6575ccf7eaf73b91a9
Sajeesh Mathew <sajeeshm@juniper.net>
2018-11-15 21:10:45 -0800

    RBAC development
     Implemented API level RBAC . If rbac is enabled
     all resource access is permitted only based on the RBAC
     rules configured.
    
     ValidateRequest() API will be invoked from service.tmpl
     for all CRUD requests.
    
     TODO
         - Database access caching .
    
    Change-Id: I3558d693c669d5c25434a4be8f67684bb19ccf56

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -7,3 +7,14 @@ const (
 	UPDATE    = "update"
 	DELETE    = "delete"
 )
+
+// OpCRUD CRUD opeartions type
+type OpCRUD int
+
+// CRUD operations
+const (
+	OpCreate OpCRUD = 1
+	OpRead   OpCRUD = 2
+	OpUpdate OpCRUD = 3
+	OpDelete OpCRUD = 4
+)
