cb8b926567467f5249083d4a2038892533255931
Sajeesh Mathew <sajeeshm@juniper.net>
2018-11-15 21:10:45 -0800

    RBAC development
     Implemented API level RBAC . If rbac is enabled
     all resource access is permitted only based on the RBAC
     rules configured.
    
     ValidateRequest() API will be invoked from service.tmpl
     for all CRUD requests.
    
     Fixed code review comments.
    
     TODO
         - Database access caching .
    
    Change-Id: I3558d693c669d5c25434a4be8f67684bb19ccf56
    Closes-Bug: 1803617

---
+++ pkg/services/contrail_service.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/auth"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services/baseservices"
+)
+
+func (service *ContrailService) getAllAPIAccessLists(ctx context.Context) []*models.APIAccessList {
+
+	noAuthCtx := auth.NoAuth(ctx)
+	listRequest := &ListAPIAccessListRequest{
+		Spec: &baseservices.ListSpec{},
+	}
+	// Use a context with No auth for internal calls
+	result, err := service.ListAPIAccessList(noAuthCtx, listRequest)
+	if err != nil {
+		return nil
+	}
+
+	return result.APIAccessLists
+}
