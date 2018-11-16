6bfde2cc81c230931b1f29a8226eeff1fc962ba6
Sajeesh Mathew <sajeeshm@juniper.net>
2018-11-15 21:10:45 -0800

    ATOM RBAC development
     Implemented API level RBAC for ATOM. If rbac is enabled
     all resource access is permitted only based on the RBAC
     rules configured.
    
     ValidateRequest() API will be invoked from service.tmpl
     for all CRUD requests.
    
     TODO
         - Filter rbac rules based on domain
         - Database access caching .
    
    Change-Id: I3558d693c669d5c25434a4be8f67684bb19ccf56

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -7,3 +7,14 @@ const (
 	UPDATE    = "update"
 	DELETE    = "delete"
 )
+
+// OpCrud CRUD opeartions type
+type OpCrud int
+
+// CRUD operations
+const (
+	OpCreate OpCrud = 1
+	OpRead   OpCrud = 2
+	OpUpdate OpCrud = 3
+	OpDelete OpCrud = 4
+)
