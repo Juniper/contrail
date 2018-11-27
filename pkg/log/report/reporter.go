8b940c72b1c461d78a124c37862ac6b029048630
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2018-11-27 10:56:00 -0800

    Seperated generic funcs from cluster package
    These generic funcs can also be used by other packages like cloud
    
    Change-Id: Icefacab4e178390a68fe2f58b1e7e60e4b9f9ad5
    Partial-Bug: #1793603

---
+++ pkg/log/report/reporter.go
@@ -0,0 +1,53 @@
+package report
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/Juniper/contrail/pkg/apisrv/client"
+)
+
+// Reporter reports provisioing status and log
+type Reporter struct {
+	api      *client.HTTP
+	resource string
+	log      *logrus.Entry
+}
+
+// NewReporter creates a reporter object
+func NewReporter(apiServer *client.HTTP, resource string, logger *logrus.Entry) *Reporter {
+
+	return &Reporter{
+		api:      apiServer,
+		resource: resource,
+		log:      logger,
+	}
+}
+
+// ReportStatus reports status to a particular resource
+func (r *Reporter) ReportStatus(ctx context.Context, status map[string]interface{}, resource string) {
+	data := map[string]map[string]interface{}{resource: status}
+	var response interface{}
+	//TODO(nati) fixed context
+	_, err := r.api.Update(ctx, r.resource, data, &response)
+	if err != nil {
+		r.log.Infof("update %s status failed: %s", resource, err)
+	}
+	r.log.Infof("%s status updated: %s", resource, status)
+}
+
+// ReportLog reports log
+func (r *Reporter) ReportLog(stdio io.Reader) {
+	var output bytes.Buffer
+	scanner := bufio.NewScanner(stdio)
+	for scanner.Scan() {
+		m := scanner.Text()
+		output.WriteString(m)
+		r.log.Info(m)
+	}
+	//TODO(ijohnson) Implement status update to db
+}
---
+++ pkg/osutil/cmd.go
@@ -0,0 +1,34 @@
+package osutil
+
+import (
+	"os/exec"
+
+	"github.com/Juniper/contrail/pkg/log/report"
+)
+
+// ExecCmdAndWait execs cmd, reports the stdout & stderr and waits for cmd to complete
+func ExecCmdAndWait(r *report.Reporter, cmd string,
+	args []string, dir string) error {
+
+	cmdline := exec.Command(cmd, args...)
+	if dir != "" {
+		cmdline.Dir = dir
+	}
+	stdout, err := cmdline.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := cmdline.StderrPipe()
+	if err != nil {
+		return err
+	}
+	if err := cmdline.Start(); err != nil {
+		return err
+	}
+	// Report progress log periodically to stdout/db
+	go r.ReportLog(stdout)
+	go r.ReportLog(stderr)
+
+	// wait for command to complete
+	return cmdline.Wait()
+}
