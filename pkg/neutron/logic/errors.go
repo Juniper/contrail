f20159a0e4a393009ed3fafef3d492bed0bab7b0
Józef Wołoch <jozef.woloch@codilime.com>
2018-11-30 14:32:24 +0100

    Core vnc_openstack architecture
    Change-Id: I627a9260a3857f9f92264d47814d4721ac9286ba
    Partial-Bug: 1802271

---
+++ pkg/neutron/logic/errors.go
@@ -0,0 +1,44 @@
+package logic
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type NeutronError struct {
+	fields map[string]interface{}
+}
+
+func NewNeutronError(name string, fields map[string]interface{}) *NeutronError {
+	e := &NeutronError{
+		fields: fields,
+	}
+	if fields == nil {
+		e.fields = map[string]interface{}{}
+	}
+	e.fields["exception"] = name
+	return e
+}
+
+func (e *NeutronError) Error() string {
+	str, err := e.JSON()
+	if err != nil {
+		return fmt.Sprintf("failed to marshall error description. error: %v", err)
+	}
+	return str
+}
+
+func (e *NeutronError) JSON() (string, error) {
+	b, err := json.Marshal(e.fields)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// here is the place to define constants for neutron exception names
+// https://docs.openstack.org/neutron-lib/queens/reference/modules/neutron_lib.exceptions.html
+
+const (
+	BadRequest = "BadRequest"
+)
