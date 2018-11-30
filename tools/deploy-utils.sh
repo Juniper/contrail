27f3858818ca5afb73c5c5e122d9a8f484e08129
Tomasz Młynarczyk <t.mlynarczyk5@gmail.com>
2018-11-30 13:52:32 +0100

    Deploy contrail-go on contrail with openstack cluster
    Common part between deployment with openstack and k8s
    is extracted to deploy-utils.sh.
    This commit also enables building with OS in CI.
    
    Depends-On: I1746959c57c841c2ae9015898d11fdd16a0a2626
    Change-Id: Ic464b1cae22bb24028fcc592a9a5456edb3b04e3
    Partial-Bug: 1806019
    closes-jira-bug: JBE-673

---
+++ tools/deploy-utils.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+build_and_run_contrail-go_docker()
+{
+    local ContrailGoDocker='contrail-go-config-node'
+    [ "$(docker ps -a -f "name=$ContrailGoDocker" --format '{{.ID}}' | wc -l)" -ne 0 ] && docker rm -f "$ContrailGoDocker"
+    docker run -d --name "$ContrailGoDocker" --net host --volume /etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro \
+        contrail-go-config
+}
+
+# Modify k8s config (subst contrail-go-config IP address as config-node) and restart if needed
+ensure_kubemanager_config_nodes()
+{
+    local GoConfigIP="$1"
+    local ModifyKubeConfig=1
+    grep -qE "^CONFIG_NODES\\W*=\\W*$GoConfigIP" /etc/contrail/common_kubemanager.env && ModifyKubeConfig=0
+    if [ $ModifyKubeConfig -eq 1 ]; then
+        sudo sed "-ibak$(date +%s)" "s/^CONFIG_NODES=.*/CONFIG_NODES=$GoConfigIP/" /etc/contrail/common_kubemanager.env
+    fi
+}
+
+compose_up()
+{
+    for docker_dir in "$@"
+    do
+        docker-compose -f "/etc/contrail/${docker_dir}/docker-compose.yaml" up -d
+    done
+}
+
+compose_down()
+{
+    for docker_dir in "$@"
+    do
+        docker-compose -f "/etc/contrail/${docker_dir}/docker-compose.yaml" down
+    done
+}
+
+clear_config_database()
+{
+    docker-compose -f /etc/contrail/config_database/docker-compose.yaml down -v
+    docker-compose -f /etc/contrail/config_database/docker-compose.yaml up -d zookeeper
+}
+
+sync_contrail_openstack_projects()
+{
+    docker exec kolla_toolbox sh -c "\
+        source /var/lib/kolla/config_files/admin-openrc.sh && \
+        openstack project list -c ID -f value | \
+        python -c 'import sys,uuid; print \"\n\".join(str(uuid.UUID(proj_id.strip())) for proj_id in sys.stdin)' | \
+        xargs -I PROJ_ID curl $(uname -n):8082/project/PROJ_ID"
+}
