28533a34db25ff3d993b48bb5cdbf7c398c71455
Józef Wołoch <jozef.woloch@codilime.com>
2018-11-30 14:32:24 +0100

    Core vnc_openstack architecture
    Change-Id: I627a9260a3857f9f92264d47814d4721ac9286ba
    Partial-Bug: 1802271

---
+++ pkg/neutron/logic/common_test.go
@@ -0,0 +1,41 @@
+package logic_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Juniper/contrail/pkg/db/basedb"
+	"github.com/Juniper/contrail/pkg/fileutil"
+	"github.com/Juniper/contrail/pkg/format"
+	"github.com/Juniper/contrail/pkg/neutron/logic"
+	"github.com/Juniper/contrail/pkg/testutil"
+	"github.com/Juniper/contrail/pkg/testutil/integration"
+)
+
+func runTest(t *testing.T, test func(*testing.T, *integration.HTTPAPIClient)) {
+	for _, driver := range []string{basedb.DriverMySQL, basedb.DriverPostgreSQL} {
+		func() {
+			s := integration.NewRunningAPIServer(t, &integration.APIServerConfig{
+				DBDriver:     driver,
+				RepoRootPath: "../../..",
+			})
+			defer func() {
+				assert.NoError(t, s.Close())
+			}()
+
+			test(t, integration.NewTestingHTTPClient(t, s.URL()))
+		}()
+	}
+}
+
+func loadRequestFromJSONFile(t *testing.T, path string) *logic.Request {
+	r := &logic.Request{}
+	require.NoError(t, fileutil.LoadFile(path, r))
+	return r
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}) {
+	testutil.AssertEqual(t, format.MustYAML(expected), format.MustYAML(actual))
+}
---
+++ pkg/neutron/logic/request.go
@@ -0,0 +1,58 @@
+package logic
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
+// Request defines an API request.
+type Request struct {
+	Data    Data           `json:"data" yaml:"data"`
+	Context RequestContext `json:"context" yaml:"context"`
+}
+
+// Data defines API request data.
+type Data struct {
+	Filters  Filters  `json:"filters" yaml:"filters"`
+	ID       string   `json:"id" yaml:"id"`
+	Fields   Fields   `json:"fields" yaml:"fields"`
+	Resource Resource `json:"resource" yaml:"resource"`
+}
+
+// GetType returns resource type of the Request.
+func (r *Request) GetType() string {
+	return r.Context.Type
+}
+
+// UnmarshalJSON custom unmarshalling of Request
+func (r *Request) UnmarshalJSON(data []byte) error {
+	var rawJSON map[string]json.RawMessage
+	err := json.Unmarshal(data, &rawJSON)
+	if err != nil {
+		return err
+	}
+
+	err = parseField(rawJSON, "context", &r.Context)
+	if err != nil {
+		return err
+	}
+
+	resource, err := MakeResource(r.Context.Type)
+	if err != nil {
+		return err
+	}
+
+	r.Data.Resource = resource
+	return parseField(rawJSON, "data", &r.Data)
+}
+
+func parseField(rawJSON map[string]json.RawMessage, key string, dst interface{}) error {
+	if val, ok := rawJSON[key]; ok {
+		if err := json.Unmarshal(val, dst); err != nil {
+			return errors.Errorf("invalid '%s' format: %v", key, err)
+		}
+		delete(rawJSON, key)
+	}
+	return nil
+}
