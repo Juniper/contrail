65f8d4d322f004ae010a31fe7035484e94142bf6
Tomasz Młynarczyk <t.mlynarczyk5@gmail.com>
2018-11-30 13:52:32 +0100

    Deploy contrail-go on contrail with openstack cluster
    Common part between deployment with openstack and k8s
    is extracted to deploy-utils.sh.
    This commit also enables building with OS in CI.
    
    Depends-On: I1746959c57c841c2ae9015898d11fdd16a0a2626
    Change-Id: Ic464b1cae22bb24028fcc592a9a5456edb3b04e3
    Partial-Bug: 1806019
    closes-jira-bug: JBE-673

--- tools/deploy-for_openstack.sh
+++ tools/deploy-for_openstack.sh
@@ -6,9 +6,9 @@ set -o xtrace
 
 RealPath()
 {
-	pushd "$1" &> /dev/null
-	pwd
-	popd &> /dev/null
+    pushd "$1" &> /dev/null
+    pwd
+    popd &> /dev/null
 }
 
 ThisDir=$(RealPath "$(dirname "$0")")
@@ -22,6 +22,8 @@ source "$ThisDir/ensure_golang_installed.sh"
 
 ensure_golang_installed
 
+sync_contrail_openstack_projects
+
 ContrailRootDir=$(RealPath "$ThisDir/..")
 cd "$ContrailRootDir"
 make deps
@@ -34,36 +36,26 @@ make docker_config_api
 # Ensure patroni installed
 ./tools/patroni/pull_patroni.sh
 
-./tools/testenv.sh -n host patroni
+./tools/testenv.sh -n host patroni etcd
 
 # Stop services using docker-compose
-compose_down kubemanager config control vrouter
+compose_down config control vrouter
 
-# Clear old config-node databases
+# Dump config-node database
+contrailutil convert --intype cassandra --in "$(uname -n)" -p 9041 --outtype yaml --out db_dump.yaml
+
 clear_config_database
 
 # Prepare fresh database in contrail-go
 make zero_psql
 
-# Drop contrail related content from etcd
-docker exec "$(docker ps -q -f name=k8s_etcd_etcd)" sh -c "ETCDCTL_API=3 etcdctl del /contrail --prefix"
-
-# Build patched kube_manager with etcd support
-docker build "$ContrailRootDir/docker/kube_manager_etcd/" -t contrail-kubernetes-kube-manager:etcd
-
-# Update kube_manager docker compose file
-sudo ./tools/kube_manager_etcd/update-docker-compose.py
-
 # Update control-node docker compose file
 sudo ./tools/control-node_etcd/update-docker-compose.py
 
 # Load init data to rdbms
-contrailutil convert --intype yaml --in tools/init_data.yaml --outtype rdbms -c sample/contrail-config_api.yml
+contrailutil convert --intype yaml --in ./db_dump.yaml --outtype rdbms -c sample/contrail-config_api.yml
 
 build_and_run_contrail-go_docker
 
-GoConfigIP='127.0.0.1' # networking mode 'host'
-ensure_kubemanager_config_nodes "${GoConfigIP}"
-
 # Start services using docker-compose
-compose_up control vrouter kubemanager
+compose_up control vrouter
