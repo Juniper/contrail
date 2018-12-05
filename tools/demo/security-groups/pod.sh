94d862ce5757c482c2ca43fe79d7a01d424acc08
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-12-05 18:04:42 +0100

    [WIP] Fix minor problems in demos
    Change-Id: Ic7614723fcee30f7fee715eb955ad9e8057ca98e
    Closes-bug: 1806965

--- tools/demo/security-groups/pod.sh
+++ tools/demo/security-groups/pod.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-kubectl run -i --tty "$1" --image=busybox --attach=false -l "name=$1" --namespace "atom-pink"
+PATH="$PATH:/usr/go/bin"
+PATH="$(go env GOPATH)/bin/:$PATH"
+
+sudo kubectl run -i --tty "$1" --image=busybox --attach=false -l "name=$1" --namespace "atom-pink"
 POD_NAME=$(kubectl get pod -l "name=$1" -o jsonpath="{.items[0].metadata.name}" --namespace "atom-pink")
 
 echo "Waiting for container to start..."
-kubectl rollout status deployment "$1" --namespace "atom-pink"
+sudo kubectl rollout status deployment "$1" --namespace "atom-pink"
 
 cat > echo.sh << EOF
 echo "Running simple HTTP server on port 8080..."
 while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo 'echo'; } | nc -l -p 8080; done
 EOF
 chmod +x echo.sh
-kubectl cp echo.sh "$POD_NAME:/echo.sh" --namespace "atom-pink"
+sudo kubectl cp echo.sh "$POD_NAME:/echo.sh" --namespace "atom-pink"
 
 echo "Container running, pod name: $POD_NAME"
 echo "To attach a shell, run 'kubectl attach -it $POD_NAME --namespace atom-pink' as root"
