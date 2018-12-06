032af6bcae4d77b50aac1daa9933cd03374110bf
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-12-06 14:05:14 +0100

    WIP - provide correct hostname in control node
    Change-Id: Icc598849d4da14210ebbd145d1fe59c59a11f1ce

--- tools/control-node_etcd/update-docker-compose.py
+++ tools/control-node_etcd/update-docker-compose.py
@@ -6,7 +6,7 @@ docker_compose_path = "/etc/contrail/control/docker-compose.yaml"
 with open(docker_compose_path) as f:
     docker_compose = yaml.load(f)
 
-docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync"
+docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:fixed_etcd"
 
 with open(docker_compose_path, "w") as f:
     yaml.dump(docker_compose, f)
