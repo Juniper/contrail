0cc00e775e5fc0e6bca8eb3f93d3d5c38ff4538d
aakimov <github@akimov.me>
2018-12-07 16:03:06 +0300

    Create PhysicalInterface missing logic
    Change-Id: Ifa77e7f6a72a9b0003104ef1930d60e4b2e91907
    Closes-Bug: #1807383

--- pkg/apisrv/server_test.go
+++ pkg/apisrv/server_test.go
@@ -195,6 +195,11 @@ func TestAlarm(t *testing.T) {
 	RunTest(t, "./test_data/test_alarm.yml")
 }
 
+func TestPhysicalInterface(t *testing.T) {
+	integration.AddKeystoneProjectAndUser(server.APIServer, t.Name())
+	RunTest(t, "./test_data/test_physical_interface.yml")
+}
+
 func restLogin(ctx context.Context, t *testing.T) (authToken string) {
 	restClient := client.NewHTTP(
 		server.URL(),
---
+++ pkg/types/physical_interface_test.go
@@ -0,0 +1,204 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/models"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/Juniper/contrail/pkg/services"
+	servicesmock "github.com/Juniper/contrail/pkg/services/mock"
+)
+
+type testPhysicalInterfaceParams struct {
+	UUID                      string
+	DisplayName               string
+	EthernetSegmentIdentifier string
+}
+
+func physicalInterfaceNextServMocks(service *ContrailTypeLogicService) {
+	nextServiceMock := service.Next().(*servicesmock.MockService) //nolint: errcheck
+	nextServiceMock.EXPECT().CreatePhysicalInterface(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(
+			_ context.Context, request *services.CreatePhysicalInterfaceRequest,
+		) (response *services.CreatePhysicalInterfaceResponse, err error) {
+			return &services.CreatePhysicalInterfaceResponse{PhysicalInterface: request.PhysicalInterface}, nil
+		}).AnyTimes()
+	nextServiceMock.EXPECT().UpdatePhysicalInterface(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(
+			_ context.Context, request *services.UpdatePhysicalInterfaceRequest,
+		) (response *services.UpdatePhysicalInterfaceResponse, err error) {
+			return &services.UpdatePhysicalInterfaceResponse{PhysicalInterface: request.PhysicalInterface}, nil
+		}).AnyTimes()
+}
+
+func physicalInterfaceReadServiceMocks(
+	s *ContrailTypeLogicService,
+	parentRouter *models.PhysicalRouter,
+) {
+	readService := s.ReadService.(*servicesmock.MockReadService) //nolint: errcheck
+	if parentRouter == nil {
+		parentRouter = new(models.PhysicalRouter)
+	}
+	readService.EXPECT().GetPhysicalRouter(gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil())).Return(
+		&services.GetPhysicalRouterResponse{
+			PhysicalRouter: parentRouter,
+		}, nil).AnyTimes()
+	// Try to find requested physical interface within parent router data
+	readService.EXPECT().GetPhysicalInterface(gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil())).DoAndReturn(
+		func(
+			_ context.Context, request *services.GetPhysicalInterfaceRequest,
+		) (response *services.GetPhysicalInterfaceResponse, err error) {
+			for _, pi := range parentRouter.PhysicalInterfaces {
+				if pi.UUID == request.ID {
+					return &services.GetPhysicalInterfaceResponse{PhysicalInterface: pi}, nil
+				}
+			}
+			return nil, grpc.Errorf(codes.NotFound, "physical interface with uuid %s not found", request.ID)
+		}).AnyTimes()
+}
+
+func TestCreatePhysicalInterface(t *testing.T) {
+	tests := []struct {
+		name          string
+		createRequest *services.CreatePhysicalInterfaceRequest
+		parentRouter  *models.PhysicalRouter
+		errorCode     codes.Code
+	}{
+		{
+			name: "Try create physical interface with empty display name and ESI",
+			createRequest: &services.CreatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID: "uuid",
+			}},
+			errorCode: codes.OK,
+		},
+		{
+			name: "Try create physical interface with valid display name and ESI",
+			createRequest: &services.CreatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID:                      "uuid",
+				DisplayName:               "unique",
+				EthernetSegmentIdentifier: "01:23:45:67:89:1A:BC:DE:F0:01",
+			}},
+			parentRouter: &models.PhysicalRouter{
+				PhysicalInterfaces: []*models.PhysicalInterface{{
+					UUID:        "uuid",
+					DisplayName: "other",
+				}},
+			},
+			errorCode: codes.OK,
+		},
+		{
+			name: "Try create physical interface with same display name",
+			createRequest: &services.CreatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID:        "uuid",
+				DisplayName: "double",
+			}},
+			parentRouter: &models.PhysicalRouter{
+				PhysicalInterfaces: []*models.PhysicalInterface{{
+					UUID:        "uuid",
+					DisplayName: "double",
+				}},
+			},
+			errorCode: codes.AlreadyExists,
+		},
+		{
+			name: "Try create physical interface with wrong ESI",
+			createRequest: &services.CreatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID:                      "uuid",
+				EthernetSegmentIdentifier: "0123:4567:891A:BCDE:F001",
+			}},
+			errorCode: codes.InvalidArgument,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			service := makeMockedContrailTypeLogicService(mockCtrl)
+			physicalInterfaceNextServMocks(service)
+			ctx := context.Background()
+
+			physicalInterfaceReadServiceMocks(service, tt.parentRouter)
+
+			createPhysicalInterfaceResponse, err := service.CreatePhysicalInterface(ctx, tt.createRequest)
+			if tt.errorCode != codes.OK {
+				assert.Error(t, err, "create succeeded but shouldn't")
+				assert.Nil(t, createPhysicalInterfaceResponse)
+
+				status, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, tt.errorCode, status.Code())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, createPhysicalInterfaceResponse)
+			}
+		})
+	}
+}
+
+func TestUpdatePhysicalInterface(t *testing.T) {
+	tests := []struct {
+		name          string
+		updateRequest *services.UpdatePhysicalInterfaceRequest
+		parentRouter  *models.PhysicalRouter
+		errorCode     codes.Code
+	}{
+		{
+			name: "Try update physical interface without display name and ESI",
+			updateRequest: &services.UpdatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID: "uuid",
+			}},
+			parentRouter: &models.PhysicalRouter{
+				PhysicalInterfaces: []*models.PhysicalInterface{{
+					UUID: "uuid",
+				}},
+			},
+			errorCode: codes.OK,
+		},
+		{
+			name: "Try update physical interface with different display name",
+			updateRequest: &services.UpdatePhysicalInterfaceRequest{PhysicalInterface: &models.PhysicalInterface{
+				UUID:        "uuid",
+				DisplayName: "second",
+			}},
+			parentRouter: &models.PhysicalRouter{
+				PhysicalInterfaces: []*models.PhysicalInterface{{
+					UUID:        "uuid",
+					DisplayName: "first",
+				}},
+			},
+			errorCode: codes.InvalidArgument,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			service := makeMockedContrailTypeLogicService(mockCtrl)
+			physicalInterfaceNextServMocks(service)
+			ctx := context.Background()
+
+			physicalInterfaceReadServiceMocks(service, tt.parentRouter)
+
+			updatePhysicalInterfaceResponse, err := service.UpdatePhysicalInterface(ctx, tt.updateRequest)
+			if tt.errorCode != codes.OK {
+				assert.Error(t, err, "update succeeded but shouldn't")
+				assert.Nil(t, updatePhysicalInterfaceResponse)
+
+				status, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, tt.errorCode, status.Code())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, updatePhysicalInterfaceResponse)
+			}
+		})
+	}
+}
