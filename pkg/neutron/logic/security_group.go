a3f8220f29c950373f7fa8a83c6d30c4405cbe06
Pawel Drapiewski <pawel.drapiewski@codilime.com>
2018-12-11 13:41:38 +0100

    Neutron security group READ
    * Integration tests in yaml. No unit tests, because there is no need to
    duplicate the integration ones.
    * Additionally I cleaned service.go after invalid merging (caused by me). Now it should be similar to that one from master branch.
    * Fix bug in Error handling from file service.
    
    Change-Id: I62844bcf441ea63ec48eacb23d1bff23aab46a79
    Partial-Bug: 1802271
    closes-jira-bug: JBE-765

---
+++ pkg/neutron/logic/security_group.go
@@ -0,0 +1,140 @@
+package logic
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/Juniper/contrail/pkg/errutil"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+const (
+	localSecurityGroup = "local"
+)
+
+func (s *SecurityGroup) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
+	resp, err := rp.ReadService.GetSecurityGroup(ctx, &services.GetSecurityGroupRequest{
+		ID: id,
+	})
+	if err != nil {
+		if errutil.IsNotFound(err) {
+			err = NewNeutronError("SecurityGroupNotFound", map[string]interface{}{
+				"id": id,
+			})
+		} else {
+			err = NewNeutronError(BadRequest, nil)
+		}
+		return nil, err
+	}
+	return securityGroupContrailToNeutron(resp.SecurityGroup)
+}
+
+func securityGroupContrailToNeutron(sg *models.SecurityGroup) (sgNeutron *SecurityGroupResponse, err error) {
+
+	sgNeutron = &SecurityGroupResponse{
+		ID:          sg.GetUUID(),
+		TenantID:    contrailUUIDToNeutronID(sg.GetParentUUID()),
+		CreatedAt:   sg.GetIDPerms().GetCreated(),
+		UpdatedAt:   sg.GetIDPerms().GetLastModified(),
+		Description: sg.GetIDPerms().GetDescription(),
+	}
+
+	if sg.GetDisplayName() != "" {
+		sgNeutron.Name = sg.GetDisplayName()
+	} else {
+		fqName := sg.GetFQName()
+		sgNeutron.Name = fqName[len(fqName)-1]
+	}
+
+	sgNeutron.SecurityGroupRules, err = readSecurityGroupRules(sg)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Implement 'if loop' when contrail extension will be available.
+	sgNeutron.FQName = sg.GetFQName()
+
+	return sgNeutron, nil
+}
+
+func readSecurityGroupRules(sg *models.SecurityGroup) (rules []*SecurityGroupRuleResponse, err error) {
+	for _, rule := range sg.GetSecurityGroupEntries().GetPolicyRule() {
+		sgr, err := securityGroupRuleContrailToNeutron(sg, rule)
+		if err != nil {
+			return nil, err
+		}
+		rules = append(rules, sgr)
+	}
+
+	return rules, nil
+}
+
+func securityGroupRuleContrailToNeutron(
+	sg *models.SecurityGroup, rule *models.PolicyRuleType,
+) (*SecurityGroupRuleResponse, error) {
+
+	sgr := SecurityGroupRuleResponse{
+		ID:              rule.GetRuleUUID(),
+		TenantID:        contrailUUIDToNeutronID(sg.GetParentUUID()),
+		CreatedAt:       rule.GetCreated(),
+		UpdatedAt:       rule.GetLastModified(),
+		SecurityGroupID: sg.GetUUID(),
+		Ethertype:       rule.GetEthertype(),
+		Protocol:        rule.GetProtocol(),
+	}
+
+	if err := castAddressTypeContrailToNeutron(rule, sg, &sgr); err != nil {
+		return nil, err
+	}
+
+	if len(rule.GetDSTPorts()) != 0 {
+		sgr.PortRangeMin = rule.GetDSTPorts()[0].GetStartPort()
+		sgr.PortRangeMax = rule.GetDSTPorts()[0].GetEndPort()
+	} else {
+		sgr.PortRangeMin = 0
+		sgr.PortRangeMax = 65535
+	}
+
+	return &sgr, nil
+}
+
+func castAddressTypeContrailToNeutron(
+	rule *models.PolicyRuleType,
+	sg *models.SecurityGroup,
+	sgr *SecurityGroupRuleResponse,
+) error {
+	var addr *models.AddressType
+	srcAddr := rule.GetSRCAddresses()[0]
+	dstAddr := rule.GetDSTAddresses()[0]
+
+	if srcAddr.GetSecurityGroup() == localSecurityGroup {
+		sgr.Direction = "egress"
+		addr = dstAddr
+	} else if dstAddr.GetSecurityGroup() == localSecurityGroup {
+		sgr.Direction = "ingress"
+		addr = srcAddr
+	} else {
+		return NewNeutronError("SecurityGroupRuleNotFound", map[string]interface{}{
+			"id": rule.GetRuleUUID(),
+		})
+	}
+
+	if subnet := addr.GetSubnet(); subnet != nil {
+		sgr.RemoteIPPrefix = getFullNetworkAddress(subnet.GetIPPrefix(), subnet.GetIPPrefixLen())
+	} else if remoteSG := addr.GetSecurityGroup(); remoteSG != "" && remoteSG != "any" && remoteSG != localSecurityGroup {
+		if remoteSG != basemodels.FQNameToString(sg.GetFQName()) {
+			// TODO implement it when service FQNameToID will be available in Neutron package.
+			// Origin python code: /src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py:1273
+		} else {
+			sgr.RemoteGroupID = sg.GetUUID()
+		}
+	}
+
+	return nil
+}
+
+func getFullNetworkAddress(ip string, cidr int64) string {
+	return ip + "/" + strconv.FormatInt(cidr, 10)
+}
