ddd2479c3070c5f0b9fe8c01bc5a8c2ec5e82912
Pawel Zadrozny <pawel.zny@gmail.com>
2018-12-12 13:08:34 +0100

    Neutron: Subnet ReadALL
    Read all method for vnc_openstack subnet resource.
    
    Change-Id: I921535c45494a2663b04067c85463e2bd6807281
    closes-jira-bug: JBE-773
    Partial-Bug: #1802271

---
+++ pkg/neutron/logic/filters.go
@@ -0,0 +1,18 @@
+package logic
+
+// filtersHas checks if one or more keys are present in filters.
+// Will return true if at least one key has been defined and all keys are present and not empty.
+func filtersHas(filters Filters, keys ...string) bool {
+	if len(keys) == 0 {
+		return false
+	}
+
+	for _, key := range keys {
+		filter, ok := filters[key]
+		if !ok || len(filter) == 0 {
+			return false
+		}
+	}
+
+	return true
+}
