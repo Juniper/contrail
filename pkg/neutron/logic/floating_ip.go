252ff133b91ae6195efc04cf0501a10d442053e7
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-12-12 14:26:44 +0100

    Adding FloatingIP Schema and ReadAll stub
    Change-Id: I3182ea6776d8c99092a6174f31fe2ebfa133eccb
    Partial-Bug: 1802271

---
+++ pkg/neutron/logic/floating_ip.go
@@ -0,0 +1,7 @@
+package logic
+
+// ReadAll logic
+func (fip *Floatingip) ReadAll(ctx RequestParameters, filters Filters, fields Fields) (Response, error) {
+	// TODO implement ReadAll logic
+	return []FloatingipResponse{}, nil
+}
