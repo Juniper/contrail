817c6bd9033eb1102020fb8bed2a60fa0f9bb1d8
Pawel Zadrozny <pawel.zny@gmail.com>
2018-12-12 13:08:34 +0100

    [WIP] Neutron: Subnet ReadALL
    Change-Id: I921535c45494a2663b04067c85463e2bd6807281
    Partial-Bug: #1802271

---
+++ pkg/neutron/logic/netutils.go
@@ -0,0 +1,25 @@
+package logic
+
+import (
+	"net"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	ipV4 = 4
+	ipV6 = 6
+)
+
+func getIPVersion(ipAddress string) (int8, error) {
+	ip := net.ParseIP(ipAddress)
+	if ip == nil {
+		return -1, errors.Errorf("Invalid ip address: %s", ipAddress)
+	}
+
+	if v := ip.To4(); v != nil {
+		return ipV4, nil
+	}
+
+	return ipV6, nil
+}
