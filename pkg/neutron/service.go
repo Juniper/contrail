cdbfde55b951d7e85bc94783575de4f2288508d6
Pawel Zadrozny <pawel.zny@gmail.com>
2018-12-12 13:08:34 +0100

    Neutron: Subnet ReadALL and Network Create, Read, ReadAll Minimal implementation of Network and Subnet read for Neutron
    Co-authored-by: Pawel Zadrozny <pawel.zny@gmail.com>
    Co-authored-by: Pawel Marchewka <pawel.marchewka@codilime.com>
    Co-authored-by: Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
    
    Partial-Bug: 1802271
    closes-jira-bug: JBE-776
    closes-jira-bug: JBE-775
    closes-jira-bug: JBE-773
    
    Change-Id: I8b98e7146d29c5d30f8c60e6d5deb33f77c447c5

--- pkg/neutron/service.go
+++ pkg/neutron/service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 
+	google_protobuf3 "github.com/gogo/protobuf/types"
 	"github.com/labstack/echo"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -18,6 +19,7 @@ import (
 type Service struct {
 	ReadService  services.ReadService
 	WriteService services.WriteService
+	UserAgentKV  userAgentKVServer
 }
 
 // RegisterNeutronAPI registers Neutron endpoints on given routeRegistry.
@@ -53,6 +55,7 @@ func (s *Service) handle(ctx context.Context, r *logic.Request) (logic.Response,
 	rp := logic.RequestParameters{
 		ReadService:    s.ReadService,
 		WriteService:   s.WriteService,
+		UserAgentKV:    s.UserAgentKV,
 		RequestContext: r.Context,
 	}
 	switch r.Context.Operation {
@@ -77,6 +80,13 @@ func (s *Service) handle(ctx context.Context, r *logic.Request) (logic.Response,
 		log.WithError(err).WithField("request", r).Errorf("failed to handle")
 		return nil, err
 	}
+}
+
+type userAgentKVServer interface {
+	StoreKeyValue(context.Context, *services.StoreKeyValueRequest) (*google_protobuf3.Empty, error)
+	RetrieveValues(context.Context, *services.RetrieveValuesRequest) (*services.RetrieveValuesResponse, error)
+	RetrieveKVPs(context.Context, *google_protobuf3.Empty) (*services.RetrieveKVPsResponse, error)
+	DeleteKey(context.Context, *services.DeleteKeyRequest) (*google_protobuf3.Empty, error)
 }
 
 type routeRegistry interface {
