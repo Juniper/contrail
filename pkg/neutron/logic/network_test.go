77be37744d4e6b87996f87eddc1588b0b4fc1a43
Pawel Zadrozny <pawel.zny@gmail.com>
2018-12-12 13:08:34 +0100

    Neutron: Subnet ReadALL and Network Create, Read, ReadAll Minimal implementation of Network and Subnet read for Neutron
    Co-authored-by: Pawel Zadrozny <pawel.zny@gmail.com>
    Co-authored-by: Pawel Marchewka <pawel.marchewka@codilime.com>
    Co-authored-by: Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
    
    Partial-Bug: 1802271
    closes-jira-bug: JBE-776
    closes-jira-bug: JBE-775
    closes-jira-bug: JBE-773
    
    Author:     Kamil Renczewski <kamil.renczewski@codilime.com>
    Change-Id: I8b98e7146d29c5d30f8c60e6d5deb33f77c447c5

---
+++ pkg/neutron/logic/network_test.go
@@ -0,0 +1,122 @@
+package logic
+
+import (
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"context"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	servicesmock "github.com/Juniper/contrail/pkg/services/mock"
+)
+
+func TestNetworkReadAll(t *testing.T) {
+	type readData struct {
+		vnRes *services.ListVirtualNetworkResponse
+	}
+
+	tests := []struct {
+		name     string
+		n        *Network
+		filters  Filters
+		fail     bool
+		readData *readData
+		expRes   Response
+	}{
+		{
+			name: "read all vns",
+			n:    &Network{},
+			filters: Filters{
+				"shared":    []string{"false"},
+				"tenant_id": []string{"uuidp"},
+			},
+			readData: &readData{
+				vnRes: &services.ListVirtualNetworkResponse{
+					VirtualNetworks: []*models.VirtualNetwork{
+						{
+							UUID:        "uuid-vn",
+							ParentType:  "project",
+							ParentUUID:  "uuid-p",
+							DisplayName: "vn-test",
+							IDPerms: &models.IdPermsType{
+								Enable:       true,
+								Created:      "data1",
+								LastModified: "data2",
+							},
+							NetworkIpamRefs: []*models.VirtualNetworkNetworkIpamRef{
+								{
+									Attr: &models.VnSubnetsType{
+										IpamSubnets: []*models.IpamSubnetType{
+											{
+												SubnetUUID: "uuid-sn",
+												Subnet: &models.SubnetType{
+													IPPrefix: "10.0.1.0",
+													IPPrefixLen: 24,
+												},
+											},
+										},
+									},
+									To: []string{"fqname", "ipam", "test"},
+								},
+							},
+							PortSecurityEnabled: true,
+							FQName:              []string{"fqname", "vn", "test"},
+						},
+					},
+				},
+			},
+			expRes: []*NetworkResponse{
+				{
+					Status:              netStatusActive,
+					RouterExternal:      false,
+					Subnets:             []string{"uuid-sn"},
+					FQName:              []string{"fqname", "vn", "test"},
+					Name:                "vn-test",
+					AdminStateUp:        true,
+					TenantID:            "uuidp",
+					ProjectID:           "uuidp",
+					CreatedAt:           "data1",
+					UpdatedAt:           "data2",
+					PortSecurityEnabled: true,
+					Shared:              false,
+					ID:                  "uuid-vn",
+					SubnetIpam: []*SubnetIpam{
+						{
+							SubnetCidr: "10.0.1.0/24",
+							IpamFQName: []string{"fqname", "ipam", "test"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockServ := servicesmock.NewMockService(mockCtrl)
+			ctx := context.Background()
+
+			if tt.readData.vnRes != nil {
+				mockServ.EXPECT().ListVirtualNetwork(gomock.Any(), gomock.Any()).Return(tt.readData.vnRes, nil)
+			}
+
+			rp := RequestParameters{
+				ReadService:  mockServ,
+				WriteService: mockServ,
+			}
+
+			r, err := tt.n.ReadAll(ctx, rp, nil, nil)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expRes, r)
+
+			if tt.fail {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
