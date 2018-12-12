6634ca4a4a3e6148776475f4f27508d2aa30f61b
Sajeesh Mathew <sajeeshm@juniper.net>
2018-12-12 13:32:52 -0800

    RBAC development
    Implemented API level RBAC . If rbac is enabled
    all resource access is permitted only based on the RBAC
    rules configured.
    
    ValidateRequest() API will be invoked from service.tmpl
    for all CRUD requests.
    
    Fixed code review comments.Made changes to support caching
    later
    
    TODO
        - Database access caching .
    
    Change-Id: I3558d693c669d5c25434a4be8f67684bb19ccf56
    Closes-Bug: 1803617

---
+++ pkg/apisrv/rbac/rbac.go
@@ -0,0 +1,571 @@
+package rbac
+
+import (
+	"context"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/Juniper/contrail/pkg/auth"
+	"github.com/Juniper/contrail/pkg/errutil"
+	"github.com/Juniper/contrail/pkg/models"
+)
+
+// CRUD operations rune constants
+const (
+	UpperCaseC rune = 'C'
+	UpperCaseR rune = 'R'
+	UpperCaseU rune = 'U'
+	UpperCaseD rune = 'D'
+)
+
+// Action enumerates CRUD actions.
+type Action int
+
+// Enumeration of Actions.
+const (
+	ActionInvalid Action = iota
+	ActionCreate
+	ActionRead
+	ActionUpdate
+	ActionDelete
+)
+
+const aaaModeRBAC = "rbac"
+
+type actionSet map[Action]struct{}
+
+// allowedActions is a hash table of roles and it's allowed actions.
+type allowedActions map[string]actionSet
+
+type resourceKey string
+
+// resourcePermission is the 2st level hash table to get kind specific rules.
+type resourcePermission map[resourceKey]allowedActions
+
+type tenantKey string
+
+// tenantPermission is the 1st level hash table to get tenant specific rules.
+type tenantPermission map[tenantKey]resourcePermission
+
+// globalPermission is the global hash table with RBAC rules. Currently it will be populated for every request. Later
+// it will be enhanced to get updated only with a database change for APIAccessList config.
+var globalPermission = make(tenantPermission)
+
+// request structure.
+type request struct {
+	op      Action          // Requested CRUD operation.
+	roles   map[string]bool // Roles of the user making request.
+	kind    string          // kind of the resource.
+	domain  string          // Domain name of the user.
+	project string          // project name of the user.
+}
+
+// CheckPermissions checks whether resource operation is allowed based on RBAC config.
+func CheckPermissions(ctx context.Context, l []*models.APIAccessList, aaaMode string, kind string, op Action) error {
+
+	if ctx == nil {
+
+		return errutil.ErrorForbiddenf("invalid context. access denied")
+	}
+
+	authCtx := auth.GetAuthCTX(ctx)
+
+	if authCtx == nil {
+
+		return errutil.ErrorForbiddenf("invalid auth context. access denied ")
+	}
+
+	if validateRoleAndConfig(authCtx, aaaMode) {
+		return nil
+	}
+
+	if isROAccessAllowed(authCtx, op) {
+		return nil
+	}
+
+	// If no rules to allow access, deny permission
+
+	if len(l) == 0 {
+
+		return errutil.ErrorForbiddenf("no API access list rules present.access denied ")
+	}
+
+	g := make(tenantPermission)
+	err := g.accessListAdd(l)
+	if err != nil {
+		return err
+	}
+
+	if g.validateAPILevel(newRequest(ctx, kind, op)) {
+		return nil
+	}
+
+	return errutil.ErrorForbiddenf("RBAC : no matching  API access list rules. access denied ")
+
+}
+
+// getTenantKey get the Hash key for first level hash.
+func getTenantKey(projectName string, domainName string) tenantKey {
+	return tenantKey(projectName + "." + domainName)
+}
+
+// addTenantLevelPolicy create a project specific policy.
+func (t tenantPermission) addTenantLevelPolicy(
+	global []*models.APIAccessList,
+	domain map[string][]*models.APIAccessList,
+	project map[tenantKey][]*models.APIAccessList,
+) error  {
+	for k, list := range project {
+
+		s := strings.Split(string(k), ".")
+		domainName := s[0]
+		domainList := domain[domainName]
+		list = append(list, global...)
+		list = append(list, domainList...)
+		entry, err  := t.addAPIAccessRulesToHash(list)
+		if err != nil {
+		  return err
+		}
+		t[k] = entry
+	}
+	return nil
+}
+
+// addDomainLevelPolicy create a domain specific policy.
+func (t tenantPermission) addDomainLevelPolicy(
+	global []*models.APIAccessList, domain map[string][]*models.APIAccessList) error {
+	for k, list := range domain {
+		list = append(list, global...)
+		key := getTenantKey(k, "*")
+		entry, err := t.addAPIAccessRulesToHash(list)
+		if err != nil {
+		  return err
+		}
+		t[key] = entry
+	}
+	return nil
+}
+
+// addGlobalLevelPolicy create a  global policy.
+func (t tenantPermission) addGlobalLevelPolicy(global []*models.APIAccessList) error {
+
+	key := getTenantKey("*", "*")
+	entry, err := t.addAPIAccessRulesToHash(global)
+	if err != nil {
+	  return err
+	}
+	t[key] = entry
+	return nil
+}
+
+// accessListAdd creates project, domain and  global policies.
+func (t tenantPermission) accessListAdd(lists []*models.APIAccessList) error {
+
+	global := make([]*models.APIAccessList, 0)
+	domains := make(map[string][]*models.APIAccessList)
+	projects := make(map[tenantKey][]*models.APIAccessList)
+
+	for _, list := range lists {
+
+		parentType := list.GetParentType()
+		fqName := list.GetFQName()
+
+		switch parentType  {
+			case  models.KindGlobalSystemConfig :
+				global = append(global, list)
+			case   models.KindDomain  :
+				domainName := fqName[0]
+				domains[domainName] = append(domains[domainName], list)
+			case    models.KindProject :
+				domainName, projectName := fqName[0], fqName[1]
+				key := getTenantKey(domainName, projectName)
+				projects[key] = append(projects[key], list)
+			default :
+				return errutil.ErrorInternalf("invalid parent type present in RBAC rule.")
+		}
+	}
+
+	err := t.addTenantLevelPolicy(global, domains, projects)
+	if err != nil {
+	   return err
+	}
+
+	err = t.addDomainLevelPolicy(global, domains)
+	if err != nil {
+	   return err
+	}
+
+	err = t.addGlobalLevelPolicy(global)
+	if err != nil {
+	   return err
+	}
+
+	return nil
+}
+
+// addAPIAccessRulesToHash adds rules matching kind and wild card rules from the rule list to
+// resourcePermission table.
+// For example input apiAccessRules may contains rules from global-config, default-domain & project
+// different all object types. There can be multiple roles and CRUD for every object type.
+// Following  is an input example.
+//
+// Rule 1
+// 1)  <global-config, virtual-network, network-policy> => Member:CRUD
+// Rule 2
+// 2)  <default-domain,virtual-network, network-ipam> => Development:CRUD
+// Rule 3
+// 1)  <project,virtual-ip, *>		 => Member:CRUD
+// 2)  <project,virtual-network, *>     => Member:CRUD, Development:CRUD
+//
+// This Function will do the following
+// i) Filter Rules based on project and domain
+// ii) Create request.hash in the following form
+//  kind.* ==>{ role1 : actionSet1  ,  role2 : actionSet2 }
+// For example result could be
+// virtual-network.* ==> { Member 	 : { ActionCreate:true,ActionDelete:true  }  ,
+//			    Development  : { ActionUpdate:true } }
+func (t tenantPermission) addAPIAccessRulesToHash(apiAccessRules []*models.APIAccessList) (resourcePermission,error ) {
+
+	res := make(resourcePermission)
+	for _, apiRule := range apiAccessRules {
+
+		rbacRules := getRBACRules(apiRule)
+		err := res.rulesAddToHash(rbacRules)
+		if err != nil {
+		   return nil,err
+		}
+	}
+
+	return res,nil
+}
+
+// getProjectHashTable retrieves matching first level hash table entry .
+func (t tenantPermission) getProjectHashTable(rq *request) resourcePermission {
+
+	key := getTenantKey(rq.domain, rq.project)
+
+	if val, ok := t[key]; ok {
+		return val
+	}
+	key = getTenantKey("*", rq.project)
+
+	if val, ok := t[key]; ok {
+		return val
+	}
+	key = getTenantKey("*", "*")
+
+	if val, ok := t[key]; ok {
+		return val
+	}
+
+	return nil
+}
+
+// validateResourceAPILevel checks whether any hash entry is matching the key.
+func (t *tenantPermission) validateResourceAPILevel(rq *request, kind string) bool {
+
+	projectHashTable := t.getProjectHashTable(rq)
+
+	if projectHashTable == nil {
+		return false
+	}
+
+	key := getResourceHashKey(kind)
+	if val, ok := projectHashTable[key]; ok {
+
+		return val.isRuleMatching(rq.roles, rq.op)
+
+	}
+	return false
+}
+
+// validateAPILevel checks whether any obj type rules or wildcard allow this operation
+// for any roles which user is part of.
+func (t *tenantPermission) validateAPILevel(rq *request) bool {
+
+	// check for objtype rule .
+	if t.validateResourceAPILevel(rq, rq.kind) {
+		return true
+	}
+
+	// check for wild card rule
+	if t.validateResourceAPILevel(rq, "*") {
+		return true
+	}
+
+	return false
+}
+
+// crudToActionMap converts crud string to op slice.
+func crudToActionMap(crud string) ([]Action, error) {
+
+	var res []Action
+
+	for _, c := range crud {
+		switch c {
+		case UpperCaseC:
+			res = append(res, ActionCreate)
+		case UpperCaseR:
+			res = append(res, ActionRead)
+		case UpperCaseU:
+			res = append(res, ActionUpdate)
+		case UpperCaseD:
+			res = append(res, ActionDelete)
+		default:
+			log.Debug("Invalid crud operation")
+			return nil,errutil.ErrorInternalf("invalid crud action present in RBAC rule.")
+		}
+	}
+	return res,nil
+}
+
+// isROAccessAllowed checks whether is Read only  Resource access is allowed if operation is READ.
+func isROAccessAllowed(ctx *auth.Context, op Action) bool {
+
+	if op != ActionRead {
+		return false
+	}
+
+	if ctx.IsGlobalRORole() {
+		return true
+	}
+
+	return false
+}
+
+func newAllowedAction() allowedActions {
+
+	return make(map[string]actionSet)
+}
+
+// updateEntry adds a new role to action  mapping entry. For example, "Member" ==> "CR".
+func (m allowedActions) updateEntry(role string, crud []Action) {
+
+	if cs, ok := m[role]; ok {
+
+		cs.setCRUD(crud)
+
+	} else {
+		n := newCRUDSet()
+		n.setCRUD(crud)
+		m[role] = n
+	}
+	return
+}
+
+// isRuleMatching checks whether there is any rule is allowing this op.
+func (m allowedActions) isRuleMatching(roleSet map[string]bool, op Action) bool {
+
+	// Check against all user roles
+	for role := range roleSet {
+		if cs, ok := m[role]; ok {
+
+			return cs.isActionAllowed(op)
+		}
+	}
+	// Check against wild card role rule
+	if cs, ok := m["*"]; ok {
+
+		return cs.isActionAllowed(op)
+	}
+	return false
+
+}
+
+// newCRUDSet create a new actionSet.
+func newCRUDSet() actionSet {
+	return make(map[Action]struct{})
+}
+
+// setCRUD add an operation to actionSet.
+func (s actionSet) setCRUD(crud []Action) {
+
+	for _, op := range crud {
+		s[op] = struct{}{}
+	}
+	return
+}
+
+// isActionAllowed checks whether an operation is is present in actionSet.
+func (s actionSet) isActionAllowed(op Action) bool {
+
+	if _, ok := s[op]; ok {
+		return true
+
+	}
+	return false
+}
+
+// newRequest creates a new request Object. This object will be initialized with user roles
+// project,domain ,kind info.
+func newRequest(
+	ctx context.Context, kind string, op Action,
+) *request {
+
+	authCtx := auth.GetAuthCTX(ctx)
+	n := request{
+		kind:    kind,
+		op:      op,
+		domain:  authCtx.DomainID(),
+		project: authCtx.ProjectID(),
+		roles:   getUserRoles(ctx),
+	}
+
+	return &n
+}
+
+// getUserRoles converts user roles from ctx to a map.
+func getUserRoles(ctx context.Context) map[string]bool {
+
+	if ctx == nil {
+		return nil
+	}
+
+	aCtx := auth.GetAuthCTX(ctx)
+
+	if aCtx == nil {
+		return nil
+	}
+
+	rset := make(map[string]bool)
+	for _, role := range aCtx.Roles() {
+		rset[role] = true
+	}
+
+	return rset
+}
+
+// getResourceHashKey forms a hash key from objName,objField.
+func getResourceHashKey(kind string) (key resourceKey) {
+
+	// Not implemented field name based rules yet
+	return resourceKey(kind + "." + "*")
+
+}
+
+// updateRuleToHash adds rules to the resourcePermission hash table. resourcePermission hash table will have entries of the form
+// key => {{role1:actionSet1},{role2:actionSet2}} . This will merge the object_type CRUD in global,domain and
+// project level.
+func (r resourcePermission) updateRuleToHash(ruleKind string, role string, crud []Action) {
+
+	key := getResourceHashKey(ruleKind)
+
+	if val, ok := r[key]; ok {
+
+		val.updateEntry(role, crud)
+
+	} else {
+		val := newAllowedAction()
+		val.updateEntry(role, crud)
+		r[key] = val
+	}
+
+	return
+
+}
+
+// kindPermsAddToHash adds kind or wildcard RBAC rules to hash.
+// Input "perm" is in the following form
+// Member:CRUD
+func (r resourcePermission) kindPermAddToHash(ruleKind string, perm *models.RbacPermType) error {
+
+	if perm == nil {
+		return nil
+	}
+	role := perm.GetRoleName()
+	crud,err := crudToActionMap(perm.GetRoleCrud())
+	if err != nil {
+	   return err
+	}
+	r.updateRuleToHash(ruleKind, role, crud)
+	return nil
+
+}
+
+// kindPermsAddToHash adds kind or wildcard RBAC rules to hash.
+// Input "perms" is in the  following form
+// Member:CRUD ,Development:CRUD
+func (r resourcePermission) kindPermsAddToHash(ruleKind string, perms []*models.RbacPermType) error {
+
+	for _, perm := range perms {
+
+		err := r.kindPermAddToHash(ruleKind, perm)
+		if err != nil{
+		  return err
+		}
+	}
+	return nil
+
+}
+
+// kindRuleAddToHash add kind or wildcard RBAC rules to hash.
+// Input rbacRule is in the the following form
+// < virtual-network, *> => Member:CRUD ,Development:CRUD.
+func (r resourcePermission) kindRuleAddToHash(rbacRule *models.RbacRuleType) error {
+
+	if rbacRule == nil {
+		return nil
+	}
+
+	ruleKind := pluralNameToKind(rbacRule.GetRuleObject())
+	perms := rbacRule.GetRulePerms()
+
+	err := r.kindPermsAddToHash(ruleKind, perms)
+	if err != nil{
+	    return err
+	}
+	return nil
+}
+
+// rulesAddToHash adds  matching the kind or wildcard RBAC rules to hash.
+// Input is in the  following form
+// < virtual-network, *> => Member:CRUD ,Development:CRUD
+// < virtual-ip, *> => Member:CRUD
+func (r resourcePermission) rulesAddToHash(rbacRules []*models.RbacRuleType) error {
+
+	for _, obj := range rbacRules {
+
+		err := r.kindRuleAddToHash(obj)
+		if err != nil{
+  	           return err
+		}
+
+	}
+	return nil
+}
+
+// getRBACRules retrieves RBAC rules from API access list entry.
+func getRBACRules(l *models.APIAccessList) []*models.RbacRuleType {
+	return l.GetAPIAccessListEntries().GetRbacRule()
+}
+
+// pluralNameToKind converts plural object name to object kind.  eg.virtual-networks => virtual-network.
+func pluralNameToKind(name string) string {
+
+	return strings.TrimSuffix(name, "s")
+}
+
+// isRBACEnabled returns whether RBAC is enabled.
+func isRBACEnabled(aaaMode string) bool {
+
+	return aaaMode == aaaModeRBAC
+}
+
+// validateRoleAndConfig checks whether is Resource access is allowed purely based on  RBAC config.
+// or user roles.
+func validateRoleAndConfig(ctx *auth.Context, aaaMode string) bool {
+
+	// RBAC is not configured
+	if !isRBACEnabled(aaaMode) {
+		return true
+	}
+
+	// If User is cloud_admin allow access
+
+	if ctx.IsAdmin() {
+		return true
+	}
+
+	return false
+}
