fb7fdb7fa74c2bb4a51e92b78f6b9fe7d1dd877a
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-12-13 16:05:25 +0100

    [WIP] Neutron security group READALL
    Change-Id: I7ec54f3adbe7c5d1a35af19a58e7cf2933174399
    closes-jira-bug: JBE-766

--- pkg/neutron/logic/uuid.go
+++ pkg/neutron/logic/uuid.go
@@ -2,6 +2,10 @@ package logic
 
 import "strings"
 
-func contrailUUIDToNeutronID(contrailUUID string) string {
-	return strings.Replace(contrailUUID, "-", "", -1)
+func contrailUUIDToNeutronID(uuid string) string {
+	return strings.Replace(uuid, "-", "", -1)
+}
+
+func neutronIDToContrailUUID(id string) string {
+	return id[:8] + "-" + id[8:12] + "-" + id[12:16] + "-" + id[16:20] + "-" + id[20:]
 }
