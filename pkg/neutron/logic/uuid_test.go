0280deb4b11b22ddd606c08a64cc0a079d957b6d
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-12-13 16:05:25 +0100

    Neutron security group READALL
    co-author: Bartlomiej Gajda <bartlomiej.gajda@codilime.com>
    Change-Id: I7ec54f3adbe7c5d1a35af19a58e7cf2933174399
    closes-jira-bug: JBE-766

---
+++ pkg/neutron/logic/uuid_test.go
@@ -0,0 +1,52 @@
+package logic
+
+import "testing"
+
+func TestContrailUUIDToNeutronID(t *testing.T) {
+	tests := []struct {
+		input  string
+		output string
+	}{
+		{"ff93b90f-74d0-4214-980a-86fbdf952ab8", "ff93b90f74d04214980a86fbdf952ab8"},
+		{"abcdefgh-abcd-abcd-abcd-abcdefghijkl", "abcdefghabcdabcdabcdabcdefghijkl"},
+		{"dde59c13739e46ac9f5cfe0d7f1c4567", "dde59c13739e46ac9f5cfe0d7f1c4567"},
+		{"", ""},
+	}
+
+	for _, test := range tests {
+		result := contrailUUIDToNeutronID(test.input)
+		if result != test.output {
+			t.Errorf("Tranlating contrail uuid (%s) to neutron was incorrect, got: %s, want: %s.",
+				test.input, result, test.output)
+		}
+	}
+}
+
+func TestNeutronIDToContrailUUID(t *testing.T) {
+	tests := []struct {
+		input       string
+		output      string
+		expectError bool
+	}{
+		{"ff93b90f74d04214980a86fbdf952ab8", "ff93b90f-74d0-4214-980a-86fbdf952ab8", false},
+		{"abcdefghabcdabcdabcdabcdefghijkl", "", true},
+		{"", "", true},
+		{"\n", "", true},
+		{"ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b" +
+			"90f74d04214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04" +
+			"214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04214980a86fbdf952ab8ff93b90f74d04214980a8",
+			"", true},
+	}
+
+	for _, test := range tests {
+		result, err := neutronIDToContrailUUID(test.input)
+
+		if err != nil && !test.expectError {
+			t.Errorf("Expected no error but got: \" %+v\" while parsing \"%s\".", err, test.input)
+		}
+		if result != test.output {
+			t.Errorf("Tranlating neutron id (%s) to contrail uuid was incorrect, got: %s, want: %s.",
+				test.input, result, test.output)
+		}
+	}
+}
