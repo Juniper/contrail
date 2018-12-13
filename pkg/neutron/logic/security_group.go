dfce35ed812d1476601587cfdf3f016539e2a38d
Bartłomiej Gajda <bartlomiej.gajda@codilime.com>
2018-12-13 16:05:25 +0100

    [WIP] Neutron security group READALL
    Change-Id: I7ec54f3adbe7c5d1a35af19a58e7cf2933174399
    closes-jira-bug: JBE-766

--- pkg/neutron/logic/security_group.go
+++ pkg/neutron/logic/security_group.go
@@ -3,17 +3,26 @@ package logic
 import (
 	"context"
 	"strconv"
+	"strings"
+
+	modelUUID "github.com/satori/go.uuid"
 
 	"github.com/Juniper/contrail/pkg/errutil"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/models/basemodels"
 	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/baseservices"
 )
+
+var sgNoRuleFQName = []string{"default-domain", "default-project", "__no_rule__"}
 
 const (
-	localSecurityGroup = "local"
+	projectFieldChildrenSecurityGroups = "security_groups"
+	defaultSGName                      = "default"
+	localSecurityGroup                 = "local"
 )
 
+// Read security group logic..
 func (s *SecurityGroup) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
 	resp, err := rp.ReadService.GetSecurityGroup(ctx, &services.GetSecurityGroupRequest{
 		ID: id,
@@ -29,6 +38,207 @@ func (s *SecurityGroup) Read(ctx context.Context, rp RequestParameters, id strin
 		return nil, err
 	}
 	return securityGroupContrailToNeutron(resp.SecurityGroup)
+}
+
+// ReadAll security group logic.
+func (sg *SecurityGroup) ReadAll(rp RequestParameters, f Filters, fields Fields) (Response, error) {
+	ctx := context.Background()
+	err := ensureDefaultSecurityGroupExists(ctx, rp)
+	if err != nil {
+		return nil, err
+	}
+
+	sgs, err := getSecurityGroupsFromDB(ctx, rp, f)
+	if err != nil {
+		return nil, err
+	}
+
+	return getFixedSecurityGroups(ctx, sgs, f), nil
+}
+
+func ensureDefaultSecurityGroupExists(ctx context.Context, rp RequestParameters) error {
+	projectID := neutronIDToContrailUUID(rp.RequestContext.TenantID)
+	projectResponse, err := rp.ReadService.GetProject(
+		ctx,
+		&services.GetProjectRequest{
+			ID: projectID,
+			Fields: []string{
+				projectFieldChildrenSecurityGroups,
+			},
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	project := projectResponse.GetProject()
+	for _, sg := range project.GetSecurityGroups() {
+		if sg.GetFQName()[len(sg.GetFQName())-1] == defaultSGName {
+			return nil
+		}
+	}
+
+	return createDefaultSecurityGroup(ctx, rp, project)
+}
+
+func createDefaultSecurityGroup(
+	ctx context.Context, rp RequestParameters, project *models.Project) error {
+	createRule := func(ingress bool, sg string, prefix string, ethertype string) *models.PolicyRuleType {
+		uuid := modelUUID.NewV4().String()
+		localAddr := models.AddressType{
+			SecurityGroup: "local",
+		}
+
+		var addr models.AddressType
+		if sg != "" {
+			fqName := basemodels.FQNameToString(project.GetFQName())
+			addr = models.AddressType{
+				SecurityGroup: strings.Join([]string{fqName, sg}, ":"),
+			}
+		}
+
+		if prefix != "" {
+			addr = models.AddressType{
+				Subnet: &models.SubnetType{
+					IPPrefix:    prefix,
+					IPPrefixLen: 0,
+				},
+			}
+		}
+
+		rule := models.PolicyRuleType{
+			RuleUUID:  uuid,
+			Direction: ">",
+			Protocol:  "any",
+			SRCPorts: []*models.PortType{
+				{
+					StartPort: 0,
+					EndPort:   65535,
+				},
+			},
+			DSTPorts: []*models.PortType{
+				{
+					StartPort: 0,
+					EndPort:   65535,
+				},
+			},
+			Ethertype: ethertype,
+		}
+		if ingress {
+			rule.SRCAddresses = []*models.AddressType{&addr}
+			rule.DSTAddresses = []*models.AddressType{&localAddr}
+			return &rule
+		}
+
+		rule.SRCAddresses = []*models.AddressType{&localAddr}
+		rule.DSTAddresses = []*models.AddressType{&addr}
+		return &rule
+	}
+
+	sg := models.SecurityGroup{
+		Name:       "default",
+		ParentUUID: project.GetUUID(),
+		ParentType: models.KindProject,
+		IDPerms: &models.IdPermsType{
+			Enable:      true,
+			Description: "Default security group",
+		},
+		SecurityGroupEntries: &models.PolicyEntriesType{
+			PolicyRule: []*models.PolicyRuleType{
+				createRule(true, "default", "", "IPv4"),
+				createRule(true, "default", "", "IPv6"),
+				createRule(false, "", "0.0.0.0", "IPv4"),
+				createRule(false, "", "::", "IPv6"),
+			},
+		},
+	}
+
+	_, err := rp.WriteService.CreateSecurityGroup(
+		ctx,
+		&services.CreateSecurityGroupRequest{
+			SecurityGroup: &sg,
+		},
+	)
+
+	// TODO chown(sqResponse.GetSecurityGroup().GetUUID(), project.GetUUID())
+	return err
+}
+
+func getSecurityGroupsFromDB(
+	ctx context.Context, rp RequestParameters, f Filters,
+) ([]*models.SecurityGroup, error) {
+	if ids, ok := f["id"]; ok {
+		return listSecurityGroups(ctx, rp, ids, nil)
+	}
+
+	if !rp.RequestContext.IsAdmin {
+		return listSecurityGroups(ctx, rp, nil, []string{rp.RequestContext.Tenant})
+
+	}
+
+	if projectIDs, ok := f["tenant_id"]; ok {
+		return listSecurityGroups(ctx, rp, nil, projectIDs)
+	}
+
+	return listSecurityGroups(ctx, rp, nil, nil)
+}
+
+func listSecurityGroups(
+	ctx context.Context, rp RequestParameters, uuids []string, tenantUUIDs []string,
+) ([]*models.SecurityGroup, error) {
+
+	var parentUUIDs []string
+	for _, uuid := range tenantUUIDs {
+		parentUUIDs = append(parentUUIDs, neutronIDToContrailUUID(uuid))
+	}
+
+	sgResponse, err := rp.ReadService.ListSecurityGroup(
+		ctx,
+		&services.ListSecurityGroupRequest{
+			Spec: &baseservices.ListSpec{
+				ObjectUUIDs: uuids,
+				ParentUUIDs: parentUUIDs,
+				Detail:      true,
+			},
+		},
+	)
+
+	return sgResponse.GetSecurityGroups(), err
+}
+
+func getFixedSecurityGroups(
+	ctx context.Context, sgs []*models.SecurityGroup, f Filters,
+) []*SecurityGroupResponse {
+
+	var neutronSGs []*SecurityGroupResponse
+	for _, sg := range sgs {
+		if basemodels.FQNameEquals(sg.GetFQName(), sgNoRuleFQName) {
+			continue
+		}
+
+		if !isPresentInFilters(f, "name", getSecurityGroupName(sg)) {
+			continue
+		}
+
+		neutronSG, err := securityGroupContrailToNeutron(sg)
+		if err != nil {
+			return nil // TODO add handling errors to this function. See python code before writing.
+		}
+
+		if neutronSG != nil {
+			neutronSGs = append(neutronSGs, neutronSG)
+		}
+	}
+
+	return neutronSGs
+}
+
+func getSecurityGroupName(sg *models.SecurityGroup) string {
+	if sg.GetDisplayName() != "" {
+		return sg.GetDisplayName()
+	}
+
+	return sg.GetName()
 }
 
 func securityGroupContrailToNeutron(sg *models.SecurityGroup) (*SecurityGroupResponse, error) {
