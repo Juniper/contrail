a6799074eea88719eb26ab5b0daeb62d39aea723
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-12-19 18:59:40 +0100

    [WIP] Neutron port logic
    Change-Id: Id7da525f1ee11355e7abc5f4f381622b810bf9f4

---
+++ pkg/neutron/logic/port.go
@@ -0,0 +1,117 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/twinj/uuid"
+
+	"github.com/Juniper/contrail/pkg/models"
+
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/baseservices"
+	"github.com/pkg/errors"
+)
+
+// Create logic
+func (port *Port) Create(ctx context.Context, rp RequestParameters) (Response, error) {
+	portRes := &PortResponse{
+		Name:                "c9088d8c-ecd1-472a-9190-905c28a22f53",
+		PortSecurityEnabled: true,
+		Status:              "DOWN",
+		BindingHostID:       "host_id",
+	}
+
+	// if mac-address is specified, check against the exisitng ports
+	// to see if there exists a port with the same mac-address
+
+	if err := port.checkMacAddress(ctx, rp); err != nil {
+		return nil, err
+	}
+
+	vn, err := port.getVirtualNetwork(ctx, rp)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = port.createVirtualMachineInterface(ctx, rp, vn); err != nil {
+		return nil, err
+	}
+
+	// TODO implement create logic
+	return portRes, nil
+}
+
+func (port *Port) getVirtualNetwork(
+	ctx context.Context, rp RequestParameters,
+) (*models.VirtualNetwork, error) {
+	res, err := rp.ReadService.GetVirtualNetwork(ctx, &services.GetVirtualNetworkRequest{
+		ID: port.NetworkID,
+	})
+	return res.GetVirtualNetwork(), err
+}
+
+func (port *Port) createVirtualMachineInterface(
+	ctx context.Context, rp RequestParameters, vn *models.VirtualNetwork,
+) error {
+
+	vmi := &models.VirtualMachineInterface{
+		IDPerms: &models.IdPermsType{
+			Enable: true,
+		},
+	}
+
+	portUUID := uuid.NewV4().String()
+
+	if len(port.Name) == 0 {
+		vmi.Name = portUUID
+	} else {
+		vmi.Name = port.Name
+	}
+
+	_, err := rp.WriteService.CreateVirtualMachineInterface(ctx, &services.CreateVirtualMachineInterfaceRequest{
+		VirtualMachineInterface: vmi,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (port *Port) checkMacAddress(ctx context.Context, rp RequestParameters) error {
+	if len(port.MacAddress) == 0 {
+		return nil
+	}
+
+	res, err := rp.ReadService.ListVirtualMachineInterface(ctx, &services.ListVirtualMachineInterfaceRequest{
+		Spec: &baseservices.ListSpec{
+			Filters: []*baseservices.Filter{
+				{
+					Key:    "virtual_machine_interface_mac_addresses",
+					Values: []string{port.MacAddress},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		return nil
+	}
+
+	if res.GetVirtualMachineInterfaceCount() != 0 {
+		errors.Errorf("MacAddressInUse: mac_address = %s", port.MacAddress)
+	}
+
+	return nil
+}
+
+// Read default implementation
+func (port *Port) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
+	return PortResponse{}, nil
+}
+
+// ReadAll logic
+func (port *Port) ReadAll(ctx context.Context, rp RequestParameters, filters Filters, fields Fields) (Response, error) {
+	// TODO implement ReadAll logic
+	return []PortResponse{}, nil
+}
---
+++ pkg/neutron/logic/port_test.go
@@ -0,0 +1,80 @@
+package logic
+
+import (
+	"testing"
+
+	"github.com/Juniper/contrail/pkg/services"
+
+	"github.com/Juniper/contrail/pkg/models"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/Juniper/contrail/pkg/services/mock"
+	"github.com/golang/mock/gomock"
+)
+
+func TestPortCreate(t *testing.T) {
+	type readData struct {
+		virtualNetworkReq *services.GetVirtualNetworkResponse
+	}
+
+	type writeData struct {
+		vmiReq *services.CreateVirtualMachineInterfaceRequest
+	}
+
+	tests := []struct {
+		name      string
+		port      *Port
+		wantErr   bool
+		readData  *readData
+		writeData *writeData
+	}{
+		{
+			name: "Port simple create",
+			port: &Port{
+				Name: "hoge-hoge",
+			},
+			readData: &readData{
+				virtualNetworkReq: &services.GetVirtualNetworkResponse{
+					VirtualNetwork: &models.VirtualNetwork{},
+				},
+			},
+			writeData: &writeData{
+				vmiReq: &services.CreateVirtualMachineInterfaceRequest{
+					VirtualMachineInterface: &models.VirtualMachineInterface{
+						Name: "hoge-hoge",
+						IDPerms: &models.IdPermsType{
+							Enable: true,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockServ := servicesmock.NewMockService(mockCtrl)
+
+			if tt.readData.virtualNetworkReq != nil {
+				mockServ.EXPECT().GetVirtualNetwork(gomock.Any(), gomock.Any()).Return(tt.readData.virtualNetworkReq, nil)
+			}
+
+			if tt.writeData.vmiReq != nil {
+				mockServ.EXPECT().CreateVirtualMachineInterface(gomock.Any(), tt.writeData.vmiReq)
+			}
+
+			rp := RequestParameters{
+				ReadService:  mockServ,
+				WriteService: mockServ,
+			}
+
+			_, err := tt.port.Create(nil, rp)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
