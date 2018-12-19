644e5dc130e44314422bc39da1abd9926396615b
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-12-19 17:07:21 +0100

    Use context correctly in Neutron plugin
    Pass context from echo to all handlers
    
    Change-Id: Iab61c9665175a6807cf15c76517a7f472a345381
    partial-jira-bug: JBE-669

--- pkg/neutron/logic/network.go
+++ pkg/neutron/logic/network.go
@@ -1,7 +1,9 @@
 package logic
 
+import "context"
+
 // Create logic
-func (n *Network) Create(rp RequestParameters) (Response, error) {
+func (n *Network) Create(ctx context.Context, rp RequestParameters) (Response, error) {
 	return &NetworkResponse{
 		Name: n.Name,
 	}, nil
