88889e73af3b26e0138892bde26af88814bb686c
Daniel Furman <daniel.furman@codilime.com>
2018-12-19 11:00:36 +0100

    Make deploy-for_k8s.sh idempotent
    Change-Id: Iadf36d70f079c9f5dd0ef1eafececa9e7dd64cd8
    partial-jira-bug: JBE-763

--- tools/control-node_etcd/update-docker-compose.py
+++ tools/control-node_etcd/update-docker-compose.py
@@ -7,7 +7,10 @@ with open(docker_compose_path) as f:
     docker_compose = yaml.load(f)
 
 docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync"
-docker_compose["services"]["control"]["volumes"].append("/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro")
+
+etcd_pki_mount = "/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro"
+if etcd_pki_mount in docker_compose["services"]["control"]["volumes"]:
+    docker_compose["services"]["control"]["volumes"].append(etcd_pki_mount)
 
 with open(docker_compose_path, "w") as f:
     yaml.dump(docker_compose, f)
--- tools/kube_manager_etcd/update-docker-compose.py
+++ tools/kube_manager_etcd/update-docker-compose.py
@@ -7,7 +7,10 @@ with open(docker_compose_path) as f:
     docker_compose = yaml.load(f)
 
 docker_compose["services"]["kubemanager"]["image"] = "contrail-kubernetes-kube-manager:etcd"
-docker_compose["services"]["kubemanager"]["volumes"].append("/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro")
+
+etcd_pki_mount = "/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro"
+if etcd_pki_mount in docker_compose["services"]["kubemanager"]["volumes"]:
+    docker_compose["services"]["kubemanager"]["volumes"].append(etcd_pki_mount)
 
 with open(docker_compose_path, "w") as f:
     yaml.dump(docker_compose, f)
