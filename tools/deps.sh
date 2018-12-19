e61c6e41fddd1b01b842d76e167afde275dbef27
Daniel Furman <daniel.furman@codilime.com>
2018-12-19 15:15:50 +0100

    [WiP] Update golangci-lint
    Change-Id: Id641475440b21f5134a5a75eaeb6b0f21d81563b

--- tools/deps.sh
+++ tools/deps.sh
@@ -5,7 +5,6 @@ set -o errexit
 # TODO: uncomment this once this dep is fixed
 # go get -u github.com/go-openapi/spec
 go get -u github.com/golang/dep/cmd/dep
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u github.com/hashicorp/terraform/terraform
 go get -u github.com/mattn/goveralls
 go get -u github.com/mitchellh/gox
@@ -16,7 +15,7 @@ go install ./vendor/github.com/gogo/protobuf/protoc-gen-gogo
 go install ./vendor/github.com/golang/mock/mockgen
 
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | \
-	bash -s -- -b $(go env GOPATH)/bin v1.10.2
+    sh -s -- -b "$(go env GOPATH)/bin" v1.12.2
 
 if [ "$(uname)" == 'Darwin' ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
