e8683669c4421d702c3384d274f08a44a297f9aa
aakimov <github@akimov.me>
2018-12-24 18:16:13 +0300

    Enable domain sharing
    Change-Id: I09852a28699c8eb9f8cd20a8842a6ca13016431f
    closes-jira-bug: JBE-339

---
+++ pkg/types/domain.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+//CreateDomain does pre-check for create domain
+func (sv *ContrailTypeLogicService) CreateDomain(
+	ctx context.Context,
+	request *services.CreateDomainRequest,
+) (response *services.CreateDomainResponse, err error) {
+	domain := request.GetDomain()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			if domain.Perms2.Share == nil {
+				domain.Perms2.Share = []*models.ShareType{}
+			}
+			domain.Perms2.Share = append(domain.Perms2.Share, &models.ShareType{
+				TenantAccess: basemodels.PermsRW,
+				Tenant:       "domain:" + domain.UUID,
+			})
+			response, err = sv.BaseService.CreateDomain(ctx, request)
+			return err
+		})
+	return response, err
+}
