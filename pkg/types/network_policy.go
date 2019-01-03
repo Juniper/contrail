990a22c4917d3cd8296019cc3273f4b24466a1a5
Ignacy Osetek <ignacy.osetek@codilime.com>
2019-01-03 11:30:58 +0100

    Network Policy: Implemented APISRV logic
    Implemented APISRV logic for Network Policy.
    Added check policy rules for Security Group
    
    Co-authored-by: Michał Błotniak <michal.blotniak@codilime.com>
    
    Change-Id: I3d89f15d19b158e7746b9533bd376b2597ea3a9f
    Closes-Jira-Bug: JBE-719

---
+++ pkg/types/network_policy.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateNetworkPolicy does pre-check for network policy create.
+func (sv *ContrailTypeLogicService) CreateNetworkPolicy(
+	ctx context.Context,
+	request *services.CreateNetworkPolicyRequest,
+) (response *services.CreateNetworkPolicyResponse, err error) {
+	np := request.GetNetworkPolicy()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			if err = np.GetNetworkPolicyEntries().CheckNetworkPolicyRules(); err != nil {
+				return errors.Wrapf(err, "failed to check Policy Rules")
+			}
+			np.NetworkPolicyEntries.FillRuleUUIDs()
+			response, err = sv.BaseService.CreateNetworkPolicy(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// UpdateNetworkPolicy does pre-check for network policy update.
+func (sv *ContrailTypeLogicService) UpdateNetworkPolicy(
+	ctx context.Context,
+	request *services.UpdateNetworkPolicyRequest,
+) (response *services.UpdateNetworkPolicyResponse, err error) {
+	np := request.GetNetworkPolicy()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			if err = np.GetNetworkPolicyEntries().CheckNetworkPolicyRules(); err != nil {
+				return errors.Wrapf(err, "failed to check Policy Rules")
+			}
+			np.NetworkPolicyEntries.FillRuleUUIDs()
+			response, err = sv.BaseService.UpdateNetworkPolicy(ctx, request)
+			return err
+		})
+	return response, err
+}
