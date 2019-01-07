6c9576e771d941db5943a74a1169236c4367236a
Paweł Drapiewski <pawel.drapiewski@codilime.com>
2019-01-07 15:28:26 +0100

    Neutron security group CREATE
    * logic for neutron security group CREATE operation
    * add new error type 'QuotaExceeded'
    
    Co-authored-by: Pawel Zadrozny <pawel.zny@gmail.com>
    Change-Id: Ie518cdb32dd624f1d06f174048a5345d02b01fc5
    closes-jira-bug: JBE-815

---
+++ pkg/neutron/logic/netutils_test.go
@@ -0,0 +1,50 @@
+package logic
+
+import "testing"
+
+func TestGetIPPrefixAndPrefixLen(t *testing.T) {
+
+	tests := []struct {
+		name              string
+		ipCIDR            string
+		expectedIPPrefix  string
+		expectedNetIP     string
+		expectedPrefixLen int64
+		expectError       bool
+	}{
+		{"good ipv4 1", "137.105.215.54/23", "137.105.215.54", "137.105.214.0", 23, false},
+		{"good ipv4 2", "3.37.8.253/13", "3.37.8.253", "3.32.0.0", 13, false},
+		{"good ipv6 1", "FE80:0000:0000:0000:0202:B3FF:FE1E:8329/64", "fe80::202:b3ff:fe1e:8329", "fe80::", 64, false},
+		{"good ipv6 2", "2001:DB8:ABCD:12::/96", "2001:db8:abcd:12::", "2001:db8:abcd:12::", 96, false},
+		{"good ipv6 3 no mask", "2001:DB8:ABCD:12::", "", "", 0, true},
+		{"zero ipv4 address", "0.0.0.0/0", "0.0.0.0", "0.0.0.0", 0, false},
+		{"zero ipv6 address", "::/0", "::", "::", 0, false},
+		{"empty ip address", "", "", "", 0, true},
+		{"wrong ip - invalid separator", "137.105.215.54:23", "", "", 0, true},
+		{"wrong ip - invalid mask", "137.105.215.54/23a", "", "", 0, true},
+		{"wrong ip - random chars", "abcd", "", "", 0, true},
+		{"wrong ip - without mask", "137.105.215.54", "", "", 0, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ipPrefix, netIP, prefixLen, err := getIPPrefixAndPrefixLen(tt.ipCIDR)
+
+			if err != nil && !tt.expectError {
+				t.Errorf("expected no error but got: '%+v'", err)
+			}
+
+			if ipPrefix != tt.expectedIPPrefix {
+				t.Errorf("error wrong IP adress. Expected: '%+v', got: '%+v'", tt.expectedIPPrefix, ipPrefix)
+			}
+
+			if netIP != tt.expectedNetIP {
+				t.Errorf("error wrong IP Network adress. Expected: '%+v', got: '%+v'", tt.expectedNetIP, netIP)
+			}
+
+			if prefixLen != tt.expectedPrefixLen {
+				t.Errorf("error wrong IP Prefix lenght. Expected: '%+v', got: '%+v'", tt.expectedPrefixLen, prefixLen)
+			}
+		})
+	}
+}
