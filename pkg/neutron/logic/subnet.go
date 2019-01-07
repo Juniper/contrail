c8ac84f6ffb9d92ac5408f24c4330ab7b5c502c3
Paweł Drapiewski <pawel.drapiewski@codilime.com>
2019-01-07 15:28:26 +0100

    Neutron security group CREATE
    * logic for neutron security group CREATE operation
    * add new error type 'QuotaExceeded'
    
    Change-Id: Ie518cdb32dd624f1d06f174048a5345d02b01fc5
    closes-jira-bug: JBE-815

--- pkg/neutron/logic/subnet.go
+++ pkg/neutron/logic/subnet.go
@@ -479,7 +479,7 @@ func subnetVncToNeutron(vn *models.VirtualNetwork, subnetVnc *models.IpamSubnetT
 	subnet := &SubnetResponse{
 		ID:         subnetVnc.GetSubnetUUID(),
 		Name:       subnetVnc.GetSubnetName(),
-		TenantID:   contrailUUIDToNeutronID(vn.GetParentUUID()),
+		TenantID:   vncUUIDToNeutronID(vn.GetParentUUID()),
 		NetworkID:  vn.GetUUID(),
 		EnableDHCP: subnetVnc.GetEnableDHCP(),
 		Shared:     vn.GetIsShared() || (vn.GetPerms2() != nil && len(vn.GetPerms2().GetShare()) > 0),
