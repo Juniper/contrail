d7f5ad7c8757b8f4bd466c9fffceb6010136eab6
Mateusz Neumann <mateusz.neumann@codilime.com>
2019-01-08 14:45:20 +0100

    Refactoring of int-pool endpoint.
    Due to migration of Schema Transformer to Etcd, int-pool endpoint
    code has to be refactored.  Instead of using /int-pool/:pool-name
    for POSTs and /int-pool/:pool-name/:value for DELETEs, consistency
    of the API would be maintained by moving the parameters to JSON
    passed in request body.
    
    Change-Id: Ia5b2d653c29055ec74f91537b982cf5a5c5c3d3e
    partial-jira-bug: JBE-787

--- pkg/apisrv/client/http.go
+++ pkg/apisrv/client/http.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"path"
 
 	"github.com/labstack/echo"
 	"github.com/pkg/errors"
@@ -207,27 +206,42 @@ func (h *HTTP) EnsureDeleted(ctx context.Context, path string, output interface{
 
 // AllocateInt sends an allocate int request to remote int-pool.
 func (h *HTTP) AllocateInt(ctx context.Context, pool string) (int64, error) {
+	// TODO
 	var output struct {
-		Value int64 `json:"value"`
+		Pool  string `json:"pool"`
+		Value int64  `json:"value"`
 	}
+	output.Pool = pool
 	expected := []int{http.StatusOK}
-	_, err := h.Do(ctx, echo.POST, path.Join("/int-pool", pool), nil, nil, &output, expected)
+	_, err := h.Do(ctx, echo.POST, "/int-pool", nil, nil, &output, expected)
 	return output.Value, errors.Wrap(err, "error allocating int in int-pool via HTTP")
 }
 
 // SetInt sends a set int request to remote int-pool.
 func (h *HTTP) SetInt(ctx context.Context, pool string, value int64) error {
-	var output struct{}
+	// TODO
+	var output struct {
+		Pool  string `json:"pool"`
+		Value int64  `json:"value"`
+	}
+	output.Pool = pool
+	output.Value = value
 	expected := []int{http.StatusOK}
-	_, err := h.Do(ctx, echo.POST, path.Join("/int-pool", pool, fmt.Sprint(value)), nil, nil, &output, expected)
+	_, err := h.Do(ctx, echo.POST, "/int-pool", nil, nil, &output, expected)
 	return errors.Wrap(err, "error setting int in int-pool via HTTP")
 }
 
 // DeallocateInt sends a deallocate int request to remote int pool.
 func (h *HTTP) DeallocateInt(ctx context.Context, pool string, value int64) error {
-	var output struct{}
+	// TODO
+	var output struct {
+		Pool  string `json:"pool"`
+		Value int64  `json:"value"`
+	}
+	output.Pool = pool
+	output.Value = value
 	expected := []int{http.StatusOK}
-	_, err := h.Do(ctx, echo.DELETE, path.Join("/int-pool", pool, fmt.Sprint(value)), nil, nil, &output, expected)
+	_, err := h.Do(ctx, echo.DELETE, "/int-pool", nil, nil, &output, expected)
 	return errors.Wrap(err, "error deallocating int in int-pool via HTTP")
 }
 
