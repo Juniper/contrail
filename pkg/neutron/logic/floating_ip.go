4af1d22806853dab1c0119171cab43fc36bbd51c
Kamil Renczewski <kamil.renczewski@codilime.com>
2019-01-08 12:49:32 +0100

    FloatingIP operation READ
    Change-Id: I382c3402fbff34502cbb2a9e48c352a4cdef2ff7
    Closes-Jira-Bug: JBE-809

--- pkg/neutron/logic/floating_ip.go
+++ pkg/neutron/logic/floating_ip.go
@@ -1,6 +1,31 @@
 package logic
 
-import "context"
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/errutil"
+	"github.com/Juniper/contrail/pkg/services"
+
+	"github.com/pkg/errors"
+)
+
+// Read floating_ip by UUID
+func (fip *Floatingip) Read(ctx context.Context, rp RequestParameters, id string) (Response, error) {
+	uuid, err := neutronIDToContrailUUID(id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid uuid format %v for READ FloatingIp", id)
+	}
+	resp, err := rp.ReadService.GetFloatingIP(ctx, &services.GetFloatingIPRequest{ID: uuid})
+	if errutil.IsNotFound(err) {
+		return nil, newNeutronError(floatingIPNotFound, errorFields{
+			"floatingip_id": id,
+			"message":       err,
+		})
+	} else if err != nil {
+		return nil, err
+	}
+	return makeFloatingipResponse(ctx, rp, resp.FloatingIP, nil, nil)
+}
 
 // ReadAll logic
 func (fip *Floatingip) ReadAll(
