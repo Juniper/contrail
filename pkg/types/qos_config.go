c0180131f242980556132f3cf969211458c98c1e
Ignacy Osetek <ignacy.osetek@codilime.com>
2019-01-10 12:56:22 +0100

    QoS Config - APISRV logic (check QoS values)
    Implemented APISRV logic for QoS Config.
    
    Change-Id: I3d487ffb02083f2637d401a334b20c8da20f7c09
    Closes-Jira-Bug: JBE-360

---
+++ pkg/types/qos_config.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// CreateQosConfig does pre-check for QoS Config create.
+func (sv *ContrailTypeLogicService) CreateQosConfig(
+	ctx context.Context,
+	request *services.CreateQosConfigRequest,
+) (response *services.CreateQosConfigResponse, err error) {
+	qos := request.GetQosConfig()
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			defaultGSCUUID, err := sv.getDefaultGlobalSystemConfigUUID(ctx)
+			if err != nil {
+				return err
+			}
+
+			qos.AddDefaultGlobalSystemConfigRef(defaultGSCUUID, sv.getDefaultGlobalSystemConfigFqName())
+
+			if err = qos.CheckQoSValues(); err != nil {
+				return err
+			}
+
+			response, err = sv.BaseService.CreateQosConfig(ctx, request)
+			return err
+		})
+	return response, err
+}
+
+// UpdateQosConfig does pre-check for QoS Config update.
+func (sv *ContrailTypeLogicService) UpdateQosConfig(
+	ctx context.Context,
+	request *services.UpdateQosConfigRequest,
+) (response *services.UpdateQosConfigResponse, err error) {
+	qos := request.GetQosConfig()
+
+	if err = qos.CheckQoSValues(); err != nil {
+		return response, err
+	}
+
+	return sv.BaseService.UpdateQosConfig(ctx, request)
+}
