565609484f185b098a10a148a12aa1de34b71765
Daniel Furman <daniel.furman@codilime.com>
2019-01-11 12:44:53 +0100

    Enable client certificates in etcd clients
    - Enable etcd client certs in contrail-config_api.yml
    - Enable etcd client certs in Python workers
    - Provide Control Node image with etcd certs support
    
    This commit depends on k8s 1.9 -> 1.12 update
    which provides etcd TLS certs on test environment.
    
    Change-Id: I32991a5024fb42cba74de553863fdf22b56cad74
    Depends-On: I949844efdf21cbcede5464842881c9e03b562b7c
    closes-jira-bug: JBE-763

--- tools/deploy-for_k8s.sh
+++ tools/deploy-for_k8s.sh
@@ -45,23 +45,23 @@ clear_config_database
 make zero_psql
 
 # Drop contrail related content from etcd
-docker exec "$(docker ps -q -f name=k8s_etcd_etcd)" sh -c "ETCDCTL_API=3 etcdctl del /contrail --prefix"
-docker exec "$(docker ps -q -f name=k8s_etcd_etcd)" sh -c "ETCDCTL_API=3 etcdctl del /vnc --prefix"
+etcdctl_tls="etcdctl --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/peer.crt \
+    --key=/etc/kubernetes/pki/etcd/peer.key"
+docker exec "$(docker ps -q -f name=k8s_etcd_etcd)" sh -c "ETCDCTL_API=3 $etcdctl_tls del /contrail --prefix"
+docker exec "$(docker ps -q -f name=k8s_etcd_etcd)" sh -c "ETCDCTL_API=3 $etcdctl_tls del /vnc --prefix"
 
+export ORCHESTRATOR=k8s
 # Update kube_manager docker compose file
-sudo ./tools/kube_manager_etcd/update-docker-compose.py
+sudo --preserve-env ./tools/kube_manager_etcd/update-docker-compose.py
 
 # Update control-node docker compose file
-sudo ./tools/control-node_etcd/update-docker-compose.py
+sudo --preserve-env ./tools/control-node_etcd/update-docker-compose.py
 
 # Update schema transformer docker compose file
-sudo ./tools/schema_transformer_etcd/update-docker-compose.py
-
-# Update device manager docker compose file
-sudo ./tools/device_manager_etcd/update-docker-compose.py
+sudo --preserve-env ./tools/schema_transformer_etcd/update-docker-compose.py
 
 # Update svnmonitor docker compose file
-sudo ./tools/svcmonitor_etcd/update-docker-compose.py
+sudo --preserve-env ./tools/svcmonitor_etcd/update-docker-compose.py
 
 # Load init data to rdbms
 contrailutil convert --intype yaml --in tools/init_data.yaml --outtype rdbms -c sample/contrail-k8s.yml
@@ -74,4 +74,4 @@ GoConfigIP='127.0.0.1' # networking mode 'host'
 ensure_kubemanager_config_nodes "${GoConfigIP}"
 
 # Start services using docker-compose
-compose_up config:schema config:svcmonitor config:devicemgr control vrouter kubemanager
+compose_up config:schema config:svcmonitor control vrouter kubemanager
