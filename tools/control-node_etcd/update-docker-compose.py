5155ca4ca5ce5586fd1f1ec93340cb09c9d69c4f
Daniel Furman <daniel.furman@codilime.com>
2019-01-11 12:44:53 +0100

    Enable client certificates in etcd clients
    - Enable etcd client certs in contrail-config_api.yml
    - Enable etcd client certs in Python workers
    - Provide Control Node image with etcd certs support
    
    This commit depends on k8s 1.9 -> 1.12 update
    which provides etcd TLS certs on test environment.
    
    Change-Id: I32991a5024fb42cba74de553863fdf22b56cad74
    Depends-On: I949844efdf21cbcede5464842881c9e03b562b7c
    closes-jira-bug: JBE-763

--- tools/control-node_etcd/update-docker-compose.py
+++ tools/control-node_etcd/update-docker-compose.py
@@ -6,12 +6,23 @@ docker_compose_path = "/etc/contrail/control/docker-compose.yaml"
 with open(docker_compose_path) as f:
     docker_compose = yaml.load(f)
 
-docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync_2"
+docker_compose["services"]["control"]["image"] = "danielfurmancl/contrail-controller-control-control:etcd_sync_2"
 
 etcd_pki_mount = "/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro"
 volumes = docker_compose["services"]["control"].setdefault("volumes", [])
 if etcd_pki_mount not in volumes:
     volumes.append(etcd_pki_mount)
+
+environment = docker_compose["services"]["control"].setdefault("environment", [])
+for entry in [
+    "CONFIGDB_USE_ETCD=true",
+    "CONFIGDB_ETCD_USE_SSL=true",
+    "CONFIGDB_ETCD_KEY_FILE=/etc/kubernetes/pki/etcd/peer.key",
+    "CONFIGDB_ETCD_CERT_FILE=/etc/kubernetes/pki/etcd/peer.crt",
+    "CONFIGDB_ETCD_CA_CERT_FILE=/etc/kubernetes/pki/etcd/ca.crt",
+]:
+    if entry not in environment:
+        environment.append(entry)
 
 with open(docker_compose_path, "w") as f:
     yaml.dump(docker_compose, f)
