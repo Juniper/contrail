7d53f38f8b651fea90efa5bfbec9bb3c665fd4cc
Michał Błotniak <michal.blotniak@codilime.com>
2019-01-15 19:22:39 +0100

    [WIP] Refactor db.tmpl
    Reduced files:
    - pkg/db/gen_db.go - 239019 LOC => 231195 LOC
    
    closes-jira-bug: JBE-877
    Change-Id: I53b7f96e8241ec676c7b9f9f5fbab57fa398e099

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,13 +1,19 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 
+	"github.com/Juniper/contrail/pkg/auth"
 	"github.com/Juniper/contrail/pkg/db/basedb"
+	"github.com/Juniper/contrail/pkg/errutil"
+	"github.com/Juniper/contrail/pkg/format"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
 	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/services/baseservices"
 )
 
 //Service for DB.
@@ -33,4 +39,76 @@ func NewService(db *sql.DB, dialect string) *Service {
 	}
 	dbService.initQueryBuilders()
 	return dbService
+}
+
+func (db *Service) count(ctx context.Context, qb *basedb.QueryBuilder, spec *baseservices.ListSpec) (count int64, err error) {
+	query, values := qb.CountQuery(auth.GetAuthCTX(ctx), spec)
+
+	tx := basedb.GetTransaction(ctx)
+	row := tx.QueryRowContext(ctx, query, values...)
+
+	if err = row.Scan(&count); err != nil {
+		return 0, errors.Wrap(basedb.FormatDBError(err), "count query failed")
+	}
+
+	return count, nil
+}
+
+// checkPolicy check ownership of resources.
+func (db *Service) checkPolicy(ctx context.Context, qb *basedb.QueryBuilder, uuid string) (err error) {
+	tx := basedb.GetTransaction(ctx)
+	auth := auth.GetAuthCTX(ctx)
+
+	selectQuery := qb.SelectAuthQuery(auth.IsAdmin())
+
+	var count int
+
+	if auth.IsAdmin() {
+		row := tx.QueryRowContext(ctx, selectQuery, uuid)
+		if err != nil {
+			return basedb.FormatDBError(err)
+		}
+		row.Scan(&count)
+		if count == 0 {
+			return errutil.ErrorNotFound
+		}
+	} else {
+		row := tx.QueryRowContext(ctx, selectQuery, uuid, auth.ProjectID())
+		if err != nil {
+			return basedb.FormatDBError(err)
+		}
+		row.Scan(&count)
+		if count == 0 {
+			return errutil.ErrorNotFound
+		}
+	}
+	return nil
+}
+
+type childObject interface {
+	GetUUID() string
+	GetParentUUID() string
+	GetParentType() string
+}
+
+func (db *Service) createParentReference(
+	ctx context.Context,
+	obj childObject,
+	qb *basedb.QueryBuilder,
+	possibleParents []string,
+	optional bool,
+) (err error) {
+	parentSchemaID := basemodels.KindToSchemaID(obj.GetParentType())
+
+	if !format.ContainsString(possibleParents, parentSchemaID) {
+		if optional {
+			return nil
+		}
+		return errutil.ErrorBadRequest("invalid parent type")
+	}
+
+	tx := basedb.GetTransaction(ctx)
+	_, err = tx.ExecContext(ctx, qb.CreateParentRefQuery(parentSchemaID), obj.GetUUID(), obj.GetParentUUID())
+
+	return errors.Wrapf(basedb.FormatDBError(err), "creating resource %T with UUID '%v' in DB failed", obj, obj.GetUUID())
 }
