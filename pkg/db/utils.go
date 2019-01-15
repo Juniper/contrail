ce9021782f895b9f2db6c777fa1a5465f9d55c8f
Michał Błotniak <michal.blotniak@codilime.com>
2019-01-15 19:22:39 +0100

    Refactor db.tmpl
    Reduced files:
    - pkg/db/gen_db.go - 239019 LOC => 159746 LOC
    - pkg/models/gen_models.go - 135708 LOC => 135114 LOC
    
    Linter stats for './pkg/db/...':
    total time: 4m30.52007443s(OOM) => 44.171460906s
    avg mem: 6798.0MB => 3770.1MB
    max mem: 7580.8MB => 6927.0MB
    
    closes-jira-bug: JBE-877
    Change-Id: I53b7f96e8241ec676c7b9f9f5fbab57fa398e099

---
+++ pkg/db/utils.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"github.com/Juniper/contrail/pkg/db/basedb"
+	"github.com/Juniper/contrail/pkg/format"
+	"github.com/Juniper/contrail/pkg/services/baseservices"
+)
+
+func listSpecForGet(uuid string, fields []string) *baseservices.ListSpec {
+	return &baseservices.ListSpec{
+		Limit:  1,
+		Detail: true,
+		Fields: fields,
+		Filters: []*baseservices.Filter{
+			{
+				Key:    "uuid",
+				Values: []string{uuid},
+			},
+		},
+	}
+}
+
+func resolveUUIDAndFQNameFromMap(m map[string]interface{}) (uuid string, fqName []string, err error) {
+	uuid = format.InterfaceToString(m["to"])
+	if uuid == "" {
+		return "", nil, nil
+	}
+	fqNameStr := format.InterfaceToString(m["fq_name"])
+	fqName, err = basedb.ParseFQName(fqNameStr)
+	if err != nil {
+		return "", nil, err
+	}
+	return uuid, fqName, nil
+}
