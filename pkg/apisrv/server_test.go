5d9b2100b1963a471c6182048b303ed3921770d1
Michał Błotniak <michal.blotniak@codilime.com>
2019-01-15 19:06:22 +0100

    Prop collection update gRPC
    Change-Id: Ibd2c1e169a8494c14558b1b5260b1fc0da72c886
    closes-jira-bug: JBE-287

--- pkg/apisrv/server_test.go
+++ pkg/apisrv/server_test.go
@@ -2,26 +2,17 @@ package apisrv_test
 
 import (
 	"context"
-	"crypto/tls"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/twinj/uuid"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/metadata"
 
 	"github.com/Juniper/contrail/pkg/apisrv/client"
-	"github.com/Juniper/contrail/pkg/db"
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 	"github.com/Juniper/contrail/pkg/services/baseservices"
-	"github.com/Juniper/contrail/pkg/testutil"
 	"github.com/Juniper/contrail/pkg/testutil/integration"
-	"github.com/Juniper/contrail/pkg/types"
 )
 
 func TestKVStore(t *testing.T) {
@@ -263,223 +254,6 @@ func restLogin(ctx context.Context, t *testing.T) (authToken string) {
 	return restClient.AuthToken
 }
 
-func TestGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(), func(ctx context.Context, conn *grpc.ClientConn) {
-		c := services.NewContrailServiceClient(conn)
-		project := models.Project{
-			UUID:                 uuid.NewV4().String(),
-			FQName:               []string{"default-domain", "project", "my-project"},
-			ParentType:           "domain",
-			ParentUUID:           integration.DefaultDomainUUID,
-			ConfigurationVersion: 1,
-		}
-		_, err := c.CreateProject(ctx, &services.CreateProjectRequest{
-			Project: &project,
-		})
-		assert.NoError(t, err)
-
-		t.Run("create and delete namespace and project_namespace_ref", testNamespaceRef(ctx, c, project.UUID))
-		t.Run("list and get project", testProjectRead(ctx, c, project.UUID))
-
-		_, err = c.DeleteProject(ctx, &services.DeleteProjectRequest{
-			ID: project.UUID,
-		})
-		assert.NoError(t, err)
-	})
-}
-func testNamespaceRef(ctx context.Context, c services.ContrailServiceClient, projectUUID string) func(*testing.T) {
-	return func(t *testing.T) {
-		ns := models.Namespace{
-			UUID:       uuid.NewV4().String(),
-			ParentType: "domain",
-			ParentUUID: integration.DefaultDomainUUID,
-			Name:       "my-namespace",
-		}
-		_, err := c.CreateNamespace(ctx, &services.CreateNamespaceRequest{
-			Namespace: &ns,
-		})
-		assert.NoError(t, err)
-		_, err = c.CreateProjectNamespaceRef(ctx, &services.CreateProjectNamespaceRefRequest{
-			ID:                  projectUUID,
-			ProjectNamespaceRef: &models.ProjectNamespaceRef{UUID: ns.UUID},
-		})
-		assert.NoError(t, err)
-
-		_, err = c.DeleteProjectNamespaceRef(ctx, &services.DeleteProjectNamespaceRefRequest{
-			ID:                  projectUUID,
-			ProjectNamespaceRef: &models.ProjectNamespaceRef{UUID: ns.UUID},
-		})
-		assert.NoError(t, err)
-
-		_, err = c.DeleteNamespace(ctx, &services.DeleteNamespaceRequest{
-			ID: ns.UUID,
-		})
-		assert.NoError(t, err)
-	}
-}
-
-func testProjectRead(ctx context.Context, c services.ContrailServiceClient, projectUUID string) func(*testing.T) {
-	return func(t *testing.T) {
-		response, err := c.ListProject(ctx, &services.ListProjectRequest{
-			Spec: &baseservices.ListSpec{
-				Limit: 1,
-			},
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		assert.Equal(t, 1, len(response.Projects))
-
-		getResponse, err := c.GetProject(ctx, &services.GetProjectRequest{
-			ID: projectUUID,
-		})
-		assert.NoError(t, err)
-		assert.NotNil(t, getResponse.Project)
-	}
-}
-
-func testGRPCServer(t *testing.T, testName string, testBody func(ctx context.Context, conn *grpc.ClientConn)) {
-	ctx := context.Background()
-	integration.AddKeystoneProjectAndUser(server.APIServer, testName)
-	authToken := restLogin(ctx, t)
-
-	creds := credentials.NewTLS(&tls.Config{
-		InsecureSkipVerify: true,
-	})
-	dial := strings.TrimPrefix(server.URL(), "https://")
-
-	conn, err := grpc.Dial(dial, grpc.WithTransportCredentials(creds))
-	assert.NoError(t, err)
-	defer testutil.LogFatalIfError(conn.Close)
-	md := metadata.Pairs("X-Auth-Token", authToken)
-	ctx = metadata.NewOutgoingContext(ctx, md)
-
-	testBody(ctx, conn)
-}
-
-func TestFQNameToIDGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(),
-		func(ctx context.Context, conn *grpc.ClientConn) {
-			c := services.NewFQNameToIDClient(conn)
-			resp, err := c.FQNameToID(ctx, &services.FQNameToIDRequest{
-				FQName: []string{"default-domain"},
-				Type:   "domain",
-			})
-			assert.NoError(t, err)
-			assert.NotNil(t, resp)
-			assert.Equal(t, integration.DefaultDomainUUID, resp.UUID)
-		})
-}
-
-func TestChownGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(),
-		func(ctx context.Context, conn *grpc.ClientConn) {
-			c := services.NewChownClient(conn)
-
-			// This is only a stub implementation
-			_, err := c.Chown(ctx, &services.ChownRequest{})
-			assert.NoError(t, err)
-		})
-}
-
-func TestIPAMGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(),
-		func(ctx context.Context, conn *grpc.ClientConn) {
-			c := services.NewIPAMClient(conn)
-			allocateResp, err := c.AllocateInt(ctx, &services.AllocateIntRequest{
-				Pool:  types.VirtualNetworkIDPoolKey,
-				Owner: db.EmptyIntOwner,
-			})
-			assert.NoError(t, err)
-
-			_, err = c.DeallocateInt(ctx, &services.DeallocateIntRequest{
-				Pool:  types.VirtualNetworkIDPoolKey,
-				Value: allocateResp.GetValue(),
-			})
-			assert.NoError(t, err)
-		})
-}
-
-func TestRefRelaxGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(), func(ctx context.Context, conn *grpc.ClientConn) {
-		c := client.NewGRPC(services.NewContrailServiceClient(conn))
-
-		projectResponse, err := c.CreateProject(ctx, &services.CreateProjectRequest{
-			Project: &models.Project{
-				Name:       fmt.Sprintf("%s_project", t.Name()),
-				ParentType: "domain",
-				ParentUUID: integration.DefaultDomainUUID,
-			},
-		})
-		require.NoError(t, err)
-		project := projectResponse.GetProject()
-
-		defer func() {
-			_, err = c.DeleteProject(ctx, &services.DeleteProjectRequest{
-				ID: project.GetUUID(),
-			})
-			assert.NoError(t, err)
-		}()
-
-		policyResponse, err := c.CreateNetworkPolicy(ctx, &services.CreateNetworkPolicyRequest{
-			NetworkPolicy: &models.NetworkPolicy{
-				ParentType: models.KindProject,
-				ParentUUID: project.GetUUID(),
-			},
-		})
-		require.NoError(t, err)
-		policy := policyResponse.GetNetworkPolicy()
-
-		defer func() {
-			if policy == nil {
-				return
-			}
-			_, err = c.DeleteNetworkPolicy(ctx, &services.DeleteNetworkPolicyRequest{
-				ID: policy.GetUUID(),
-			})
-			assert.NoError(t, err)
-		}()
-
-		networkResponse, err := c.CreateVirtualNetwork(ctx, &services.CreateVirtualNetworkRequest{
-			VirtualNetwork: &models.VirtualNetwork{
-				ParentType: models.KindProject,
-				ParentUUID: project.GetUUID(),
-				NetworkPolicyRefs: []*models.VirtualNetworkNetworkPolicyRef{
-					{
-						UUID: policy.GetUUID(),
-					},
-				},
-			},
-		})
-		require.NoError(t, err)
-		network := networkResponse.GetVirtualNetwork()
-
-		defer func() {
-			_, err = c.DeleteVirtualNetwork(ctx, &services.DeleteVirtualNetworkRequest{
-				ID: network.GetUUID(),
-			})
-			assert.NoError(t, err)
-		}()
-
-		r := services.NewRefRelaxClient(conn)
-		response, err := r.RelaxRef(ctx, &services.RelaxRefRequest{
-			UUID:    network.GetUUID(),
-			RefUUID: policy.GetUUID(),
-		})
-		assert.NoError(t, err)
-		assert.Equal(t, &services.RelaxRefResponse{
-			UUID: network.GetUUID(),
-		}, response)
-
-		_, err = c.DeleteNetworkPolicy(ctx, &services.DeleteNetworkPolicyRequest{
-			ID: policy.GetUUID(),
-		})
-		if assert.NoError(t, err) {
-			policy = nil
-		}
-	})
-}
-
 func TestRESTClient(t *testing.T) {
 	ctx := context.Background()
 	integration.AddKeystoneProjectAndUser(server.APIServer, t.Name())
@@ -564,17 +338,4 @@ func TestPagination(t *testing.T) {
 
 	integration.AddKeystoneProjectAndUser(server.APIServer, t.Name())
 	RunTestTemplate(t, "./test_data/test_pagination.tmpl", context)
-}
-
-func TestIDToTypeGRPC(t *testing.T) {
-	testGRPCServer(t, t.Name(),
-		func(ctx context.Context, conn *grpc.ClientConn) {
-			c := services.NewIDToTypeClient(conn)
-			resp, err := c.IDToType(ctx, &services.IDToTypeRequest{
-				UUID: integration.DefaultDomainUUID,
-			})
-			assert.NoError(t, err)
-			assert.NotNil(t, resp)
-			assert.Equal(t, models.KindDomain, resp.Type)
-		})
 }
