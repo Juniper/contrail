1d7c766c8ac12c7a328670997617aba41163b63b
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-01-15 13:23:01 +0100

    [WIP] Service template logic
    Change-Id: If8eaae0735a0c3241ee1fbd610d609962a9c6701
    closes-jira-bug: JBE-725

---
+++ pkg/types/service_template.go
@@ -0,0 +1,64 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+//CreateServiceTemplate does pre check for service template.
+func (sv *ContrailTypeLogicService) CreateServiceTemplate(
+	ctx context.Context,
+	request *services.CreateServiceTemplateRequest,
+) (response *services.CreateServiceTemplateResponse, err error) {
+
+	serviceTemplate := request.GetServiceTemplate()
+
+	err = sv.InTransactionDoer.DoInTransaction(
+		ctx,
+		func(ctx context.Context) error {
+			domainUUID := serviceTemplate.GetParentUUID()
+			if domainUUID == "" {
+				domainUUID, err = sv.getParentDomainUUIDFromFQName(ctx, serviceTemplate)
+				if err != nil {
+					return err
+				}
+			}
+
+			shareItem := models.ShareType{
+				Tenant:       "domain:" + domainUUID,
+				TenantAccess: 5, // RX_PERMS
+			}
+
+			perms2 := serviceTemplate.GetPerms2()
+			if perms2 == nil {
+				serviceTemplate.Perms2 = &models.PermType2{}
+				perms2 = serviceTemplate.GetPerms2()
+			}
+			perms2.Share = append(perms2.GetShare(), &shareItem)
+
+			response, err = sv.BaseService.CreateServiceTemplate(ctx, request)
+			return err
+		})
+
+	return response, err
+}
+
+func (sv *ContrailTypeLogicService) getParentDomainUUIDFromFQName(
+	ctx context.Context,
+	serviceTemplate *models.ServiceTemplate,
+) (string, error) {
+	fqRequest := services.FQNameToIDRequest{
+		FQName: serviceTemplate.GetFQName(),
+		Type:   serviceTemplate.GetParentType(),
+	}
+
+	metadata, err := sv.MetadataGetter.GetMetadata(ctx, basemodels.Metadata{Type: fqRequest.Type, FQName: fqRequest.FQName})
+	if err != nil {
+		return "", err
+	}
+
+	return metadata.UUID, nil
+}
---
+++ pkg/types/service_template_test.go
@@ -0,0 +1,144 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/Juniper/contrail/pkg/errutil"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+	servicesmock "github.com/Juniper/contrail/pkg/services/mock"
+	typesmock "github.com/Juniper/contrail/pkg/types/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateServiceTemplate(t *testing.T) {
+	tests := []struct {
+		name                string
+		testServiceTemplate *models.ServiceTemplate
+		errorCode           codes.Code
+	}{
+		{
+			name: "create service template with parent UUID",
+			testServiceTemplate: &models.ServiceTemplate{
+				ParentUUID: "beefbeef-beef-beef-beef-beefbeef0002",
+				ParentType: "domain",
+			},
+			errorCode: codes.OK,
+		},
+		{
+			name: "create service template with fq name",
+			testServiceTemplate: &models.ServiceTemplate{
+				FQName:     []string{"default-domain", "service-template-name"},
+				ParentType: "domain",
+			},
+			errorCode: codes.OK,
+		},
+		{
+			name: "create service template with perms2 structure",
+			testServiceTemplate: &models.ServiceTemplate{
+				FQName:     []string{"default-domain", "service-template-name"},
+				ParentType: "domain",
+				Perms2: &models.PermType2{
+					Share: []*models.ShareType{},
+				},
+			},
+			errorCode: codes.OK,
+		},
+		{
+			name: "create service template without fq name or parent UUID",
+			testServiceTemplate: &models.ServiceTemplate{
+				ParentType: "domain",
+			},
+			errorCode: codes.InvalidArgument,
+		},
+		{
+			name: "create service template with incorrect domain in fq name",
+			testServiceTemplate: &models.ServiceTemplate{
+				FQName:     []string{"bad-domain", "service-template-name"},
+				ParentType: "domain",
+			},
+			errorCode: codes.NotFound,
+		},
+		{
+			name: "create service template with incorrect parent type",
+			testServiceTemplate: &models.ServiceTemplate{
+				FQName:     []string{"bad-domain", "service-template-name"},
+				ParentType: "project",
+			},
+			errorCode: codes.InvalidArgument,
+		},
+	}
+
+	for _, tt := range tests {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		service := makeMockedContrailTypeLogicService(mockCtrl)
+
+		ctx := context.Background()
+		paramRequest := services.CreateServiceTemplateRequest{ServiceTemplate: tt.testServiceTemplate}
+		expectedResponse := services.CreateServiceTemplateResponse{ServiceTemplate: tt.testServiceTemplate}
+
+		serviceTemplateSetupCreateMock(service, tt.testServiceTemplate)
+		serviceTemplateSetupMetadataMock(service)
+
+		createServiceTemplateResponse, err := service.CreateServiceTemplate(ctx, &paramRequest)
+		if tt.errorCode != codes.OK {
+			assert.Error(t, err)
+			status, ok := status.FromError(err)
+			assert.True(t, ok)
+			assert.Equal(t, tt.errorCode, status.Code())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, &expectedResponse, createServiceTemplateResponse)
+		}
+	}
+}
+
+func serviceTemplateSetupCreateMock(service *ContrailTypeLogicService, r *models.ServiceTemplate) {
+	service.Next().(*servicesmock.MockService).EXPECT().CreateServiceTemplate(
+		gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil()),
+	).DoAndReturn(
+		func(_ context.Context, _ *services.CreateServiceTemplateRequest,
+		) (response *services.CreateServiceTemplateResponse, err error) {
+			return &services.CreateServiceTemplateResponse{ServiceTemplate: r}, nil
+		},
+	).AnyTimes()
+}
+
+func serviceTemplateSetupMetadataMock(s *ContrailTypeLogicService) {
+	s.MetadataGetter.(*typesmock.MockMetadataGetter).EXPECT().GetMetadata(
+		gomock.Not(gomock.Nil()), gomock.Not(gomock.Nil()),
+	).DoAndReturn(
+		func(_ context.Context, requested basemodels.Metadata) (
+			response *basemodels.Metadata, err error,
+		) {
+			return serviceTemplateSetupMetadata(requested)
+		},
+	).AnyTimes()
+}
+
+func serviceTemplateSetupMetadata(requested basemodels.Metadata) (
+	*basemodels.Metadata, error) {
+	if len(requested.FQName) == 0 {
+		return nil, errutil.ErrorBadRequest("FQ name is empty")
+	}
+
+	if requested.Type == models.KindDomain {
+		if requested.FQName[0] != "default-domain" {
+			return nil, errutil.ErrorNotFound
+		}
+
+		return &basemodels.Metadata{
+			UUID:   "beefbeef-beef-beef-beef-beefbeef0002",
+			FQName: requested.FQName,
+		}, nil
+	}
+
+	return nil, errutil.ErrorBadRequest("No parent found")
+}
