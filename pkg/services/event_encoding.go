cc25e28c696addc25e11553537a478030d25feec
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-01-17 17:43:21 +0100

    Refactor Event.UnmarshalYAML and Event.UnmarshalJSON
    Change-Id: I5076087f59caf7dcfeae93695c4a1efd38caa91c
    Partial-Bug: 1802271

--- pkg/services/event_encoding.go
+++ pkg/services/event_encoding.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gogo/protobuf/types"
 	"github.com/pkg/errors"
 
+	"github.com/Juniper/contrail/pkg/fileutil"
+	"github.com/Juniper/contrail/pkg/format"
 	"github.com/Juniper/contrail/pkg/models/basemodels"
 )
 
@@ -219,21 +221,21 @@ func makeRefEventList(r basemodels.Object, operation RefOperation) (EventList, e
 	return el, nil
 }
 
-//MarshalJSON marshal event.
+// MarshalJSON marshal event.
 func (e *Event) MarshalJSON() ([]byte, error) {
 	return json.Marshal(e.ToMap())
 }
 
-//MarshalYAML marshal event to yaml.
+// MarshalYAML marshal event to yaml.
 func (e *Event) MarshalYAML() (interface{}, error) {
 	return e.ToMap(), nil
 }
 
-//NewEvent makes event from interface.
+// NewEvent makes event from interface.
 func NewEvent(option *EventOption) (*Event, error) {
-	option.sanitizeKind()
+	option.Kind = sanitizeKind(option.Kind)
 
-	switch option.getOperationOrDefault() {
+	switch o := sanitizeOperation(option.Operation); o {
 	case OperationCreate:
 		return NewCreateEvent(option)
 	case OperationUpdate:
@@ -241,7 +243,7 @@ func NewEvent(option *EventOption) (*Event, error) {
 	case OperationDelete:
 		return NewDeleteEvent(option)
 	default:
-		return nil, errors.Errorf("operation %s not supported", option.getOperationOrDefault())
+		return nil, errors.Errorf("operation %s not supported", o)
 	}
 }
 
@@ -274,10 +276,22 @@ func (e *Event) data() interface{} {
 	}
 }
 
+func sanitizeKind(kind string) string {
+	return basemodels.SchemaIDToKind(kind)
+}
+
+func sanitizeOperation(operation string) string {
+	if operation == "" {
+		return OperationCreate
+	}
+	return operation
+}
+
 // CreateEventRequest interface.
 type CreateEventRequest interface {
 	isEvent_Request
 	GetResource() basemodels.Object
+	SetFieldMask(types.FieldMask)
 }
 
 // UpdateEventRequest interface.
@@ -300,6 +314,7 @@ func NewCreateEvent(option *EventOption) (*Event, error) {
 		return nil, errors.Wrapf(err, "failed to create event from option %v", option)
 	}
 	request.GetResource().ApplyMap(option.Data)
+	request.SetFieldMask(option.getFieldMask())
 	return &Event{
 		Request: request,
 	}, nil
@@ -331,13 +346,6 @@ func NewDeleteEvent(option *EventOption) (*Event, error) {
 	}, nil
 }
 
-func (o *EventOption) getOperationOrDefault() string {
-	if o.Operation == "" {
-		return OperationCreate
-	}
-	return o.Operation
-}
-
 func (o *EventOption) getFieldMask() types.FieldMask {
 	if o.FieldMask == nil {
 		return basemodels.MapToFieldMask(o.Data)
@@ -345,6 +353,46 @@ func (o *EventOption) getFieldMask() types.FieldMask {
 	return *o.FieldMask
 }
 
-func (o *EventOption) sanitizeKind() {
-	o.Kind = basemodels.SchemaIDToKind(o.Kind)
+// UnmarshalJSON unmarshalls Event.
+func (e *Event) UnmarshalJSON(data []byte) error {
+	var m map[string]interface{}
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return err
+	}
+	return e.ApplyMap(m)
+}
+
+// UnmarshalYAML unmarshalls Event.
+func (e *Event) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var i interface{}
+	if err := unmarshal(&i); err != nil {
+		return err
+	}
+	m, ok := fileutil.YAMLtoJSONCompat(i).(map[string]interface{})
+	if !ok {
+		return errors.Errorf("failed to unmarshal, got invalid data %v", i)
+	}
+	return e.ApplyMap(m)
+}
+
+// ApplyMap applies map onto event.
+func (e *Event) ApplyMap(m map[string]interface{}) error {
+	data, ok := m["data"].(map[string]interface{})
+	if !ok {
+		return errors.Errorf("got invalid data %v", m["data"])
+	}
+	fm := basemodels.MapToFieldMask(data)
+	event, err := NewEvent(&EventOption{
+		UUID:      format.InterfaceToString(data["uuid"]),
+		Operation: format.InterfaceToString(m["operation"]),
+		Kind:      format.InterfaceToString(m["kind"]),
+		Data:      data,
+		FieldMask: &fm,
+	})
+	if err != nil {
+		return err
+	}
+	*e = *event
+	return nil
 }
