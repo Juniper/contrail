5d36299b1a563d3fcfc736b34a7fd98a86e01832
Daniel Furman <daniel.furman@codilime.com>
2019-01-17 12:29:18 +0100

    Fix intra-VN demo scenario
    - Fix scenario to work on clean k8s test environment
    
    Change-Id: I4f45198861c758f1612dd15aedee6594f298f369
    partial-jira-bug: JBE-763

--- tools/demo/intra-vn-ping/run.sh
+++ tools/demo/intra-vn-ping/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -x
 
 PATH="$PATH:/usr/go/bin"
@@ -14,7 +15,7 @@ tee namespace-blue.json << "EOF"
       "name": "blue"
     },
     "annotations": {
-      "opencontrail.org/network": "{'project': 'k8s-blue', 'domain': 'default-domain', 'name': 'vn_blue'}"
+      "opencontrail.org/network": "{'project': 'k8s-default', 'domain': 'default-domain', 'name': 'vn-blue'}"
     }
   }
 }
@@ -24,32 +25,52 @@ read
 sudo kubectl create -f namespace-blue.json
 read
 
-tee vn_blue.yml << "EOF"
+tee k8s-pod-ipam.yml << "EOF"
+resources:
+- kind: network_ipam
+  data:
+    parent_type: project
+    fq_name:
+    - default-domain
+    - k8s-default
+    - k8s-pod-ipam
+    ipam_subnet_method: flat-subnet
+    ipam_subnets:
+      subnets:
+      - addr_from_start: true
+        alloc_unit: 0
+        default_gateway: 13.32.0.1
+        dns_server_address: 13.32.0.2
+        enable_dhcp: true
+        subnet:
+          ip_prefix: 13.32.0.0
+          ip_prefix_len: 12
+EOF
+read
+
+contrailcli -c config.yml sync k8s-pod-ipam.yml
+read
+
+tee vn-blue.yml << "EOF"
 resources:
 - kind: virtual_network
   data:
     virtual_network_properties:
       forwarding_mode: l3
-      allow_transit:
-      network_id:
-      max_flow_rate:
-      mirror_destination: false
-      vxlan_network_identifier:
-      max_flows:
-      rpf:
-    fq_name: ["default-domain", "k8s-blue", "vn_blue"]
+    fq_name:
+    - default-domain
+    - k8s-default
+    - vn-blue
     address_allocation_mode: flat-subnet-only
     parent_type: project
     network_ipam_refs:
     - to: ["default-domain", "k8s-default", "k8s-pod-ipam"]
       attr:
         ipam_subnets: []
-        host_routes:
-    fabric_snat: false
 EOF
 read
 
-contrailcli -c config.yml sync vn_blue.yml
+contrailcli -c config.yml sync vn-blue.yml
 read
 
 read -p 'Run pod in second terminal using command: "sudo kubectl run -i --tty busybox-one --image=busybox --namespace blue -- sh"'
