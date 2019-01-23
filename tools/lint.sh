19c1e7639fa886ec54abe0f3f4129af32b508ad8
Pawel Zadrozny <pawel.zny@gmail.com>
2019-01-23 14:24:24 +0100

    Separate unittests logs from linter logs
    Redirect logs to files, and upload them to CI server.
    Remove run_goimports function because golangci-lint
    has enabled goimports check.
    
    Change-Id: I228d63851cea888f26c545b9de7288464c90a319
    partial-jira-bug: JBE-792

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 ContrailPackage="github.com/Juniper/contrail"
+TOP=$(cd "$(dirname "$0")" && cd ../ && pwd)
 
 function run_go_tool_fix() {
 	local issues
@@ -9,16 +12,6 @@ function run_go_tool_fix() {
 	[[ -z "$issues" ]] || (echo "Go tool fix found issues: $issues" && return 1)
 }
 
-function run_goimports() {
-	local dirty_files
-	dirty_files="$(goimports -l -local "$ContrailPackage" ./cmd/ ./pkg/ | grep -v _mock.go)"
-
-	[[ -z "$dirty_files" ]] || (echo "Goimports found issues in files: $dirty_files" && return 1)
-}
-
 run_go_tool_fix || exit 1
 
-# TODO: remove when goimports tool is re-enabled in golangci-lint
-run_goimports || exit 1
-
-golangci-lint --config .golangci.yml --verbose run ./... 2>&1 || exit 1
+golangci-lint --config .golangci.yml --verbose run ./... 2>&1 | tee -a "$TOP/linter.log" || exit 1
