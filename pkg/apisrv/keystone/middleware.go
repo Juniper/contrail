477f2de43d61f32b3fefc39b1c6fed13aea811d1
Kaweue <pawel.marchewka@codilime.com>
2019-01-28 20:36:35 +0100

    Implementing ResourceApiDriver for Neutron plugin
    Basic support for pre project read
    
    closes-jira-bug: JBE-531
    Change-Id: Idadd5a5968039c375be64509314755a48f7781a8

--- pkg/apisrv/keystone/middleware.go
+++ pkg/apisrv/keystone/middleware.go
@@ -48,6 +48,8 @@ func authenticate(ctx context.Context, auth *keystone.Auth, tokenString string) 
 
 	var authKey interface{} = "auth"
 	newCtx := context.WithValue(ctx, authKey, authContext)
+	var tokenKey interface{} = "token"
+	newCtx = context.WithValue(newCtx, tokenKey, tokenString)
 	return newCtx, nil
 }
 
@@ -84,7 +86,7 @@ func GetAuthSkipPaths() ([]string, error) {
 		"/contrail-clusters?fields=uuid,name",
 		"/keystone/v3/auth/tokens",
 		"/proxy/keystone/v3/auth/tokens",
-		"/keystone/v3/auth/projects",
+		"/keystone/v3/projects",
 		"/v3/auth/tokens",
 	}
 	// skip auth for all the static fileutil
