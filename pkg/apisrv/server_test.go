dbace28897c2f8e935d27729297f5b01734c5615
aakimov <github@akimov.me>
2019-01-29 17:34:08 +0300

    Support exclude_children and exclude_back_refs query parameters
    Change-Id: Iab0fef69e929172bb0d55b8ea292d61a4eecbdbe
    Closes-Bug: #1805437
    Depends-On: I5ff1925fdea54c50d3d3aa957f1a4f77a155309b

--- pkg/apisrv/server_test.go
+++ pkg/apisrv/server_test.go
@@ -263,6 +263,11 @@ func TestIDToFQName(t *testing.T) {
 	RunTest(t, "./test_data/test_id_to_fqname.yml")
 }
 
+func TestExclude(t *testing.T) {
+	integration.AddKeystoneProjectAndUser(server.APIServer, t.Name())
+	RunTest(t, "./test_data/test_exclude.yml")
+}
+
 func restLogin(ctx context.Context, t *testing.T) (authToken string) {
 	restClient := client.NewHTTP(
 		server.URL(),
