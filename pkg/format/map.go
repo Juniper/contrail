53cf8bedd89736d34425d3bb58ed8f10b41c41f1
Jozef Woloch <jozef.woloch@codilime.com>
2019-01-30 15:40:35 +0100

    Support fieldmask in neutron
    Change-Id: I225429cf3f519d08e1601ba6ccce4fa19a821a79
    closes-jira-bug: JBE-782

---
+++ pkg/format/map.go
@@ -0,0 +1,182 @@
+package format
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// MapApplier interface for defining custom map applying logic.
+type MapApplier interface {
+	ApplyMap(map[string]interface{}) error
+}
+
+// ApplyMap applies map onto interface which is a struct.
+func ApplyMap(m map[string]interface{}, o interface{}) error {
+	if len(m) == 0 {
+		return nil
+	}
+	sv, err := getSettableStructValue(o)
+	if err != nil {
+		return err
+	}
+	applied, err := useMapApplierIfAvailable(sv, m)
+	if err != nil {
+		return err
+	}
+	if applied {
+		return nil
+	}
+	for i := 0; i < sv.NumField(); i++ {
+		k := fieldKey(sv.Type().Field(i))
+		fieldValue, ok := m[k]
+		if !ok || fieldValue == nil {
+			continue
+		}
+		if isPointerToNonStruct(sv.Field(i)) {
+			return errors.Errorf("only pointer to struct fields can be applied, field %s", k)
+		}
+		err := applyValue(sv.Field(i), fieldValue)
+		if err != nil {
+			return errors.Wrapf(err, "failed to apply field %s", k)
+		}
+	}
+	return nil
+}
+
+func getSettableStructValue(o interface{}) (reflect.Value, error) {
+	ov := reflect.ValueOf(o)
+	if ov.Kind() != reflect.Ptr {
+		return reflect.Value{}, errors.Errorf("cannot apply to non pointer %T", o)
+	}
+	if ov.IsNil() {
+		return reflect.Value{}, errors.Errorf("cannot apply to nil pointer %T", o)
+	}
+	iv := reflect.Indirect(ov)
+	if iv.Kind() == reflect.Ptr && iv.IsNil() {
+		iv.Set(reflect.New(iv.Type().Elem()))
+	}
+	sv := reflect.Indirect(iv)
+	if sv.Kind() != reflect.Struct {
+		return reflect.Value{}, errors.Errorf("cannot apply map to %T", sv.Interface())
+	}
+	return sv, nil
+}
+
+func applyValue(v reflect.Value, i interface{}) error {
+	if !isSettable(v) {
+		return errors.Errorf("cannot set value of %s", v.Type().Name())
+	}
+	applied, err := useMapApplierIfAvailable(v, i)
+	if err != nil {
+		return err
+	}
+	if applied {
+		return nil
+	}
+	return apply(v, i)
+}
+
+func isSettable(v reflect.Value) bool {
+	return v.CanSet() && v.CanInterface() && v.CanAddr()
+}
+
+func useMapApplierIfAvailable(v reflect.Value, i interface{}) (bool, error) {
+	a, ok := v.Addr().Interface().(MapApplier)
+	if !ok {
+		return false, nil
+	}
+	if v.Addr().IsNil() {
+		v.Addr().Set(reflect.New(v.Type()))
+	}
+	sm, ok := i.(map[string]interface{})
+	if !ok {
+		return false, errors.Errorf("cannot apply %T onto %T", i, v.Interface())
+	}
+	return true, a.ApplyMap(sm)
+}
+
+// nolint: gocyclo
+func apply(v reflect.Value, i interface{}) error {
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v.SetInt(InterfaceToInt64(i))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v.SetUint(InterfaceToUint64(i))
+	case reflect.Bool:
+		v.SetBool(InterfaceToBool(i))
+	case reflect.String:
+		v.SetString(InterfaceToString(i))
+	case reflect.Float32, reflect.Float64:
+		v.SetFloat(InterfaceToFloat(i))
+	case reflect.Array, reflect.Slice:
+		return applySlice(v, i)
+	case reflect.Ptr, reflect.Struct:
+		return applyStruct(i, v.Addr().Interface())
+	case reflect.Interface:
+		return applyInterface(v, i)
+	case reflect.Map:
+		return errors.New("map field needs to implement MapApplier interface")
+	default:
+		return errors.Errorf("applying field of type: '%s' not implemented", v.Kind())
+	}
+	return nil
+}
+
+func applySlice(v reflect.Value, i interface{}) error {
+	vv := reflect.ValueOf(i)
+	if vv.IsNil() {
+		return nil
+	}
+	if !(vv.Type().Kind() == reflect.Array || vv.Type().Kind() == reflect.Slice) {
+		return errors.Errorf("cannot apply %T onto %T", i, v.Interface())
+	}
+	for n := 0; n < vv.Len(); n++ {
+		tmp := reflect.New(v.Type().Elem())
+		err := applyValue(tmp.Elem(), vv.Index(n).Interface())
+		if err != nil {
+			return err
+		}
+		v.Set(reflect.Append(v, tmp.Elem()))
+	}
+	return nil
+}
+
+func applyStruct(i interface{}, s interface{}) error {
+	sm, ok := i.(map[string]interface{})
+	if !ok {
+		return errors.Errorf("cannot apply %T onto %T", i, s)
+	}
+	return ApplyMap(sm, s)
+}
+
+func applyInterface(v reflect.Value, i interface{}) error {
+	if v.IsNil() {
+		return nil
+	}
+	if v.Elem().Kind() != reflect.Ptr {
+		return errors.Errorf("cannot mutate non pointer %T", v.Interface())
+	}
+	return applyStruct(i, v.Interface())
+}
+
+func isPointerToNonStruct(v reflect.Value) bool {
+	return v.Kind() == reflect.Ptr && v.Type().Elem().Kind() != reflect.Struct
+}
+
+func fieldKey(s reflect.StructField) string {
+	tag, ok := s.Tag.Lookup("json")
+	if !ok {
+		return s.Name
+	}
+	ss := strings.Split(tag, ",")
+	return ss[0]
+}
+
+func indirect(t reflect.Type) reflect.Type {
+	for t != nil && t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	return t
+}
