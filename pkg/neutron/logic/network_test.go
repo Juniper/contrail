4985a3b24fd38457e83f215b32e7e8ef40af5acd
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2019-01-31 16:05:37 +0100

    Create and delete neutron network
    partial-jira-bug: JBE-883
    Change-Id: Ib5722fce7e6cafe80913584aab074f30ec473ccb

--- pkg/neutron/logic/network_test.go
+++ pkg/neutron/logic/network_test.go
@@ -1,12 +1,11 @@
 package logic
 
 import (
+	"context"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-
-	"context"
 
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
@@ -116,6 +115,156 @@ func TestNetworkReadAll(t *testing.T) {
 
 			if tt.fail {
 				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestNetworkDelete(t *testing.T) {
+	type readData struct {
+		vnRes   *services.GetVirtualNetworkResponse
+		fippRes *services.ListFloatingIPPoolResponse
+		fipsRes *services.ListFloatingIPResponse
+	}
+
+	type deleteData struct {
+		vnReq   *services.DeleteVirtualNetworkRequest
+		fippReq *services.DeleteFloatingIPPoolRequest
+		fipReq  *services.DeleteFloatingIPRequest
+	}
+
+	tests := []struct {
+		name       string
+		n          *Network
+		expected   *NetworkResponse
+		fail       bool
+		id         string
+		readData   *readData
+		deleteData *deleteData
+	}{
+		{
+			name:     "Delete network succeeds",
+			n:        &Network{},
+			expected: &NetworkResponse{},
+			id:       "test-vn",
+			readData: &readData{
+				vnRes: &services.GetVirtualNetworkResponse{
+					VirtualNetwork: &models.VirtualNetwork{
+						UUID:           "test-vn",
+						RouterExternal: true,
+						FloatingIPPools: []*models.FloatingIPPool{
+							{UUID: "test-fipp"},
+						},
+					},
+				},
+				fippRes: &services.ListFloatingIPPoolResponse{
+					FloatingIPPools: []*models.FloatingIPPool{
+						{
+							UUID: "test-fipp",
+							FloatingIPs: []*models.FloatingIP{
+								{UUID: "test-fip"},
+							},
+						},
+					},
+				},
+				fipsRes: &services.ListFloatingIPResponse{
+					FloatingIPs: []*models.FloatingIP{
+						{
+							UUID: "test-fip",
+						},
+					},
+				},
+			},
+			deleteData: &deleteData{
+				vnReq:   &services.DeleteVirtualNetworkRequest{ID: "test-vn"},
+				fippReq: &services.DeleteFloatingIPPoolRequest{ID: "test-fipp"},
+				fipReq:  &services.DeleteFloatingIPRequest{ID: "test-fip"},
+			},
+		},
+		{
+			name:     "Delete network fails due to network in use",
+			n:        &Network{},
+			expected: &NetworkResponse{},
+			id:       "test-vn",
+			fail:     true,
+			readData: &readData{
+				vnRes: &services.GetVirtualNetworkResponse{
+					VirtualNetwork: &models.VirtualNetwork{
+						UUID:           "test-vn",
+						RouterExternal: true,
+						FloatingIPPools: []*models.FloatingIPPool{
+							{UUID: "test-fipp"},
+						},
+					},
+				},
+				fippRes: &services.ListFloatingIPPoolResponse{
+					FloatingIPPools: []*models.FloatingIPPool{
+						{
+							UUID: "test-fipp",
+							FloatingIPs: []*models.FloatingIP{
+								{UUID: "test-fip"},
+							},
+						},
+					},
+				},
+				fipsRes: &services.ListFloatingIPResponse{
+					FloatingIPs: []*models.FloatingIP{
+						{
+							UUID: "test-fip",
+							VirtualMachineInterfaceRefs: []*models.FloatingIPVirtualMachineInterfaceRef{
+								{UUID: "test-vmi"},
+							},
+						},
+					},
+				},
+			},
+			deleteData: &deleteData{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockServ := servicesmock.NewMockService(mockCtrl)
+			ctx := context.Background()
+
+			if tt.readData.vnRes != nil {
+				mockServ.EXPECT().GetVirtualNetwork(gomock.Any(), gomock.Any()).Return(
+					tt.readData.vnRes, nil,
+				)
+			}
+			if tt.readData.fippRes != nil {
+				mockServ.EXPECT().ListFloatingIPPool(gomock.Any(), gomock.Any()).Return(
+					tt.readData.fippRes, nil,
+				)
+			}
+			if tt.readData.fipsRes != nil {
+				mockServ.EXPECT().ListFloatingIP(gomock.Any(), gomock.Any()).Return(
+					tt.readData.fipsRes, nil,
+				)
+			}
+
+			if tt.deleteData.vnReq != nil {
+				mockServ.EXPECT().DeleteVirtualNetwork(gomock.Any(), tt.deleteData.vnReq)
+			}
+			if tt.deleteData.fippReq != nil {
+				mockServ.EXPECT().DeleteFloatingIPPool(gomock.Any(), tt.deleteData.fippReq)
+			}
+			if tt.deleteData.fipReq != nil {
+				mockServ.EXPECT().DeleteFloatingIP(gomock.Any(), tt.deleteData.fipReq)
+			}
+
+			rp := RequestParameters{
+				ReadService:  mockServ,
+				WriteService: mockServ,
+			}
+
+			res, err := tt.n.Delete(ctx, rp, tt.id)
+
+			if tt.fail {
+				assert.Error(t, err)
+			} else {
+				assert.EqualValues(t, tt.expected, res)
 			}
 		})
 	}
