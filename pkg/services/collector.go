a749511da682d280bc2ff5c6ddc3e6e1a5ce86bc
aakimov <github@akimov.me>
2019-01-31 18:54:46 +0300

    Send RestApiTrace payload message to Json2Sandesh proxy
    Change-Id: I83b955e65c3aec8b4c8f9fc5b3dffadf880070a3
    closes-jira-bug: JBE-895

---
+++ pkg/services/collector.go
@@ -0,0 +1,132 @@
+package services
+
+/* to use add to config.yml a section:
+collector:
+  url: http://collectorproxyhost:port/url
+  queuesize: 32
+*/
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo"
+	uuid "github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	defaultCollectorTimeout   = 30 * time.Second
+	defaultCollectorQueueSize = 16
+)
+
+const (
+	typeRestApiTrace = "RestApiTrace"
+)
+
+type collectorMessage struct {
+	SandeshType string      `json:"sandesh_type"`
+	Payload     interface{} `json:"payload"`
+}
+
+type collectorReastApiTrace struct {
+	RequestID    string `json:"request_id"`
+	URL          string `json:"url"`
+	Method       string `json:"method"`
+	RequestData  string `json:"request_data"`
+	Status       string `json:"status"`
+	ResponseBody string `json:"response_body"`
+	RequestError string `json:"request_error"`
+}
+
+type Collector struct {
+	url        string
+	timeout    time.Duration
+	client     *http.Client
+	queueSize  int
+	queueCount int
+	queueMutex sync.Mutex
+}
+
+func NewCollector(url string, queueSize int) *Collector {
+	if url == "" {
+		logrus.Info("collector.url not specified in configuration file")
+	}
+	if queueSize == 0 {
+		queueSize = defaultCollectorQueueSize
+	}
+	return &Collector{
+		url:       url,
+		timeout:   defaultCollectorTimeout,
+		queueSize: queueSize,
+	}
+}
+
+func (collector *Collector) RestAPITrace(c echo.Context, reqBody, resBody []byte) {
+	if collector.url == "" {
+		return
+	}
+	if c.Request().Method == "GET" {
+		return
+	}
+
+	m := &collectorMessage{
+		SandeshType: typeRestApiTrace,
+		Payload: &collectorReastApiTrace{
+			RequestID:    "req-" + uuid.NewV4().String(),
+			URL:          c.Request().URL.String(),
+			Method:       c.Request().Method,
+			RequestData:  string(reqBody),
+			Status:       strconv.Itoa(c.Response().Status) + " " + http.StatusText(c.Response().Status),
+			ResponseBody: string(resBody),
+			RequestError: "",
+		},
+	}
+
+	data, err := json.Marshal(m)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"err": err}).Info("send message to collector failed")
+		return
+	}
+	go collector.sendMessage(data)
+}
+
+func (collector *Collector) sendMessage(data []byte) {
+	collector.queueMutex.Lock()
+	if collector.queueCount > collector.queueSize {
+		collector.queueMutex.Unlock()
+		logrus.Info("collector message queue is full")
+		return
+	}
+	collector.queueCount++
+	collector.queueMutex.Unlock()
+	defer func() {
+		collector.queueMutex.Lock()
+		collector.queueCount--
+		collector.queueMutex.Unlock()
+	}()
+	if collector.client == nil {
+		collector.client = &http.Client{
+			Timeout: collector.timeout,
+		}
+	}
+	req, err := http.NewRequest("POST", collector.url, bytes.NewReader(data))
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"err": err}).Info("send message to collector failed")
+		return
+	}
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	resp, err := collector.client.Do(req)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"err": err}).Info("send message to collector failed")
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		logrus.WithFields(logrus.Fields{"status_code": resp.StatusCode}).Info("send message to collector failed")
+	}
+}
