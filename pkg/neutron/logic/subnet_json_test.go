717201dcc0374cddbdd51a74937b3285fc010855
Jozef Woloch <jozef.woloch@codilime.com>
2019-02-06 16:31:41 +0100

    Parse Subet.IpamFQName in neutron
    Change-Id: I45711c25070e1e3b2ef03e8f3e4c1e972e88ed11
    partial-jira-bug: JBE-885

---
+++ pkg/neutron/logic/subnet_json_test.go
@@ -0,0 +1,60 @@
+package logic
+
+import (
+	"github.com/Juniper/contrail/pkg/format"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSubnet_ApplyMap(t *testing.T) {
+	tests := []struct {
+		name     string
+		m map[string]interface{}
+		expected Subnet
+		fails    bool
+	}{
+		{
+			name: "integer_instead_of_ipam_fq_name",
+			m: map[string]interface{}{
+				"ipam_fq_name": 1,
+			},
+			fails: true,
+		},
+		{
+			name: "array_of_strings",
+			m: map[string]interface{}{
+				"ipam_fq_name": []interface{}{
+					"default-domain",
+					"default-project",
+					"default-network-ipam",
+				},
+			},
+			expected:Subnet{
+				IpamFQName: []string{
+					"default-domain",
+					"default-project",
+					"default-network-ipam",
+				},
+			},
+		},
+		{
+			name: "string",
+			m: map[string]interface{}{
+				"ipam_fq_name": "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Subnet{}
+			err := format.ApplyMap(tt.m, &s)
+			if tt.fails {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.expected, s)
+		})
+	}
+}
