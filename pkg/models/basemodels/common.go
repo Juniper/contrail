d6a93d0201cc00779210d8ea5bde389cece5c9d4
Ignacy Osetek <ignacy.osetek@codilime.com>
2019-02-08 14:50:47 +0100

    Neutron Subnet: Add Update method.
    Change-Id: I57115f92b75547acd86f53450f703c32aad99e64
    partial-jira-bug: JBE-885

--- pkg/models/basemodels/common.go
+++ pkg/models/basemodels/common.go
@@ -3,6 +3,7 @@ package basemodels
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 // CommonFieldPerms2 is a resource field that stores PermType2 data.
@@ -90,4 +91,20 @@ func SchemaIDToKind(kind string) string {
 // ReferenceKind constructs reference kind for given from and to kinds.
 func ReferenceKind(fromKind, toKind string) string {
 	return fmt.Sprintf("%s-%s", fromKind, toKind)
+}
+
+const (
+	vncFormatWithoutNanoseconds = "2006-01-02T15:04:05"
+	vncFormatWithNanoseconds    = "2006-01-02T15:04:05.000000"
+)
+
+// ToVNCTime returns time string in VNC format.
+func ToVNCTime(t time.Time) string {
+	var format string
+	if t.Nanosecond() < 1000 {
+		format = vncFormatWithoutNanoseconds
+	} else {
+		format = vncFormatWithNanoseconds
+	}
+	return t.UTC().Format(format)
 }
