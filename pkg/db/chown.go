8b9a1f8e08d4b6f478cbed7b3efc7514f71eaf0e
Mateusz Neumann <mateusz.neumann@codilime.com>
2019-02-12 14:33:25 +0100

    Change owner of the resource
    Change-Id: I2174fb983c634a251d5cf3d0dc2a63b2edf7731b
    Co-authored-by: "Witaut Bajaryn <witaut.bajaryn@codilime.com>"
    closes-jira-bug: JBE-291

---
+++ pkg/db/chown.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+
+	"github.com/gogo/protobuf/types"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// Chown changes ownership of an object given its uuid and type.
+func (db *Service) Chown(ctx context.Context, uuid string, objectType string, owner string) error {
+	return db.DoInTransaction(ctx, func(ctx context.Context) error {
+		var fm types.FieldMask
+		basemodels.FieldMaskAppend(&fm, models.VirtualNetworkFieldPerms2, models.PermType2FieldOwner)
+
+		event, err := services.NewEvent(&services.EventOption{
+			UUID:      uuid,
+			Kind:      objectType,
+			Operation: services.OperationUpdate,
+			Data: map[string]interface{}{
+				"perms2": map[string]interface{}{
+					"owner": owner,
+				},
+			},
+			FieldMask: &fm,
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = event.Process(ctx, db)
+		return err
+	})
+}
