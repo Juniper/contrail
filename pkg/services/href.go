fa1ad7928ab8ad174ed553670813789f2e693136
Jozef Woloch <jozef.woloch@codilime.com>
2019-02-12 08:38:11 +0100

    Apply hrefs in references and children
    Change-Id: Ide5c8df6fb28d501c9a265d670c60632eebd1141
    Closes-Bug: #1803146

---
+++ pkg/services/href.go
@@ -0,0 +1,82 @@
+package services
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/pkg/errors"
+)
+
+type withHref interface {
+	Kind() string
+	GetUUID() string
+	SetHref(string)
+}
+
+func applyHref(r basemodels.Object, req *http.Request) error {
+	err := applyHrefOnResource(r, req)
+	if err != nil {
+		return err
+	}
+	for _, ref := range r.GetReferences() {
+		err = applyHrefOnReference(r, newRefToHrefAdapter(ref), req)
+		if err != nil {
+			return err
+		}
+	}
+	for _, backRef := range r.GetBackReferences() {
+		err = applyHrefOnReference(r, backRef, req)
+		if err != nil {
+			return err
+		}
+	}
+	for _, child := range r.GetChildren() {
+		err = applyHrefOnReference(r, child, req)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func applyHrefOnResource(o withHref, req *http.Request) error {
+	objURL, err := url.Parse(GetRequestSchema(req) + req.Host + "/" + o.Kind() + "/")
+	if err != nil {
+		return errors.Wrap(err, "failed to parse connection url")
+	}
+	uuidURL, err := url.Parse(o.GetUUID())
+	if err != nil {
+		return errors.Wrap(err, "failed to parse uuid")
+	}
+	o.SetHref(objURL.ResolveReference(uuidURL).String())
+	return nil
+}
+
+func applyHrefOnReference(from, to withHref, req *http.Request) error {
+	refURL, err := url.Parse(GetRequestSchema(req) + req.Host + "/" + to.Kind() + "/")
+	if err != nil {
+		return errors.Wrapf(err, "failed to resolve '%s-%s' reference url", from.Kind(), to.Kind())
+	}
+	uuidURL, err := url.Parse(to.GetUUID())
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse '%s-%s' reference url with uuid '%s'",
+			from.Kind(), to.Kind(), to.GetUUID())
+	}
+	to.SetHref(refURL.ResolveReference(uuidURL).String())
+	return nil
+}
+
+type refToHrefAdapter struct {
+	basemodels.Reference
+}
+
+func newRefToHrefAdapter(r basemodels.Reference) *refToHrefAdapter {
+	return &refToHrefAdapter{
+		Reference: r,
+	}
+}
+
+func (r *refToHrefAdapter) Kind() string {
+	return r.GetReferredKind()
+}
---
+++ pkg/services/href_test.go
@@ -0,0 +1,158 @@
+package services_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/Juniper/contrail/pkg/db/basedb"
+	"github.com/Juniper/contrail/pkg/fileutil"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/testutil/integration"
+)
+
+const (
+	projectBluePath        = "test_data/project_blue.yml"
+	virtualNetworkBluePath = "test_data/virtual_network_blue.yml"
+	networkIpamBluePath    = "test_data/network_ipam_blue.yml"
+)
+
+type uuidAndKind interface {
+	GetUUID() string
+	Kind() string
+}
+
+func TestApplyHref(t *testing.T) {
+	runTest(t, func(t *testing.T, client *integration.HTTPAPIClient, server *integration.APIServer) {
+		ctx := context.Background()
+
+		p, deleteProject := createResource(t, ctx, client, loadProject(t, projectBluePath))
+		defer deleteProject()
+		_, deleteNetworkIpam := createResource(t, ctx, client, loadNetworkIpam(t, networkIpamBluePath))
+		defer deleteNetworkIpam()
+		vn, deleteVirtualNetwork := createResource(t, ctx, client, loadVirtualNetwork(t, virtualNetworkBluePath))
+		defer deleteVirtualNetwork()
+
+		virtualNetwork := integration.GetVirtualNetwork(t, client, vn.GetUUID())
+		require.Equal(t, 1, len(virtualNetwork.NetworkIpamRefs))
+		ipamRef := virtualNetwork.NetworkIpamRefs[0]
+
+		project := integration.GetProject(t, client, p.GetUUID())
+		require.Equal(t, 1, len(project.VirtualNetworks))
+		childNetwork := project.VirtualNetworks[0]
+
+		networkIpam := integration.GetNetworkIpam(t, client, ipamRef.GetUUID())
+		require.Equal(t, 1, len(networkIpam.VirtualNetworkBackRefs))
+		backReferencedVirtualNetwork := networkIpam.VirtualNetworkBackRefs[0]
+
+		tests := []struct {
+			name        string
+			uuidAndKind uuidAndKind
+			actual      string
+		}{
+			{
+				name:        "resource href",
+				uuidAndKind: virtualNetwork,
+				actual:      virtualNetwork.Href,
+			},
+			{
+				name:        "network ipam reference href",
+				uuidAndKind: newRefToUUIDAndKindAdapter(ipamRef),
+				actual:      ipamRef.Href,
+			},
+			{
+				name:        "project child vn href",
+				uuidAndKind: childNetwork,
+				actual:      childNetwork.Href,
+			},
+			{
+				name:        "network ipam back-referenced vn href",
+				uuidAndKind: backReferencedVirtualNetwork,
+				actual:      backReferencedVirtualNetwork.Href,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				expectedHref := server.URL() + "/" + tt.uuidAndKind.Kind() + "/" + tt.uuidAndKind.GetUUID()
+				assert.Equal(t, expectedHref, tt.actual)
+			})
+		}
+	})
+}
+
+// nolint: golint
+func deleteResource(t *testing.T, ctx context.Context, client services.Service, object basemodels.Object) {
+	e, err := services.NewEvent(&services.EventOption{
+		UUID:      object.GetUUID(),
+		Kind:      object.Kind(),
+		Operation: services.OperationDelete,
+	})
+	require.NoError(t, err)
+	_, err = e.Process(ctx, client)
+	require.NoError(t, err)
+}
+
+// nolint: golint
+func createResource(t *testing.T, ctx context.Context, client services.Service, object basemodels.Object) (basemodels.Object, func()) {
+	e, err := services.NewEvent(&services.EventOption{
+		UUID: object.GetUUID(),
+		Kind: object.Kind(),
+		Data: object.ToMap(),
+	})
+	require.NoError(t, err)
+	resp, err := e.Process(ctx, client)
+	require.NoError(t, err)
+	return resp.GetResource(), func() {
+		deleteResource(t, ctx, client, resp.GetResource())
+	}
+}
+
+func loadProject(t *testing.T, path string) (project *models.Project) {
+	require.NoError(t, fileutil.LoadFile(path, &project))
+	return project
+}
+
+func loadVirtualNetwork(t *testing.T, path string) (vn *models.VirtualNetwork) {
+	require.NoError(t, fileutil.LoadFile(path, &vn))
+	return vn
+}
+
+func loadNetworkIpam(t *testing.T, path string) (ni *models.NetworkIpam) {
+	require.NoError(t, fileutil.LoadFile(path, &ni))
+	return ni
+}
+
+func runTest(t *testing.T, test func(*testing.T, *integration.HTTPAPIClient, *integration.APIServer)) {
+	for _, driver := range []string{basedb.DriverMySQL, basedb.DriverPostgreSQL} {
+		func() {
+			s := integration.NewRunningAPIServer(t, &integration.APIServerConfig{
+				DBDriver:     driver,
+				RepoRootPath: "../../..",
+			})
+			defer func() {
+				assert.NoError(t, s.Close())
+			}()
+
+			test(t, integration.NewTestingHTTPClient(t, s.URL()), s)
+		}()
+	}
+}
+
+type refToUUIDAndKindAdapter struct {
+	basemodels.Reference
+}
+
+func newRefToUUIDAndKindAdapter(r basemodels.Reference) *refToUUIDAndKindAdapter {
+	return &refToUUIDAndKindAdapter{
+		Reference: r,
+	}
+}
+
+func (r *refToUUIDAndKindAdapter) Kind() string {
+	return r.GetReferredKind()
+}
