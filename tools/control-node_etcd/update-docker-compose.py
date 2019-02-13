cdf312ea6970ad0beb012ee2ef33f740254d6798
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2019-02-13 14:40:21 +0100

    Update control node image
    Change-Id: I57970553d58a11957856e744162189ba40b88bbd

--- tools/control-node_etcd/update-docker-compose.py
+++ tools/control-node_etcd/update-docker-compose.py
@@ -6,7 +6,7 @@ docker_compose_path = "/etc/contrail/control/docker-compose.yaml"
 with open(docker_compose_path) as f:
     docker_compose = yaml.load(f)
 
-docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync"
+docker_compose["services"]["control"]["image"] = "katrybacka/contrail-controller-control-control:etcd_sync2"
 
 etcd_pki_mount = "/etc/kubernetes/pki/etcd:/etc/kubernetes/pki/etcd:ro"
 volumes = docker_compose["services"]["control"].setdefault("volumes", [])
