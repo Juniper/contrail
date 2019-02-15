4d2def747c956bc7e9cbc5ffee0141e8d5b486ae
Ignatious Johnson <ijohnson@juniper.net>
2019-02-15 13:17:47 -0800

    Read the TOR resources(physical_router) and
    apply it in topology template to generate topology
    with TOR configs.
    
    * UT to verify this feature
    
    Change-Id: I79a8cdfa722f32a2028eddafb06b524b2749c7a5
    close-jira-bug: CEM-548
    close-jira-bug: CEM-549
    close-jira-bug: CEM-550

--- pkg/cloud/data.go
+++ pkg/cloud/data.go
@@ -27,6 +27,7 @@ type Data struct {
 	securityGroups []*sgData
 	providers      []*providerData
 	instances      []*instanceData
+	tors           []*torData
 }
 
 type apiServer struct {
@@ -53,6 +54,7 @@ type virtualCloudData struct {
 	info         *models.VirtualCloud
 	sgs          []*sgData
 	instances    []*instanceData
+	tors         []*torData
 	subnets      []*subnetData
 	apiServer
 }
@@ -66,6 +68,15 @@ type instanceData struct {
 	pvtIntf       *models.Port
 	gateway       string
 	tags          map[string]string
+	apiServer
+}
+
+type torData struct {
+	parentVC               *virtualCloudData
+	info                   *models.PhysicalRouter
+	provision              string
+	autonomousSystemNumber int
+	interfaceName          string
 	apiServer
 }
 
@@ -142,6 +153,18 @@ func (i *instanceData) getNodeObject() (*models.Node, error) {
 		return nil, err
 	}
 	return instResp.GetNode(), nil
+}
+
+func (i *torData) getTorObject() (*models.PhysicalRouter, error) {
+
+	request := new(services.GetPhysicalRouterRequest)
+	request.ID = i.info.UUID
+
+	torResp, err := i.client.GetPhysicalRouter(i.ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	return torResp.GetPhysicalRouter(), nil
 }
 
 // nolint: gocyclo
@@ -223,6 +246,42 @@ func (v *virtualCloudData) newInstance(instance *models.Node) (*instanceData, er
 	return inst, nil
 }
 
+func (v *virtualCloudData) newTorInstance(p *models.PhysicalRouter) (tor *torData, err error) {
+	data := v.parentRegion.parentProvider.parentCloud
+	if !data.isCloudPrivate() {
+		return nil, nil
+	}
+	tor = &torData{
+		parentVC: v,
+		info:     p,
+		apiServer: apiServer{
+			client: v.client,
+			ctx:    v.ctx,
+		},
+	}
+	tor.info, err = tor.getTorObject()
+	if err != nil {
+		return nil, err
+	}
+	var k []*models.KeyValuePair
+	if a := tor.info.GetAnnotations(); a != nil {
+		k = a.GetKeyValuePair()
+	}
+	for _, keyValuePair := range k {
+		switch keyValuePair.Key {
+		case "autonomous_system":
+			tor.autonomousSystemNumber, err = strconv.Atoi(keyValuePair.Value)
+		case "interface":
+			tor.interfaceName = keyValuePair.Value
+		}
+	}
+
+	if tor.provision == "" {
+		tor.provision = strconv.FormatBool(true)
+	}
+	return tor, nil
+}
+
 func (v *virtualCloudData) updateInstances() error {
 
 	nodes, err := v.getInstancesWithTag(v.info.TagRefs)
@@ -240,6 +299,26 @@ func (v *virtualCloudData) updateInstances() error {
 
 	data := v.parentRegion.parentProvider.parentCloud
 	data.instances = append(data.instances, v.instances...)
+	return nil
+}
+
+func (v *virtualCloudData) updateTorInstances() error {
+
+	physicalRouters, err := v.getTorInstancesWithTag(v.info.TagRefs)
+	if err != nil {
+		return err
+	}
+
+	for _, physicalRouter := range physicalRouters {
+		newI, err := v.newTorInstance(physicalRouter)
+		if err != nil {
+			return err
+		}
+		v.tors = append(v.tors, newI)
+	}
+
+	data := v.parentRegion.parentProvider.parentCloud
+	data.tors = append(data.tors, v.tors...)
 	return nil
 }
 
@@ -269,6 +348,23 @@ func (v *virtualCloudData) getInstancesWithTag(tagRefs []*models.VirtualCloudTag
 		return nil, errors.New("virtual cloud tag is not used by any nodes")
 	}
 	return nodesOfVC, nil
+}
+
+func (v *virtualCloudData) getTorInstancesWithTag(
+	tagRefs []*models.VirtualCloudTagRef) ([]*models.PhysicalRouter, error) {
+	var torOfVC []*models.PhysicalRouter
+
+	for _, tag := range tagRefs {
+		tagResp, err := v.client.GetTag(v.ctx, &services.GetTagRequest{ID: tag.UUID})
+		if err != nil {
+			return nil, err
+		}
+		torOfVC = append(torOfVC, tagResp.Tag.PhysicalRouterBackRefs...)
+	}
+	if len(torOfVC) == 0 {
+		return nil, errors.New("virtual cloud tag is not used by any Tor")
+	}
+	return torOfVC, nil
 }
 
 func (v *virtualCloudData) newSG(mSG *models.CloudSecurityGroup) (*sgData, error) {
@@ -357,6 +453,11 @@ func (r *regionData) updateVClouds() error {
 		}
 
 		err = newVC.updateInstances()
+		if err != nil {
+			return err
+		}
+
+		err = newVC.updateTorInstances()
 		if err != nil {
 			return err
 		}
