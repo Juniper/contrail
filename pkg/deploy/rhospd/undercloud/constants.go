78ea70e3051e391fa29d4f6ecd4646eedbee62c3
Ignatious Johnson <ijohnson@juniper.net>
2019-02-17 01:51:24 -0800

    Update the node with nic details from the
    introspection file.
    * Rename rhospd_overcloud to rhospd_overcloud_node,
    because we need to group the list of overcloud nodes
    under rhospd_overcloud_node, so that in a non ztp workflow
    we have the list of nodes to be used/introspected as part
    of the contrail-cloud-deployer inventory assignment.
    
    closes-jira-bug: CEM-2788
    
    Change-Id: I2068e7f6ace2d134b169fd78387717e3c830686a

--- pkg/deploy/rhospd/undercloud/constants.go
+++ pkg/deploy/rhospd/undercloud/constants.go
@@ -4,15 +4,19 @@ const (
 	defaultResource     = "rhospd-cloud-manager"
 	defaultResourcePath = "/" + defaultResource
 
-	defaultWorkRoot                = "/var/tmp/rhospd_cloud_manager/"
-	defaultSiteTemplate            = "site.tmpl"
-	defaultSiteFile                = "site.yml"
-	defaultContrailCloudDir        = "/var/lib/contrail_cloud/"
-	defaultContrailCloudScriptsDir = defaultContrailCloudDir + "scripts/"
-	defaultContrailCloudConfigDir  = defaultContrailCloudDir + "config/"
+	defaultWorkRoot                   = "/var/tmp/rhospd_cloud_manager/"
+	defaultSiteTemplate               = "site.tmpl"
+	defaultSiteFile                   = "site.yml"
+	defaultInventoryTemplate          = "inventory.tmpl"
+	defaultInventoryFile              = "inventory.yml"
+	defaultContrailCloudDir           = "/var/lib/contrail_cloud/"
+	defaultContrailCloudScriptsDir    = defaultContrailCloudDir + "scripts/"
+	defaultContrailCloudConfigDir     = defaultContrailCloudDir + "config/"
+	defaultContrailCloudIntrospectDir = defaultContrailCloudDir + "introspection/"
 
 	addKnownHostsCmd        = "ssh-keyscan -H localhost >> ~/.ssh/known_hosts"
 	installContrailCloudCmd = "sudo " + defaultContrailCloudScriptsDir + "install_contrail_cloud_manager.sh"
+	inventoryAssignCmd      = "sudo " + defaultContrailCloudScriptsDir + "inventory-assign.sh"
 
 	statusField          = "provisioning_state"
 	statusNoState        = "NOSTATE"
