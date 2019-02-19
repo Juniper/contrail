6048a1ac44e5f3c360ec5488fd8ba55fb6dd4ca1
aakimov <github@akimov.me>
2019-02-19 11:29:51 +0300

    Send ModuleCpuStateTrace payload message to Json2Sandesh proxy
    Change-Id: I4d57c33e02857f67f6f8673c79e809d8605a671e
    closes-jira-bug: JBE-913

---
+++ pkg/collector/cpu_state_test.go
@@ -0,0 +1,29 @@
+package collector
+
+import (
+	"testing"
+	"time"
+
+	"github.com/Juniper/contrail/pkg/version"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCPUState(t *testing.T) {
+	c := &mockCollector{}
+	t.Run("ModuleCpuStateTrace", func(t *testing.T) {
+		c.Send(ModuleCPUStateTrace("localhost:9091"))
+		assert.NotNil(t, c.message)
+		assert.Equal(t, c.message.SandeshType, typeModuleCPUStateTrace)
+		m, ok := c.message.Payload.(*payloadModuleCPUStateTrace)
+		assert.True(t, ok)
+		assert.Equal(t, m.Name, "localhost")
+		assert.Equal(t, m.Table, "ObjectConfigNode")
+		assert.Equal(t, m.BuildInfo.BuildVersion, version.Version)
+		_, err := time.Parse("2006-01-02 15:04:05.999999999", m.BuildInfo.BuildTime)
+		assert.NoError(t, err)
+		assert.Equal(t, m.BuildInfo.BuildUser, version.BuildUser)
+		assert.Equal(t, m.BuildInfo.BuildHostname, version.BuildHostname)
+		assert.Equal(t, m.BuildInfo.BuildID, version.BuildID)
+		assert.Equal(t, m.BuildInfo.BuildNumber, version.BuildNumber)
+	})
+}
---
+++ pkg/version/version.go
@@ -0,0 +1,6 @@
+package version
+
+var (
+	// Version contains the main version number that is being run at the moment.
+	Version = "5.1.0a"
+)
