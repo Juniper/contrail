2fce45e7ffc5cb4832727bcec7f61ebb74a1b60c
Ignatious Johnson Christopher <ijohnson@juniper.net>
2019-02-21 02:56:57 +0000

    Use default_gateway from the contrail_vrouter_node
    resource if provided, else use the one from the
    contrail_cluster resource.
    
    Change-Id: I167ba5b7a9d6e4cbeab02af1b89d6ccf2fb4001e
    close-jira-bug: CEM-2829

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -135,7 +135,7 @@ func TestDBCreateRef(t *testing.T) {
 		{
 			name: "objects missing",
 			request: services.CreateRoutingInstanceRouteTargetRefRequest{
-				ID: "foo",
+				ID:                            "foo",
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			fails: true,
@@ -143,7 +143,7 @@ func TestDBCreateRef(t *testing.T) {
 		{
 			name: "valid ID invalid ref UUID",
 			request: services.CreateRoutingInstanceRouteTargetRefRequest{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			fails: true,
@@ -151,7 +151,7 @@ func TestDBCreateRef(t *testing.T) {
 		{
 			name: "valid ID valid ref UUID",
 			request: services.CreateRoutingInstanceRouteTargetRefRequest{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: rtUUID},
 			},
 			expected: &services.CreateRoutingInstanceRouteTargetRefResponse{
@@ -235,11 +235,11 @@ func TestDBDeleteRef(t *testing.T) {
 		{
 			name: "objects missing",
 			request: services.DeleteRoutingInstanceRouteTargetRefRequest{
-				ID: "foo",
+				ID:                            "foo",
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			expected: &services.DeleteRoutingInstanceRouteTargetRefResponse{
-				ID: "foo",
+				ID:                            "foo",
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			shouldRefExist: true,
@@ -247,11 +247,11 @@ func TestDBDeleteRef(t *testing.T) {
 		{
 			name: "valid ID invalid ref UUID",
 			request: services.DeleteRoutingInstanceRouteTargetRefRequest{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			expected: &services.DeleteRoutingInstanceRouteTargetRefResponse{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: "bar"},
 			},
 			shouldRefExist: true,
@@ -259,11 +259,11 @@ func TestDBDeleteRef(t *testing.T) {
 		{
 			name: "valid ID valid ref UUID",
 			request: services.DeleteRoutingInstanceRouteTargetRefRequest{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: rtUUID},
 			},
 			expected: &services.DeleteRoutingInstanceRouteTargetRefResponse{
-				ID: riUUID,
+				ID:                            riUUID,
 				RoutingInstanceRouteTargetRef: &models.RoutingInstanceRouteTargetRef{UUID: rtUUID},
 			},
 			shouldRefExist: false,
