25dac5dd4b5a7cea554695f4d8beb73980b88ba2
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-02-27 16:56:45 +0100

    [WIP] neutron: Implement basic CRUD for Router
    Implement the parts
    test_basic_snat_behavior_without_external_connectivity
    sanity test uses.
    
    TODO Implement Create.
    TODO Implement Read.
    TODO Implement AddInterface.
    TODO Implement RemoveInterface.
    TODO Implement Delete.
    
    Change-Id: I0a30810e54e3d1f0da243945c0d189aae3d9d6c6
    closes-jira-bug: JBE-881

--- pkg/neutron/logic/router.go
+++ pkg/neutron/logic/router.go
@@ -1,6 +1,11 @@
 package logic
 
-import "context"
+import (
+	"context"
+
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+)
 
 // ReadAll logic
 func (r *Router) ReadAll(
@@ -8,4 +13,67 @@ func (r *Router) ReadAll(
 ) (Response, error) {
 	// TODO implement ReadAll logic
 	return []RouterResponse{}, nil
+}
+
+// Create logic
+func (r *Router) Create(ctx context.Context, rp RequestParameters) (Response, error) {
+	lr, err := r.neutronToVnc(ctx, rp)
+	if err != nil {
+		return nil, err
+	}
+
+	lrResponse, err := rp.WriteService.CreateLogicalRouter(ctx, &services.CreateLogicalRouterRequest{
+		LogicalRouter: lr,
+	})
+	if err != nil {
+		// TODO Wrap.
+		return nil, err
+	}
+
+	// TODO: Update gateway.
+
+	// TODO Wrap err.
+	return r.vncToNeutron(lrResponse.GetLogicalRouter())
+}
+
+func (r *Router) neutronToVnc(ctx context.Context, rp RequestParameters) (*models.LogicalRouter, error) {
+	projectUUID, err := neutronIDToVncUUID(rp.RequestContext.TenantID)
+	if err != nil {
+		// TODO Wrap.
+		return nil, err
+	}
+
+	return &models.LogicalRouter{
+		Name:        r.Name,
+		DisplayName: r.Name,
+		UUID:        r.ID,
+		ParentUUID:  projectUUID,
+		ParentType:  models.KindProject,
+		IDPerms: &models.IdPermsType{
+			Enable:      r.AdminStateUp,
+			Description: r.Description,
+		},
+	}, nil
+}
+
+func (r *Router) vncToNeutron(lr *models.LogicalRouter) (*RouterResponse, error) {
+	response := &RouterResponse{
+		ID:           lr.GetUUID(),
+		Name:         lr.GetDisplayName(), // TODO or Name if it's empty.
+		TenantID:     VncUUIDToNeutronID(lr.GetParentUUID()),
+		AdminStateUp: lr.GetIDPerms().GetEnable(),
+		Shared:       false,
+		Status:       netStatusActive,
+		GWPortID:     "",
+		// TODO ExternalGatewayInfo.
+		Description: lr.GetIDPerms().GetDescription(),
+		CreatedAt:   lr.GetIDPerms().GetCreated(),
+		UpdatedAt:   lr.GetIDPerms().GetLastModified(),
+	}
+
+	if contrailExtensionsEnabled {
+		response.FQName = lr.GetFQName()
+	}
+
+	return response, nil
 }
