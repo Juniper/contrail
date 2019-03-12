1dadab32fe66911cbdc504a5fa6b29738e9ac4f7
Michał Błotniak <michal.blotniak@codilime.com>
2019-03-11 17:18:21 -0700

    Refactor sync to process events
    This was required as first step of transaction handling in sync.
    
    Since Sync creation there were a lot of additions to event procesing
    logic. Those features could be used for code simplification.
    
    Removed sink concept in favor of simpler row to event processing.
    
    In next step events are going to be aggergated and then pushed to
    processor after a "commit" message.
    
    Change-Id: I518a88e5a11e76fdb321b07e64364a450a96044e
    Partial-Jira-Bug: JBE-986

---
+++ pkg/db/event_decoder.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/Juniper/contrail/pkg/format"
+	"github.com/Juniper/contrail/pkg/models/basemodels"
+	"github.com/Juniper/contrail/pkg/schema"
+	"github.com/Juniper/contrail/pkg/services"
+)
+
+// DecodeRowEvent transforms row data into *services.Event.
+func (db *Service) DecodeRowEvent(
+	operation, resourceName string, pk []string, properties map[string]interface{},
+) (*services.Event, error) {
+	obj, fields, err := db.ScanRow(resourceName, properties)
+	if err != nil {
+		return nil, fmt.Errorf("error scanning row: %v", err)
+	}
+
+	pkLen := len(pk)
+	isRef := strings.HasPrefix(resourceName, schema.RefPrefix)
+	switch {
+	case pkLen == 1 && !isRef:
+		m := obj.ToMap()
+		if fields != nil {
+			m = basemodels.ApplyFieldMask(m, *fields)
+		}
+		return services.NewEvent(&services.EventOption{
+			UUID:      pk[0],
+			Kind:      resourceName,
+			Data:      m,
+			Operation: operation,
+			FieldMask: fields,
+		})
+	case pkLen == 2 && isRef:
+		if operation == services.OperationUpdate {
+			return nil, errors.New("method UPDATE not available on ref_* resources - received ref-relax event")
+		}
+		return services.NewRefUpdateEvent(services.RefUpdateOption{
+			Operation:     services.ParseRefOperation(operation),
+			ReferenceType: refTableNameToReferenceName(resourceName),
+			FromUUID:      pk[0],
+			ToUUID:        pk[1],
+			AttrData:      json.RawMessage(format.MustJSON(obj)),
+		})
+	}
+	return nil, errors.Errorf(
+		"%s row: unhandled case with table %v and primary key with %v elements", operation, resourceName, pkLen,
+	)
+}
+
+func refTableNameToReferenceName(name string) string {
+	return strings.Replace(strings.TrimPrefix(name, "ref_"), "_", "-", -1)
+}
