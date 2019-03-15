8ce7b388d14a2cdb8f2a7f3c190bbc0a6316bb35
Pawel Marchewka <pawel.marchewka@codilime.com>
2019-03-15 15:04:40 +0100

    Use ram based db images for test environment
    Use databases in ram for integration and sanity tests
    
    Change-Id: I56d70850155df81cca3fb35a44cdabfa861f4e12
    parital-jira-bug: JBE-955

--- tools/patroni/build_patroni.sh
+++ tools/patroni/build_patroni.sh
@@ -9,6 +9,8 @@ echo "Downloading patroni"
 (cd $tmpdir && curl -LO "https://github.com/zalando/patroni/archive/v$patroni_version.zip" --connect-timeout 60) || { echo "Failed to download patroni repository" ; exit 1; }
 echo "Upacking repository archive"
 unzip -q "$tmpdir/v$patroni_version.zip" -d $tmpdir && rm "$tmpdir/v$patroni_version.zip" || { echo "Failed to exctract repository archive" ; exit 1; }
+
 echo "Archive unpacked"
-docker build -t patroni "$tmpdir/patroni-$patroni_version" || { echo "Failed to build docker image" ; exit 1; }
+sed -i 's/FROM postgres:10/FROM circleci\/postgres:10-ram/g' $tmpdir/patroni-$patroni_version/Dockerfile
+docker build --no-cache -t patroni "$tmpdir/patroni-$patroni_version" || { echo "Failed to build docker image" ; exit 1; }
 rm -rf $tmpdir || { echo "Failed to remove temporary directory" ; exit 1; }
--- tools/patroni/pull_patroni.sh
+++ tools/patroni/pull_patroni.sh
@@ -2,6 +2,6 @@
 
 [[ "$(docker images -q patroni)" == "" ]] || { echo "Patroni image already exists. Skipping pulling docker image." ; exit 0; }
 
-docker pull katrybacka/patroni:latest || { echo "Error while pulling repository" ; exit 1; }
+docker pull kaweue/patroni:latest || { echo "Error while pulling repository" ; exit 1; }
 docker tag katrybacka/patroni:latest patroni:latest || { echo "Failed to tag repository" ; exit 1; }
 
