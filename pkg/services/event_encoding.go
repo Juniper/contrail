8066ca08b668da63f725e8dbe5621b52a1e521a6
Jozef Woloch <jozef.woloch@codilime.com>
2019-03-18 17:19:02 +0100

    Sync add to field to references
    Change-Id: Ia374c54fdd771f6d72c4e07d0b8988d6de2d5719
    partial-jira-bug: JBE-984

--- pkg/services/event_encoding.go
+++ pkg/services/event_encoding.go
@@ -452,8 +452,7 @@ type CreateRefEventRequest interface {
 type CreateRefRequest interface {
 	GetID() string
 	SetID(string)
-	SetRefUUID(string)
-	GetAttr() basemodels.RefAttribute
+	GetReference() basemodels.Reference
 	isCreateRefRequest()
 }
 
@@ -467,7 +466,7 @@ type DeleteRefEventRequest interface {
 type DeleteRefRequest interface {
 	GetID() string
 	SetID(string)
-	SetRefUUID(string)
+	GetReference() basemodels.Reference
 	isDeleteRefRequest()
 }
 
@@ -518,8 +517,8 @@ func NewCreateRefEvent(option RefUpdateOption) (*Event, error) {
 		return nil, errors.Wrapf(err, "failed to create event from option %v", option)
 	}
 	r.GetRequest().SetID(option.FromUUID)
-	r.GetRequest().SetRefUUID(option.ToUUID)
-	if attr := r.GetRequest().GetAttr(); attr != nil {
+	r.GetRequest().GetReference().SetUUID(option.ToUUID)
+	if attr := r.GetRequest().GetReference().GetAttribute(); attr != nil {
 		err = format.ApplyMap(option.Attr, attr)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to unmarshal reference attribute data %v, error %v", option.Attr, err)
@@ -538,7 +537,7 @@ func NewDeleteRefEvent(option RefUpdateOption) (*Event, error) {
 		return nil, errors.Wrapf(err, "failed to create event from option %v", option)
 	}
 	r.GetRequest().SetID(option.FromUUID)
-	r.GetRequest().SetRefUUID(option.ToUUID)
+	r.GetRequest().GetReference().SetUUID(option.ToUUID)
 	return &Event{
 		Request: r,
 	}, nil
