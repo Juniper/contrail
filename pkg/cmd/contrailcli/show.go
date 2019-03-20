1c4cb26f29833c6bdcd1753860ca123f3633a14f
Jozef Woloch <jozef.woloch@codilime.com>
2019-03-20 20:33:49 +0100

    Generic processing of events
    Change-Id: I46b75c3797c35c67d5a37422cc692f1459894776
    partial-jira-bug: JBE-984

--- pkg/cmd/contrailcli/show.go
+++ pkg/cmd/contrailcli/show.go
@@ -53,7 +53,7 @@ func showResource(schemaID, uuid string) (string, error) {
 		return "", err
 	}
 	data, _ := response[dashedCase(schemaID)].(map[string]interface{}) //nolint: errcheck
-	event, err := services.NewEvent(&services.EventOption{
+	event, err := services.NewEvent(services.EventOption{
 		Kind: schemaID,
 		Data: data,
 	})
