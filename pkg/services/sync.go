dcc25dae4ddf62a2ee0165bd2e6be1ba4044b7ee
Jozef Woloch <jozef.woloch@codilime.com>
2019-04-03 11:47:56 +0200

    Describe error handling in Sync REST API
    Change-Id: I0eba42ab692593499045419f6f4574069173e266
    partial-jira-bug: JBE-1005

---
+++ pkg/services/sync.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	"github.com/Juniper/contrail/pkg/errutil"
+)
+
+// RESTSync handles Sync API request.
+func (service *ContrailService) RESTSync(c echo.Context) error {
+	events := &EventList{}
+	if err := c.Bind(events); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("invalid JSON format: %v", err))
+	}
+
+	// TODO: Call events.Sort()
+
+	responses, err := events.Process(c.Request().Context(), service)
+	if err != nil {
+		return errutil.ToHTTPError(err)
+	}
+	return c.JSON(http.StatusOK, responses.Events)
+}
