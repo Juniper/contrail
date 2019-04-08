781c6e4f3a85d7153f3219d07cbc8b5cba562f83
Ignacy Osetek <ignacy.osetek@codilime.com>
2019-04-08 14:05:18 +0200

    Translate events to eventNodes.
    EventNode is a structure of event that makes
    event sorting easier.
    
    Change-Id: Idbb25415cb1ebb8c35ead17c7cb95a96b307aea9
    partial-jira-bug: JBE-999

--- pkg/services/event_encoding_test.go
+++ pkg/services/event_encoding_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/gogo/protobuf/types"
@@ -475,4 +476,74 @@ func ensureValidYAML(t *testing.T, b []byte) {
 	var i interface{}
 	err := yaml.Unmarshal(b, &i)
 	require.NoError(t, err)
+}
+
+func Test_eventsToEventNodes(t *testing.T) {
+	project := &models.Project{
+		UUID: "Project",
+	}
+	virtualNetwork := &models.VirtualNetwork{
+		UUID:       "VirtualNetwork",
+		ParentUUID: "Project",
+		NetworkIpamRefs: []*models.VirtualNetworkNetworkIpamRef{
+			{
+				UUID: "NetworkIpam",
+			},
+		},
+	}
+	networkIpam := &models.NetworkIpam{
+		UUID:       "NetworkIpam",
+		ParentUUID: "Project",
+	}
+
+	events := []*Event{
+		{
+			Request: &Event_CreateVirtualNetworkRequest{
+				CreateVirtualNetworkRequest: &CreateVirtualNetworkRequest{
+					VirtualNetwork: virtualNetwork,
+				},
+			},
+		},
+		{
+			Request: &Event_CreateNetworkIpamRequest{
+				CreateNetworkIpamRequest: &CreateNetworkIpamRequest{
+					NetworkIpam: networkIpam,
+				},
+			},
+		},
+		{
+			Request: &Event_CreateProjectRequest{
+				CreateProjectRequest: &CreateProjectRequest{
+					Project: project,
+				},
+			},
+		},
+	}
+
+	eventNodes, err := eventsToEventNodes(events)
+
+	assert.NoError(t, err)
+
+	assert.True(t, reflect.DeepEqual(events[0], eventNodes[0].event))
+	assert.True(t, reflect.DeepEqual(events[1], eventNodes[1].event))
+	assert.True(t, reflect.DeepEqual(events[2], eventNodes[2].event))
+
+	assert.True(t, containsRefsTo(eventNodes[0], []*eventNode{eventNodes[1], eventNodes[2]}))
+	assert.True(t, containsRefsTo(eventNodes[1], []*eventNode{eventNodes[2]}))
+	assert.False(t, containsRefsTo(eventNodes[1], []*eventNode{eventNodes[0]}))
+	assert.False(t, containsRefsTo(eventNodes[2], []*eventNode{eventNodes[0], eventNodes[1]}))
+}
+
+func containsRefsTo(original *eventNode, referencedNodes []*eventNode) bool {
+	refs := make(map[*eventNode]bool)
+	for _, nodeRef := range original.refs {
+		refs[nodeRef] = true
+	}
+
+	for _, ref := range referencedNodes {
+		if !refs[ref] {
+			return false
+		}
+	}
+	return true
 }
