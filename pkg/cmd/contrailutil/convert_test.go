949557263d5825b3878886acd945241691398976
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-04-15 15:18:04 +0200

    [WIP] contrailutil convert: Add a test with refs
    To verify that it works with parent/child and ref relations,
    possibly listed out of order.
    
    TODO Add a reference cycle to the test.
    TODO Check that the database has the correct data.
    
    Change-Id: I801780de5e54a47e6501075fe3317848880f29f4
    closes-jira-bug: JBE-1017

--- pkg/cmd/contrailutil/convert_test.go
+++ pkg/cmd/contrailutil/convert_test.go
@@ -4,8 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/Juniper/contrail/pkg/convert"
+	"github.com/Juniper/contrail/pkg/db/basedb"
+	"github.com/Juniper/contrail/pkg/testutil/integration"
 )
 
 const (
@@ -22,6 +25,41 @@ func TestConvertYAMLToRDBMS(t *testing.T) {
 		InFile:  initDataFile,
 		OutType: convert.RDBMSType,
 	})
-
 	assert.NoError(t, err)
+}
+
+func TestConvertYAMLToRDBMSWithRefs(t *testing.T) {
+	configFile = contrailConfigFile
+	initConfig()
+
+	err := convert.Convert(&convert.Config{
+		InType:  convert.YAMLType,
+		InFile:  initDataFile,
+		OutType: convert.RDBMSType,
+	})
+	require.NoError(t, err)
+
+	err = convert.Convert(&convert.Config{
+		InType:  convert.YAMLType,
+		InFile:  "test_data/test_with_refs.yaml",
+		OutType: convert.RDBMSType,
+	})
+	assert.NoError(t, err)
+
+	s := integration.NewRunningAPIServer(t, &integration.APIServerConfig{
+		DBDriver:           basedb.DriverPostgreSQL,
+		RepoRootPath:       "../../..",
+		EnableEtcdNotifier: false,
+	})
+	defer s.CloseT(t)
+	hc := integration.NewTestingHTTPClient(t, s.URL())
+
+	projectUUID := "9a76fa43-3c35-4c33-92e9-1133629df0ce"
+	vnUUID := "85fa1791-65a3-4797-8732-1d55ba398395"
+	vn := integration.GetVirtualNetwork(t, hc, vnUUID)
+	assert.Equal(t, projectUUID, vn.GetParentUUID())
+
+	// TODO Check that the children/refs are correct.
+
+	// TODO Clean up.
 }
