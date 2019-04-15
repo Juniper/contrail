e9758adec9d321695d0cc845ede462abbe33fd49
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-04-15 15:18:04 +0200

    [WIP] contrailutil convert: Add a test with refs
    To verify that it works with parent/child and ref relations,
    possibly listed out of order.
    
    TODO Add a reference cycle to the test.
    TODO Check that the database has the correct data.
    
    Change-Id: I801780de5e54a47e6501075fe3317848880f29f4
    closes-jira-bug: JBE-1017

--- pkg/cmd/contrailutil/convert_test.go
+++ pkg/cmd/contrailutil/convert_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/Juniper/contrail/pkg/convert"
 )
@@ -22,6 +23,26 @@ func TestConvertYAMLToRDBMS(t *testing.T) {
 		InFile:  initDataFile,
 		OutType: convert.RDBMSType,
 	})
-
 	assert.NoError(t, err)
+}
+
+func TestConvertYAMLToRDBMSWithRefs(t *testing.T) {
+	configFile = contrailConfigFile
+	initConfig()
+
+	err := convert.Convert(&convert.Config{
+		InType:  convert.YAMLType,
+		InFile:  initDataFile,
+		OutType: convert.RDBMSType,
+	})
+	require.NoError(t, err)
+
+	err = convert.Convert(&convert.Config{
+		InType:  convert.YAMLType,
+		InFile:  "test_data/test_with_refs.yaml",
+		OutType: convert.RDBMSType,
+	})
+	assert.NoError(t, err)
+
+	// TODO Test that the expected data is in the database.
 }
