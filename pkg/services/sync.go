aed4b08999b2924c6e568b2d33c8057cf946f2cf
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-04-23 12:44:28 +0200

    Sync API - Sort mixed create and update events
    Change-Id: I67ec1f6dfff1ff0ceffc609d66916a90ad5687b8
    closes-jira-bug: JBE-1029

--- pkg/services/sync.go
+++ pkg/services/sync.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -19,7 +20,7 @@ func (service *ContrailService) RESTSync(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("invalid JSON format: %v", err))
 	}
 
-	events, err := service.sortEvents(c, events)
+	events, err := sortEvents(c.Request().Context(), service, events)
 	if err != nil {
 		return errutil.ToHTTPError(err)
 	}
@@ -29,6 +30,29 @@ func (service *ContrailService) RESTSync(c echo.Context) error {
 		return errutil.ToHTTPError(err)
 	}
 	return c.JSON(http.StatusOK, responses.Events)
+}
+
+func sortEvents(c context.Context, service *ContrailService, events *EventList) (*EventList, error) {
+	switch events.CheckOperationType() {
+	case OperationDelete:
+		return sortDelete(c, service, events)
+	case OperationCreate, "MIXED":
+		return sortMixed(events)
+	default:
+		return events, nil
+	}
+}
+
+func sortCreate(e *EventList) (*EventList, error) {
+	refMap, err := getRefMapFromEvents(e.Events)
+	if err != nil {
+		return nil, err
+	}
+	events, err := syncSort(e, refMap)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
 }
 
 func getRefMapFromEvents(events []*Event) (map[*Event]basemodels.References, error) {
@@ -42,12 +66,37 @@ func getRefMapFromEvents(events []*Event) (map[*Event]basemodels.References, err
 	return refMap, nil
 }
 
-func (service *ContrailService) getRefMapFromObjects(
-	c echo.Context, events []*Event,
+func syncSort(events *EventList, refMap map[*Event]basemodels.References) (*EventList, error) {
+	g := NewEventGraph(events.Events, refMap)
+	if g.CheckCycle() {
+		return events, errors.New("cycle found in reference graph")
+	}
+
+	if !g.isSortRequired(events, refMap) {
+		return events, nil
+	}
+
+	return g.SortEvents(), nil
+}
+
+func sortDelete(c context.Context, service *ContrailService, e *EventList) (*EventList, error) {
+	refMap, err := getRefMapFromObjects(c, service, e.Events)
+	if err != nil {
+		return nil, err
+	}
+	events, err := syncSort(e, refMap)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
+}
+
+func getRefMapFromObjects(
+	c context.Context, service *ContrailService, events []*Event,
 ) (map[*Event]basemodels.References, error) {
 	refMap := make(map[*Event]basemodels.References)
 	for i, ev := range events {
-		obj, _, err := service.getObjectAndType(c.Request().Context(), ev.GetUUID())
+		obj, _, err := service.getObjectAndType(c, ev.GetUUID())
 		if err != nil {
 			return nil, errors.Wrapf(err,
 				"failed to retrieve object for event at index: %v, operation: '%v', kind '%v', uuid '%v'",
@@ -61,35 +110,39 @@ func (service *ContrailService) getRefMapFromObjects(
 	return refMap, nil
 }
 
-func (service *ContrailService) sortEvents(c echo.Context, events *EventList) (*EventList, error) {
-	switch events.CheckOperationType() {
-	case OperationCreate:
-		refMap, err := getRefMapFromEvents(events.Events)
-		if err != nil {
-			return nil, err
-		}
-		return syncSort(events, refMap)
-	case OperationDelete:
-		refMap, err := service.getRefMapFromObjects(c, events.Events)
-		if err != nil {
-			return nil, err
-		}
-		return syncSort(events, refMap)
-	default:
-		logrus.Warn("Sort for operation of other type than CREATE or DELETE is not supported")
+func sortMixed(events *EventList) (*EventList, error) {
+	createsList, updatesList, deletesList := events.separateListByOperation()
+	var err error
+
+	if len(deletesList.Events) != 0 {
+		logrus.Warn("Sort for events mixed with deletes is not supported.")
 		return events, nil
 	}
+
+	if len(createsList.Events) != 0 {
+		events, err = sortCreate(&createsList)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(updatesList.Events) != 0 {
+		events.Events = append(events.Events, updatesList.Events...)
+	}
+	return events, nil
 }
 
-func syncSort(events *EventList, refMap map[*Event]basemodels.References) (*EventList, error) {
-	g := NewEventGraph(events.Events, refMap)
-	if g.CheckCycle() {
-		return events, errors.New("cycle found in reference graph")
+func (e *EventList) separateListByOperation() (EventList, EventList, EventList) {
+	var createList, updateList, deleteList EventList
+	for _, event := range e.Events {
+		switch event.Operation() {
+		case OperationCreate:
+			createList.Events = append(createList.Events, event)
+		case OperationUpdate:
+			updateList.Events = append(updateList.Events, event)
+		case OperationDelete:
+			deleteList.Events = append(deleteList.Events, event)
+		}
 	}
-
-	if !g.isSortRequired(events, refMap) {
-		return events, nil
-	}
-
-	return g.SortEvents(), nil
+	return createList, updateList, deleteList
 }
