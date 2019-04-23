b1e7142fc7de19a70f5adaf1a0691c8422e44868
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-04-23 12:44:28 +0200

    Sync API - Sort mixed create and update events
    Change-Id: I67ec1f6dfff1ff0ceffc609d66916a90ad5687b8
    closes-jira-bug: JBE-1029

--- pkg/services/sync.go
+++ pkg/services/sync.go
@@ -31,6 +31,32 @@ func (service *ContrailService) RESTSync(c echo.Context) error {
 	return c.JSON(http.StatusOK, responses.Events)
 }
 
+func (service *ContrailService) sortEvents(c echo.Context, events *EventList) (*EventList, error) {
+	switch events.CheckOperationType() {
+	case OperationCreate:
+		return sortCreate(events)
+	case OperationDelete:
+		return service.sortDelete(c, events)
+	case "MIXED":
+		return sortMixed(events)
+	default:
+		logrus.Warn("Sort for operation of other type than CREATE or DELETE is not supported")
+		return events, nil
+	}
+}
+
+func sortCreate(e *EventList) (*EventList, error) {
+	refMap, err := getRefMapFromEvents(e.Events)
+	if err != nil {
+		return nil, err
+	}
+	events, err := syncSort(e, refMap)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
+}
+
 func getRefMapFromEvents(events []*Event) (map[*Event]basemodels.References, error) {
 	refMap := make(map[*Event]basemodels.References)
 	var err error
@@ -40,6 +66,32 @@ func getRefMapFromEvents(events []*Event) (map[*Event]basemodels.References, err
 		}
 	}
 	return refMap, nil
+}
+
+func syncSort(events *EventList, refMap map[*Event]basemodels.References) (*EventList, error) {
+	g := NewEventGraph(events.Events, refMap)
+	if g.CheckCycle() {
+		return events, errors.New("cycle found in reference graph")
+	}
+
+	if !events.isSortRequired(g, refMap) {
+		return events, nil
+	}
+
+	return g.SortEvents(), nil
+}
+
+func (service *ContrailService) sortDelete(c echo.Context, e *EventList) (*EventList, error) {
+	logrus.Info("sorting delete")
+	refMap, err := service.getRefMapFromObjects(c, e.Events)
+	if err != nil {
+		return nil, err
+	}
+	events, err := syncSort(e, refMap)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
 }
 
 func (service *ContrailService) getRefMapFromObjects(
@@ -61,35 +113,39 @@ func (service *ContrailService) getRefMapFromObjects(
 	return refMap, nil
 }
 
-func (service *ContrailService) sortEvents(c echo.Context, events *EventList) (*EventList, error) {
-	switch events.CheckOperationType() {
-	case OperationCreate:
-		refMap, err := getRefMapFromEvents(events.Events)
-		if err != nil {
-			return nil, err
-		}
-		return syncSort(events, refMap)
-	case OperationDelete:
-		refMap, err := service.getRefMapFromObjects(c, events.Events)
-		if err != nil {
-			return nil, err
-		}
-		return syncSort(events, refMap)
-	default:
-		logrus.Warn("Sort for operation of other type than CREATE or DELETE is not supported")
+func sortMixed(events *EventList) (*EventList, error) {
+	createsList, updatesList, deletesList := events.separateListByOperation()
+	var err error
+
+	if len(deletesList.Events) != 0 {
+		logrus.Warn("Sort for events mixed with deletes is not supported.")
 		return events, nil
 	}
+
+	if len(createsList.Events) != 0 {
+		events, err = sortCreate(&createsList)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(updatesList.Events) != 0 {
+		events.Events = append(events.Events, updatesList.Events...)
+	}
+	return events, nil
 }
 
-func syncSort(events *EventList, refMap map[*Event]basemodels.References) (*EventList, error) {
-	g := NewEventGraph(events.Events, refMap)
-	if g.CheckCycle() {
-		return events, errors.New("cycle found in reference graph")
+func (e *EventList) separateListByOperation() (EventList, EventList, EventList) {
+	var createList, updateList, deleteList EventList
+	for _, event := range e.Events {
+		switch event.Operation() {
+		case OperationCreate:
+			createList.Events = append(createList.Events, event)
+		case OperationUpdate:
+			updateList.Events = append(updateList.Events, event)
+		case OperationDelete:
+			deleteList.Events = append(deleteList.Events, event)
+		}
 	}
-
-	if !events.isSortRequired(g, refMap) {
-		return events, nil
-	}
-
-	return g.SortEvents(), nil
+	return createList, updateList, deleteList
 }
