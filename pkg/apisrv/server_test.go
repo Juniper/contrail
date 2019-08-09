698dee0410f81a504bca1ac4ae6fd1ac21c0eb02
Daniel Furman <daniel.furman@codilime.com>
2019-08-09 13:43:19 +0200

    Add /upload-cloud-keys endpoint to API Server
    /upload-cloud-key is a custom endpoint used
    specifically to upload clous credentials to
    the api-server host.
    
    Change-Id: I15c89c260ab3d7a3c6cc4ad1b9684f594e218eb3
    Co-authored-by: Witaut Bajaryn <witaut.bajaryn@codilime.com>
    partial-jira-bug: CEM-8026

--- pkg/apisrv/server_test.go
+++ pkg/apisrv/server_test.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/twinj/uuid"
-
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 	"github.com/Juniper/contrail/pkg/services/baseservices"
 	"github.com/Juniper/contrail/pkg/testutil/integration"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/twinj/uuid"
 )
 
 func TestTypeLogic(t *testing.T) {
