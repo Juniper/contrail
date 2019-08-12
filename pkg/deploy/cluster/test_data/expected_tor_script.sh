676c1ea829a467bfe014778ee8a1d97937ac2418
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-08-12 15:04:15 +0200

    Fix ansible playbook directory
    Change-Id: I0a9f2b675bd5b1c606c5d1018cc90a8635807828

--- pkg/deploy/cluster/test_data/expected_tor_script.sh
+++ pkg/deploy/cluster/test_data/expected_tor_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /multicloud_tor_config/bin/activate
-ansible-playbook -i /tmp/contrail_cluster/test_cluster_uuid/multi-cloud/inventories/inventory.yml ansible/tor/playbooks/deploy_and_run_all.yml
+ansible-playbook -i /tmp/contrail_cluster/test_cluster_uuid/multi-cloud/inventories/inventory.yml ansible-multicloud/tor/playbooks/deploy_and_run_all.yml
