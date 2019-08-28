3537745f4f7527b20a14460b94c421dac090b7a5
Ignacy Osetek <ignacy.osetek@codilime.com>
2019-08-28 14:56:34 +0200

    Switch MultiCloud to Python CLI deployment
    Change-Id: I8b301a8eaf5cd6bb479d849513c2dec4ffbe0269
    Closes-Jira-Bug: CEM-7738

--- pkg/ansible/client.go
+++ pkg/ansible/client.go
@@ -95,6 +95,17 @@ func prepareCmd(repositoryPath string, ansibleArgs []string, virtualenvPath stri
 	return cmd, nil
 }
 
+// GetWorkingDirectory returns working directory.
+func (c *CLIClient) GetWorkingDirectory() string {
+	return c.workingDirectory
+}
+
+// IsTest returns true for testing instance.
+// TODO: move test logic to separate Mock type and use dependency injection using ansible.Player interface.
+func (c *CLIClient) IsTest() bool {
+	return c.isTestInstance
+}
+
 func (c *CLIClient) mockPlay(ansibleArgs []string) error {
 	playBookIndex := len(ansibleArgs) - 1
 	content, err := template.Apply("./test_data/test_ansible_playbook.tmpl", pongo2.Context{
