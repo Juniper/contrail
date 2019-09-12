5aa8851b4528912a2bb38f6dfe27469df229be6d
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-09-12 18:00:03 +0200

    [WIP] Add a test for dynamic proxy passing WebSocket
    Change-Id: I4c854f669e95db9252a550799a6e8fa3b2fd84c3
    closes-jira-bug: CEM-5441

--- pkg/apisrv/proxy_test.go
+++ pkg/apisrv/proxy_test.go
@@ -2,9 +2,12 @@ package apisrv_test
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -16,6 +19,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/websocket"
 )
 
 const (
@@ -473,4 +477,70 @@ func TestMultipleClusterKeystoneEndpoint(t *testing.T) {
 		break
 	}
 	server.ForceProxyUpdate()
+}
+
+func TestWebsocketEndpoint(t *testing.T) {
+	clusterName := t.Name() + "_clusterC"
+
+	srv := echoWebsocketServer(t)
+	defer srv.Close()
+	cleanup := createWebsocketEndpoint(t, srv, clusterName)
+	defer cleanup()
+
+	wsURLBase := strings.ReplaceAll(server.URL(), "https://", "wss://")
+	url := wsURLBase + "/proxy/" + clusterName + "_uuid/websocket"
+
+	config, err := websocket.NewConfig(url, "http://localhost/")
+	assert.NoError(t, err, "failed to create websocket config from proxy URL")
+	config.TlsConfig = &tls.Config{InsecureSkipVerify: true} // TODO Take this from the scenario's clients
+	ws, err := websocket.DialConfig(config)
+	assert.NoError(t, err, "failed to connect to a websocket endpoint through the proxy")
+	defer func() {
+		if err := ws.Close(); err != nil {
+			t.Error("Failed to close websocket: ", err)
+		}
+	}()
+
+	sentMsg := []byte("test message")
+	_, err = ws.Write(sentMsg)
+	assert.NoError(t, err, "failed to send a message through the proxied websocket")
+
+	receivedMsg := make([]byte, 100)
+	n, err := ws.Read(receivedMsg)
+	assert.NoError(t, err, "failed to receive a message through the proxied websocket")
+	assert.Equal(t, sentMsg, receivedMsg[:n])
+}
+
+func echoWebsocketServer(t *testing.T) *httptest.Server {
+	server := integration.NewWellKnownServer("", websocket.Handler(func(ws *websocket.Conn) {
+		if _, err := io.Copy(ws, ws); err != nil {
+			t.Error("Failed to echo the message back to the client: ", err)
+		}
+	}))
+	server.Start()
+	return server
+}
+
+func createWebsocketEndpoint(t *testing.T, target *httptest.Server, clusterName string) (cleanup func()) {
+	clusterCUser := clusterName + "_admin"
+	ts, err := integration.LoadTest(
+		testEndpointFile,
+		pongo2.Context{
+			"cluster_name":    clusterName,
+			"endpoint_name":   clusterName + "_websocket",
+			"endpoint_prefix": "websocket",
+			"private_url":     target.URL,
+			"public_url":      target.URL, // TODO Make a "public" server as well?
+			"manage_parent":   true,
+			"admin_user":      clusterCUser,
+		})
+	require.NoError(t, err, "failed to load endpoint create test data")
+	cleanup = integration.RunDirtyTestScenario(t, ts, server)
+
+	server.ForceProxyUpdate()
+
+	return func() {
+		cleanup()
+		server.ForceProxyUpdate()
+	}
 }
