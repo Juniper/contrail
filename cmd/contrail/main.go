83b17931b38995c719c8555ff0616d1624086143
Edwin Jacques <ejacques@juniper.net>
2019-10-22 05:48:27 -0400

    Enable HTTPS redirect for HTTP requests
    When trying to access Contrail Command UI, which is server by default on
    port 9091, the response hangs unless HTTPS:// is specified in the
    browers URL.  This is because server is expecting HTTPS request, while a
    HTTP request is made.  Inform the browser the protocol used is HTTPS.
    
    Closes-Jira-bug: CEM-8872
    Change-Id: I524699bc5f8073a6572f0772877c50b405a62fde

--- cmd/contrail/main.go
+++ cmd/contrail/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"github.com/Juniper/contrail/pkg/cmd/contrail"
 	"github.com/Juniper/contrail/pkg/logutil"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	echoRedirect := echo.New()
+	echoRedirect.Pre(middleware.HTTPSRedirect())
+
 	err := contrail.Contrail.Execute()
 	if err != nil {
 		logutil.FatalWithStackTrace(err)
