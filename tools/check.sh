c791b81f475bd8e9abb6b76f771f254aac7dfa4d
Michał Błotniak <michal.blotniak@codilime.com>
2019-10-22 14:02:58 -0700

    Use go mod instead of dep
    The vendor directory size went down from 252MB to 36MB.
    This change still preserves vendor directory to avoid CI slowdown
    (approx. 7 mins).
    
    Change-Id: I90d9c2f857160e3d052c28b236f2a8393a3ccb26
    Closes-Jira-Bug: CEM-9973

--- tools/check.sh
+++ tools/check.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-dep check
+go mod vendor
+[[ -z $(git status --porcelain -- go.mod) ]] || { echo 'go.mod file has changed. Please update the go.mod file and commit it into repository.'; git diff -- go.mod; exit 1; }
+[[ -z $(git status --porcelain -- vendor/) ]] || { echo "Project's vendor is not up to date. Please update it with 'go mod vendor' command."; exit 1; }
