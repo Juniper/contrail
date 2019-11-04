af81c8ad9dfffc7054e0dacd3a77a2ae6f2fb5f9
Dianqi Ji <dji@juniper.net>
2019-11-04 12:24:25 -0800

    Implemented manipulation of containerized ansible deployer
    Renamed client.go to cli_client.go
    Implemented manipulation of containerized ansible deployer in docker_client.go
    Implemented unit test for docker_client.go in docker_client_test.go
    
    Partial-Jira-Bug: CEM-6497
    Change-Id: I70271912e108cf78a130350d68cc97b2a96a6943

---
+++ vendor/github.com/docker/docker/client/swarm_leave_test.go
@@ -0,0 +1,67 @@
+package client // import "github.com/docker/docker/client"
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/docker/docker/errdefs"
+)
+
+func TestSwarmLeaveError(t *testing.T) {
+	client := &Client{
+		client: newMockClient(errorMock(http.StatusInternalServerError, "Server error")),
+	}
+
+	err := client.SwarmLeave(context.Background(), false)
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %[1]T: %[1]v", err)
+	}
+}
+
+func TestSwarmLeave(t *testing.T) {
+	expectedURL := "/swarm/leave"
+
+	leaveCases := []struct {
+		force         bool
+		expectedForce string
+	}{
+		{
+			expectedForce: "",
+		},
+		{
+			force:         true,
+			expectedForce: "1",
+		},
+	}
+
+	for _, leaveCase := range leaveCases {
+		client := &Client{
+			client: newMockClient(func(req *http.Request) (*http.Response, error) {
+				if !strings.HasPrefix(req.URL.Path, expectedURL) {
+					return nil, fmt.Errorf("Expected URL '%s', got '%s'", expectedURL, req.URL)
+				}
+				if req.Method != http.MethodPost {
+					return nil, fmt.Errorf("expected POST method, got %s", req.Method)
+				}
+				force := req.URL.Query().Get("force")
+				if force != leaveCase.expectedForce {
+					return nil, fmt.Errorf("force not set in URL query properly. expected '%s', got %s", leaveCase.expectedForce, force)
+				}
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
+				}, nil
+			}),
+		}
+
+		err := client.SwarmLeave(context.Background(), leaveCase.force)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
---
+++ vendor/github.com/docker/docker/integration-cli/docker_cli_v2_only_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/docker/docker/testutil/registry"
+	"gotest.tools/assert"
+)
+
+func makefile(path string, contents string) (string, error) {
+	f, err := ioutil.TempFile(path, "tmp")
+	if err != nil {
+		return "", err
+	}
+	err = ioutil.WriteFile(f.Name(), []byte(contents), os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	return f.Name(), nil
+}
+
+// TestV2Only ensures that a daemon does not
+// attempt to contact any v1 registry endpoints.
+func (s *DockerRegistrySuite) TestV2Only(c *testing.T) {
+	reg, err := registry.NewMock(c)
+	assert.NilError(c, err)
+	defer reg.Close()
+
+	reg.RegisterHandler("/v2/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+	})
+
+	reg.RegisterHandler("/v1/.*", func(w http.ResponseWriter, r *http.Request) {
+		c.Fatal("V1 registry contacted")
+	})
+
+	repoName := fmt.Sprintf("%s/busybox", reg.URL())
+
+	s.d.Start(c, "--insecure-registry", reg.URL())
+
+	tmp, err := ioutil.TempDir("", "integration-cli-")
+	assert.NilError(c, err)
+	defer os.RemoveAll(tmp)
+
+	dockerfileName, err := makefile(tmp, fmt.Sprintf("FROM %s/busybox", reg.URL()))
+	assert.NilError(c, err, "Unable to create test dockerfile")
+
+	_, _ = s.d.Cmd("build", "--file", dockerfileName, tmp)
+	_, _ = s.d.Cmd("run", repoName)
+	_, _ = s.d.Cmd("login", "-u", "richard", "-p", "testtest", reg.URL())
+	_, _ = s.d.Cmd("tag", "busybox", repoName)
+	_, _ = s.d.Cmd("push", repoName)
+	_, _ = s.d.Cmd("pull", repoName)
+}
