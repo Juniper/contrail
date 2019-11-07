52dd69c76fe55208cc68e86994ca63fa5d5c46fc
Edwin Jacques <ejacques@juniper.net>
2019-11-07 14:20:53 -0500

    Detect error when contrail_version is a float
    It was reported in CEM-10012 that a cluster is not brought up with the
    proper image using contrail command provisioning cluster cmd.  It was
    believed this could be because the API is not giving an error if the
    contrail_version is specified as a float (only digits with one decimal).
    Add sufficient testing to prove this is the case, and finally to resolve
    the issue.
    
    Closes-Jira-Bug: CEM-10291
    Change-Id: Ib3689a37f402708de0dcc4c53b62c2214a12c1e7

--- pkg/format/cast.go
+++ pkg/format/cast.go
@@ -9,11 +9,18 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//InterfaceToInt makes an int from interface
-// nolint: gocyclo
+//InterfaceToInt makes an int from interface with error reporting.
 func InterfaceToInt(i interface{}) int {
+	var ret, _ = InterfaceToIntE(i)
+	return ret
+}
+
+//InterfaceToIntE makes an int from interface with error reporting with error reporting.
+// nolint: gocyclo
+func InterfaceToIntE(i interface{}) (int, error) {
+	var err error
 	if i == nil {
-		return 0
+		return 0, err
 	}
 	switch t := i.(type) {
 	case []byte:
@@ -21,36 +28,45 @@ func InterfaceToInt(i interface{}) int {
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int", t)
 		}
-		return i
+		return i, err
 	case string:
 		i, err := strconv.Atoi(t)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int", t)
 		}
-		return i
+		return i, err
 	case int:
-		return t
+		return t, err
 	case int64:
-		return int(t)
+		return int(t), err
 	case float64:
-		return int(t)
+		return int(t), err
 	case json.Number:
 		i64, err := t.Int64()
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int", t)
 		}
-		return int(i64)
+		return int(i64), err
+	case nil:
 	default:
-		logrus.Debugf("Could not convert %#v to int", i)
+		err = fmt.Errorf("Could not convert %#v to int", i)
+		logrus.Debug(err.Error())
 	}
-	return 0
+	return 0, err
 }
 
-//InterfaceToInt64 makes an int64 from interface
-// nolint: gocyclo
+//InterfaceToInt64 makes an int64 from interface.
 func InterfaceToInt64(i interface{}) int64 {
+	var ret, _ = InterfaceToInt64E(i)
+	return ret
+}
+
+//InterfaceToInt64E makes an int64 from interface with error reporting with error reporting.
+// nolint: gocyclo
+func InterfaceToInt64E(i interface{}) (int64, error) {
+	var err error
 	if i == nil {
-		return 0
+		return 0, err
 	}
 	switch t := i.(type) {
 	case []byte:
@@ -58,38 +74,47 @@ func InterfaceToInt64(i interface{}) int64 {
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int64", t)
 		}
-		return i64
+		return i64, err
 	case string:
 		i64, err := strconv.ParseInt(t, 10, 64)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int64", t)
 		}
-		return i64
+		return i64, err
 	case int:
-		return int64(t)
+		return int64(t), err
 	case int32:
-		return int64(t)
+		return int64(t), err
 	case int64:
-		return t
+		return t, err
 	case float64:
-		return int64(t)
+		return int64(t), err
 	case json.Number:
 		i64, err := t.Int64()
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to int64", t)
 		}
-		return i64
+		return i64, err
+	case nil:
 	default:
-		logrus.Debugf("Could not convert (%T) %#v to int64", i, i)
+		err = fmt.Errorf("Could not convert (%T) %#v to int64", i, i)
+		logrus.Debug(err.Error())
 	}
-	return 0
+	return 0, err
 }
 
-//InterfaceToUint64 makes an uint64 from interface
-// nolint: gocyclo
+//InterfaceToUint64 makes a uint64 from interface.
 func InterfaceToUint64(i interface{}) uint64 {
+	var ret, _ = InterfaceToUint64E(i)
+	return ret
+}
+
+//InterfaceToUint64E makes a uint64 from interface with error reporting.
+// nolint: gocyclo
+func InterfaceToUint64E(i interface{}) (uint64, error) {
+	var err error
 	if i == nil {
-		return 0
+		return 0, err
 	}
 	switch t := i.(type) {
 	case []byte:
@@ -97,191 +122,308 @@ func InterfaceToUint64(i interface{}) uint64 {
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to uint64", t)
 		}
-		return i
+		return i, err
 	case string:
 		i, err := strconv.ParseUint(t, 10, 64)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to uint64", t)
 		}
-		return i
+		return i, err
 	case int:
-		return uint64(t)
+		return uint64(t), err
 	case int64:
-		return uint64(t)
+		return uint64(t), err
 	case uint:
-		return uint64(t)
+		return uint64(t), err
 	case uint64:
-		return t
+		return t, err
 	case float64:
-		return uint64(t)
+		return uint64(t), err
 	case json.Number:
 		i, err := strconv.ParseUint(t.String(), 10, 64)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to uint64", t)
 		}
-		return i
+		return i, err
+	case nil:
 	default:
-		logrus.Debugf("Could not convert %#v to uint64", i)
+		err = fmt.Errorf("Could not convert %#v to uint64", i)
+		logrus.Debug(err.Error())
 	}
-	return 0
+	return 0, err
 }
 
-//InterfaceToBool makes a bool from interface
+//InterfaceToBool makes a bool from interface.
 func InterfaceToBool(i interface{}) bool {
+	var ret, _ = InterfaceToBoolE(i)
+	return ret
+}
+
+//InterfaceToBoolE makes a bool from interface with error reporting.
+func InterfaceToBoolE(i interface{}) (bool, error) {
+	var err error
 	switch t := i.(type) {
 	case []byte:
 		b, err := strconv.ParseBool(string(t))
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to bool", t)
 		}
-		return b
+		return b, err
 	case string:
 		b, err := strconv.ParseBool(t)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to bool", t)
 		}
-		return b
+		return b, err
 	case bool:
-		return t
+		return t, err
 	case int64:
-		return t == 1
+		return t == 1, err
 	case float64:
-		return t == 1
+		return t == 1, err
+	case nil:
 	default:
-		logrus.Debugf("Could not convert %#v to bool", i)
+		err = fmt.Errorf("Could not convert %#v to bool", i)
+		logrus.Debug(err.Error())
 	}
-	return false
+	return false, err
 }
 
-//InterfaceToString makes a string from interface
+//InterfaceToString makes a string from interface.
 func InterfaceToString(i interface{}) string {
+	var ret, _ = InterfaceToStringE(i)
+	return ret
+}
+
+//InterfaceToStringE makes a string from interface with error reporting.
+func InterfaceToStringE(i interface{}) (string, error) {
+	var err error
 	switch t := i.(type) {
 	case []byte:
-		return string(t)
+		return string(t), err
 	case string:
-		return t
+		return t, err
 	case nil:
-		return ""
 	default:
-		logrus.Debugf("Could not convert %#v to string", i)
+		err = fmt.Errorf("Could not convert %#v to string", i)
+		logrus.Debug(err.Error())
 	}
-	return ""
+	return "", err
 }
 
-//InterfaceToStringList makes a string list from interface
+//InterfaceToStringList makes a string list from interface.
 func InterfaceToStringList(i interface{}) []string {
+	var ret, _ = InterfaceToStringListE(i)
+	return ret
+}
+
+//InterfaceToStringListE makes a string list from interface with error reporting.
+func InterfaceToStringListE(i interface{}) ([]string, error) {
+	var err error
 	switch t := i.(type) {
 	case []string:
-		return t
+		return t, err
 	case []interface{}:
 		result := []string{}
 		for _, s := range t {
-			result = append(result, InterfaceToString(s))
+			var str string
+			var lastError error
+			str, lastError = InterfaceToStringE(s)
+			if (lastError != nil) {
+				logrus.WithError(err).Debugf("Could not convert %#v to []string", i)
+				err = lastError
+			}
+			result = append(result, str)
 		}
-		return result
+		return result, err
 	case nil:
-		return nil
+		return nil, err
 	default:
-		logrus.Debugf("Could not convert %#v to []string", i)
+		err = fmt.Errorf("Could not convert %#v to []string", i)
+		logrus.Debug(err.Error())
 	}
-	return nil
+	return nil, err
 }
 
-//InterfaceToInt64List makes a int64 list from interface
+//InterfaceToInt64List makes a int64 list from interface.
 func InterfaceToInt64List(i interface{}) []int64 {
+	var ret, _ = InterfaceToInt64ListE(i)
+	return ret
+}
+
+//InterfaceToInt64ListE makes a int64 list from interface with error reporting.
+func InterfaceToInt64ListE(i interface{}) ([]int64, error) {
+	var err error
 	switch t := i.(type) {
 	case []int64:
-		return t
+		return t, err
 	case []interface{}:
 		result := []int64{}
 		for _, s := range t {
-			result = append(result, InterfaceToInt64(s))
+			var i64 = int64(0)
+			var lastError error
+			i64, lastError = InterfaceToInt64E(s)
+			if (lastError != nil) {
+				logrus.WithError(err).Debugf("Could not convert %#v to []int64", i)
+				err = lastError
+			}
+			result = append(result, i64)
 		}
-		return result
+		return result, err
 	case nil:
-		return nil
 	default:
-		logrus.Debugf("Could not convert %#v to []int64", i)
+		err = fmt.Errorf("Could not convert %#v to []int64", i)
+		logrus.Debug(err.Error())
 	}
-	return nil
+	return nil, err
 }
 
-//InterfaceToInterfaceList makes a interface list from interface
+//InterfaceToInterfaceList makes a interface list from interface.
 func InterfaceToInterfaceList(i interface{}) []interface{} {
-	t, _ := i.([]interface{}) //nolint: errcheck
-	return t
+	var ret, _ = InterfaceToInterfaceListE(i)
+	return ret
+}
+
+//InterfaceToInterfaceListE makes a interface list from interface with error reporting.
+func InterfaceToInterfaceListE(i interface{}) ([]interface{}, error) {
+	var ret = false
+	t, ret := i.([]interface{})
+	var err error
+	if (!ret) {
+		err = fmt.Errorf("Could not convert %#v to []interface{}", i)
+	}
+	return t, err
 }
 
 //InterfaceToStringMap makes a string map.
 func InterfaceToStringMap(i interface{}) map[string]string {
-	t, _ := i.(map[string]string) //nolint: errcheck
-	return t
+	var ret, _ = InterfaceToStringMapE(i)
+	return ret
+}
+
+//InterfaceToStringMapE makes a string map with error reporting.
+func InterfaceToStringMapE(i interface{}) (map[string]string, error) {
+	var ret = false
+	t, ret := i.(map[string]string)
+	var err error
+	if (!ret) {
+		err = fmt.Errorf("Could not convert %#v to map[string]string", i)
+	}
+	return t, err
 }
 
 //InterfaceToInt64Map makes a string map.
 func InterfaceToInt64Map(i interface{}) map[string]int64 {
-	t, _ := i.(map[string]int64) //nolint: errcheck
-	return t
+	var ret, _ = InterfaceToInt64MapE(i)
+	return ret
+}
+
+//InterfaceToInt64MapE makes a string map with error reporting.
+func InterfaceToInt64MapE(i interface{}) (map[string]int64, error) {
+	var ret = false
+	t, ret := i.(map[string]int64)
+	var err error
+	if (!ret) {
+		err = fmt.Errorf("Could not convert %#v to map[string]int64", i)
+	}
+	return t, err
 }
 
 //InterfaceToInterfaceMap makes a interface map.
 func InterfaceToInterfaceMap(i interface{}) map[string]interface{} {
-	t, _ := i.(map[string]interface{}) //nolint: errcheck
-	return t
+	var ret, _ = InterfaceToInterfaceMapE(i)
+	return ret
+}
+
+//InterfaceToInterfaceMapE makes a interface map with error reporting.
+func InterfaceToInterfaceMapE(i interface{}) (map[string]interface{}, error) {
+	var ret = false
+	t, ret := i.(map[string]interface{})
+	var err error
+	if (!ret) {
+		err = fmt.Errorf("Could not convert %#v to map[string]interface{}", i)
+	}
+	return t, err
 }
 
 //InterfaceToFloat makes a float.
-// nolint: gocyclo
 func InterfaceToFloat(i interface{}) float64 {
-	t, _ := i.(float64) //nolint: errcheck
+	var ret, _ = InterfaceToFloatE(i)
+	return ret
+}
+
+//InterfaceToFloatE makes a float with error reporting.
+// nolint: gocyclo
+func InterfaceToFloatE(i interface{}) (float64, error) {
+	var ret = false
+	t, ret := i.(float64)
+	var err error
+	if (!ret) {
+		return t, fmt.Errorf("Could not convert %#v to float64", i)
+	}
 	switch t := i.(type) {
 	case []byte:
 		f, err := strconv.ParseFloat(string(t), 64)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to float ", t)
 		}
-		return f
+		return f, err
 	case string:
 		f, err := strconv.ParseFloat(t, 64)
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to float ", t)
 		}
-		return f
+		return f, err
 	case int:
-		return float64(t)
+		return float64(t), err
 	case int64:
-		return float64(t)
+		return float64(t), err
 	case float64:
-		return t
+		return t, err
 	case nil:
-		return 0
+		return 0, err
 	case json.Number:
 		f64, err := t.Float64()
 		if err != nil {
 			logrus.WithError(err).Debugf("Could not convert %#v to float64", t)
 		}
-		return f64
+		return f64, err
 	default:
-		logrus.Debugf("Could not convert %#v to float64", i)
+		err = fmt.Errorf("Could not convert %#v to float64", i)
+		logrus.Debug(err.Error())
 	}
-	return t
+	return t, err
 }
 
-//InterfaceToBytes makes a bytes from interface
+//InterfaceToBytes makes a bytes from interface.
 func InterfaceToBytes(i interface{}) []byte {
+	var ret, _ = InterfaceToBytesE(i)
+	return ret
+}
+
+//InterfaceToBytesE makes a bytes from interface with error reporting.
+func InterfaceToBytesE(i interface{}) ([]byte, error) {
+	var err error
 	switch t := i.(type) { //nolint: errcheck
 	case []byte:
-		return t
+		return t, err
 	case string:
-		return []byte(t)
+		return []byte(t), err
 	default:
-		logrus.Debugf("Could not convert %#v to []byte", i)
+		err = fmt.Errorf("Could not convert %#v to []byte", i )
+		logrus.Debug(err.Error())
 	}
-	return []byte{}
+	return []byte{}, err
 }
 
-//GetUUIDFromInterface get a UUID from an interface.
-func GetUUIDFromInterface(rawProperties interface{}) (string, error) {
+//GetUUIDFromInterface get a UUID from an interface with error reporting.
+func GetUUIDFromInterface(rawProperties interface{}) string {
+	var ret, _ = GetUUIDFromInterfaceE(rawProperties)
+	return ret
+}
+
+//GetUUIDFromInterfaceE get a UUID from an interface with error reporting.
+func GetUUIDFromInterfaceE(rawProperties interface{}) (string, error) {
 	properties, ok := rawProperties.(map[string]interface{})
 	if !ok {
 		return "", fmt.Errorf("invalid data format: no properties mapping")
@@ -298,3 +440,4 @@ func GetUUIDFromInterface(rawProperties interface{}) (string, error) {
 	}
 	return uuid, nil
 }
+
