77d6184d03a28bc4acc5397883600079cd57d206
Marcin Wierzbicki <marcin.wierzbicki@codilime.com>
2019-11-14 16:50:29 +0100

    Fixed server-side fields filtering
    Closes-Jira-Bug: CEM-7542
    Change-Id: I823dd16ea58740a1743afd7e4fc4714769c9eb04

--- pkg/apisrv/client/cli_test.go
+++ pkg/apisrv/client/cli_test.go
@@ -265,19 +265,76 @@ func testList(cli *client.CLI) func(t *testing.T) {
 				expected: resources(vnRed(t), vnBlue(t)),
 			},
 			{
-				name: "with parent UUID and fields",
+				name: "with object UUID and fields",
 				lp: &client.ListParameters{
-					ParentUUIDs: projectUUID,
+					ObjectUUIDs: vnRedUUID,
 					Fields:      "name,uuid",
 				},
-				expected: resources(vnRedFiltered(t), vnBlueFiltered(t)),
+				expected: resources(vnRedFiltered(t)),
 				assert: func(t *testing.T, response string) {
 					assert.Equal(
 						t,
-						resources(vnRedFiltered(t), vnBlueFiltered(t)),
+						resources(vnRedFiltered(t)),
 						unmarshalData(t, response),
 					)
 				},
+			},
+			{
+				name: "with object UUID and invalid field",
+				lp: &client.ListParameters{
+					ObjectUUIDs: vnRedUUID,
+					Fields:      "name,uuid,invalid_field123",
+				},
+				expected: resources(vnRedFiltered(t)),
+				assert: func(t *testing.T, response string) {
+					assert.Equal(
+						t,
+						resources(vnRedFiltered(t)),
+						unmarshalData(t, response),
+					)
+				},
+			},
+			{
+				name: "with object UUID and no valid field",
+				lp: &client.ListParameters{
+					ObjectUUIDs: vnRedUUID,
+					Fields:      "invalid_field123",
+				},
+				expected: vnEmptyResource(),
+				assert: func(t *testing.T, response string) {
+					assert.Equal(
+						t,
+						vnEmptyResource(),
+						unmarshalData(t, response),
+					)
+				},
+			},
+			{
+				name: "with object UUID, fields and detail",
+				lp: &client.ListParameters{
+					ObjectUUIDs: vnRedUUID,
+					Fields:      "name,uuid",
+					Detail:      true,
+				},
+				expected: resources(vnRed(t)),
+			},
+			{
+				name: "with object UUID, invalid fields and detail",
+				lp: &client.ListParameters{
+					ObjectUUIDs: vnRedUUID,
+					Fields:      "name,uuid,invalidfield123",
+					Detail:      true,
+				},
+				expected: resources(vnRed(t)),
+			},
+			{
+				name: "with object UUID, no valid field and detail",
+				lp: &client.ListParameters{
+					ObjectUUIDs: vnRedUUID,
+					Fields:      "invalidfield123",
+					Detail:      true,
+				},
+				expected: resources(vnRed(t)),
 			},
 		}
 		for _, tt := range tests {
@@ -395,10 +452,6 @@ func vnBlue(t *testing.T) map[interface{}]interface{} {
 	return unmarshalResource(t, vnBlueYAML())
 }
 
-func vnBlueFiltered(t *testing.T) map[interface{}]interface{} {
-	return unmarshalResource(t, vnBlueFilteredYAML())
-}
-
 func vnRed(t *testing.T) map[interface{}]interface{} {
 	return unmarshalResource(t, vnRedYAML())
 }
@@ -409,6 +462,14 @@ func vnRedFiltered(t *testing.T) map[interface{}]interface{} {
 
 func vnGreen(t *testing.T) map[interface{}]interface{} {
 	return unmarshalResource(t, vnGreenYAML())
+}
+
+func vnEmptyResource() map[interface{}]interface{} {
+	return map[interface{}]interface{}{
+		"resources": []interface{}{
+			map[interface{}]interface{}{
+				"data": map[interface{}]interface{}{},
+				"kind": "virtual_network"}}}
 }
 
 func vnBlueYAML() string {
@@ -423,14 +484,6 @@ data:
   parent_uuid: project-cli-test-uuid
   perms2:
     owner: TestCLI
-  uuid: efb6aa60-9d8e-11e9-b056-13df9df3688a`
-}
-
-func vnBlueFilteredYAML() string {
-	return `
-kind: virtual_network
-data:
-  name: vn-blue
   uuid: efb6aa60-9d8e-11e9-b056-13df9df3688a`
 }
 
