d4be1954114fe40d6eb2d00f48488ea45d6bda2f
Daniel Furman <daniel.furman@codilime.com>
2019-11-14 09:53:03 +0100

    Update Go toolchain to 1.13
    Go 1.13 introduces http.Request.Clone() method which is needed for
    CEM-9562 implementation.
    
    Change-Id: I144d73182e40c5603090a1d71ffa97feb6f9b495
    partial-jira-bug: CEM-9562
    partial-jira-bug: CEM-4381

--- tools/deps.sh
+++ tools/deps.sh
@@ -11,7 +11,7 @@ go install ./vendor/github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 go install ./vendor/golang.org/x/tools/cmd/goimports
 
 curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
-	bash -s -- -b $(go env GOPATH)/bin v1.10.2
+    sh -s -- -b "$(go env GOPATH)"/bin v1.20.0
 
 if [ "$(uname)" == 'Darwin' ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
@@ -22,6 +22,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     unzip -o protoc-3.5.1-linux-x86_64.zip "bin/protoc"
     rm protoc-3.5.1-linux-x86_64.zip
 else
-	echo "Your platform ($(uname -a)) is not supported."
+    echo "Your platform ($(uname -a)) is not supported."
     echo "Please manually install protoc"
 fi
