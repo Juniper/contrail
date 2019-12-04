998bc79097018a19d4700e9cd53bc737e831b166
Daniel Furman <daniel.furman@codilime.com>
2019-12-04 12:56:34 +0100

    Use ASF templates
    Other clients need access to templates, so they need to be moved from Contrail
    to ASF.
    
    partial-jira-bug: CEM-10205
    Change-Id: Ia1ec56c5eb00c04eb4bca69a744531b64f67a3b2

--- tools/deps.sh
+++ tools/deps.sh
@@ -13,6 +13,11 @@ go install ./vendor/golang.org/x/tools/cmd/goimports
 curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
 	bash -s -- -b $(go env GOPATH)/bin v1.10.2
 
+if [[ $(./bin/protoc --version) == "libprotoc 3.5.1" ]]; then
+    echo "Protoc already installed - skipping"
+    exit 0
+fi
+
 if [ "$(uname)" == 'Darwin' ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-osx-x86_64.zip
     unzip -o protoc-3.5.1-osx-x86_64.zip "bin/protoc"
