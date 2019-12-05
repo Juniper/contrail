76b8f165fb4a606d28c8ea78fc1c0b120a9e5397
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-12-05 11:43:38 +0100

    Public cloud yaml marshaling
    Logic for creating public cloud topology uses templates which contain addional logic inside. Goal of this patch is to use Go yaml marshaling in place of this templates.
    
    Change-Id: I83b396be0978006aae038a32b4d0ad4a92c7c4fd
    partial-jira-bug: CEM-10836

--- pkg/cloud/manager.go
+++ pkg/cloud/manager.go
@@ -220,6 +220,13 @@ func (c *Cloud) create() error {
 		return err
 	}
 
+	pubData := &publicCloud{}
+	if !data.isCloudPrivate() {
+		if err = pubData.fill(c.ctx, c.APIServer, c.config.CloudID); err != nil {
+			return err
+		}
+	}
+
 	status := map[string]interface{}{statusField: statusCreateProgress}
 	topo, secret, err := c.initialize(data)
 	if err != nil {
@@ -239,7 +246,11 @@ func (c *Cloud) create() error {
 	c.reporter.ReportStatus(c.ctx, status, defaultCloudResource)
 	status[statusField] = statusCreateFailed
 
-	err = topo.createTopologyFile(GetTopoFile(c.config.CloudID))
+	if data.isCloudPrivate() {
+		err = topo.createOnPremTopologyFile(GetTopoFile(c.config.CloudID))
+	} else {
+		err = topo.marshalAndSave(GetTopoFile(c.config.CloudID), pubData)
+	}
 	if err != nil {
 		c.reporter.ReportStatus(c.ctx, status, defaultCloudResource)
 		return err
@@ -286,7 +297,7 @@ func (d *Data) modifiedProviders() []string {
 		s = append(s, azure)
 	}
 	if d.info.GCPModified {
-		s = append(s, gcp)
+		s = append(s, google)
 	}
 	return s
 }
@@ -324,16 +335,11 @@ func (c *Cloud) update() error {
 	}
 
 	topo := newTopology(c, data)
-	if data.info.ProvisioningState != statusNoState {
-		topoUpToDate, tErr := topo.isUpToDate(defaultCloudResource)
-		if tErr != nil {
-			c.reporter.ReportStatus(c.ctx, map[string]interface{}{statusField: statusUpdateFailed}, defaultCloudResource)
-			return errors.Wrapf(tErr, "failed to check if topology is up to date for cloud %s", c.config.CloudID)
-		}
 
-		if topoUpToDate {
-			c.log.WithField("cloudID", c.config.CloudID).Debug("Topology is already up to date - skipping update")
-			return nil
+	pubData := &publicCloud{}
+	if !data.isCloudPrivate() {
+		if err = pubData.fill(c.ctx, c.APIServer, c.config.CloudID); err != nil {
+			return err
 		}
 	}
 
@@ -353,7 +359,11 @@ func (c *Cloud) update() error {
 	c.reporter.ReportStatus(c.ctx, status, defaultCloudResource)
 	status[statusField] = statusUpdateFailed
 
-	err = topo.createTopologyFile(GetTopoFile(topo.cloud.config.CloudID))
+	if data.isCloudPrivate() {
+		err = topo.createOnPremTopologyFile(GetTopoFile(c.config.CloudID))
+	} else {
+		err = topo.marshalAndSave(GetTopoFile(c.config.CloudID), pubData)
+	}
 	if err != nil {
 		c.reporter.ReportStatus(c.ctx, status, defaultCloudResource)
 		return err
