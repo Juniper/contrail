2a559e3a450dbc03b2f2a011151265e1f96e136d
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-12-05 11:43:38 +0100

    Public cloud yaml marshaling
    Logic for creating public cloud topology uses templates which contain addional logic inside. Goal of this patch is to use Go yaml marshaling in place of this templates.
    
    Change-Id: I83b396be0978006aae038a32b4d0ad4a92c7c4fd
    closes-jira-bug: CEM-11428

--- pkg/cloud/topology.go
+++ pkg/cloud/topology.go
@@ -1,20 +1,19 @@
 package cloud
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"path/filepath"
-
-	"github.com/flosch/pongo2"
-	"github.com/sirupsen/logrus"
 
 	"github.com/Juniper/asf/pkg/fileutil"
 	"github.com/Juniper/asf/pkg/fileutil/template"
 	"github.com/Juniper/asf/pkg/logutil"
 	"github.com/Juniper/asf/pkg/logutil/report"
+	"github.com/flosch/pongo2"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 type topology struct {
@@ -25,16 +24,8 @@ type topology struct {
 	ctx       context.Context
 }
 
-func (t *topology) getTmplFilePath() string {
-	return filepath.Join(t.cloud.getTemplateRoot(), t.getTopoTemplate())
-}
-
-func (t *topology) getTopoTemplate() string {
-
-	if t.cloudData.isCloudPrivate() {
-		return defaultOnPremTopoTemplate
-	}
-	return defaultPublicCloudTopoTemplate
+func (t *topology) getOnPremTemplatePath() string {
+	return filepath.Join(t.cloud.getTemplateRoot(), defaultOnPremTopoTemplate)
 }
 
 func newTopology(c *Cloud, data *Data) *topology {
@@ -51,11 +42,11 @@ func newTopology(c *Cloud, data *Data) *topology {
 	}
 }
 
-func (t *topology) createTopologyFile(topoFile string) error {
+func (t *topology) createOnPremTopologyFile(topoFile string) error {
 	context := pongo2.Context{
 		"cloud": t.cloudData,
 	}
-	content, err := template.Apply(t.getTmplFilePath(), context)
+	content, err := template.Apply(t.getOnPremTemplatePath(), context)
 	if err != nil {
 		return err
 	}
@@ -68,48 +59,10 @@ func (t *topology) createTopologyFile(topoFile string) error {
 	return nil
 }
 
-//TODO(madhukar) common func logic in cluster and cloud pkg
-func (t *topology) compareTopoFile() (bool, error) {
-
-	tmpfile, err := ioutil.TempFile("", "topology")
+func (p *publicCloud) marshalAndSave(destination string) error {
+	marshaled, err := yaml.Marshal(p.Providers)
 	if err != nil {
-		return false, err
+		return errors.Wrapf(err, "cannot marshal topology")
 	}
-	tmpFileName := tmpfile.Name()
-	defer func() {
-		if err = os.Remove(tmpFileName); err != nil {
-			t.log.Errorf("error while deleting tmpfile: %s", err)
-		}
-	}()
-
-	t.log.Debugf("Creating temporary topology %s", tmpFileName)
-	err = t.createTopologyFile(tmpFileName)
-	if err != nil {
-		return false, err
-	}
-
-	newTopoFile, err := ioutil.ReadFile(tmpFileName)
-	if err != nil {
-		return false, err
-	}
-	oldTopoFile, err := ioutil.ReadFile(GetTopoFile(t.cloud.config.CloudID))
-	if err != nil {
-		return false, err
-	}
-
-	return bytes.Equal(oldTopoFile, newTopoFile), nil
-}
-
-func (t *topology) isUpToDate(resource string) (bool, error) {
-	if _, err := os.Stat(GetTopoFile(t.cloud.config.CloudID)); err == nil {
-		ok, err := t.compareTopoFile()
-		if err != nil {
-			return true, err
-		}
-		if ok {
-			t.log.Infof("%s topology file is already up-to-date", resource)
-			return true, nil
-		}
-	}
-	return false, nil
+	return fileutil.WriteToFile(destination, marshaled, defaultRWOnlyPerm)
 }
