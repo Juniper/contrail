95fbb867fbd87cc60c8aac921520e818d5ba8f21
Daniel Furman <daniel.furman@codilime.com>
2019-12-18 18:57:13 +0100

    Bundle templates helpers with templates
    partial-jira-bug: CEM-10205
    Change-Id: I13141bba133affec39b38a06ae1eb71bcc22f621

--- pkg/services/obj_perms.go
+++ pkg/services/obj_perms.go
@@ -1,13 +1,1 @@
 package services
-
-import (
-	"net/http"
-
-	"github.com/Juniper/asf/pkg/auth"
-	"github.com/labstack/echo"
-)
-
-// RESTGetObjPerms handles GET operation of obj-perms request.
-func (service *ContrailService) RESTGetObjPerms(c echo.Context) error {
-	return c.JSON(http.StatusOK, auth.GetIdentity(c.Request().Context()).GetObjPerms())
-}
