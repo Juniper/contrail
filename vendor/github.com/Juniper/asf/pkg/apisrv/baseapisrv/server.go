0bf602ad1b10a282512a3581185b2cd5d284184d
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-12-24 17:34:34 +0100

    apisrv: Move homepage to baseapisrv
    Change-Id: I192eefa49d9e90a05ef230430cc712c2ceac99cf
    closes-jira-bug: CEM-8852

--- vendor/github.com/Juniper/asf/pkg/apisrv/baseapisrv/server.go
+++ vendor/github.com/Juniper/asf/pkg/apisrv/baseapisrv/server.go
@@ -56,8 +56,9 @@ type RouteOptionFunc func(*RouteOptions)
 
 // RouteOptions specifies options for handling a route.
 type RouteOptions struct {
-	noAuth     bool
-	middleware []MiddlewareFunc
+	noAuth        bool
+	middleware    []MiddlewareFunc
+	homepageEntry linkDetails
 }
 
 // WithNoAuth makes requests to the route skip authentication.
@@ -73,6 +74,37 @@ func WithMiddleware(m ...MiddlewareFunc) RouteOptionFunc {
 		rp.middleware = append(rp.middleware, m...)
 	}
 }
+
+// WithNoHomepageMethod makes the route's entry in the homepage have no "method" field.
+func WithNoHomepageMethod() RouteOptionFunc {
+	return func(rp *RouteOptions) {
+		rp.homepageEntry.Method = nil
+	}
+}
+
+// WithHomepageName makes the route's entry in the homepage have the given endpoint name.
+func WithHomepageName(name string) RouteOptionFunc {
+	return func(rp *RouteOptions) {
+		rp.homepageEntry.Name = name
+	}
+}
+
+// WithHomepageType makes the route's entry in the homepage have the given endpoint type.
+func WithHomepageType(t EndpointType) RouteOptionFunc {
+	return func(rp *RouteOptions) {
+		rp.homepageEntry.Rel = string(t)
+	}
+}
+
+// EndpointType is the value of the "rel" field in a homepage entry.
+type EndpointType string
+
+// Endpoint types.
+const (
+	CollectionEndpoint   EndpointType = "collection"
+	ResourceBaseEndpoint EndpointType = "resource-base"
+	ActionEndpoint       EndpointType = "action"
+)
 
 // GRPCRouter allows registering GRPC services.
 type GRPCRouter interface {
@@ -109,7 +141,8 @@ func NewServer(plugins []APIPlugin, noAuthPaths []string) (*Server, error) {
 	s.setupCORS()
 
 	r := &httpRouter{
-		echo: s.Echo,
+		echo:     s.Echo,
+		homepage: NewHomepageHandler(),
 	}
 	for _, plugin := range plugins {
 		plugin.RegisterHTTPAPI(r)
@@ -132,6 +165,10 @@ func NewServer(plugins []APIPlugin, noAuthPaths []string) (*Server, error) {
 
 	if err := s.setupGRPC(authGRPCOpts, plugins); err != nil {
 		return nil, err
+	}
+
+	if viper.GetBool("homepage.enabled") {
+		s.Echo.GET("/", r.homepage.Handle)
 	}
 
 	if viper.GetBool("recorder.enabled") {
@@ -263,6 +300,7 @@ func (s *Server) authMiddleware(noAuthPaths []string) (httpMiddleware []Middlewa
 
 type httpRouter struct {
 	echo        *echo.Echo
+	homepage    *HomepageHandler
 	noAuthPaths []string
 }
 
@@ -288,29 +326,31 @@ func (r *httpRouter) DELETE(path string, h HandlerFunc, optionFuncs ...RouteOpti
 
 // Add registers a handler for a route.
 func (r *httpRouter) Add(method string, path string, h HandlerFunc, optionFuncs ...RouteOptionFunc) {
-	options := makeRouteOptions(optionFuncs)
+	options := makeRouteOptions(optionFuncs, method, path)
 	r.applyRouteOptions(options, path)
 	r.echo.Add(method, path, echo.HandlerFunc(h), echoMiddleware(options.middleware)...)
 }
 
 // Group makes the middleware specified by optionFuncs run for all requests with paths starting with prefix.
 func (r *httpRouter) Group(prefix string, optionFuncs ...RouteOptionFunc) {
-	options := makeRouteOptions(optionFuncs)
+	options := makeRouteOptions(optionFuncs, "", prefix)
 	r.applyRouteOptions(options, prefix)
 	r.echo.Group(prefix, echoMiddleware(options.middleware)...)
 }
 
-func makeRouteOptions(optionFuncs []RouteOptionFunc) (rp RouteOptions) {
+func makeRouteOptions(optionFuncs []RouteOptionFunc, method, path string) (options RouteOptions) {
+	options.homepageEntry = defaultHomepageEntry(method, path)
 	for _, optionFunc := range optionFuncs {
-		optionFunc(&rp)
+		optionFunc(&options)
 	}
-	return rp
+	return options
 }
 
 func (r *httpRouter) applyRouteOptions(options RouteOptions, path string) {
 	if options.noAuth {
 		r.noAuthPaths = append(r.noAuthPaths, path)
 	}
+	r.homepage.Register(options.homepageEntry)
 }
 
 // Use makes middleware run for all requests.
