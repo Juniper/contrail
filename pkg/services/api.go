e6bba6a32cabeeac2481c92b4b0222f158a37320
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-12-24 17:34:34 +0100

    apisrv: Move homepage to baseapisrv
    Change-Id: I192eefa49d9e90a05ef230430cc712c2ceac99cf
    closes-jira-bug: CEM-8852

---
+++ pkg/services/api.go
@@ -0,0 +1,14 @@
+package services
+
+import "github.com/Juniper/asf/pkg/apisrv/baseapisrv"
+
+// TODO Move this into a template in asf, probably into serivices_common.tmpl
+
+func registerAPIHomepage(r baseapisrv.HTTPRouter) {
+	// TODO action resources
+	// TODO documentation
+	// TODO VN IP alloc
+	// TODO VN IP free
+	// TODO subnet IP count
+	// TODO security policy draft
+}
