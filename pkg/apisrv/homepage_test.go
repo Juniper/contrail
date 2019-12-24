93b208bf7c5387fd72c4584b0b6e80f31a1b0974
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-12-24 17:34:34 +0100

    apisrv: Move homepage to baseapisrv
    Change-Id: I192eefa49d9e90a05ef230430cc712c2ceac99cf
    closes-jira-bug: CEM-8852

--- pkg/apisrv/homepage_test.go
+++ pkg/apisrv/homepage_test.go
@@ -172,6 +172,9 @@ func (r *routeSet) Group(prefix string, _ ...baseapisrv.RouteOption) {
 	r.add(prefix)
 }
 
+func (r *routeSet) Register(_, _, _, _ string) {
+}
+
 func resolve(base string, parts ...string) string {
 	base = strings.TrimSuffix(base, "/")
 	return strings.Join(append([]string{base}, parts...), "/")
