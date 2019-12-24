a9af9a23e6a8ad3a48ac96d147a2453a58d945f0
Witaut Bajaryn <witaut.bajaryn@codilime.com>
2019-12-24 17:34:34 +0100

    apisrv: Move homepage to baseapisrv
    Change-Id: I192eefa49d9e90a05ef230430cc712c2ceac99cf
    closes-jira-bug: CEM-8852

--- pkg/services/api.go
+++ pkg/services/api.go
@@ -21,6 +21,7 @@ func (service *ContrailService) RegisterHTTPAPI(r baseapisrv.HTTPRouter) error {
 	r.POST(UserAgentKVPath, service.RESTUserAgentKV)
 	r.POST(UploadCloudKeysPath, service.RESTUploadCloudKeys)
 
+	registerAPIHomepage(r)
 	return nil
 }
 
@@ -38,4 +39,37 @@ func (service *ContrailService) RegisterGRPCAPI(r baseapisrv.GRPCRouter) error {
 	r.RegisterService(&_UserAgentKV_serviceDesc, service)
 
 	return nil
+}
+
+func registerAPIHomepage(r baseapisrv.HTTPRouter) {
+	RegisterSingularPaths(func(path string, name string) {
+		r.Register(path, "", name, "resource-base")
+	})
+	RegisterPluralPaths(func(path string, name string) {
+		r.Register(path, "", name, "collection")
+	})
+
+	r.Register(FQNameToIDPath, "POST", "name-to-id", "action")
+	r.Register(IDToFQNamePath, "POST", "id-to-name", "action")
+	r.Register(UserAgentKVPath, "POST", UserAgentKVPath, "action")
+	r.Register(RefUpdatePath, "POST", RefUpdatePath, "action")
+	r.Register(RefRelaxForDeletePath, "POST", RefRelaxForDeletePath, "action")
+	r.Register(PropCollectionUpdatePath, "POST", PropCollectionUpdatePath, "action")
+	r.Register(SetTagPath, "POST", SetTagPath, "action")
+	r.Register(ChownPath, "POST", ChownPath, "action")
+	r.Register(IntPoolPath, "GET", IntPoolPath, "action")
+	r.Register(IntPoolPath, "POST", IntPoolPath, "action")
+	r.Register(IntPoolPath, "DELETE", IntPoolPath, "action")
+	r.Register(IntPoolsPath, "POST", IntPoolsPath, "action")
+	r.Register(IntPoolsPath, "DELETE", IntPoolsPath, "action")
+	r.Register(ObjPerms, "GET", ObjPerms, "action")
+
+	// TODO: register sync?
+
+	// TODO action resources
+	// TODO documentation
+	// TODO VN IP alloc
+	// TODO VN IP free
+	// TODO subnet IP count
+	// TODO security policy draft
 }
