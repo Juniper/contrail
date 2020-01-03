26190ae7c514bb7e410750b9ea74cd1a396f75e0
Adam Pasyniuk <adam.pasyniuk@codilime.com>
2020-01-03 15:28:17 +0100

    Add flag to cloud and cluster config to enable deployer backend dry run.
    closes-jira-bug: CEM-10837
    Change-Id: I7377452deb5ad88e7059c531277dd034744da6b8

--- pkg/deploy/cluster/cluster.go
+++ pkg/deploy/cluster/cluster.go
@@ -2,11 +2,13 @@ package cluster
 
 import (
 	"os/exec"
+	"path/filepath"
 
 	"github.com/Juniper/asf/pkg/logutil"
 	"github.com/Juniper/asf/pkg/logutil/report"
 	"github.com/Juniper/contrail/pkg/ansible"
 	"github.com/Juniper/contrail/pkg/client"
+	"github.com/Juniper/contrail/pkg/cloud"
 	"github.com/Juniper/contrail/pkg/deploy/base"
 	"github.com/Juniper/contrail/pkg/deploy/rhospd/overcloud"
 	"github.com/Juniper/contrail/pkg/models"
@@ -43,6 +45,8 @@ type Config struct {
 	AnsibleCherryPickRevision string
 	// Optional ansible deployer revision(commit id)
 	AnsibleRevision string
+	// DryRunDeployer indicate if deployer backend will be run or only command logged
+	DryRunDeployer bool
 	// Optional Test var to run command in test mode
 	Test bool
 }
@@ -158,6 +162,29 @@ func newAnsibleDeployer(c *Cluster, cData *base.Data) *contrailAnsibleDeployer {
 
 func newMCProvisioner(c *Cluster, cData *base.Data) *multiCloudProvisioner {
 	d := newDeployCluster(c, cData, "multi-cloud-provisioner")
+
+	clusterWorkDir := d.getWorkingDir()
+	mcRepoDir := getMCDeployerRepoDir()
+	mcWorkDir := getMCWorkingDir(clusterWorkDir)
+
+	deployerConfig := cloud.DeployerConfig{
+		WorkDir:      mcRepoDir,
+		TopologyFile: getClusterTopoFile(mcWorkDir),
+		SecretFile:   getClusterSecretFile(mcWorkDir),
+	}
+	opts := []cloud.DeployerOption{
+		cloud.DeployerWithTfStateFile(getTFStateFile(cData)),
+		cloud.DeployerWithStateFile(filepath.Join(mcRepoDir, "state.yml")),
+	}
+	if c.config.DryRunDeployer {
+		opts = append(opts, cloud.DeployerWithDryRun(c.log))
+	}
+	deployer := cloud.NewDeployer(
+		deployerConfig,
+		c.commandExecutor,
+		d.Reporter,
+		opts...,
+	)
 	return &multiCloudProvisioner{
 		contrailAnsibleDeployer: contrailAnsibleDeployer{
 			deployCluster: *d,
@@ -168,6 +195,7 @@ func newMCProvisioner(c *Cluster, cData *base.Data) *multiCloudProvisioner {
 				c.config.Test,
 			),
 		},
-		workDir: "",
+		workDir:  mcWorkDir,
+		deployer: deployer,
 	}
 }
