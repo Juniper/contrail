97bb68e2785ed8681392f21f61d5f78403059cb0
Adam Pasyniuk <adam.pasyniuk@codilime.com>
2020-01-03 12:23:22 +0100

    Add flag to cloud config to enable deployer backend dry run.
    closes-jira-bug: CEM-10837
    Change-Id: I7377452deb5ad88e7059c531277dd034744da6b8

--- pkg/cloud/terraform.go
+++ pkg/cloud/terraform.go
@@ -2,31 +2,58 @@ package cloud
 
 import (
 	"github.com/Juniper/asf/pkg/fileutil"
+	"github.com/Juniper/asf/pkg/logutil/report"
+	"github.com/sirupsen/logrus"
 )
 
-func destroyTopology(c *Cloud, providers []string) error {
-	if tfStateOutputExists(c.config.CloudID) {
-		if err := fileutil.WriteToFile(GetTopoFile(c.config.CloudID), []byte{}, defaultRWOnlyPerm); err != nil {
+type providers []string
+
+type dryRunExecutor struct {
+	log *logrus.Entry
+}
+
+func (d dryRunExecutor) ExecCmdAndWait(r *report.Reporter, cmd string, args []string, dir string, envVars ...string) error {
+	d.log.Infof("Dry runing %s command with [%s] args in %s directory with [%s] env", cmd, args, dir, envVars)
+	return nil
+}
+
+func NewDeployer(cloudID string, dryRun bool, executor CommandExecutor, reporter *report.Reporter, log *logrus.Entry) deployer {
+	if dryRun {
+		executor = dryRunExecutor{log}
+	}
+	return deployer{cloudID, executor, reporter}
+
+}
+
+type deployer struct {
+	cloudID         string
+	commandExecutor CommandExecutor
+	reporter        *report.Reporter
+}
+
+func (d deployer) destroyTopology(providers providers) error {
+	if tfStateOutputExists(d.cloudID) {
+		if err := fileutil.WriteToFile(GetTopoFile(d.cloudID), []byte{}, defaultRWOnlyPerm); err != nil {
 			return err
 		}
-		if err := updateTopology(c, providers); err != nil {
+		if err := d.updateTopology(providers); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func updateTopology(c *Cloud, providers []string) error {
-	workDir := GetCloudDir(c.config.CloudID)
+func (d deployer) updateTopology(providers providers) error {
+	workDir := GetCloudDir(d.cloudID)
 	cmd := "deployer"
 	args := []string{
 		"all", "topology",
-		"--topology", GetTopoFile(c.config.CloudID),
-		"--secret", GetSecretFile(c.config.CloudID),
+		"--topology", GetTopoFile(d.cloudID),
+		"--secret", GetSecretFile(d.cloudID),
 		"--skip_validation",
 	}
 	for _, p := range providers {
 		args = append(args, []string{"--limit", p}...)
 	}
-	return c.commandExecutor.ExecCmdAndWait(c.reporter, cmd, args, workDir)
+	return d.commandExecutor.ExecCmdAndWait(d.reporter, cmd, args, workDir)
 }
