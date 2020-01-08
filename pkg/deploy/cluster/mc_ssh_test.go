da8e5643967580d39fabf21a8b086b82d0406c44
Ignacy Osetek <ignacy.osetek@codilime.com>
2020-01-08 16:51:22 +0100

    MultiCloud: Set proper permissions for pvt key
    SSH Agent uses private keys. If their permission
    is too open (600 is okay) then it will raise an error.
    Git tracks only two file permissions: 644 and 755.
    This is why we need to manually set permissions to 600
    within test.
    
    closes-jira-bug: CEM-10835
    Change-Id: I0c6feae77c2db3972c313cba0949864bae8f49a7

--- pkg/deploy/cluster/mc_ssh_test.go
+++ pkg/deploy/cluster/mc_ssh_test.go
@@ -1,16 +1,23 @@
 package cluster
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"os"
 	"os/exec"
 	"testing"
 
+	"github.com/Juniper/asf/pkg/fileutil"
 	"github.com/stretchr/testify/assert"
 )
 
+const keyPermissions = 0600
+
 func TestStoppingSSHAgent(t *testing.T) {
 	agent := &sshAgent{}
-	setProperKeyPermissions(t, "./test_data/test_pvt_key_real")
+	createPrivateKey(t, "./test_data/test_pvt_key_real")
 	assert.NoError(t, agent.Run("./test_data/test_pvt_key_real"))
 	assert.True(t, processExists(agent.pid))
 	assert.NoError(t, agent.Stop())
@@ -21,8 +28,27 @@ func processExists(pid string) bool {
 	return exec.Command("kill", "-0", pid).Run() == nil
 }
 
-func setProperKeyPermissions(t *testing.T, file string) {
-	assert.NoError(t, os.Chmod(file, 0600), "cannot set proper permissions for the key")
+func createIncorrectKey(t *testing.T, destination string) func() {
+	assert.NoError(t, fileutil.WriteToFile(destination, []byte("Hi, I am a Private Key. Trust Me."), keyPermissions),
+		"couldn't create key file")
+	return func() {
+		assert.NoError(t, os.Remove(destination), "couldn't remove created key file")
+	}
+}
+
+func createPrivateKey(t *testing.T, destination string) func() {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	assert.NoError(t, err, "cannot generate key")
+	pemdata := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(key),
+		},
+	)
+	assert.NoError(t, fileutil.WriteToFile(destination, pemdata, keyPermissions), "couldn't create key file")
+	return func() {
+		assert.NoError(t, os.Remove(destination), "couldn't remove created key file")
+	}
 }
 
 func TestRunSSHAgentWithKey(t *testing.T) {
@@ -31,6 +57,7 @@ func TestRunSSHAgentWithKey(t *testing.T) {
 		key       string
 		fails     bool
 		keyExists bool
+		createKey func(t *testing.T, destination string) (removeKey func())
 	}{
 		{
 			name:  "Empty key",
@@ -46,18 +73,20 @@ func TestRunSSHAgentWithKey(t *testing.T) {
 			key:       "./test_data/test_pvt_key_fake",
 			fails:     true,
 			keyExists: true,
+			createKey: createIncorrectKey,
 		},
 		{
 			name:      "Existing real key",
 			key:       "./test_data/test_pvt_key_real",
 			keyExists: true,
+			createKey: createPrivateKey,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			agent := &sshAgent{}
 			if tt.keyExists {
-				setProperKeyPermissions(t, tt.key)
+				defer tt.createKey(t, tt.key)()
 			}
 			err := agent.Run(tt.key)
 			if tt.fails {
