d2f832a7379ec64946bd44d279a66f94be0a3a68
Michał Błotniak <michal.blotniak@codilime.com>
2020-01-29 12:40:14 +0100

    Call /v3/auth/projects for unscoped tokens
    Change-Id: I1ac360fa3b0d0a28943dfb3570c418e8add3d5b9
    Closes-Jira-Bug: CEM-12263

--- pkg/replication/client.go
+++ pkg/replication/client.go
@@ -17,6 +17,7 @@ import (
 	"github.com/Juniper/contrail/pkg/models"
 	"github.com/Juniper/contrail/pkg/services"
 	"github.com/labstack/echo"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 
@@ -129,7 +130,7 @@ func (h *vncAPIHandle) getAuthContext(clusterID string, apiClient *client.HTTP) 
 	var projectID string
 	ctx := auth.WithXClusterID(context.Background(), clusterID)
 	if apiClient.Scope.Project.Name == "" && apiClient.Scope.Project.ID == "" {
-		projectID, err = h.getProjectIDByName(ctx, apiClient)
+		projectID, err = h.getProjectID(ctx, apiClient)
 		if err == nil {
 			apiClient.Scope = kstypes.NewScope(
 				kstypes.DefaultDomainID, kstypes.DefaultDomainName,
@@ -142,7 +143,7 @@ func (h *vncAPIHandle) getAuthContext(clusterID string, apiClient *client.HTTP) 
 	return asfauth.WithIdentity(ctx, varCtx)
 }
 
-func (h *vncAPIHandle) getProjectIDByName(ctx context.Context, apiClient *client.HTTP) (string, error) {
+func (h *vncAPIHandle) getProjectID(ctx context.Context, apiClient *client.HTTP) (string, error) {
 	token, err := apiClient.Keystone.ObtainUnscopedToken(
 		ctx, apiClient.ID, apiClient.Password, apiClient.Scope.Project.Domain,
 	)
@@ -150,9 +151,15 @@ func (h *vncAPIHandle) getProjectIDByName(ctx context.Context, apiClient *client
 		return "", err
 	}
 	ctx = kstypes.WithXAuthToken(ctx, token)
-	return apiClient.Keystone.GetProjectIDByName(
-		ctx, defaultProjectName, apiClient.Scope.Project.Domain,
-	)
+	projects, err := apiClient.Keystone.ListAvailableProjectScopes(ctx)
+	if err != nil {
+		return "", err
+	}
+	project := projects.FindByName(defaultProjectName)
+	if project == nil {
+		return "", errors.Errorf("project(name=%q) not found in available scopes for an unscoped token", defaultProjectName)
+	}
+	return project.ID, nil
 }
 
 // UpdateClient updates client for given endpoint.
