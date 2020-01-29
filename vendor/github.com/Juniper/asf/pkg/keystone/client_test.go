6a511d1fcb8192aa262adfbdb0dea34360cc26cb
Michał Błotniak <michal.blotniak@codilime.com>
2020-01-29 12:40:14 +0100

    Call auth/projects for unscoped tokens and fix header leak
    For unscoped tokens /v3/auth/projects must be used because /v3/projects
    is accessible only for scoped projects.
    
    When http.Headers were already in context, they could be mutated by some
    changes in child context. To solve this http.Headers structure must be
    cloned every time the headers is appended.
    
    Change-Id: I1ac360fa3b0d0a28943dfb3570c418e8add3d5b9
    Closes-Jira-Bug: CEM-12263

--- vendor/github.com/Juniper/asf/pkg/keystone/client_test.go
+++ vendor/github.com/Juniper/asf/pkg/keystone/client_test.go
@@ -9,6 +9,82 @@ import (
 	"testing"
 )
 
+func Test_encodeQueryParameters(t *testing.T) {
+	tests := []struct {
+		name string
+		qs   []QueryParameter
+		want string
+	}{
+		{name: "nil", want: ""},
+		{name: "empty", qs: []QueryParameter{}, want: ""},
+		{name: "two values", qs: []QueryParameter{{"a", "v"}, {"b", "n"}}, want: "?a=v&b=n"},
+		{name: "duplicated", qs: []QueryParameter{{"a", "v"}, {"a", "n"}}, want: "?a=v&a=n"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := encodeQueryParameters(tt.qs); got != tt.want {
+				t.Errorf("encodeQueryParameters() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKeystoneProjectListing(t *testing.T) {
+	tests := []struct {
+		name     string
+		HTTPDoer doer
+		ctx      context.Context
+		want     Projects
+		wantErr  bool
+	}{{
+		name: "keystone returns sample projects",
+		HTTPDoer: &mockDoer{
+			StatusCode: http.StatusOK,
+			Body:       []byte(`{"projects":[{"id":"ff4e51"},{"id":"asd"}]}`),
+		},
+		want: Projects{{ID: "ff4e51"}, {ID: "asd"}},
+	}, {
+		name:     "keystone returns invalid fields",
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte(`{"foo":{"bar":"foobar"}}`)},
+	}, {
+		name:     "keystone returns bad response",
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte(`{"foo":"bar":"foobar"}}`)},
+		wantErr:  true,
+	}, {
+		name:     "got empty response from keystone",
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte{}},
+		wantErr:  true,
+	}, {
+		name:     "keystone returns StatusForbidden",
+		HTTPDoer: &mockDoer{StatusCode: http.StatusForbidden},
+		wantErr:  true,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := &Client{HTTPDoer: tt.HTTPDoer}
+			listProjects := func(ctx context.Context) (Projects, error) { return k.ListProjects(ctx) }
+			for methodName, method := range map[string](func(context.Context) (Projects, error)){
+				"ListProjects":               listProjects,
+				"ListAvailableProjectScopes": k.ListAvailableProjectScopes,
+			} {
+				t.Run(methodName, func(t *testing.T) {
+					if tt.ctx == nil {
+						tt.ctx = context.Background()
+					}
+					got, err := method(tt.ctx)
+					if (err != nil) != tt.wantErr {
+						t.Errorf("Keystone.%s() error = %v, wantErr %v", methodName, err, tt.wantErr)
+						t.FailNow()
+					}
+					if !reflect.DeepEqual(got, tt.want) {
+						t.Errorf("Keystone.%s() = %v, want %v", methodName, got, tt.want)
+					}
+				})
+			}
+		})
+	}
+}
+
 func TestKeystone_CreateUser(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -18,22 +94,22 @@ func TestKeystone_CreateUser(t *testing.T) {
 		wantErr  bool
 	}{{
 		name:     "keystone returns sample user",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusCreated, []byte(`{"user":{"id":"ff4e51"}}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusCreated, Body: []byte(`{"user":{"id":"ff4e51"}}`)},
 		want:     User{ID: "ff4e51"},
 	}, {
 		name:     "keystone returns invalid fields",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusCreated, []byte(`{"foo":{"bar":"foobar"}}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusCreated, Body: []byte(`{"foo":{"bar":"foobar"}}`)},
 	}, {
 		name:     "keystone returns bad response",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusCreated, []byte(`{"foo":"bar":"foobar"}}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusCreated, Body: []byte(`{"foo":"bar":"foobar"}}`)},
 		wantErr:  true,
 	}, {
 		name:     "got empty response from keystone",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusCreated, []byte{})},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusCreated, Body: []byte{}},
 		wantErr:  true,
 	}, {
 		name:     "keystone returns StatusForbidden",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusForbidden, nil)},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusForbidden},
 		wantErr:  true,
 	}}
 	for _, tt := range tests {
@@ -64,15 +140,15 @@ func TestKeystone_AssignProjectRoleOnUser(t *testing.T) {
 		wantErr  bool
 	}{{
 		name:     "keystone returns StatusBadRequest",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusBadRequest, nil)},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusBadRequest},
 		wantErr:  true,
 	}, {
 		name:     "keystone returns StatusForbidden",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusForbidden, nil)},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusForbidden},
 		wantErr:  true,
 	}, {
 		name:     "keystone returns StatusNoContent",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusNoContent, nil)},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusNoContent},
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -98,18 +174,18 @@ func TestKeystone_checkServiceUserExists(t *testing.T) {
 		wantErr  bool
 	}{{
 		name:     "service user not found",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusOK, []byte(`{"users": []}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte(`{"users": []}`)},
 	}, {
 		name:     "keystone returns StatusForbidden",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusForbidden, nil)},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusForbidden},
 		wantErr:  true,
 	}, {
 		name:     "keystone returns service user",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusOK, []byte(`{"users":[{"name":"goapi", "password" : "goapi123", "roles" : [{"Name": "admin", "project": {"name": "service"}}]}]}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte(`{"users":[{"name":"goapi", "password" : "goapi123", "roles" : [{"Name": "admin", "project": {"name": "service"}}]}]}`)},
 		want:     true,
 	}, {
 		name:     "keystone returns service user without roles",
-		HTTPDoer: &mockDoer{Response: newResponse(http.StatusOK, []byte(`{"users":[{"name":"goapi", "password" : "goapi123"}]}`))},
+		HTTPDoer: &mockDoer{StatusCode: http.StatusOK, Body: []byte(`{"users":[{"name":"goapi", "password" : "goapi123"}]}`)},
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -132,11 +208,12 @@ func TestKeystone_checkServiceUserExists(t *testing.T) {
 }
 
 type mockDoer struct {
-	*http.Response
+	StatusCode int
+	Body       []byte
 }
 
 func (m *mockDoer) Do(req *http.Request) (*http.Response, error) {
-	return m.Response, nil
+	return newResponse(m.StatusCode, m.Body), nil
 }
 
 func newResponse(statusCode int, body []byte) *http.Response {
