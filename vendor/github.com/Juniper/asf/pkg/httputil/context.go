07e59f4caa7a3a2783705affc28a642ec2d38941
Michał Błotniak <michal.blotniak@codilime.com>
2020-01-30 16:24:42 +0100

    Fix header leak in httputil
    When http.Headers were already in context, they could be mutated by some
    changes in child context. To solve this http.Headers structure must be
    cloned every time the headers is appended.
    
    Change-Id: I29107ecbd1b29fa6238b8c0ca21ba68d1a1b005f
    Partial-Jira-Bug: CEM-12263

--- vendor/github.com/Juniper/asf/pkg/httputil/context.go
+++ vendor/github.com/Juniper/asf/pkg/httputil/context.go
@@ -15,10 +15,32 @@ const (
 func WithHTTPHeader(ctx context.Context, key, value string) context.Context {
 	headers := http.Header{}
 	if v, ok := ctx.Value(headersClientContextKey).(http.Header); ok && v != nil {
-		headers = v
+		headers = cloneHeader(v)
 	}
 	headers.Set(key, value)
 	return context.WithValue(ctx, headersClientContextKey, headers)
+}
+
+// cloneHeader returns a copy of h or nil if h is nil.
+// TODO(mblotniak): use http.Header.Clone() when Go version is updated to 1.13.
+func cloneHeader(h http.Header) http.Header {
+	if h == nil {
+		return nil
+	}
+
+	// Find total number of values.
+	nv := 0
+	for _, vv := range h {
+		nv += len(vv)
+	}
+	sv := make([]string, nv) // shared backing array for headers' values
+	h2 := make(http.Header, len(h))
+	for k, vv := range h {
+		n := copy(sv, vv)
+		h2[k] = sv[:n:n]
+		sv = sv[n:]
+	}
+	return h2
 }
 
 // SetContextHeaders sets extra headers that are stored in context.
