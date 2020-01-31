15acb3d44419e7848a6a0afe691f1a75fbaa83d6
Ignacy Osetek <ignacy.osetek@codilime.com>
2020-01-31 03:10:31 +0100

    Move MultiCloud backend to a separate container
    MultiCloud backend will no longer be a part of Contrail Command
    container. All deployer executions need to be replaced with
    docker executions.
    
    Change-Id: Iba31f94e0287db519e631d4124d1dd09f42fcfc7
    partial-jira-bug: CEM-8543

--- pkg/deploy/manage.go
+++ pkg/deploy/manage.go
@@ -1,17 +1,18 @@
 package deploy
 
 import (
+	"fmt"
 	"os/exec"
-
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 
 	"github.com/Juniper/asf/pkg/logutil"
 	"github.com/Juniper/asf/pkg/logutil/report"
 	"github.com/Juniper/asf/pkg/osutil"
+	"github.com/Juniper/contrail/pkg/ansible"
 	"github.com/Juniper/contrail/pkg/deploy/base"
 	"github.com/Juniper/contrail/pkg/deploy/cluster"
 	"github.com/Juniper/contrail/pkg/deploy/rhospd/undercloud"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // manager inerface to manage resources
@@ -64,6 +65,14 @@ func newManager(deploy *Deploy) (manager, error) {
 func newDeployer(deploy *Deploy) (base.Deployer, error) {
 	switch deploy.config.ResourceType {
 	case "contrail_cluster":
+		player, err := ansible.NewContainerPlayer(report.NewReporter(
+			deploy.APIServer,
+			fmt.Sprintf("%s/%s", cluster.DefaultResourcePath, deploy.config.ResourceID),
+			logutil.NewFileLogger("reporter", deploy.config.LogFile).WithField("cloudID", deploy.config.ResourceID),
+		), deploy.config.LogFile)
+		if err != nil {
+			return nil, err
+		}
 		c, err := cluster.NewCluster(&cluster.Config{
 			APIServer:                 deploy.APIServer,
 			ClusterID:                 deploy.config.ResourceID,
@@ -77,7 +86,7 @@ func newDeployer(deploy *Deploy) (base.Deployer, error) {
 			AnsibleRevision:           deploy.config.AnsibleRevision,
 			ServiceUserID:             deploy.config.ServiceUserID,
 			ServiceUserPassword:       deploy.config.ServiceUserPassword,
-		}, &osCommandExecutor{})
+		}, &osCommandExecutor{}, player)
 		if err != nil {
 			return nil, err
 		}
