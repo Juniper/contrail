e61473ba457fa155120343dad9fae2d10db4699d
Ignacy Osetek <ignacy.osetek@codilime.com>
2020-01-31 03:10:31 +0100

    Move MultiCloud backend to a separate container
    MultiCloud backend will no longer be a part of Contrail Command
    container. All deployer executions need to be replaced with
    docker executions.
    
    Change-Id: Iba31f94e0287db519e631d4124d1dd09f42fcfc7
    partial-jira-bug: CEM-8543

--- pkg/cloud/terraform.go
+++ pkg/cloud/terraform.go
@@ -2,21 +2,24 @@ package cloud
 
 import (
 	"github.com/Juniper/asf/pkg/fileutil"
+	"github.com/Juniper/contrail/pkg/ansible"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/pkg/errors"
 )
 
-func destroyTopology(c *Cloud, providers []string) error {
+func destroyTopology(c *Cloud, providers []string, clusterUUID string) error {
 	if tfStateOutputExists(c.config.CloudID) {
 		if err := fileutil.WriteToFile(GetTopoFile(c.config.CloudID), []byte{}, defaultRWOnlyPerm); err != nil {
 			return err
 		}
-		if err := updateTopology(c, providers); err != nil {
+		if err := updateTopology(c, providers, clusterUUID); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func updateTopology(c *Cloud, providers []string) error {
+func updateTopology(c *Cloud, providers []string, clusterUUID string) error {
 	workDir := GetCloudDir(c.config.CloudID)
 	cmd := "deployer"
 	args := []string{
@@ -28,5 +31,38 @@ func updateTopology(c *Cloud, providers []string) error {
 	for _, p := range providers {
 		args = append(args, []string{"--limit", p}...)
 	}
-	return c.commandExecutor.ExecCmdAndWait(c.reporter, cmd, args, workDir)
+	volumes := []ansible.Volume{
+		ansible.Volume{
+			Source: workDir,
+			Target: workDir,
+		},
+	}
+	player, err := ansible.NewContainerPlayer(c.reporter, c.config.LogFile)
+	if err != nil {
+		return errors.Wrap(err, "cannot update topology without container player")
+	}
+
+	registry, err := c.getRegistries(clusterUUID)
+	if err != nil {
+		return errors.Wrap(err, "cannot get registry username and password")
+	}
+	imageRef, err := c.getMultiCloudImageRef(clusterUUID)
+	if err != nil {
+		return err
+	}
+
+	return player.StartExecuteAndRemove(
+		c.ctx, imageRef, registry.Username, registry.Password, volumes, workDir,
+		append([]string{cmd}, args...), []string{})
+}
+
+func (c *Cloud) getMultiCloudImageRef(clusterUUID string) (string, error) {
+	clusterResp, err := c.APIServer.GetContrailCluster(c.ctx, &services.GetContrailClusterRequest{
+		ID: clusterUUID,
+	})
+	if err != nil {
+		return "", errors.Wrap(err, "cannot resolve MultiCloud Image Ref")
+	}
+	return ansible.GetContainerName(clusterResp.ContrailCluster.ContainerRegistry,
+		MultiCloudContainer, ansible.GetContrailVersion(clusterResp.ContrailCluster, c.log))
 }
