b5dff416c4eb5e3794b73316531bf8b79d38ccd6
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2020-02-05 20:13:06 +0100

    Extract subpackages from pkg/db
    Change-Id: Ia216cc9b447236355749aeaea89451dc11864eb7
    Closes-Jira-Bug: CEM-12046
    Closes-Jira-Bug: CEM-12047
    Closes-Jira-Bug: CEM-12048

--- pkg/etcd/notifier_service_test.go
+++ pkg/etcd/notifier_service_test.go
@@ -4,17 +4,16 @@ import (
 	"context"
 	"testing"
 
+	"github.com/Juniper/contrail/pkg/constants"
+	"github.com/Juniper/contrail/pkg/etcd"
+	"github.com/Juniper/contrail/pkg/models"
+	"github.com/Juniper/contrail/pkg/services"
+	"github.com/Juniper/contrail/pkg/testutil/integration"
 	"github.com/gogo/protobuf/types"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/Juniper/contrail/pkg/constants"
-
-	"github.com/Juniper/contrail/pkg/db/etcd"
-	"github.com/Juniper/contrail/pkg/models"
-	"github.com/Juniper/contrail/pkg/services"
-	"github.com/Juniper/contrail/pkg/testutil/integration"
 	integrationetcd "github.com/Juniper/contrail/pkg/testutil/integration/etcd"
 )
 
--- vendor/github.com/Juniper/asf/pkg/cmd/contrailutil/convert.go
+++ vendor/github.com/Juniper/asf/pkg/cmd/contrailutil/convert.go
@@ -12,21 +12,16 @@ import (
 func init() {
 	ContrailUtil.AddCommand(convertCmd)
 	convertCmd.Flags().StringVarP(&inType, "intype", "", "",
-		`input type: "cassandra", "cassandra_dump", "yaml" and "rdbms" are supported`)
-	convertCmd.Flags().StringVarP(&inFile, "in", "i", "", "Input file or Cassandra host")
+		`input type: "yaml" and "rdbms" are supported`)
+	convertCmd.Flags().StringVarP(&inFile, "in", "i", "", "Input file")
 	convertCmd.Flags().StringVarP(&outType, "outtype", "", "",
 		`output type: "rdbms", "yaml", "etcd" and "http" are supported`)
 	convertCmd.Flags().StringVarP(&outFile, "out", "o", "", "Output file")
-	convertCmd.Flags().IntVarP(&cassandraPort, "cassandra_port", "p", 9042, "Cassandra port")
-	convertCmd.Flags().IntVarP(&cassandraTimeout, "cassandra_timeout", "t", 3600, "Cassandra timeout in seconds")
-	convertCmd.Flags().IntVarP(&cassandraConnectTimeout, "cassandra_connect_timeout", "d", 600,
-		"Cassandra connection dial timeout in milliseconds")
 	convertCmd.Flags().StringVarP(&url, "url", "u", "", `Endpoint URL for "http" output type.`)
 }
 
 var inType, inFile string
 var outType, outFile string
-var cassandraPort, cassandraTimeout, cassandraConnectTimeout int
 var url string
 
 var convertCmd = &cobra.Command{
@@ -39,9 +34,6 @@ var convertCmd = &cobra.Command{
 			InFile:                  inFile,
 			OutType:                 outType,
 			OutFile:                 outFile,
-			CassandraPort:           cassandraPort,
-			CassandraTimeout:        cassandraTimeout,
-			CassandraConnectTimeout: cassandraConnectTimeout,
 			EtcdNotifierPath:        viper.GetString(constants.ETCDEndpointsVK),
 			URL:                     url,
 		})
