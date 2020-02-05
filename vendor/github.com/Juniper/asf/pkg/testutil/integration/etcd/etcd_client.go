3bfdc2009a1e0153f18416d7bee47f98b49a1b66
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2020-02-05 20:13:06 +0100

    Extract subpackages from pkg/db
    Change-Id: Ia216cc9b447236355749aeaea89451dc11864eb7
    Closes-Jira-Bug: CEM-12046
    Closes-Jira-Bug: CEM-12047
    Closes-Jira-Bug: CEM-12048

--- vendor/github.com/Juniper/asf/pkg/testutil/integration/etcd/etcd_client.go
+++ vendor/github.com/Juniper/asf/pkg/testutil/integration/etcd/etcd_client.go
@@ -7,16 +7,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Juniper/asf/pkg/db/etcd"
+	"github.com/Juniper/asf/pkg/etcd"
+	"github.com/Juniper/asf/pkg/logutil"
+	"github.com/Juniper/asf/pkg/testutil"
 	"github.com/coreos/etcd/clientv3"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/Juniper/asf/pkg/logutil"
-	"github.com/Juniper/asf/pkg/models"
-	"github.com/Juniper/asf/pkg/testutil"
 )
 
 // Integration test settings.
@@ -86,7 +85,7 @@ func (e *EtcdClient) DeleteSecurityGroup(t *testing.T, uuid string, opts ...clie
 
 // Clear recursively deletes all keys starting with "etcd.path" prefix.
 func (e *EtcdClient) Clear(t *testing.T) (revision int64) {
-	return e.DeleteKey(t, "/"+viper.GetString(constants.ETCDPathVK), clientv3.WithPrefix())
+	return e.DeleteKey(t, "/"+viper.GetString(etcd.ETCDPathVK), clientv3.WithPrefix())
 }
 
 // GetKey gets etcd key.
