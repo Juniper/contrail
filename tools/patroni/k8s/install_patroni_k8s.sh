9acfa2cf119043c5ace1e161a0558a78e1618bc8
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2020-05-20 16:55:30 +0200

    Replace patroni with postgres
    closes-jira-bug: CEM-15860
    Change-Id: Ib98cef00195ad8a8762f1150c0f9f71d6cd90cf0

--- tools/patroni/k8s/install_patroni_k8s.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t patroni_k8s . || { echo "Failed to build docker image" ; exit 1; }
--- tools/reset_db_psql.sh
+++ tools/reset_db_psql.sh
@@ -1,60 +1,29 @@
 #!/usr/bin/env bash
 
 reset_psql() {
-    TOOLSDIR=$(dirname "$0")
-    PROJECT="contrail"
-    DOCKER_COMPOSE="docker-compose -f $TOOLSDIR/patroni/docker-compose.yml -p $PROJECT"
-    SUCCESS_MSG="ERROR, SQLSTATE: no results to fetch"
-
-    drop_psql_db
-    create_psql_db
-    initialize_psql_db
+    drop_psql
+    create_psql
+    initialize_psql
 }
 
-drop_psql_db() {
+drop_psql(){
     echo "Dropping database contrail_test"
-    response=$(NETWORKNAME=${PROJECT} ${DOCKER_COMPOSE} exec -T dbnode bash -c \
-        "PGPASSWORD=contrail123 patronictl query -Uroot -d postgres -c \"drop database if exists contrail_test;\" testcluster")
-    if [[ "${response:20}" = "$SUCCESS_MSG" ]]; then
-        echo "Dropped database contrail_test"
-    else
-        echo "Error while dropping database ${response:20}"
-        exit 1
-    fi
+    docker exec contrail_psql bash -c \
+        "PGPASSWORD=contrail123 psql -Uroot -d postgres -c \"drop database if exists contrail_test;\""
 }
 
-create_psql_db() {
+create_psql(){
     echo "Creating new database"
-    response=$(NETWORKNAME=${PROJECT} ${DOCKER_COMPOSE} exec -T dbnode bash -c \
-        "PGPASSWORD=contrail123 patronictl query -Uroot -d postgres -c \"create database contrail_test;\" testcluster")
-    if [[ "${response:20}" = "$SUCCESS_MSG" ]]; then
-        echo "Created database contrail_test"
-    else
-        echo "Error while creating database contrail_test ${response:20}"
-        return 1
-    fi
+    docker exec contrail_psql bash -c \
+        "PGPASSWORD=contrail123 psql -Uroot -d postgres -c \"create database contrail_test;\""
 }
 
-initialize_psql_db() {
+initialize_psql(){
     echo "Initializing database with gen_init_psql.sql"
-    response=$(NETWORKNAME=${PROJECT} ${DOCKER_COMPOSE} exec -T dbnode bash -c \
-        "PGPASSWORD=contrail123 patronictl query -Uroot -d contrail_test --file /tools/gen_init_psql.sql testcluster")
-    if [[ "${response:20}" = "$SUCCESS_MSG" ]]; then
-        echo "Initialized database contrail_test with gen_init_psql.sql"
-    else
-        echo "Error while initializing database contrail_test with gen_init_psql.sql: ${response:20}"
-        return 1
-    fi
-
-    echo "Initializing database with init_psql.sql"
-    response=$(NETWORKNAME=${PROJECT} ${DOCKER_COMPOSE} exec -T dbnode bash -c \
-        "PGPASSWORD=contrail123 patronictl query -Uroot -d contrail_test --file /tools/init_psql.sql testcluster")
-    if [[ "${response:20}" = "$SUCCESS_MSG" ]]; then
-        echo "Initialized database contrail_test with init_psql.sql"
-    else
-        echo "Error while initializing database contrail_test with init_psql.sql: ${response:20}"
-        return 1
-    fi
+    docker exec contrail_psql bash -c \
+        "PGPASSWORD=contrail123 psql -Uroot -d contrail_test -q --file /tools/gen_init_psql.sql"
+    docker exec contrail_psql bash -c \
+        "PGPASSWORD=contrail123 psql -Uroot -d contrail_test -q --file /tools/init_psql.sql"
 }
 
 reset_psql
